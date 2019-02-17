953808e10f7ec48a20ca40ffce881ef4bddcfb87
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2019-02-17 12:28:47 +0100

    Implemented If/Else activity

---
+++ src/web/modules/Elsa.Web.Activities.Primitives/ViewModels/IfElseViewModel.cs
@@ -0,0 +1,9 @@
+using Elsa.Web.Components.ViewModels;
+
+namespace Elsa.Web.Activities.Primitives.ViewModels
+{
+    public class IfElseViewModel
+    {
+        public ExpressionViewModel ConditionExpression { get; set; }
+    }
+}
