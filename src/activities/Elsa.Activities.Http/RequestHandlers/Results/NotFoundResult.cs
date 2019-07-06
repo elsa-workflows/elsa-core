3b96ba676b426738cbb1899e8b31e41e951cd4b7
Sipke Schoorstra <sipke.schoorstra@outlook.com>
2019-07-06 20:35:10 +0200

    Added BadRequestResult and NotFoundResult

---
+++ src/activities/Elsa.Activities.Http/RequestHandlers/Results/NotFoundResult.cs
@@ -0,0 +1,16 @@
+using System.Net;
+using System.Threading.Tasks;
+using Elsa.Activities.Http.Services;
+using Microsoft.AspNetCore.Http;
+
+namespace Elsa.Activities.Http.RequestHandlers.Results
+{
+    public class NotFoundResult : IRequestHandlerResult
+    {
+        public Task ExecuteResultAsync(HttpContext httpContext, RequestDelegate next)
+        {
+            httpContext.Response.StatusCode = (int) HttpStatusCode.NotFound;
+            return Task.CompletedTask;
+        }
+    }
+}
