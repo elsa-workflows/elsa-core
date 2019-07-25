9070aa907019f38da33ea280e674dde08fa7d8f6
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2019-07-25 22:00:44 +0200

    Implemented activity outputs (#57)

---
+++ samples/Sample13/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+using Elsa.Activities.Console.Extensions;
+using Elsa.Extensions;
+using Elsa.Persistence.Memory;
+using Elsa.Scripting;
+using Elsa.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Sample13
+{
+    /// <summary>
+    /// A strongly-typed workflows program demonstrating scripting, and branching.
+    /// </summary>
+    class Program
+    {
+        static async Task Main()
+        {
+            // Setup a service collection.
+            var services = new ServiceCollection()
+                .AddWorkflows()
+                .AddMemoryWorkflowInstanceStore()
+                .AddConsoleActivities()
+                .AddSingleton<IScriptEngineConfigurator, ScriptEngineConfigurator>()
+                .BuildServiceProvider();
+
+            // Create a workflow.
+            var workflowFactory = services.GetRequiredService<IWorkflowFactory>();
+            var workflow = workflowFactory.CreateWorkflow<ActivityOutputWorkflow>();
+
+            // Invoke the workflow.
+            var invoker = services.GetService<IWorkflowInvoker>();
+            await invoker.InvokeAsync(workflow);
+
+            Console.ReadLine();
+        }
+    }
+}
--- src/core/Elsa.Core/Scripting/CommonScriptEngineConfigurator.cs
+++ src/core/Elsa.Core/Scripting/CommonScriptEngineConfigurator.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Dynamic;
 using Elsa.Services.Models;
 using Jint;
+using Newtonsoft.Json;
 
 namespace Elsa.Scripting
 {
@@ -10,15 +12,27 @@ namespace Elsa.Scripting
         {
             var context = workflowExecutionContext;
             var workflow = workflowExecutionContext.Workflow;
-            
+
             engine.SetValue("input", (Func<string, object>) (name => workflow.Input.GetVariable(name)));
             engine.SetValue("variable", (Func<string, object>) (name => context.CurrentScope.GetVariable(name)));
             engine.SetValue("lastResult", (Func<string, object>) (name => context.CurrentScope.LastResult));
             engine.SetValue("correlationId", (Func<string, object>) (name => context.Workflow.CorrelationId));
-            
+
             foreach (var variable in workflowExecutionContext.CurrentScope.Variables)
             {
                 engine.SetValue(variable.Key, variable.Value);
+            }
+
+            foreach (var activity in workflowExecutionContext.Workflow.Activities)
+            {
+                engine.SetValue(
+                    activity.Id,
+                    new
+                    {
+                        state = activity.State,
+                        output = JsonConvert.DeserializeObject<ExpandoObject>(activity.Output.ToString())
+                    }
+                );
             }
         }
     }
