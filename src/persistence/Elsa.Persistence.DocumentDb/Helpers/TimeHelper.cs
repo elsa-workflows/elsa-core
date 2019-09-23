cab787dda34c6534bf3f76b0c766f66767344ed6
Jonathan Ruckert <jruckert@novaworks.com.au>
2019-09-23 12:25:34 +0930

    Added in DocumentDb Persistence layer

---
+++ src/persistence/Elsa.Persistence.DocumentDb/Helpers/TimeHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Elsa.Persistence.DocumentDb.Helpers
+{
+    internal static class TimeHelper
+    {
+        private static readonly DateTime epochDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+
+        internal static int ToEpoch(this DateTime date)
+        {
+            if (date.Equals(DateTime.MinValue)) return int.MinValue;
+            TimeSpan epochTimeSpan = date - epochDateTime;
+            return (int)epochTimeSpan.TotalSeconds;
+        }
+
+        internal static DateTime ToDateTime(this int totalSeconds) => epochDateTime.AddSeconds(totalSeconds);
+
+        internal static string TryParseToEpoch(this string s)
+        {
+            return DateTime.TryParse(s, CultureInfo.InvariantCulture, DateTimeStyles.RoundtripKind, out DateTime date)
+                ? date.ToEpoch().ToString(CultureInfo.InvariantCulture)
+                : s;
+        }
+    }
+}
