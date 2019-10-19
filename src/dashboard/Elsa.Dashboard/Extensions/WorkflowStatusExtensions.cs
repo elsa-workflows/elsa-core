f3384ce49e7b6c9ed0aff0d8d5dc1b7a420af495
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2019-10-19 18:22:44 +0200

    Implement execution log view (#94)
    * Implement execution log view panel

---
+++ src/dashboard/Elsa.Dashboard/Extensions/WorkflowStatusExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Elsa.Models;
+
+namespace Elsa.Dashboard.Extensions
+{
+    public static class WorkflowStatusExtensions
+    {
+        public static string GetStatusClass(this WorkflowStatus workflowStatus) =>
+            workflowStatus switch
+            {
+                WorkflowStatus.Executing => "bg-info",
+                WorkflowStatus.Faulted => "bg-warning",
+                WorkflowStatus.Finished => "bg-success",
+                WorkflowStatus.Aborted => "bg-info",
+                _ => "bg-default",
+            };
+    }
+}
