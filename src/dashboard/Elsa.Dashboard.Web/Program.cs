43a2ca6ac3ccaaddeac6324816898582d8473072
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2019-10-20 16:49:52 +0200

    Update Program

--- src/dashboard/Elsa.Dashboard.Web/Program.cs
+++ src/dashboard/Elsa.Dashboard.Web/Program.cs
@@ -1,5 +1,5 @@
-﻿using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 
 namespace Elsa.Dashboard.Web
 {
@@ -7,11 +7,11 @@ namespace Elsa.Dashboard.Web
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
     }
 }
