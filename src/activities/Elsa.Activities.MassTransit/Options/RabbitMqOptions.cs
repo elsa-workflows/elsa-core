0c976428a2badfa1a7ca960df76b1413752a0978
Peter Davis <peter.davis@au.saabgroup.com>
2019-11-14 16:34:09 +1030

    Fix issue #134 where consumer could not be created

--- src/activities/Elsa.Activities.MassTransit/Options/RabbitMqOptions.cs
+++ src/activities/Elsa.Activities.MassTransit/Options/RabbitMqOptions.cs
@@ -3,5 +3,7 @@ namespace Elsa.Activities.MassTransit.Options
     public class RabbitMqOptions
     {
         public string Host { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
     }
 }
