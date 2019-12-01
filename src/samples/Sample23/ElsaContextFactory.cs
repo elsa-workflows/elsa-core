0e27196ae9790b29faa9431bf6790916239f5067
Nate Hughey <jhughey2@harriscomputer.com>
2019-12-01 17:24:16 -0600

    Fixed the Context factory in Sample23 to use the IDbContextCustomSchema.

--- src/samples/Sample23/ElsaContextFactory.cs
+++ src/samples/Sample23/ElsaContextFactory.cs
@@ -1,8 +1,10 @@
 using Elsa.Persistence.EntityFrameworkCore;
 using Elsa.Persistence.EntityFrameworkCore.DbContexts;
+using Elsa.Persistence.EntityFrameworkCore.Extensions;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
 using Sample23.CustomMigration;
 
 namespace Sample23
@@ -11,15 +13,26 @@ namespace Sample23
     {
         public ElsaContext CreateDbContext(string[] args)
         {
+            // Migrations added to Sample23.CustomMigration with the following Powershell command
+            // Add-Migration InitialCreate -context ElsaContext -Project Sample23.CustomMigration
+            var services = new ServiceCollection();
+            services.AddCustomSchemaSupport("elsa");
+
             var optionsBuilder = new DbContextOptionsBuilder<ElsaContext>();
-            string customDefaultSchema = "elsa";
 
             optionsBuilder.UseSqlite(
                 @"Data Source=c:\data\elsa.entity-framework-core.db;Cache=Shared",
                 x =>
                 {
                     x.MigrationsAssembly(typeof(MigrateStub).Assembly.FullName);
-                    x.MigrationsHistoryTable("__EFMigrationsHistory", customDefaultSchema);
+                    using (var scope = services.BuildServiceProvider().CreateScope())
+                    {
+                        IDbContextCustomSchema dbContextCustomSchema = scope.ServiceProvider.GetService<IDbContextCustomSchema>();
+                        if (dbContextCustomSchema != null && dbContextCustomSchema.UseCustomSchema)
+                        {
+                            x.MigrationsHistoryTable(dbContextCustomSchema.CustomMigrationsHistoryTableName, dbContextCustomSchema.CustomDefaultSchema);
+                        }
+                    }
                 });
 
             optionsBuilder.ReplaceService<IModelCacheKeyFactory, CustomSchemaModelCacheKeyFactory>();
