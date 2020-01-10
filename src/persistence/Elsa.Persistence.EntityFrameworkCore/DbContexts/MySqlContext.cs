d1b625685a12897e29e539637fd9a0c0f4032421
Terry Delph <tezzadelph@gmail.com>
2020-01-10 16:35:51 +0000

    Added MySqlContext Added MySqlContextFactory Added Pomelo.EntityFrameworkCore.MySql nuget package

---
+++ src/persistence/Elsa.Persistence.EntityFrameworkCore/DbContexts/MySqlContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Elsa.Persistence.EntityFrameworkCore.DbContexts
+{
+    public class MySqlContext : ElsaContext
+    {
+        public MySqlContext(DbContextOptions<ElsaContext> options) : base(options)
+        {
+        }
+    }
+}
