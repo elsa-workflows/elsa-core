abf24557f390f8c7f2611582bf386821256f67c8
Ankit Sarkar <ankt.srkr@gmail.com>
2020-06-14 16:03:43 +0530

    Fix#325:Elsa 2#RunWorkflowDefinitionAsync getting object reference null (#328)
    * Fix#325:Elsa 2#RunWorkflowDefinitionAsync getting object reference null
    
    * Fix#328: Rephrased error message
    
    * activityId should not be mandatory while using RunWorkflowDefinitionAsync
    
    Co-authored-by: Ankit Sarkar <ankt.srkr@live.in>

--- src/core/Elsa.Abstractions/Services/IWorkflowHost.cs
+++ src/core/Elsa.Abstractions/Services/IWorkflowHost.cs
@@ -8,15 +8,16 @@ namespace Elsa.Services
     public interface IWorkflowHost
     {
         Task<WorkflowExecutionContext> RunWorkflowAsync(Workflow workflow, string? activityId = default, object? input = default, string? correlationId = default, CancellationToken cancellationToken = default);
-        
+
         Task<WorkflowExecutionContext?> RunWorkflowInstanceAsync(string workflowInstanceId, string? activityId = default, object? input = default, CancellationToken cancellationToken = default);
+
         Task<WorkflowExecutionContext?> RunWorkflowInstanceAsync(WorkflowInstance workflowInstance, string? activityId = default, object? input = default, CancellationToken cancellationToken = default);
-        
+
         /// <summary>
         /// Run a registered workflow by its ID.
         /// </summary>
-        Task<WorkflowExecutionContext> RunWorkflowDefinitionAsync(string workflowDefinitionId, string? activityId, object? input = default, string? correlationId = default, CancellationToken cancellationToken = default);
-        
+        Task<WorkflowExecutionContext> RunWorkflowDefinitionAsync(string workflowDefinitionId, string? activityId = default, object? input = default, string? correlationId = default, CancellationToken cancellationToken = default);
+
         // /// <summary>
         // /// Resume a workflow instance.
         // /// </summary>
