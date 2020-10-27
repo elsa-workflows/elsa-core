a054419980535368f55440974426626bdb86eb5e
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2020-10-27 22:17:45 +0100

    Incremental work on cutting up layout in components

--- src/dashboards/blazor/ElsaDashboard.Blazor/Shared/Models/MenuItem.cs
+++ src/dashboards/blazor/ElsaDashboard.Blazor/Shared/Models/MenuItem.cs
@@ -2,13 +2,15 @@
 {
     public class MenuItem
     {
-        public MenuItem(string text, string icon)
+        public MenuItem(string text, string url, string icon)
         {
             Text = text;
+            Url = url;
             Icon = icon;
         }
         
         public string Text { get; set; }
         public string Icon { get; set; }
+        public string Url { get; set; }
     }
 }
