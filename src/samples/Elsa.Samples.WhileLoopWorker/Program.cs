69b76e46a97a66a84aed14d2a12656483627a11e
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2020-10-29 11:42:38 +0100

    Add While loop worker demo

--- src/samples/Elsa.Samples.WhileLoopWorker/Program.cs
+++ src/samples/Elsa.Samples.WhileLoopWorker/Program.cs
@@ -1,9 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Elsa.Samples.WhileLoopWorker.Services;
+using Elsa.Samples.WhileLoopWorker.Workflows;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using NodaTime;
+using YesSql.Provider.Sqlite;
 
 namespace Elsa.Samples.WhileLoopWorker
 {
@@ -16,6 +16,16 @@ namespace Elsa.Samples.WhileLoopWorker
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureServices((hostContext, services) => { services.AddHostedService<Worker>(); });
+                .ConfigureServices(
+                    (hostContext, services) =>
+                    {
+                        services
+                            .AddElsa(options => options.UsePersistence(db => db.UseSqLite("Data Source=elsa.db;Cache=Shared")))
+                            .AddConsoleActivities()
+                            .AddTimerActivities(options => options.SweepInterval = Duration.FromSeconds(1))
+                            .AddSingleton<PhoneCallService>()
+                            .AddHostedService<PhoneCallWorker>()
+                            .AddWorkflow<PhoneCallWorkflow>();
+                    });
     }
 }
---
+++ src/samples/Elsa.Samples.WhileLoopWorker/Workflows/PhoneCallWorkflow.cs
@@ -0,0 +1,41 @@
+﻿using Elsa.Activities.Console;
+using Elsa.Activities.ControlFlow;
+using Elsa.Activities.Timers;
+using Elsa.Builders;
+using Elsa.Samples.WhileLoopWorker.Services;
+using NodaTime;
+
+namespace Elsa.Samples.WhileLoopWorker.Workflows
+{
+    /// <summary>
+    /// This workflow prompts the user to enter an integer start value, then iterates back from that value to 0.
+    /// The workflow also demonstrates retrieving runtime values such as user input. 
+    /// </summary>
+    public class PhoneCallWorkflow : IWorkflow
+    {
+        private readonly PhoneCallService _phoneCallService;
+
+        public PhoneCallWorkflow(PhoneCallService phoneCallService)
+        {
+            _phoneCallService = phoneCallService;
+        }
+        
+        public void Build(IWorkflowBuilder workflow)
+        {
+            workflow
+                .WriteLine("Simulating a phone call... ringgg ringgg.")
+                .While(() => _phoneCallService.CallStatus != PhoneCallStatus.Finished,
+                    @while =>
+                    {
+                        @while
+                            .WriteLine("Ringgggg ringgg.")
+                            .WriteLine(() => $"Call status: {_phoneCallService.CallStatus}")
+                            .TimerEvent(Duration.FromSeconds(5))
+                            .Then(() => _phoneCallService.Progress());
+
+                    })
+                .WriteLine(() => $"Call status: {_phoneCallService.CallStatus}")
+                .WriteLine("Workflow finished.");
+        }
+    }
+}
