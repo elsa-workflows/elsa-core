5f1beae273eb839cabd6e3c978a633268de88816
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2020-10-29 21:35:52 +0100

    Add IfElse builder extension

--- src/core/Elsa.Core/Activities/ControlFlow/IfElse/IfElseBuilderExtensions.cs
+++ src/core/Elsa.Core/Activities/ControlFlow/IfElse/IfElseBuilderExtensions.cs
@@ -7,6 +7,13 @@ namespace Elsa.Activities.ControlFlow
 {
     public static class IfElseBuilderExtensions
     {
+        public static IActivityBuilder IfElse(this IBuilder builder, Action<ISetupActivity<IfElse>>? setup = default, Action<IActivityBuilder>? ifElse = default)
+        {
+            var activityBuilder = builder.Then(setup);
+            ifElse?.Invoke(activityBuilder);
+            return activityBuilder;
+        }
+
         public static IActivityBuilder IfElse(this IBuilder builder, Action<ISetupActivity<IfElse>>? setup = default, string? name = default) => builder.Then(setup).WithName(name);
         public static IActivityBuilder IfElse(this IBuilder builder, Func<ActivityExecutionContext, bool> condition, string? name = default) => builder.IfElse(activity => activity.Set(x => x.Condition, condition), name);
         public static IActivityBuilder IfElse(this IBuilder builder, Func<bool> condition, string? name = default) => builder.IfElse(activity => activity.Set(x => x.Condition, condition), name);
