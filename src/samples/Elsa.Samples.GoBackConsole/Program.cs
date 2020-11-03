70d3569343b9b5fcf93be414e23cca7a994018b6
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2020-11-03 12:14:25 +0100

    Add GoBackResult + sample

---
+++ src/samples/Elsa.Samples.GoBackConsole/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using Elsa.Samples.GoBackConsole.Activities;
+using Elsa.Samples.GoBackConsole.Workflows;
+using Elsa.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Elsa.Samples.GoBackConsole
+{
+    /// <summary>
+    /// Demonstrates a workflow with a While looping construct.
+    /// </summary>
+    static class Program
+    {
+        private static async Task Main()
+        {
+            // Create a service container with Elsa services.
+            var services = new ServiceCollection()
+                .AddElsa()
+                .AddConsoleActivities()
+                .AddActivity<BrickWallActivity>()
+                .AddWorkflow<WalkAroundWorkflow>()
+                .BuildServiceProvider();
+            
+            // Run startup actions (not needed when registering Elsa with a Host).
+            var startupRunner = services.GetRequiredService<IStartupRunner>();
+            await startupRunner.StartupAsync();
+            
+            // Get a workflow runner.
+            var workflowRunner = services.GetService<IWorkflowRunner>();
+
+            // Execute the workflow.
+            await workflowRunner.RunWorkflowAsync<WalkAroundWorkflow>();
+        }
+    }
+}
---
+++ src/samples/Elsa.Samples.GoBackConsole/Workflows/WalkAroundWorkflow.cs
@@ -0,0 +1,30 @@
+﻿using Elsa.Activities.Console;
+using Elsa.Activities.ControlFlow;
+using Elsa.Builders;
+using Elsa.Samples.GoBackConsole.Activities;
+using Elsa.Services.Models;
+
+namespace Elsa.Samples.GoBackConsole.Workflows
+{
+    public class WalkAroundWorkflow : IWorkflow
+    {
+        public void Build(IWorkflowBuilder workflow)
+        {
+            workflow
+                .WriteLine("Taking a stroll...")
+                .IfElse(context => (string?)context.Input == "Brick Wall",
+                    ifElse =>
+                    {
+                        ifElse
+                            .When(IfElse.False)
+                            .WriteLine("Hitting a wall...")
+                            .Then<BrickWallActivity>();
+
+                        ifElse
+                            .When(IfElse.True)
+                            .WriteLine("Going around the wall...")
+                            .WriteLine("Made it!");
+                    });
+        }
+    }
+}
