0db3dac0ec0ab06dd8157fd2ba481a985d20dc02
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2020-11-08 22:55:03 +0100

    Incremental work on designer

--- src/dashboards/blazor/ElsaDashboard.Blazor/Shared/ClientApp/tailwind.config.js
+++ src/dashboards/blazor/ElsaDashboard.Blazor/Shared/ClientApp/tailwind.config.js
@@ -1,18 +1,26 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-  purge: ["./src/**/*.html"],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-      },
+    purge: ["./src/**/*.html"],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+            },
+        },
+        inset: {
+            '0': 0,
+            auto: 'auto',
+            'inverse-right-10': '-1rem',
+            'inverse-right-30': '-3rem'
+        }
     },
-  },
-  variants: {},
-  plugins: [
-    require('@tailwindcss/ui')({
-      layout: 'sidebar',
-    })
-  ],
+    variants: {
+        borderWidth: ['responsive', 'hover', 'focus'],
+    },
+    plugins: [
+        require('@tailwindcss/ui')({
+            layout: 'sidebar',
+        })
+    ],
 };
