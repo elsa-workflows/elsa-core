207550db751c54329ff8d1af758f7e71118ab8c7
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2020-11-09 16:56:45 +0100

    Incremental work on designer

--- src/dashboards/blazor/ElsaDashboard.Blazor/Shared/ClientApp/tailwind.config.js
+++ src/dashboards/blazor/ElsaDashboard.Blazor/Shared/ClientApp/tailwind.config.js
@@ -7,16 +7,10 @@ module.exports = {
             fontFamily: {
                 sans: ['Inter var', ...defaultTheme.fontFamily.sans],
             },
-        },
-        inset: {
-            '0': 0,
-            auto: 'auto',
-            'inverse-right-10': '-1rem',
-            'inverse-right-30': '-3rem'
         }
     },
     variants: {
-        borderWidth: ['responsive', 'hover', 'focus'],
+        borderColor: ['responsive', 'hover', 'focus']
     },
     plugins: [
         require('@tailwindcss/ui')({
