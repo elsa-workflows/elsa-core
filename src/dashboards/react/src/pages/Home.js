431e2797b00462ad2a1b47b5905138d87d4652b4
NintendoEngineer <44787580+NintendoEngineer@users.noreply.github.com>
2020-11-20 15:49:14 +0100

    Cleared react folder contents for fresh start (#469)
    Co-authored-by: Arjuna Enait <nintendoengineer12@gmail.com>

--- src/dashboards/react/src/pages/Home.js
+++
@@ -1,5 +0,0 @@
-﻿import React from "react";
-
-export default function Home() {
-    return <h1>Hello Home</h1>;
-}
--- src/dashboards/react/src/pages/Workflows.js
+++
@@ -1,5 +0,0 @@
-﻿import React from "react";
-
-export default function Workflows() {
-    return <h1>Hello Workflows</h1>;
-}
