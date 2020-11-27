dcd46d2d0bdabfeedc71d5b9e1a990c0fd8f0b75
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2020-11-27 21:39:58 +0100

    Restructure samples

---
+++ src/samples/worker/Elsa.Samples.RebusWorker/Program.cs
@@ -0,0 +1,28 @@
+using Elsa.Samples.RebusWorker.Messages;
+using Elsa.Samples.RebusWorker.Workflows;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Elsa.Samples.RebusWorker
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services
+                        .AddElsa()
+                        .AddConsoleActivities()
+                        .AddTimerActivities()
+                        .AddRebusActivities<Greeting>()
+                        .AddWorkflow<ProducerWorkflow>()
+                    .AddWorkflow<ConsumerWorkflow>();
+                });
+    }
+}
