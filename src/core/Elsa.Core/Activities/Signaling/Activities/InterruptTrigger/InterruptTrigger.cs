a21ba060d5df77ae41edf07e671179915c7a02fd
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2020-12-06 13:22:24 +0100

    Add InterruptTrigger activity

---
+++ src/core/Elsa.Core/Activities/Signaling/Activities/InterruptTrigger/InterruptTrigger.cs
@@ -0,0 +1,46 @@
+using System.Threading.Tasks;
+using Elsa.ActivityResults;
+using Elsa.Attributes;
+using Elsa.Extensions;
+using Elsa.Services;
+using Elsa.Services.Models;
+
+// ReSharper disable once CheckNamespace
+namespace Elsa.Activities.Signaling
+{
+    /// <summary>
+    /// Resumes workflow execution that is blocked on a given activity.
+    /// </summary>
+    [Trigger(
+        Category = "Workflows",
+        Description = "Resumes suspended workflows that are blocked on a specific trigger.",
+        Outcomes = new[] { OutcomeNames.Done }
+    )]
+    public class InterruptTrigger : Activity
+    {
+        private readonly IWorkflowTriggerInterruptor _workflowTriggerInterruptor;
+        private readonly IWorkflowInstanceManager _workflowInstanceManager;
+
+        public InterruptTrigger(IWorkflowTriggerInterruptor workflowTriggerInterruptor, IWorkflowInstanceManager workflowInstanceManager)
+        {
+            _workflowTriggerInterruptor = workflowTriggerInterruptor;
+            _workflowInstanceManager = workflowInstanceManager;
+        }
+        
+        [ActivityProperty(Hint = "The ID of the workflow instance to resume.")]
+        public string WorkflowInstanceId { get; set; } = default!;
+        
+        [ActivityProperty(Hint = "The ID of the blocking activity to trigger.")]
+        public string BlockingActivityId { get; set; } = default!;
+        
+        [ActivityProperty(Hint = "An optional input to pass to the blocking activity.")]
+        public object? Input { get; set; } = default!;
+
+        protected override async ValueTask<IActivityExecutionResult> OnExecuteAsync(ActivityExecutionContext context)
+        {
+            var workflowInstance = await _workflowInstanceManager.GetByIdAsync(WorkflowInstanceId, context.CancellationToken);
+            await _workflowTriggerInterruptor.InterruptActivityAsync(workflowInstance!, BlockingActivityId, Input, context.CancellationToken);
+            return Done();
+        }
+    }
+}
