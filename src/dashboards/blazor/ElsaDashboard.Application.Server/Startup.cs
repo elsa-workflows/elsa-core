8dab2adca3b12bea797a84a546385cb3f0a6acd8
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2020-12-12 14:58:52 +0100

    Cleanup

--- src/dashboards/blazor/ElsaDashboard.Application.Server/Startup.cs
+++ src/dashboards/blazor/ElsaDashboard.Application.Server/Startup.cs
@@ -33,7 +33,6 @@ namespace ElsaDashboard.Application.Server
                 });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -46,7 +45,6 @@ namespace ElsaDashboard.Application.Server
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
