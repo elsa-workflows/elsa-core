3776b651c37592c00b3dc40b479ae385ef86f12e
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2020-12-21 17:33:18 +0100

    Make WorkflowExecutionLogRecordDocument properties writable

--- src/persistence/Elsa.Persistence.YesSql/Documents/WorkflowExecutionLogRecordDocument.cs
+++ src/persistence/Elsa.Persistence.YesSql/Documents/WorkflowExecutionLogRecordDocument.cs
@@ -5,11 +5,11 @@ namespace Elsa.Persistence.YesSql.Documents
 {
     public class WorkflowExecutionLogRecordDocument : YesSqlDocument
     {
-        public string RecordId { get; } = default!;
-        public string? TenantId { get; }
+        public string RecordId { get; set; } = default!;
+        public string? TenantId { get; set; }
         public string WorkflowInstanceId { get; set; } = default!;
         public string ActivityId { get; set; } = default!;
-        public Instant Timestamp { get; set; }= default!;
+        public Instant Timestamp { get; set; } = default!;
         public string? Message { get; set; }
         public JObject? Data { get; set; }
     }
