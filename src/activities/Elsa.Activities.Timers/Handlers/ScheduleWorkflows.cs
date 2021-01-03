96464c988ca088a208dbfeddcf1e5e5a1781bd5d
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2021-01-03 19:21:50 +0100

    Update ScheduleWorkflowResult to schedule job AFTER workflow is suspended

---
+++ src/activities/Elsa.Activities.Timers/Handlers/ScheduleWorkflows.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Elsa.Activities.Timers.ActivityResults;
+using Elsa.Events;
+using MediatR;
+
+namespace Elsa.Activities.Timers.Handlers
+{
+    public class ScheduleWorkflows : INotificationHandler<WorkflowSuspended>
+    {
+        public async Task Handle(WorkflowSuspended notification, CancellationToken cancellationToken)
+        {
+            await notification.WorkflowExecutionContext.ExecuteRegisteredTasksAsync(nameof(ScheduleWorkflowResult), cancellationToken);
+        }
+    }
+}
