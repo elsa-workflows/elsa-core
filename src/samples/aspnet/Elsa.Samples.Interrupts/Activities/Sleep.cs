3853df25722456ec7c10da7b48833aebaf9f7e5a
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2021-01-03 19:24:18 +0100

    Fix Sleep base constructor

--- src/samples/aspnet/Elsa.Samples.Interrupts/Activities/Sleep.cs
+++ src/samples/aspnet/Elsa.Samples.Interrupts/Activities/Sleep.cs
@@ -2,6 +2,7 @@ using Elsa.Activities.Timers;
 using Elsa.Activities.Timers.Services;
 using Elsa.Attributes;
 using Elsa.Persistence;
+using Microsoft.Extensions.Logging;
 using NodaTime;
 
 namespace Elsa.Samples.Interrupts.Activities
@@ -9,7 +10,7 @@ namespace Elsa.Samples.Interrupts.Activities
     [Activity]
     public class Sleep : Timer
     {
-        public Sleep(IWorkflowInstanceStore workflowInstanceStore, IWorkflowScheduler workflowScheduler, IClock clock) : base(clock)
+        public Sleep(IWorkflowInstanceStore workflowInstanceStore, IWorkflowScheduler workflowScheduler, IClock clock, ILogger<Sleep> logger) : base(clock, logger)
         {
         }
     }
