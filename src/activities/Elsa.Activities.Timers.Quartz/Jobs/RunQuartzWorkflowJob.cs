c487e6e32858af873ab6b5d44557b9a21e4afa3f
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2021-01-08 11:00:01 +0100

    Synchronize quartz job and run instance consumer with distributed locking

--- src/activities/Elsa.Activities.Timers.Quartz/Jobs/RunQuartzWorkflowJob.cs
+++ src/activities/Elsa.Activities.Timers.Quartz/Jobs/RunQuartzWorkflowJob.cs
@@ -1,36 +1,53 @@
 ﻿using System.Threading.Tasks;
 using Elsa.Activities.Timers.Quartz.Services;
+using Elsa.DistributedLock;
 using Elsa.Models;
 using Elsa.Persistence;
 using Elsa.Persistence.Specifications;
 using Elsa.Services;
+using Microsoft.Extensions.Logging;
 using Quartz;
 
 namespace Elsa.Activities.Timers.Quartz.Jobs
 {
     public class RunQuartzWorkflowJob : IJob
     {
-        private readonly IWorkflowRunner _workflowRunner;
         private readonly IWorkflowRegistry _workflowRegistry;
         private readonly IWorkflowInstanceStore _workflowInstanceStore;
         private readonly IWorkflowQueue _workflowQueue;
+        private readonly IDistributedLockProvider _distributedLockProvider;
+        private readonly ILogger _logger;
 
-        public RunQuartzWorkflowJob(IWorkflowRunner workflowRunner, IWorkflowRegistry workflowRegistry, IWorkflowInstanceStore workflowInstanceStore, IWorkflowQueue workflowQueue)
+        public RunQuartzWorkflowJob(
+            IWorkflowRegistry workflowRegistry, 
+            IWorkflowInstanceStore workflowInstanceStore, 
+            IWorkflowQueue workflowQueue, 
+            IDistributedLockProvider distributedLockProvider,
+            ILogger<RunQuartzWorkflowJob> logger)
         {
-            _workflowRunner = workflowRunner;
             _workflowRegistry = workflowRegistry;
             _workflowInstanceStore = workflowInstanceStore;
             _workflowQueue = workflowQueue;
+            _distributedLockProvider = distributedLockProvider;
+            _logger = logger;
         }
 
         public async Task Execute(IJobExecutionContext context)
         {
             var dataMap = context.MergedJobDataMap;
+            var cancellationToken = context.CancellationToken;
+            var workflowInstanceId = dataMap.GetString("WorkflowInstanceId");
             var tenantId = dataMap.GetString("TenantId");
             var workflowDefinitionId = dataMap.GetString("WorkflowDefinitionId")!;
-            var workflowInstanceId = dataMap.GetString("WorkflowInstanceId");
             var activityId = dataMap.GetString("ActivityId")!;
-            var cancellationToken = context.CancellationToken;
+            var lockKey = (workflowInstanceId, workflowDefinitionId, activityId).GetHashCode().ToString();
+
+            if (!await _distributedLockProvider.AcquireLockAsync(lockKey, cancellationToken))
+            {
+                _logger.LogDebug("Failed to acquire lock on {WorkflowInstanceId} / {WorkflowDefinitionId} / {ActivityId}", workflowInstanceId, workflowDefinitionId, activityId);
+                return;
+            }
+
             var workflowBlueprint = (await _workflowRegistry.GetWorkflowAsync(workflowDefinitionId, tenantId, VersionOptions.Published, cancellationToken))!;
 
             if (workflowInstanceId == null)
--- src/core/Elsa.Core/Consumers/RunWorkflowInstanceConsumer.cs
+++ src/core/Elsa.Core/Consumers/RunWorkflowInstanceConsumer.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Threading.Tasks;
+using Elsa.DistributedLock;
 using Elsa.Exceptions;
 using Elsa.Messages;
 using Elsa.Models;
@@ -13,29 +15,57 @@ namespace Elsa.Consumers
     {
         private readonly IWorkflowRunner _workflowRunner;
         private readonly IWorkflowInstanceStore _workflowInstanceManager;
+        private readonly IDistributedLockProvider _distributedLockProvider;
+        private readonly IEventPublisher _eventPublisher;
         private readonly ILogger _logger;
 
-        public RunWorkflowInstanceConsumer(IWorkflowRunner workflowRunner, IWorkflowInstanceStore workflowInstanceStore, ILogger<RunWorkflowInstanceConsumer> logger)
+        public RunWorkflowInstanceConsumer(
+            IWorkflowRunner workflowRunner, 
+            IWorkflowInstanceStore workflowInstanceStore, 
+            IDistributedLockProvider distributedLockProvider, 
+            IEventPublisher eventPublisher,
+            ILogger<RunWorkflowInstanceConsumer> logger)
         {
             _workflowRunner = workflowRunner;
             _workflowInstanceManager = workflowInstanceStore;
+            _distributedLockProvider = distributedLockProvider;
+            _eventPublisher = eventPublisher;
             _logger = logger;
         }
 
         public async Task Handle(RunWorkflowInstance message)
         {
             var workflowInstanceId = message.WorkflowInstanceId;
-            var workflowInstance = await _workflowInstanceManager.FindByIdAsync(message.WorkflowInstanceId);
-
-            if (!ValidatePreconditions(workflowInstanceId, workflowInstance))
-                return;
             
-            await _workflowRunner.RunWorkflowAsync(
-                workflowInstance!,
-                message.ActivityId,
-                message.Input);
+            _logger.LogDebug("Acquiring lock on workflow instance {WorkflowInstanceId}.", workflowInstanceId);
+
+            if (!await _distributedLockProvider.AcquireLockAsync(workflowInstanceId))
+            {
+                // Reschedule message.
+                _logger.LogDebug("Failed to acquire lock on workflow instance {WorkflowInstanceId}. Rescheduling message.", workflowInstanceId);
+                await Task.Delay(TimeSpan.FromSeconds(1));
+                await _eventPublisher.PublishAsync(message);
+                return;
+            }
+            
+            try
+            {
+                var workflowInstance = await _workflowInstanceManager.FindByIdAsync(message.WorkflowInstanceId);
+
+                if (!ValidatePreconditions(workflowInstanceId, workflowInstance))
+                    return;
+
+                await _workflowRunner.RunWorkflowAsync(
+                    workflowInstance!,
+                    message.ActivityId,
+                    message.Input);
+            }
+            finally
+            {
+                await _distributedLockProvider.ReleaseLockAsync(workflowInstanceId);
+            }
         }
-        
+
         private bool ValidatePreconditions(string? workflowInstanceId, WorkflowInstance? workflowInstance)
         {
             if (workflowInstance == null)
