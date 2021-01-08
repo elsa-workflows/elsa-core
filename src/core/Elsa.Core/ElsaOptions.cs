a33c2bfd3811cc9d29b9d7a16d1560ebe0313607
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2021-01-08 10:11:49 +0100

    Update Rebus logging to use Microsoft Extensions Logging

--- src/core/Elsa.Core/ElsaOptions.cs
+++ src/core/Elsa.Core/ElsaOptions.cs
@@ -13,14 +13,16 @@ using Elsa.Persistence.InMemory;
 using Elsa.Serialization;
 using Elsa.Services;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Rebus.Config;
 using Rebus.DataBus.InMem;
-using Rebus.Logging;
 using Rebus.Persistence.InMem;
 using Rebus.Routing.TypeBased;
 using Rebus.Transport.InMem;
 using Storage.Net;
 using Storage.Net.Blobs;
+using LogLevel = Rebus.Logging.LogLevel;
 
 namespace Elsa
 {
@@ -224,12 +226,13 @@ namespace Elsa
         private static void ConfigureInMemoryServiceBusEndpoint(ServiceBusEndpointConfigurationContext context)
         {
             var serviceProvider = context.ServiceProvider;
+            var loggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>();
             var transport = serviceProvider.GetService<InMemNetwork>();
             var store = serviceProvider.GetRequiredService<InMemorySubscriberStore>();
             var queueName = context.QueueName;
 
             context.Configurer
-                .Logging(l => l.ColoredConsole(LogLevel.Info))
+                .Logging(l => l.MicrosoftExtensionsLogging(loggerFactory))
                 .Subscriptions(s => s.StoreInMemory(store))
                 .Transport(t => t.UseInMemoryTransport(transport, queueName))
                 .Routing(r => r.TypeBased().Map(context.MessageTypeMap));
--- src/servicebus/Elsa.Rebus.AzureServiceBus/Extensions/ElsaOptionsExtensions.cs
+++ src/servicebus/Elsa.Rebus.AzureServiceBus/Extensions/ElsaOptionsExtensions.cs
@@ -1,24 +1,27 @@
 ﻿using Elsa.Services;
 using Microsoft.Azure.ServiceBus.Primitives;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Rebus.Config;
-using Rebus.Logging;
 using Rebus.Routing.TypeBased;
+using LogLevel = Rebus.Logging.LogLevel;
 
 namespace Elsa.Rebus.AzureServiceBus
 {
     public static class ElsaOptionsExtensions
     {
-        public static ElsaOptions UseAzureServiceBus(this ElsaOptions elsaOptions, string connectionString, LogLevel logLevel = LogLevel.Info, ITokenProvider? tokenProvider = default)
+        public static ElsaOptions UseAzureServiceBus(this ElsaOptions elsaOptions, string connectionString, ITokenProvider? tokenProvider = default)
         {
-            return elsaOptions.UseServiceBus(context => ConfigureAzureServiceBusEndpoint(context, connectionString, logLevel, tokenProvider));
+            return elsaOptions.UseServiceBus(context => ConfigureAzureServiceBusEndpoint(context, connectionString, tokenProvider));
         } 
         
-        private static void ConfigureAzureServiceBusEndpoint(ServiceBusEndpointConfigurationContext context, string connectionString, LogLevel logLevel, ITokenProvider? tokenProvider)
+        private static void ConfigureAzureServiceBusEndpoint(ServiceBusEndpointConfigurationContext context, string connectionString, ITokenProvider? tokenProvider)
         {
             var queueName = context.QueueName;
+            var loggerFactory = context.ServiceProvider.GetRequiredService<ILoggerFactory>();
 
             context.Configurer
-                .Logging(l => l.ColoredConsole(logLevel))
+                .Logging(l => l.MicrosoftExtensionsLogging(loggerFactory))
                 .Transport(t => t.UseAzureServiceBus(connectionString, queueName, tokenProvider))
                 .Routing(r => r.TypeBased().Map(context.MessageTypeMap));
         }
