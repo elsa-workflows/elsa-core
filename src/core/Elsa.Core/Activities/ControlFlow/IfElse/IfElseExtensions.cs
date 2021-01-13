02a3df468c39a405620ee0751cfbfd39aaf19981
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2021-01-13 19:57:49 +0100

    Add IfTrue overload

--- src/core/Elsa.Core/Activities/ControlFlow/IfElse/IfElseExtensions.cs
+++ src/core/Elsa.Core/Activities/ControlFlow/IfElse/IfElseExtensions.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading.Tasks;
 using Elsa.Builders;
 using Elsa.Services.Models;
 
@@ -7,6 +8,7 @@ namespace Elsa.Activities.ControlFlow
 {
     public static class IfElseExtensions
     {
+        public static ISetupActivity<IfElse> WithCondition(this ISetupActivity<IfElse> activity, Func<ActivityExecutionContext, ValueTask<bool>> value) => activity.Set(x => x.Condition, value);
         public static ISetupActivity<IfElse> WithCondition(this ISetupActivity<IfElse> activity, Func<ActivityExecutionContext, bool> value) => activity.Set(x => x.Condition, value);
         public static ISetupActivity<IfElse> WithCondition(this ISetupActivity<IfElse> activity, Func<bool> value) => activity.Set(x => x.Condition, value);
         public static ISetupActivity<IfElse> WithCondition(this ISetupActivity<IfElse> activity, bool value) => activity.Set(x => x.Condition, value);
