860c045e6fd360ef2723399b84ba0b7b779d6a61
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2021-01-19 18:20:05 +0100

    Remove unused namespaces

--- src/activities/Elsa.Activities.AzureServiceBus/Services/QueueWorker.cs
+++ src/activities/Elsa.Activities.AzureServiceBus/Services/QueueWorker.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Diagnostics;
-using System.Linq;
-using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
 using Elsa.Activities.AzureServiceBus.Triggers;
