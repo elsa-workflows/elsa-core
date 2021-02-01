d0523439adb0ee11ca33f67dfdc88c7a7a70bea3
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2021-02-01 16:14:32 +0100

    Fix WorkflowInstanceDocument mapping issue and use SimpleStack

--- src/persistence/Elsa.Persistence.YesSql/Documents/WorkflowInstanceDocument.cs
+++ src/persistence/Elsa.Persistence.YesSql/Documents/WorkflowInstanceDocument.cs
@@ -36,8 +36,8 @@ namespace Elsa.Persistence.YesSql.Documents
         }
         
         public WorkflowFault? Fault { get; set; }
-        public Stack<ScheduledActivity> ScheduledActivities { get; set; } = new();
-        public Stack<string> Scopes { get; set; } = new();
+        public SimpleStack<ScheduledActivity> ScheduledActivities { get; set; } = new();
+        public SimpleStack<string> Scopes { get; set; } = new();
         public ScheduledActivity? CurrentActivity { get; set; }
     }
 }
