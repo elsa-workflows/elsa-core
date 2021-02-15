daac27338df4197acad47a5c66b3660ee96b677f
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2021-02-15 17:56:16 +0100

    Fix expression type conversion

--- src/core/Elsa.Core/Expressions/LiteralHandler.cs
+++ src/core/Elsa.Core/Expressions/LiteralHandler.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel;
 using System.Threading;
 using System.Threading.Tasks;
 using Elsa.Services.Models;
@@ -19,7 +20,8 @@ namespace Elsa.Expressions
             if (returnType == typeof(string))
                 return Task.FromResult<object?>(expression);
 
-            var value = Convert.ChangeType(expression, returnType);
+            var converter = TypeDescriptor.GetConverter(returnType);
+            var value = converter.CanConvertFrom(typeof(string)) ? converter.ConvertFrom(expression) : default;
             return Task.FromResult(value)!;
         }
     }
