6a01bd08de04a5c790dd7f34116f40780f693a3d
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2021-02-15 22:26:52 +0100

    Add Import menu item

--- src/designer/elsa-workflows-studio/src/components/editors/elsa-workflow-publish-button/elsa-workflow-publish-button.tsx
+++ src/designer/elsa-workflows-studio/src/components/editors/elsa-workflow-publish-button/elsa-workflow-publish-button.tsx
@@ -58,6 +58,10 @@ export class ElsaWorkflowPublishButton {
                   Export
                 </a>
 
+                <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem">
+                  Import
+                </a>
+
                 {/*<a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem">*/}
                 {/*  Publish later*/}
                 {/*</a>*/}
