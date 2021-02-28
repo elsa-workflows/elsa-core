a61ecda036f345c0af1f9ca928688c63f294a453
Craig Fowler <craig@csf-dev.com>
2021-02-28 15:10:11 +0000

    WIP #564 - Add test coverage for app startup
    This indicates that you can't start an app with just timer/temporal
    activities, but you could start with either Hangfire or Quartz
    activities.

---
+++ test/integration/Elsa.Core.IntegrationTests/Extensions/TemporalServiceCollectionExtensionsTests.cs
@@ -0,0 +1,119 @@
+using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Xunit;
+using Hangfire;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Elsa.UnitTests.Extensions
+{
+    public class TemporalServiceCollectionExtensionsTests
+    {
+        [Fact(DisplayName = "Starting a hosted app which uses only AddTimerActivities should throw InvalidOperationException because of the missing impl")]
+        public void AddTimerActivitiesThrowsDuringStartupIfNoTemporalImplementationPresent()
+        {
+            var hostBuilder = CreateHostBuilderUsingServices(services => {
+                services.AddElsa(elsa => {
+                    elsa.AddTimerActivities();
+                });
+            });
+
+            var cancellationSource = new CancellationTokenSource();
+            try
+            {
+                Assert.ThrowsAsync<InvalidOperationException>(() => StartAppAndAllowToRun(hostBuilder, cancellationToken: cancellationSource.Token));
+            }
+            finally
+            {
+                // If the test passed then this line doesn't do anything.
+                // If the test fails though, then this prevents it from stalling test execution.
+                CancelAndSquelchExceptions(cancellationSource);
+            }
+        }
+
+        [Fact(DisplayName = "Starting a hosted app which uses AddHangfireTimerActivities should not throw")]
+        public void AddHangfireTimerActivitiesDoesNotThrowDuringStartup()
+        {
+            var hostBuilder = CreateHostBuilderUsingServices(services => {
+                services.AddElsa(elsa => {
+                    elsa.AddHangfireTimerActivities(config => config.UseInMemoryStorage());
+                });
+            });
+
+            var cancellationSource = new CancellationTokenSource();
+            try
+            {
+                StartAppAndAllowToRun(hostBuilder, cancellationToken: cancellationSource.Token);
+            }
+            catch(Exception e)
+            {
+                Assert.False(true, $"Test failed because starting the app threw an exception:\n\n{e}");
+            }
+            finally
+            {
+                CancelAndSquelchExceptions(cancellationSource);
+            }
+        }
+
+        [Fact(DisplayName = "Starting a hosted app which uses AddQuartzTimerActivities should not throw")]
+        public void AddQuartzTimerActivitiesDoesNotThrowDuringStartup()
+        {
+            var hostBuilder = CreateHostBuilderUsingServices(services => {
+                services.AddElsa(elsa => {
+                    elsa.AddQuartzTimerActivities();
+                });
+            });
+
+            var cancellationSource = new CancellationTokenSource();
+            try
+            {
+                StartAppAndAllowToRun(hostBuilder, cancellationToken: cancellationSource.Token);
+            }
+            catch(Exception e)
+            {
+                Assert.False(true, $"Test failed because starting the app threw an exception:\n\n{e}");
+            }
+            finally
+            {
+                CancelAndSquelchExceptions(cancellationSource);
+            }
+        }
+
+        /// <summary>
+        /// Convenience method creates an <see cref="IHostBuilder"/> using the specified services.
+        /// </summary>
+        /// <param name="serviceConfig">Services to use in the created host builder</param>
+        /// <returns>A host builder</returns>
+        static IHostBuilder CreateHostBuilderUsingServices(Action<IServiceCollection> serviceConfig)
+            => Host.CreateDefaultBuilder()
+                .ConfigureServices((hostBuilder, services) => serviceConfig(services));
+
+        /// <summary>
+        /// Starts up the specified <paramref name="hostBuilder"/> instance using console lifetime.
+        /// The current thread is then blocked for a short while, giving the app a chance to start up and run.
+        /// </summary>
+        /// <param name="hostBuilder">A host builder</param>
+        /// <param name="millisecondsToWait">The duration to block the current thread, default = 1000 (1 second)</param>
+        /// <param name="cancellationToken">A cancellation token for killing the app later</param>
+        /// <returns>A <see cref="Task"/> for the running app, created from the host builder</returns>
+        static Task StartAppAndAllowToRun(IHostBuilder hostBuilder, int millisecondsToWait = 1000, CancellationToken cancellationToken = default)
+        {
+            var applicationTask = Task.Run(() => hostBuilder.UseConsoleLifetime().Build().Run(), cancellationToken);
+            Thread.Sleep(millisecondsToWait);
+            return applicationTask;
+        }
+
+        /// <summary>
+        /// Cancels anything controlled by the cancellation source and ignores/suppresses any exceptions encountered.
+        /// </summary>
+        /// <param name="cancellationSource">The cancellation source</param>
+        static void CancelAndSquelchExceptions(CancellationTokenSource cancellationSource)
+        {
+                try { cancellationSource.Cancel(); }
+                catch(Exception) {
+                    // This is only ever used as teardown code, which is why we don't care about exceptions.
+                }
+        }
+    }
+}
