4fc259e15e73cdf4767953b2aaa0b81aa8839845
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2021-03-04 12:05:52 +0100

    Re-index workflow triggers when workflow definition gets published

---
+++ src/core/Elsa.Core/Handlers/UpdateWorkflowTriggersIndex.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Elsa.Events;
+using Elsa.Triggers;
+using MediatR;
+
+namespace Elsa.Handlers
+{
+    public class UpdateWorkflowTriggersIndex : INotificationHandler<WorkflowDefinitionPublished>
+    {
+        private readonly ITriggerIndexer _triggerIndexer;
+
+        public UpdateWorkflowTriggersIndex(ITriggerIndexer triggerIndexer)
+        {
+            _triggerIndexer = triggerIndexer;
+        }
+        
+        public async Task Handle(WorkflowDefinitionPublished notification, CancellationToken cancellationToken)
+        {
+            await _triggerIndexer.IndexTriggersAsync(cancellationToken);
+        }
+    }
+}
