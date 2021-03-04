b719250e3dd7bcfc9f8b61fbd24564b98af3a774
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2021-03-04 11:47:20 +0100

    WIP: Update HttpEndpoint with support for multiple HTTP methods

---
+++ src/activities/Elsa.Activities.Http/Bookmarks/HttpEndpointBookmark.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Elsa.Bookmarks;
+using Microsoft.AspNetCore.Http;
+
+namespace Elsa.Activities.Http.Bookmarks
+{
+    public record HttpEndpointBookmark(PathString Path, string? Method, string? CorrelationId) : IBookmark
+    {
+    }
+
+    public class HttpEndpointBookmarkProvider : BookmarkProvider<HttpEndpointBookmark, HttpEndpoint>
+    {
+        public override async ValueTask<IEnumerable<IBookmark>> GetBookmarksAsync(BookmarkProviderContext<HttpEndpoint> context, CancellationToken cancellationToken)
+        {
+            var path = ToLower(await context.Activity.GetPropertyValueAsync(x => x.Path, cancellationToken))!;
+            var correlationId = ToLower(context.ActivityExecutionContext.WorkflowExecutionContext.CorrelationId);
+            var methods = (await context.Activity.GetPropertyValueAsync(x => x.Methods, cancellationToken))?.Select(x => x.ToLowerInvariant()) ?? Enumerable.Empty<string>();
+
+            HttpEndpointBookmark CreateBookmark(string method) => new(path, method, correlationId);
+            return methods.Select(CreateBookmark);
+        }
+
+        private static string? ToLower(string? s) => s?.ToLowerInvariant();
+    }
+}
