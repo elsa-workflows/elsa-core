51f9ed11f81a1cfeaf752eeb5e452fed04000162
David Peters <dpeters@directs.com>
2021-03-03 18:13:51 -0600

    Added PostgreSql EFcore provider

---
+++ src/persistence/Elsa.Persistence.EntityFramework/Elsa.Persistence.EntityFramework.PostgreSql/PostgreSqlElsaContextFactory.cs
@@ -0,0 +1,20 @@
+using System.Linq;
+using Elsa.Persistence.EntityFramework.Core;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Elsa.Persistence.EntityFramework.PostgreSql
+{
+    public class PostgreSqlElsaContextFactory : IDesignTimeDbContextFactory<ElsaContext>
+    {
+        public ElsaContext CreateDbContext(string[] args)
+        {
+            var builder = new DbContextOptionsBuilder<ElsaContext>();
+            var connectionString = args.Any() ? args[0] : "Server=127.0.0.1;Port=5432;Database=elsa;User Id=postgres;Password=password;";
+            builder.UseNpgsql(
+                connectionString, 
+                db => db.MigrationsAssembly(typeof(PostgreSqlElsaContextFactory).Assembly.GetName().Name));
+            return new ElsaContext(builder.Options);
+        }
+    }
+}
