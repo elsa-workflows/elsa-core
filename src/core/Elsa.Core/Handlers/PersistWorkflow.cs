7f5a6228cfb2d9a51d0597ecd5aa182c8d119e9d
Craig Fowler <craig@csf-dev.com>
2021-03-09 20:09:36 +0000

    Resolve #728 - Fix ActivityExecuted persistence
    The PersistWorkflow service will now handle ActivityExecuted
    persistence behaviour, presumably as-intended.

--- src/core/Elsa.Core/Handlers/PersistWorkflow.cs
+++ src/core/Elsa.Core/Handlers/PersistWorkflow.cs
@@ -15,7 +15,8 @@ namespace Elsa.Handlers
         INotificationHandler<WorkflowExecutionPassCompleted>,
         INotificationHandler<WorkflowExecutionBurstStarting>,
         INotificationHandler<WorkflowExecutionBurstCompleted>,
-        INotificationHandler<WorkflowExecutionFinished>
+        INotificationHandler<WorkflowExecutionFinished>,
+        INotificationHandler<ActivityExecuted>
     {
         private readonly IWorkflowInstanceStore _workflowInstanceStore;
         private readonly ILogger _logger;
@@ -92,6 +93,16 @@ namespace Elsa.Handlers
                 if (behavior == WorkflowPersistenceBehavior.WorkflowPassCompleted || behavior == WorkflowPersistenceBehavior.WorkflowBurst)
                     await SaveWorkflowAsync(notification.WorkflowExecutionContext, cancellationToken);
             }
+        }
+
+        public async Task Handle(ActivityExecuted notification, CancellationToken cancellationToken)
+        {
+            _logger.LogTrace("Received notification {notification}", notification);
+            
+            var behavior = notification.WorkflowExecutionContext.WorkflowBlueprint.PersistenceBehavior;
+
+            if (behavior == WorkflowPersistenceBehavior.ActivityExecuted)
+                await SaveWorkflowAsync(notification.WorkflowExecutionContext, cancellationToken);
         }
 
         private async ValueTask SaveWorkflowAsync(WorkflowExecutionContext workflowExecutionContext, CancellationToken cancellationToken)
