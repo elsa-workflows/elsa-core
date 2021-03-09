8751bead5a5a875eb897dd762aeeb8619556fe7e
Craig Fowler <craig@csf-dev.com>
2021-03-09 19:52:36 +0000

    WIP #728 - Make WorkflowBurst default behaviour
    In the comments to #728 it was suggested that
    WorkflowBurst persistence behaviour was a more
    sane default.

--- src/core/Elsa.Abstractions/Models/WorkflowPersistenceBehavior.cs
+++ src/core/Elsa.Abstractions/Models/WorkflowPersistenceBehavior.cs
@@ -6,15 +6,21 @@ namespace Elsa.Models
     [JsonConverter(typeof(StringEnumConverter))]
     public enum WorkflowPersistenceBehavior
     {
+        // TODO: Consider a new service to "get the default persistence behaviour".
+        // Currently it is set to WorkflowBurst implicitly because it is the default
+        // for the enum type.  This would be more clear if explicit.
+        // This includes altering all locations which need a default persistence
+        // behaviour though.
+
+        /// <summary>
+        /// Workflow instances are persisted after the workflow completed a burst of execution.
+        /// </summary>
+        WorkflowBurst = 0,
+
         /// <summary>
         /// Workflow instances are persisted only when being suspended. 
         /// </summary>
         Suspended,
-        
-        /// <summary>
-        /// Workflow instances are persisted after the workflow completed a burst of execution.
-        /// </summary>
-        WorkflowBurst,
         
         /// <summary>
         /// Workflow instances are persisted after the workflow executed scheduled activities.
