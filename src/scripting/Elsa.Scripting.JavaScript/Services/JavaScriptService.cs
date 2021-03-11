94d71c54f3d741da638c847bba4224c9b6ea77b2
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2021-03-11 22:51:29 +0100

    JavaScript engine type conversion improvements

--- src/scripting/Elsa.Scripting.JavaScript/Services/JavaScriptService.cs
+++ src/scripting/Elsa.Scripting.JavaScript/Services/JavaScriptService.cs
@@ -1,19 +1,17 @@
 ﻿using System;
-using System.Dynamic;
-using System.Linq;
+using System.Collections;
+using System.ComponentModel;
+using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
 using Elsa.Scripting.JavaScript.Converters;
-using Elsa.Scripting.JavaScript.Extensions;
 using Elsa.Scripting.JavaScript.Messages;
 using Elsa.Scripting.JavaScript.Options;
 using Elsa.Services.Models;
 using Jint;
-using Jint.Native;
 using MediatR;
 using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using NodaTime;
 using NodaTime.Serialization.JsonNet;
 
@@ -42,97 +40,25 @@ namespace Elsa.Scripting.JavaScript.Services
             await _mediator.Publish(new EvaluatingJavaScriptExpression(engine, context), cancellationToken);
             engine.Execute(expression);
 
-            return ConvertValue(engine.GetCompletionValue(), returnType);
+            var returnValue = engine.GetCompletionValue().ToObject();
+            var converter = TypeDescriptor.GetConverter(returnValue);
+
+            if (converter.CanConvertTo(returnType))
+                converter.ConvertTo(returnValue, returnType);
+
+            if (returnValue is IEnumerable)
+            {
+                var json = JsonConvert.SerializeObject(returnValue);
+                return JsonConvert.DeserializeObject(json, returnType);
+            }
+            
+            return Convert.ChangeType(returnValue, returnType);
         }
         
         private void ConfigureJintEngine(Jint.Options options)
         {
             if (_options.AllowClr)
                 options.AllowClr();
-        }
-
-        private object? ConvertValue(JsValue value, Type targetType)
-        {
-            if (value.IsUndefined())
-                return default;
-
-            if (value.IsNull())
-                return default;
-
-            var targetIsObject = targetType == typeof(object);
-            
-            if (value.IsBoolean())
-                return value.AsBoolean();
-
-            if ((targetIsObject || targetType == typeof(DateTime)) && value.IsDate())
-                return value.AsDate().ToDateTime();
-
-            if (targetType.IsNumeric() && value.IsNumber())
-            {
-                var numericType = targetType.IsNumeric() ? targetType : typeof(double);
-                return Convert.ChangeType(value.AsNumber(), numericType);
-            }
-
-            if (targetType == typeof(string))
-                return value.ToString();
-
-            if (value.IsString())
-            {
-                var stringValue = value.AsString();
-                return targetType != null
-                    ? stringValue.Parse(targetType)
-                    : value.AsString();
-            }
-
-            if (value.IsArray())
-            {
-                var arrayInstance = value.AsArray();
-                var elementType = targetType.GetElementType() ?? targetType.GenericTypeArguments.First();
-
-                if (elementType == typeof(byte))
-                {
-                    var bytes = new byte[arrayInstance.Length];
-
-                    for (uint i = 0; i < arrayInstance.Length; i++)
-                    {
-                        var jsValue = arrayInstance[i];
-                        bytes[i] = (byte) jsValue.AsNumber();
-                    }
-
-                    return bytes;
-                }
-
-                var array = Array.CreateInstance(elementType, arrayInstance.Length);
-
-                for (uint i = 0; i < array.Length; i++)
-                {
-                    var jsValue = arrayInstance[i];
-                    var convertedValue = ConvertValue(jsValue, elementType);
-                    array.SetValue(convertedValue, i);
-                }
-
-                return array;
-            }
-
-            if (value.IsObject())
-            {
-                var obj = value.AsObject().ToObject();
-
-                switch (obj)
-                {
-                    case ExpandoObject _:
-                    {
-                        var json = JsonConvert.SerializeObject(obj, _serializerSettings);
-                        return JsonConvert.DeserializeObject(json, targetType, _serializerSettings);
-                    }
-                    case JValue jValue:
-                        return jValue.Value;
-                    default:
-                        return obj;
-                }
-            }
-
-            throw new ArgumentException($"Value type {value.Type} is not supported.", nameof(value));
         }
     }
 }
