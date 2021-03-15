55cc47a659429af25eb8505b6b3ea130e1e126f0
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2021-03-15 22:24:47 +0100

    Make Telnyx Notification activity blocking

--- src/activities/Elsa.Activities.Telnyx/Activities/TelnyxNotification.cs
+++ src/activities/Elsa.Activities.Telnyx/Activities/TelnyxNotification.cs
@@ -1,6 +1,9 @@
 ﻿using System.ComponentModel;
+using System.Threading.Tasks;
+using Elsa.ActivityResults;
 using Elsa.Attributes;
 using Elsa.Services;
+using Elsa.Services.Models;
 
 namespace Elsa.Activities.Telnyx.Activities
 {
@@ -9,5 +12,14 @@ namespace Elsa.Activities.Telnyx.Activities
     public class TelnyxNotification : Activity
     {
         public string EventType { get; set; } = default!;
+
+        protected override IActivityExecutionResult OnExecute(ActivityExecutionContext context) => context.WorkflowExecutionContext.IsFirstPass ? ExecuteInternal() : Suspend();
+
+        protected override IActivityExecutionResult OnResume(ActivityExecutionContext context) => ExecuteInternal();
+        
+        private IActivityExecutionResult ExecuteInternal()
+        {
+            return Done();
+        }
     }
 }
