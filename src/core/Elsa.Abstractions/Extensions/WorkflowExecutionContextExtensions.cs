e49e85f532df8f0b266d0714cee7236f532d80bf
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2021-03-23 12:58:26 +0100

    Turn Fork into self-cleaning scoped activity

--- src/core/Elsa.Abstractions/Extensions/WorkflowExecutionContextExtensions.cs
+++ src/core/Elsa.Abstractions/Extensions/WorkflowExecutionContextExtensions.cs
@@ -13,6 +13,7 @@ namespace Elsa
             workflowExecutionContext.WorkflowBlueprint.Connections.Where(x => x.Source.Activity.Id == activityId);
 
         public static IEnumerable<string> GetInboundActivityPath(this WorkflowExecutionContext workflowExecutionContext, string activityId) => workflowExecutionContext.GetInboundActivityPathInternal(activityId, activityId).Distinct();
+        public static IEnumerable<string> GetOutboundActivityPath(this WorkflowExecutionContext workflowExecutionContext, string activityId) => workflowExecutionContext.GetOutboundActivityPathInternal(activityId, activityId).Distinct();
         public static T GetOutputFrom<T>(this WorkflowExecutionContext workflowExecutionContext, string activityName) => (T)workflowExecutionContext.GetOutputFrom(activityName)!;
 
         private static IEnumerable<string> GetInboundActivityPathInternal(this WorkflowExecutionContext workflowExecutionContext, string activityId, string startingPointActivityId)
@@ -29,6 +30,23 @@ namespace Elsa
                     .GetInboundActivityPathInternal(connection.Source.Activity.Id!, startingPointActivityId)
                     .Distinct())
                     yield return parentActivityId;
+            }
+        }
+        
+        private static IEnumerable<string> GetOutboundActivityPathInternal(this WorkflowExecutionContext workflowExecutionContext, string activityId, string startingPointActivityId)
+        {
+            foreach (var connection in workflowExecutionContext.GetOutboundConnections(activityId))
+            {
+                // Circuit breaker: Detect workflows that implement repeating flows to prevent an infinite loop here.
+                if (connection.Target.Activity.Id == startingPointActivityId)
+                    yield break;
+
+                yield return connection.Target.Activity.Id!;
+
+                foreach (var childActivityId in workflowExecutionContext
+                    .GetOutboundActivityPathInternal(connection.Target.Activity.Id!, startingPointActivityId)
+                    .Distinct())
+                    yield return childActivityId;
             }
         }
     }
