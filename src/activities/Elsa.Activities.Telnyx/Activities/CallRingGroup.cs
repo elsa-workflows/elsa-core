c1793d71cdfa80f2f596bf50e05db9de7dc3a480
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2021-03-24 21:42:20 +0100

    Incremental work on call ring group activity

--- src/activities/Elsa.Activities.Telnyx/Activities/CallRingGroup.cs
+++ src/activities/Elsa.Activities.Telnyx/Activities/CallRingGroup.cs
@@ -1,6 +1,10 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
+using System.Threading.Tasks;
 using Elsa.Activities.ControlFlow;
+using Elsa.Activities.Telnyx.Client.Models;
+using Elsa.Activities.Telnyx.Client.Services;
 using Elsa.Activities.Telnyx.Webhooks.Models;
 using Elsa.Activities.Telnyx.Webhooks.Payloads.Call;
 using Elsa.Activities.Temporal;
@@ -9,6 +13,7 @@ using Elsa.Builders;
 using Elsa.Design;
 using Elsa.Metadata;
 using Elsa.Services;
+using Elsa.Services.Models;
 using NodaTime;
 
 namespace Elsa.Activities.Telnyx.Activities
@@ -64,6 +69,12 @@ namespace Elsa.Activities.Telnyx.Activities
             get => GetState(() => Duration.FromSeconds(20));
             set => SetState(value);
         }
+        
+        private IList<string> CollectedCallControlIds
+        {
+            get => GetState(() => new List<string>());
+            set => SetState(value);
+        }
 
         public override void Build(ICompositeActivityBuilder builder) =>
             builder.Switch(cases =>
@@ -72,12 +83,27 @@ namespace Elsa.Activities.Telnyx.Activities
                 cases.Add(RingGroupStrategy.RingAll.ToString(), () => Strategy == RingGroupStrategy.RingAll, BuildRingAllFlow);
             });
 
+        protected override async ValueTask OnExitAsync(ActivityExecutionContext context, object? output)
+        {
+            string? answeredCallControlId = null;
+            
+            if (output is CallAnsweredPayload callAnsweredPayload) 
+                answeredCallControlId = callAnsweredPayload.CallControlId;
+            
+            // Hang up any pending calls.
+            var outgoingCalls = CollectedCallControlIds.Where(x => x != answeredCallControlId).ToList();
+            var client = context.GetService<ITelnyxClient>();
+
+            foreach (var outgoingCall in outgoingCalls) 
+                await client.Calls.HangupCallAsync(outgoingCall, new HangupCallRequest(null, null), context.CancellationToken);
+        }
+
         private void BuildPrioritizedHuntFlow(IOutcomeBuilder builder) =>
             builder
                 .ForEach(() => Extensions, iterate => iterate
                     .Then<ResolveExtension>(a => a.WithExtension(context => context.GetInput<string>()))
-                    .Then<TransferCall>(a => a
-                        .WithCallControlAppId(() => CallControlId)
+                    .Then<Dial>(a => a
+                        .WithCallControlId(() => CallControlId)
                         .WithTo(context => context.GetInput<string>())
                         .WithTimeoutSecs(() => (int) RingTime.TotalSeconds)
                         .WithFrom(() => From)
@@ -88,6 +114,7 @@ namespace Elsa.Activities.Telnyx.Activities
                         fork
                             .When("Connected")
                             .ThenTypeNamed(CallAnsweredPayload.ActivityTypeName)
+                            // Bridge
                             .Then<Finish>(finish => finish.WithOutcome("Connected").WithOutput(context => context.GetInput<TelnyxWebhook>()!.Data.Payload));
 
                         fork
@@ -104,6 +131,7 @@ namespace Elsa.Activities.Telnyx.Activities
                     fork
                         .When("Connected")
                         .ThenTypeNamed(CallAnsweredPayload.ActivityTypeName)
+                        // Bridge
                         .Then<Finish>(finish => finish.WithOutcome("Connected").WithOutput(context => context.GetInput<TelnyxWebhook>()!.Data.Payload));
 
                     fork
@@ -115,16 +143,25 @@ namespace Elsa.Activities.Telnyx.Activities
                         .When("Dial Everyone")
                         .ParallelForEach(() => Extensions, iterate => iterate
                             .Then<ResolveExtension>(a => a.WithExtension(context => context.GetInput<string>()))
-                            .Then<TransferCall>(a => a
-                                .WithCallControlAppId(() => CallControlId)
+                            .Then<Dial>(a => a
+                                .WithCallControlId(() => CallControlId)
                                 .WithTo(context => context.GetInput<string>())
                                 .WithTimeoutSecs(() => (int) RingTime.TotalSeconds)
                                 .WithFrom(() => From)
                                 .WithFromDisplayName(() => FromDisplayName)
-                            ));
+                            )
+                            .Then(CollectCallControlIds));
 
                 });
 
+        private void CollectCallControlIds(ActivityExecutionContext context)
+        {
+            var collection = CollectedCallControlIds;
+            var callControlId = context.GetInput<DialResponse>()!.CallControlId;
+            collection.Add(callControlId);
+            CollectedCallControlIds = collection;
+        }
+        
         object IActivityPropertyDefaultValueProvider.GetDefaultValue(PropertyInfo property) => Duration.FromSeconds(20);
     }
 }
