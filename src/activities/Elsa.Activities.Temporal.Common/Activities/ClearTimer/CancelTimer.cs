8db51bb047e1eee2a464efd7c4a8dd6959df6f72
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2021-04-03 21:01:37 +0200

    Configure Temporal activities

--- src/activities/Elsa.Activities.Temporal.Common/Activities/ClearTimer/CancelTimer.cs
+++ src/activities/Elsa.Activities.Temporal.Common/Activities/ClearTimer/CancelTimer.cs
@@ -2,14 +2,14 @@ using System.Threading.Tasks;
 using Elsa.Activities.Temporal.Common.Services;
 using Elsa.ActivityResults;
 using Elsa.Attributes;
+using Elsa.Expressions;
 using Elsa.Services;
 using Elsa.Services.Models;
 
 // ReSharper disable once CheckNamespace
 namespace Elsa.Activities.Temporal
 {
-
-    [Trigger(Category = "Timers", Description = "Cancel a timer (Cron, StartAt, Timer) so that it is not executed. ")]
+    [Trigger(Category = "Timers", Description = "Cancel a timer (Cron, StartAt, Timer) so that it is not executed.")]
     public class ClearTimer : Activity
     {
         private readonly IWorkflowScheduler _workflowScheduler;
@@ -19,7 +19,7 @@ namespace Elsa.Activities.Temporal
             _workflowScheduler = workflowScheduler;
         }
 
-        [ActivityProperty(Hint = "The id of the timer (Cron, StartAt, Timer) activity, which is to be cleared")]
+        [ActivityProperty(Hint = "The ID of the timer (Cron, StartAt, Timer) activity, which is to be cleared.", SupportedSyntaxes = new[] { SyntaxNames.JavaScript, SyntaxNames.Liquid })]
         public string ActivityId { get; set; } = default!;
 
         protected override async ValueTask<IActivityExecutionResult> OnExecuteAsync(ActivityExecutionContext context)
