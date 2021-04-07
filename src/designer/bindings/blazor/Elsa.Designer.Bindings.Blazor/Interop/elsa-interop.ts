031f75041a9581136c591038e92c6ccbb80ff1c7
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2021-04-07 11:18:58 +0200

    Update Blazor bindings

--- src/designer/bindings/blazor/Elsa.Designer.Bindings.Blazor/Interop/elsa-interop.ts
+++ src/designer/bindings/blazor/Elsa.Designer.Bindings.Blazor/Interop/elsa-interop.ts
@@ -1,2 +1,1 @@
 import "../../../../elsa-workflows-studio/dist/elsa-workflows-studio/elsa-workflows-studio.esm";
-import {WorkflowDefinition} from "@elsa-workflows/elsa-workflow-designer/dist/types/models";
