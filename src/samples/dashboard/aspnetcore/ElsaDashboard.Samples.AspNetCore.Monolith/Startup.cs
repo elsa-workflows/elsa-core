a926ed9104c5d23afda4695d53a5c84e0db78d3f
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2021-04-09 22:53:12 +0200

    Incremental work on ASP.NET Core reference application for dashboard

---
+++ src/samples/dashboard/aspnetcore/ElsaDashboard.Samples.AspNetCore.Monolith/Startup.cs
@@ -0,0 +1,71 @@
+using Elsa;
+using Elsa.Persistence.EntityFramework.Core.Extensions;
+using Elsa.Persistence.EntityFramework.Sqlite;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace ElsaDashboard.Samples.AspNetCore.Monolith
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            
+            // Elsa Server.
+            var elsaSection = Configuration.GetSection("Elsa");
+            
+            services
+                .AddElsa(options => options
+                        .UseEntityFrameworkPersistence(ef => ef.UseSqlite())
+                        .AddConsoleActivities()
+                        .AddHttpActivities(elsaSection.GetSection("Http").Bind)
+                        .AddEmailActivities(elsaSection.GetSection("Smtp").Bind)
+                        .AddQuartzTemporalActivities()
+                        .AddJavaScriptActivities()
+                );
+
+            services
+                .AddElsaApiEndpoints();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseHttpActivities();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                // Elsa Server uses ASP.NET Core Controllers.
+                endpoints.MapControllers();
+                
+                endpoints.MapFallbackToPage("/_Host");
+            });
+        }
+    }
+}
