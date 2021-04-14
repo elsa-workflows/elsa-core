59aba78133442baadf67ec16a7482790e57a2be9
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2021-04-14 20:20:49 +0200

    Formatting

--- src/scripting/Elsa.Scripting.JavaScript/Services/JintJavaScriptEvaluator.cs
+++ src/scripting/Elsa.Scripting.JavaScript/Services/JintJavaScriptEvaluator.cs
@@ -12,49 +12,52 @@ namespace Elsa.Scripting.JavaScript.Services
 {
     public class JintJavaScriptEvaluator : IJavaScriptService
     {
-        readonly IMediator mediator;
-        readonly IConvertsJintEvaluationResult resultConverter;
-        readonly ScriptOptions scriptOptions;
+        private readonly IMediator _mediator;
+        private readonly IConvertsJintEvaluationResult _resultConverter;
+        private readonly ScriptOptions _scriptOptions;
 
-        public JintJavaScriptEvaluator(IMediator mediator,
-                                       IOptions<ScriptOptions> scriptOptions,
-                                       IConvertsJintEvaluationResult resultConverter)
+        public JintJavaScriptEvaluator(
+            IMediator mediator,
+            IOptions<ScriptOptions> scriptOptions,
+            IConvertsJintEvaluationResult resultConverter)
         {
             if (scriptOptions is null)
                 throw new ArgumentNullException(nameof(scriptOptions));
 
-            this.mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
-            this.resultConverter = resultConverter ?? throw new ArgumentNullException(nameof(resultConverter));
-            this.scriptOptions = scriptOptions.Value;
+            _mediator = mediator;
+            _resultConverter = resultConverter;
+            _scriptOptions = scriptOptions.Value;
         }
-        
-        public async Task<object?> EvaluateAsync(string expression,
-                                                 Type returnType,
-                                                 ActivityExecutionContext context,
-                                                 Action<Engine>? configureEngine = default,
-                                                 CancellationToken cancellationToken = default)
+
+        public async Task<object?> EvaluateAsync(
+            string expression,
+            Type returnType,
+            ActivityExecutionContext context,
+            Action<Engine>? configureEngine = default,
+            CancellationToken cancellationToken = default)
         {
             var engine = await GetConfiguredEngine(configureEngine, context, cancellationToken);
             var result = ExecuteExpressionAndGetResult(engine, expression);
-            return resultConverter.ConvertToDesiredType(result, returnType);
+            return _resultConverter.ConvertToDesiredType(result, returnType);
         }
 
         async Task<Engine> GetConfiguredEngine(Action<Engine>? configureEngine, ActivityExecutionContext context, CancellationToken cancellationToken)
         {
-            var engine = new Engine(opts => {
-                if (scriptOptions.AllowClr)
+            var engine = new Engine(opts =>
+            {
+                if (_scriptOptions.AllowClr)
                     opts.AllowClr();
             });
 
             configureEngine?.Invoke(engine);
 
             // Listeners invoked by the mediator might further-configure the engine
-            await mediator.Publish(new EvaluatingJavaScriptExpression(engine, context), cancellationToken);
+            await _mediator.Publish(new EvaluatingJavaScriptExpression(engine, context), cancellationToken);
 
             return engine;
         }
 
-        object? ExecuteExpressionAndGetResult(Engine engine, string expression)
+        private static object? ExecuteExpressionAndGetResult(Engine engine, string expression)
         {
             engine.Execute(expression);
             return engine.GetCompletionValue().ToObject();
