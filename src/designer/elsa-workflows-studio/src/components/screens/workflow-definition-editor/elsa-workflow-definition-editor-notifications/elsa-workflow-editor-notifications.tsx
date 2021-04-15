359a98f365096121d1764be3f2d5225b3eb3b274
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2021-04-15 21:39:19 +0200

    Fix notification titles

--- src/designer/elsa-workflows-studio/src/components/screens/workflow-definition-editor/elsa-workflow-definition-editor-notifications/elsa-workflow-editor-notifications.tsx
+++ src/designer/elsa-workflows-studio/src/components/screens/workflow-definition-editor/elsa-workflow-definition-editor-notifications/elsa-workflow-editor-notifications.tsx
@@ -3,41 +3,45 @@ import {eventBus} from '../../../../services/event-bus';
 import {EventTypes, WorkflowDefinition} from "../../../../models";
 
 @Component({
-    tag: 'elsa-workflow-definition-editor-notifications',
-    shadow: false,
+  tag: 'elsa-workflow-definition-editor-notifications',
+  shadow: false,
 })
 export class ElsaWorkflowEditorNotifications {
 
-    toastNotificationElement: HTMLElsaToastNotificationElement;
+  toastNotificationElement: HTMLElsaToastNotificationElement;
 
-    connectedCallback() {
-        eventBus.on(EventTypes.WorkflowPublished, this.onWorkflowPublished);
-        eventBus.on(EventTypes.WorkflowRetracted, this.onWorkflowRetracted);
-        eventBus.on(EventTypes.WorkflowImported, this.onWorkflowImported);
-    }
+  connectedCallback() {
+    eventBus.on(EventTypes.WorkflowPublished, this.onWorkflowPublished);
+    eventBus.on(EventTypes.WorkflowRetracted, this.onWorkflowRetracted);
+    eventBus.on(EventTypes.WorkflowImported, this.onWorkflowImported);
+  }
 
-    disconnectedCallback() {
-        eventBus.off(EventTypes.WorkflowPublished, this.onWorkflowPublished);
-        eventBus.off(EventTypes.WorkflowRetracted, this.onWorkflowRetracted);
-        eventBus.off(EventTypes.WorkflowImported, this.onWorkflowImported);
-    }
+  disconnectedCallback() {
+    eventBus.off(EventTypes.WorkflowPublished, this.onWorkflowPublished);
+    eventBus.off(EventTypes.WorkflowRetracted, this.onWorkflowRetracted);
+    eventBus.off(EventTypes.WorkflowImported, this.onWorkflowImported);
+  }
 
-    onWorkflowPublished = async (workflowDefinition: WorkflowDefinition) => await this.toastNotificationElement.show({autoCloseIn: 1500, title: 'Workflow Unpublished', message: `Workflow successfully retracted.`});
+  onWorkflowPublished = async (workflowDefinition: WorkflowDefinition) => await this.toastNotificationElement.show({
+    autoCloseIn: 1500,
+    title: 'Workflow Published',
+    message: `Workflow successfully published at version ${workflowDefinition.version}.`
+  });
 
-    onWorkflowRetracted = async (workflowDefinition: WorkflowDefinition) => await this.toastNotificationElement.show({
-        autoCloseIn: 1500,
-        title: 'Workflow Published',
-        message: `Workflow successfully published at version ${workflowDefinition.version}.`
-    });
+  onWorkflowRetracted = async (workflowDefinition: WorkflowDefinition) => await this.toastNotificationElement.show({
+    autoCloseIn: 1500,
+    title: 'Workflow Retracted',
+    message: `Workflow successfully retracted at version ${workflowDefinition.version}.`
+  });
 
-    onWorkflowImported = async (workflowDefinition: WorkflowDefinition) => await this.toastNotificationElement.show({autoCloseIn: 1500, title: 'Workflow Imported', message: `Workflow successfully imported.`});
+  onWorkflowImported = async (workflowDefinition: WorkflowDefinition) => await this.toastNotificationElement.show({autoCloseIn: 1500, title: 'Workflow Imported', message: `Workflow successfully imported.`});
 
-    render() {
+  render() {
 
-        return (
-            <Host class="block">
-                <elsa-toast-notification ref={el => this.toastNotificationElement = el}/>
-            </Host>
-        );
-    }
+    return (
+      <Host class="block">
+        <elsa-toast-notification ref={el => this.toastNotificationElement = el}/>
+      </Host>
+    );
+  }
 }
