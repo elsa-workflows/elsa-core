138d9be12cae19c2725026538e8b8f0215c6db1d
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2021-04-19 13:54:04 +0200

    Add Send + Receive blocking workflow sample

---
+++ src/samples/worker/Elsa.Samples.AzureServiceBusWorker/Workflows/SendAndReceiveWorkflow.cs
@@ -0,0 +1,22 @@
+﻿using Elsa.Activities.AzureServiceBus;
+using Elsa.Activities.Console;
+using Elsa.Builders;
+
+namespace Elsa.Samples.AzureServiceBusWorker.Workflows
+{
+    public class SendAndReceiveWorkflow : IWorkflow
+    {
+        public void Build(IWorkflowBuilder builder) => builder
+            .WriteLine(ctx =>
+            {
+                var correlationId = System.Guid.NewGuid().ToString("n");
+                ctx.WorkflowInstance.CorrelationId = correlationId;
+             
+                return $"Start! - correlationId: {correlationId}";
+            })
+            .SendTopicMessage("testtopic2", "\"Hello World\"")
+            .TopicMessageReceived<string>("testtopic2", "testsub")         
+            .WriteLine(ctx => "End: " + (string)ctx.Input);
+
+    }
+}
