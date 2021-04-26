da7b751105d04976b5f25561a22a0d5c539ae892
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2021-04-26 14:26:37 +0200

    Some cleanup

--- src/persistence/Elsa.Persistence.EntityFramework/Elsa.Persistence.EntityFramework.MySql/DbContextOptionsBuilderExtensions.cs
+++ src/persistence/Elsa.Persistence.EntityFramework/Elsa.Persistence.EntityFramework.MySql/DbContextOptionsBuilderExtensions.cs
@@ -7,9 +7,6 @@ namespace Elsa.Persistence.EntityFramework.MySql
         /// <summary>
         /// Configures the context to use MySql 
         /// </summary>
-        /// <param name="builder"></param>
-        /// <param name="connectionString"></param>
-        /// <returns></returns>
         public static DbContextOptionsBuilder UseMySql(this DbContextOptionsBuilder builder, string connectionString) => 
             builder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString), db => db.MigrationsAssembly(typeof(MySqlElsaContextFactory).Assembly.GetName().Name));
     }
--- src/persistence/Elsa.Persistence.EntityFramework/Elsa.Persistence.EntityFramework.MySql/MySqlElsaContextFactory.cs
+++ src/persistence/Elsa.Persistence.EntityFramework/Elsa.Persistence.EntityFramework.MySql/MySqlElsaContextFactory.cs
@@ -4,6 +4,7 @@ using Elsa.Persistence.EntityFramework.Core;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
 using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
+
 namespace Elsa.Persistence.EntityFramework.MySql
 {
     public class MySqlElsaContextFactory : IDesignTimeDbContextFactory<ElsaContext>
@@ -12,10 +13,12 @@ namespace Elsa.Persistence.EntityFramework.MySql
         {
             var builder = new DbContextOptionsBuilder<ElsaContext>();
             var connectionString = args.Any() ? args[0] : throw new InvalidOperationException("Please specify a connection string. E.g. dotnet ef database update -- \"Server=localhost;Port=3306;Database=elsa;User=root;Password=password\"");
+            
             builder.UseMySql(
-                connectionString, 
+                connectionString,
                 ServerVersion.AutoDetect(args[0]),
                 db => db.MigrationsAssembly(typeof(MySqlElsaContextFactory).Assembly.GetName().Name));
+            
             return new ElsaContext(builder.Options);
         }
     }
