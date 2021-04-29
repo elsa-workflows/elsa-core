b797dd8c43bcacc5e297c3640dfeeb168eec30a8
Cameron Cowen <cameronc94@gmail.com>
2021-04-29 01:03:16 +0100

    Added builder extensions for sendhttprequest and added initial sample

---
+++ src/samples/aspnet/Elsa.Samples.SendHttp/Startup.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Elsa.Samples.SendHttp
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddElsa(options => options.AddHttpActivities()
+                .AddWorkflowsFrom<Startup>());
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseHttpActivities();
+        }
+    }
+}
