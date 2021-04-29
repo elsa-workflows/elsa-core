27e0e768847e511cbfcf2c4aa79d299e5c090204
tomy2105 <t.petrovic@inet.hr>
2021-04-29 12:06:28 +0200

    Adding DbContextOptionsBuilderExtensions

---
+++ src/persistence/Elsa.Persistence.EntityFramework/Elsa.Persistence.EntityFramework.SqlServer/DbContextOptionsBuilderExtensions.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Elsa.Persistence.EntityFramework.SqlServer
+{
+    public static class DbContextOptionsBuilderExtensions
+    {
+        /// <summary>
+        /// Configures the context to use SqlServer.
+        /// </summary>
+        public static DbContextOptionsBuilder UseSqlServer(this DbContextOptionsBuilder builder, string connectionString) => 
+            builder.UseSqlServer(connectionString, db => db.MigrationsAssembly(typeof(SqlServerElsaContextFactory).Assembly.GetName().Name));
+    }
+}
