b7202d2de22174807af25f7425849163d29cd64b
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2021-04-29 16:00:10 +0200

    Add ListActive method to workflow registry that returns workflows that are published or have at least 1 non-finished workflow instance

--- src/core/Elsa.Core/Services/WorkflowRegistry.cs
+++ src/core/Elsa.Core/Services/WorkflowRegistry.cs
@@ -24,6 +24,7 @@ namespace Elsa.Services
         }
 
         public async Task<IEnumerable<IWorkflowBlueprint>> ListAsync(CancellationToken cancellationToken) => await GetWorkflowsInternalAsync(cancellationToken).ToListAsync(cancellationToken);
+        public async Task<IEnumerable<IWorkflowBlueprint>> ListActiveAsync(CancellationToken cancellationToken) => await ListActiveWorkflowsAsync(cancellationToken).ToListAsync(cancellationToken);
 
         public async Task<IWorkflowBlueprint?> GetAsync(string id, string? tenantId, VersionOptions version, CancellationToken cancellationToken) =>
             await FindAsync(x => x.Id == id && x.TenantId == tenantId && x.WithVersion(version), cancellationToken);
@@ -33,6 +34,26 @@ namespace Elsa.Services
 
         public async Task<IWorkflowBlueprint?> FindAsync(Func<IWorkflowBlueprint, bool> predicate, CancellationToken cancellationToken) =>
             (await ListAsync(cancellationToken).Where(predicate).OrderByDescending(x => x.Version)).FirstOrDefault();
+        
+        private async IAsyncEnumerable<IWorkflowBlueprint> ListActiveWorkflowsAsync([EnumeratorCancellation] CancellationToken cancellationToken)
+        {
+            var workflows = await ListAsync(cancellationToken);
+
+            foreach (var workflow in workflows)
+            {
+                // If a workflow is not published, only consider it for processing if it has at least one non-ended workflow instance.
+                if (!workflow.IsPublished && !await WorkflowHasNonFinishedWorkflowsAsync(workflow, cancellationToken))
+                    continue;
+
+                yield return workflow;
+            }
+        }
+
+        private async Task<bool> WorkflowHasNonFinishedWorkflowsAsync(IWorkflowBlueprint workflowBlueprint, CancellationToken cancellationToken)
+        {
+            var count = await _workflowInstanceStore.CountAsync(new NonFinalizedWorkflowSpecification().WithWorkflowDefinition(workflowBlueprint.Id), cancellationToken);
+            return count > 0;
+        }
 
         private async IAsyncEnumerable<IWorkflowBlueprint> GetWorkflowsInternalAsync([EnumeratorCancellation] CancellationToken cancellationToken)
         {
