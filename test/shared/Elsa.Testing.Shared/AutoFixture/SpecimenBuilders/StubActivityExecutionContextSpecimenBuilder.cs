173ded683fa369b0515bf303c9d4df9279f3da14
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2021-05-05 13:14:28 +0200

    Provide Id value to mocked ActivityBlueprint

--- test/shared/Elsa.Testing.Shared/AutoFixture/SpecimenBuilders/StubActivityExecutionContextSpecimenBuilder.cs
+++ test/shared/Elsa.Testing.Shared/AutoFixture/SpecimenBuilders/StubActivityExecutionContextSpecimenBuilder.cs
@@ -1,7 +1,9 @@
 using System;
+using AutoFixture;
 using AutoFixture.Kernel;
 using Elsa.Models;
 using Elsa.Services.Models;
+using Microsoft.Extensions.DependencyInjection;
 using Moq;
 
 namespace Elsa.Testing.Shared.AutoFixture.SpecimenBuilders
@@ -15,10 +17,13 @@ namespace Elsa.Testing.Shared.AutoFixture.SpecimenBuilders
 
             var serviceProvider = GetServiceProvider(context);
             var workflowExecutionContext = GetWorkflowExecutionContext(context, serviceProvider);
+            var activityBlueprint = Mock.Of<IActivityBlueprint>();
 
+            Mock.Get(activityBlueprint).SetupGet(x => x.Id).Returns(context.Create<string>());
+            
             return new ActivityExecutionContext(serviceProvider,
                                                 workflowExecutionContext,
-                                                Mock.Of<IActivityBlueprint>(),
+                                                activityBlueprint,
                                                 default,
                                                 default,
                                                 default);
