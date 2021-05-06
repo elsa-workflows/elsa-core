e84e4d0c142d99e2bfa0813c3a18db547bae12ef
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2021-05-06 19:26:32 +0200

    Update docker-compose and appveyor with Postgres

--- test/integration/Elsa.Core.IntegrationTests/Autofixture/WithPostgresYesSqlAttribute.cs
+++ test/integration/Elsa.Core.IntegrationTests/Autofixture/WithPostgresYesSqlAttribute.cs
@@ -9,24 +9,12 @@ namespace Elsa.Core.IntegrationTests.Autofixture
 {
     public class WithPostgresYesSqlAttribute : ElsaHostBuilderBuilderCustomizeAttributeBase
     {
-        /// <summary>
-        /// This password matches the password which is used by AppVeyor: https://www.appveyor.com/docs/services-databases/#postgresql
-        /// </summary>
-        /// <remarks>
-        /// <para>
-        /// We could do something more sophisticated with the password than keep it in a constant, but since
-        /// this is (so far) the only place where we configure Postgres.  There aren't any plans to do anything
-        /// with it in the foreseeable future either, so we can defer that until then.
-        /// </para>
-        /// </remarks>
-        internal const string PostgresPassword = "Password12!";
-
         public override Action<ElsaHostBuilderBuilder> GetBuilderCustomizer()
         {
             return builder => {
                 builder.ElsaCallbacks.Add(elsa => {
-                    elsa.UseYesSqlPersistence((IServiceProvider services, IConfiguration config) => {
-                        config.UsePostgreSql($"Host=localhost;Database=elsa-yessql;User ID=postgres;Password={PostgresPassword}");
+                    elsa.UseYesSqlPersistence((_, config) => {
+                        config.UsePostgreSql($"Server=localhost;Port=5432;Database=yessql;User Id=root;Password=Password12!;");
                     });
                 });
             };
