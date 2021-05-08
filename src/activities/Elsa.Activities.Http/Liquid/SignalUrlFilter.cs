e859ec187c09c173167437a2e78f1b21eff15198
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2021-05-08 23:01:38 +0200

    Fix SignalUrlFilter

--- src/activities/Elsa.Activities.Http/Liquid/SignalUrlFilter.cs
+++ src/activities/Elsa.Activities.Http/Liquid/SignalUrlFilter.cs
@@ -1,6 +1,5 @@
 ﻿using System.Threading.Tasks;
-using Elsa.Activities.Http.Models;
-using Elsa.Activities.Http.Services;
+using Elsa.Activities.Http.Extensions;
 using Elsa.Scripting.Liquid.Services;
 using Elsa.Services.Models;
 using Fluid;
@@ -10,32 +9,12 @@ namespace Elsa.Activities.Http.Liquid
 {
     public class SignalUrlFilter : ILiquidFilter
     {
-        private readonly ITokenService _tokenService;
-        private readonly IAbsoluteUrlProvider _absoluteUrlProvider;
-
-        public SignalUrlFilter(ITokenService tokenService, IAbsoluteUrlProvider absoluteUrlProvider)
-        {
-            _tokenService = tokenService;
-            _absoluteUrlProvider = absoluteUrlProvider;
-        }
-
         public ValueTask<FluidValue> ProcessAsync(FluidValue input, FilterArguments arguments, TemplateContext context)
         {
             var activityExecutionContext = (ActivityExecutionContext) context.Model;
-            var workflowExecutionContext = activityExecutionContext.WorkflowExecutionContext;
             var signalName = input.ToStringValue();
-            var url = GenerateUrl(signalName, workflowExecutionContext);
+            var url = activityExecutionContext.GenerateSignalUrl(signalName);
             return new ValueTask<FluidValue>(new StringValue(url));
-        }
-
-        private string GenerateUrl(string signal, WorkflowExecutionContext workflowExecutionContext)
-        {
-            var workflowInstanceId = workflowExecutionContext.WorkflowInstance.Id;
-            var payload = new Signal(signal, workflowInstanceId);
-            var token = _tokenService.CreateToken(payload);
-            var url = $"/workflows/signal?token={token}";
-
-            return _absoluteUrlProvider.ToAbsoluteUrl(url).ToString();
         }
     }
 }
