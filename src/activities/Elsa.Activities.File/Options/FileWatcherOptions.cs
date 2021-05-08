443d9861ed2703e71800fc372afbad426eede39b
Cameron Cowen <cameronc94@gmail.com>
2021-05-08 21:48:36 +0100

    Renamed option

--- src/activities/Elsa.Activities.File/Options/FileWatcherOptions.cs
+++ src/activities/Elsa.Activities.File/Options/FileWatcherOptions.cs
@@ -6,7 +6,7 @@ namespace Elsa.Activities.File.Options
 {
     public class FileWatcherOptions
     {
-        public string FilePattern { get; set; }
+        public string Pattern { get; set; }
         public string Path { get; set; }
         public bool SubDirectories { get; set; } = false;
     }
