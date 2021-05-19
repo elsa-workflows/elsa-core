f8b4577c707d402b22bfd8c3132716fabe00a575
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2021-05-19 20:56:18 +0200

    Introduce API options to customize or disable Api Versioning

---
+++ src/server/Elsa.Server.Api/ElsaApiOptions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Elsa.Server.Api
+{
+    public class ElsaApiOptions
+    {
+        public static void AddDefaultApiVersioning(IServiceCollection services)
+        {
+            services.AddVersionedApiExplorer(o =>
+            {
+                o.GroupNameFormat = "'v'VVV";
+                o.SubstituteApiVersionInUrl = true;
+            });
+
+            services.AddApiVersioning(
+                options =>
+                {
+                    options.ReportApiVersions = true;
+                    options.DefaultApiVersion = ApiVersion.Default;
+                    options.AssumeDefaultVersionWhenUnspecified = true;
+                });
+        }
+
+        public Action<MvcNewtonsoftJsonOptions>? SetupNewtonsoftJson { get; set; } = default;
+        public Action<IServiceCollection>? SetupApiVersioning { get; set; } = AddDefaultApiVersioning;
+
+        public void DisableApiVersioning() => SetupApiVersioning = null;
+    }
+}
