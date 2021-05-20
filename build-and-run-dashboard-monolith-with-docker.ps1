ac4ea19413a02286b31ede7ba161ddb7cedbb8c4
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2021-05-20 14:36:42 +0200

    Update docker launch script

--- build-and-run-dashboard-monolith-with-docker.ps1
+++ build-and-run-dashboard-monolith-with-docker.ps1
@@ -1,2 +1,2 @@
 docker build -t elsa-dashboard-and-server:local -f ./docker/Dockerfile-elsa-dashboard-and-server .
-docker run -t -i -e ASPNETCORE_ENVIRONMENT='Development' -p 11000:80 elsa-dashboard-and-server:local
+docker run -t -i -e ASPNETCORE_ENVIRONMENT='Development' -e ELSA__SERVER__BASEURL=http://localhost:14000 -p 14000:80 elsa-dashboard-and-server:local
