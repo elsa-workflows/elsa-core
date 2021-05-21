b28dfa92ee19e57f376df568060ca59256a42822
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2021-05-21 12:33:21 +0200

    Add sample project demonstrating signals and JSON based workflows

---
+++ src/samples/aspnet/Elsa.Samples.SignalApi/Controllers/SignalController.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Elsa.Activities.Signaling.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Elsa.Samples.SignalApi.Controllers
+{
+    [ApiController]
+    [Route("fire")]
+    public class FireController : Controller
+    {
+        private readonly ISignaler _signaler;
+
+        public FireController(ISignaler signaler)
+        {
+            _signaler = signaler;
+        }
+        
+        [HttpGet("start")]
+        public async Task<IActionResult> StartFire(CancellationToken cancellationToken)
+        {
+            var startedWorkflows = await _signaler.TriggerSignalAsync("Fire", cancellationToken: cancellationToken);
+            return Ok(startedWorkflows);
+        }
+        
+        [HttpGet("{workflowInstanceId}/extinguish")]
+        public async Task<IActionResult> Extinguish(string workflowInstanceId, CancellationToken cancellationToken)
+        {
+            var startedWorkflows = await _signaler.TriggerSignalAsync("Water", workflowInstanceId: workflowInstanceId, cancellationToken: cancellationToken);
+            return Ok(startedWorkflows);
+        }
+    }
+}
