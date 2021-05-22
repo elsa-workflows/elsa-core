2ec654d5213efcf03877ce7e11ab4253d88f8394
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2021-05-22 13:58:21 +0200

    Implement Redis distributed cache signal

---
+++ src/providers/Elsa.Providers.Redis/Extensions/RedisServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using StackExchange.Redis;
+
+namespace Elsa.Extensions
+{
+    public static class RedisServiceCollectionExtensions
+    {
+        public static IServiceCollection AddRedis(this IServiceCollection services, string connectionString)
+        {
+            return services.AddSingleton<IConnectionMultiplexer>(ConnectionMultiplexer.Connect(connectionString));
+        }
+    }
+}
--- src/servicebus/Elsa.Rebus.RabbitMq/Extensions/ElsaOptionsBuilderExtensions.cs
+++ src/servicebus/Elsa.Rebus.RabbitMq/Extensions/ElsaOptionsBuilderExtensions.cs
@@ -3,7 +3,7 @@ using Rebus.Config;
 
 namespace Elsa.Rebus.RabbitMq.Extensions
 {
-    public static class ElsaOptionsExtensions
+    public static class ElsaOptionsBuilderExtensions
     {
         public static ElsaOptionsBuilder UseRabbitMq(this ElsaOptionsBuilder elsaOptions, string connectionString) => elsaOptions.UseServiceBus(context => ConfigureRabbitMqEndpoint(context, connectionString));
         private static void ConfigureRabbitMqEndpoint(ServiceBusEndpointConfigurationContext context, string connectionString) => context.Configurer.Transport(t => t.UseRabbitMq(connectionString, context.QueueName));
