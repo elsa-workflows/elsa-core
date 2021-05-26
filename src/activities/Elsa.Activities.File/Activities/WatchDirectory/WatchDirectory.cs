804e3d23bb62487e5be562eae0f2512e9a976123
Cameron Cowen <cameronc94@gmail.com>
2021-05-27 00:02:49 +0100

    Added initial watchdirectory trigger

---
+++ src/activities/Elsa.Activities.File/Activities/WatchDirectory/WatchDirectory.cs
@@ -0,0 +1,40 @@
+using Elsa.ActivityResults;
+using Elsa.Attributes;
+using Elsa.Services;
+using Elsa.Services.Models;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elsa.Activities.File
+{
+    [Trigger(Category = "File",
+        DisplayName = "Watch Directory",
+        Description = "Watches a directory for file system changes",
+        Outcomes = new[] { OutcomeNames.Done })]
+    public class WatchDirectory : Activity
+    {
+        private ILogger _logger;
+
+        public WatchDirectory(ILogger<WatchDirectory> logger)
+        {
+            _logger = logger;
+        }
+
+        public WatcherChangeTypes ChangeType { get; set; }
+
+        public string Directory { get; set; }
+
+        public string FileName { get; set; }
+
+        public async override ValueTask<IActivityExecutionResult> ExecuteAsync(ActivityExecutionContext context)
+        {
+            _logger.LogInformation($"Directory={Directory}");
+            return Done();
+        }
+    }
+}
