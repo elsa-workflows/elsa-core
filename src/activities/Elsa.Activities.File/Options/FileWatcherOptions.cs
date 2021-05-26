677584120c95a7ca513247865fa51c59092ad824
Cameron Cowen <cameronc94@gmail.com>
2021-05-26 23:43:51 +0100

    Reorganised

--- src/activities/Elsa.Activities.File/Options/FileWatcherOptions.cs
+++ src/activities/Elsa.Activities.File/Options/FileWatcherOptions.cs
@@ -6,8 +6,8 @@ namespace Elsa.Activities.File.Options
 {
     public class FileWatcherOptions
     {
-        public string Pattern { get; set; }
         public string Path { get; set; }
+        public string Pattern { get; set; }
         public bool SubDirectories { get; set; } = false;
     }
 }
