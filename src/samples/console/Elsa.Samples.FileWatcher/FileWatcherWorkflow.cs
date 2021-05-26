36ab91c62f5524cd394257c681d11d91a20ae573
Cameron Cowen <cameronc94@gmail.com>
2021-05-26 23:43:29 +0100

    Initial filewatcherworkflow

---
+++ src/samples/console/Elsa.Samples.FileWatcher/FileWatcherWorkflow.cs
@@ -0,0 +1,23 @@
+using Elsa.Activities.File;
+using Elsa.Builders;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elsa.Samples.FileWatcher
+{
+    public class FileWatcherWorkflow : IWorkflow
+    {
+        public void Build(IWorkflowBuilder builder)
+        {
+            builder.Add<WatchDirectory>(setup =>
+            {
+                setup.Set(x => x.Directory, "C:\\Temp\\FileWatcher");
+                setup.Set(x => x.ChangeType, WatcherChangeTypes.Created);
+            });
+        }
+    }
+}
