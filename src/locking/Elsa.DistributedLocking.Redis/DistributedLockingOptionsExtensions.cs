985fd39278f1dae8c2e72c1fb8d74ec3b3b14eb5
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2021-05-27 15:49:43 +0200

    Restore Elsa.DistributedLocking.Redis library

---
+++ src/locking/Elsa.DistributedLocking.Redis/DistributedLockingOptionsExtensions.cs
@@ -0,0 +1,20 @@
+using Medallion.Threading.Redis;
+using Microsoft.Extensions.DependencyInjection;
+using StackExchange.Redis;
+
+namespace Elsa
+{
+    public static class DistributedLockingOptionsExtensions
+    {
+        public static DistributedLockingOptionsBuilder UseRedisLockProvider(this DistributedLockingOptionsBuilder options, string connectionString)
+        {
+            options.UseProviderFactory(sp =>
+            {
+                var connectionMultiplexer = sp.GetRequiredService<IConnectionMultiplexer>();
+                var database = connectionMultiplexer.GetDatabase();
+                return name => new RedisDistributedLock(name, database);
+            });
+            return options;
+        }
+    }
+}
