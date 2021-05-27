4d156c7ef70f3e48df89c2a47a88376d2ed2ecd8
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2021-05-27 20:23:18 +0200

    Change namespaces for MongoDB extensions

--- src/persistence/Elsa.Persistence.MongoDb/Stores/MongoDbStore.cs
+++ src/persistence/Elsa.Persistence.MongoDb/Stores/MongoDbStore.cs
@@ -3,7 +3,6 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Elsa.Models;
-using Elsa.Persistence.MongoDb.Extensions;
 using Elsa.Persistence.Specifications;
 using Elsa.Services;
 using MongoDB.Driver;
