75eecaf0e0baf2eaa488935d4c425c2a7e695694
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2021-05-29 13:42:43 +0200

    Fix liquid engine to handle JObject and JValue resolution

--- src/scripting/Elsa.Scripting.Liquid/Helpers/LiquidActivityModel.cs
+++ src/scripting/Elsa.Scripting.Liquid/Helpers/LiquidActivityModel.cs
@@ -2,7 +2,7 @@
 
 namespace Elsa.Scripting.Liquid.Helpers
 {
-    public record LiquidActivityModel(ActivityExecutionContext ActivityExecutionContext, string ActivityName)
+    public record LiquidActivityModel(ActivityExecutionContext ActivityExecutionContext, string? ActivityName, string? ActivityId)
     {
     }
 }
