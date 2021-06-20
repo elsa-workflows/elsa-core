bfed2819ccb1baea41718b2d38717d1340d1bba6
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2021-06-20 13:57:07 +0200

    Fix provider names in MySql and Sqlite startups

--- src/persistence/Elsa.Persistence.EntityFramework/Elsa.Persistence.EntityFramework.MySql/Startup.cs
+++ src/persistence/Elsa.Persistence.EntityFramework/Elsa.Persistence.EntityFramework.MySql/Startup.cs
@@ -7,7 +7,7 @@ namespace Elsa.Persistence.EntityFramework.MySql
     [Feature("Persistence:EntityFrameworkCore:MySql")]
     public class Startup : EntityFrameworkCoreStartupBase
     {
-        protected override string ProviderName => "SqlServer";
+        protected override string ProviderName => "MySql";
         protected override void Configure(DbContextOptionsBuilder options, string connectionString) => options.UseMySql(connectionString);
     }
 }
--- src/persistence/Elsa.Persistence.EntityFramework/Elsa.Persistence.EntityFramework.Sqlite/Startup.cs
+++ src/persistence/Elsa.Persistence.EntityFramework/Elsa.Persistence.EntityFramework.Sqlite/Startup.cs
@@ -7,7 +7,7 @@ namespace Elsa.Persistence.EntityFramework.Sqlite
     [Feature("Persistence:EntityFrameworkCore:Sqlite")]
     public class Startup : EntityFrameworkCoreStartupBase
     {
-        protected override string ProviderName => "SqlServer";
+        protected override string ProviderName => "Sqlite";
         protected override string GetDefaultConnectionString() => "Data Source=elsa.sqlite.db;Cache=Shared;";
         protected override void Configure(DbContextOptionsBuilder options, string connectionString) => options.UseSqlite(connectionString);
     }
