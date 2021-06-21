7763c88273a48cadd7478fe336e662f9547d3b03
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2021-06-21 16:06:30 +0200

    Improve BridgeCalls to find Dial activity higher up the inbound path

--- src/core/Elsa.Abstractions/Extensions/WorkflowExecutionContextExtensions.cs
+++ src/core/Elsa.Abstractions/Extensions/WorkflowExecutionContextExtensions.cs
@@ -11,13 +11,32 @@ namespace Elsa
         public static IEnumerable<IConnection> GetInboundConnections(this WorkflowExecutionContext workflowExecutionContext, string activityId) =>
             workflowExecutionContext.WorkflowBlueprint.Connections.Where(x => x.Target.Activity.Id == activityId);
 
+        public static IEnumerable<IConnection> GetInboundConnectionPath(this WorkflowExecutionContext workflowExecutionContext, string activityId) => workflowExecutionContext.GetInboundConnectionPath(activityId, activityId);
+
         public static IEnumerable<IConnection> GetOutboundConnections(this WorkflowExecutionContext workflowExecutionContext, string activityId) =>
             workflowExecutionContext.WorkflowBlueprint.Connections.Where(x => x.Source.Activity.Id == activityId);
 
         public static IEnumerable<string> GetInboundActivityPath(this WorkflowExecutionContext workflowExecutionContext, string activityId) => workflowExecutionContext.GetInboundActivityPathInternal(activityId, activityId).Distinct();
         public static IEnumerable<string> GetOutboundActivityPath(this WorkflowExecutionContext workflowExecutionContext, string activityId) => workflowExecutionContext.GetOutboundActivityPathInternal(activityId, activityId).Distinct();
         public static async ValueTask<T?> GetOutputFromAsync<T>(this WorkflowExecutionContext workflowExecutionContext, string activityName, CancellationToken cancellationToken = default) => await workflowExecutionContext.GetOutputFromAsync<T>(activityName, cancellationToken);
+        
+        private static IEnumerable<IConnection> GetInboundConnectionPath(this WorkflowExecutionContext workflowExecutionContext, string activityId, string startingPointActivityId)
+        {
+            foreach (var connection in workflowExecutionContext.GetInboundConnections(activityId))
+            {
+                // Circuit breaker: Detect workflows that implement repeating flows to prevent an infinite loop here.
+                if (connection.Source.Activity.Id == startingPointActivityId)
+                    yield break;
 
+                yield return connection;
+
+                foreach (var parentConnection in workflowExecutionContext
+                    .GetInboundConnectionPath(connection.Source.Activity.Id!, startingPointActivityId)
+                    .Distinct())
+                    yield return parentConnection;
+            }
+        }
+        
         private static IEnumerable<string> GetInboundActivityPathInternal(this WorkflowExecutionContext workflowExecutionContext, string activityId, string startingPointActivityId)
         {
             foreach (var connection in workflowExecutionContext.GetInboundConnections(activityId))
