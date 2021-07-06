05843c221e9a7f32b6337331147652e89395ec39
axeleron007 <andmis@gmail.com>
2021-07-06 10:41:37 +0300

    Code clean up
    Code clean up

--- src/samples/server/Elsa.Samples.Server.Host/Startup.cs
+++ src/samples/server/Elsa.Samples.Server.Host/Startup.cs
@@ -5,8 +5,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace Elsa.Samples.Server.Host
 {
