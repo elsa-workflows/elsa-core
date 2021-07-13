abc4e5389c0acc5ac7b8fc3b84e7d752effe23bc
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2021-07-13 13:11:32 +0200

    Fix Type serialization for MongoDB provider

---
+++ src/persistence/Elsa.Persistence.MongoDb/Serializers/TypeSerializer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Elsa.Models;
+using MongoDB.Bson.Serialization;
+using Rebus.Extensions;
+
+namespace Elsa.Persistence.MongoDb.Serializers
+{
+    public class TypeSerializer : IBsonSerializer<Type>
+    {
+        public static TypeSerializer Instance { get; } = new();
+        public Type ValueType => typeof(Variables);
+        public void Serialize(BsonSerializationContext context, BsonSerializationArgs args, object value) => Serialize(context, args, (Type) value);
+
+        public void Serialize(BsonSerializationContext context, BsonSerializationArgs args, Type value)
+        {
+            var typeName = value.GetSimpleAssemblyQualifiedName();
+            context.Writer.WriteString(typeName);
+        }
+
+        object IBsonSerializer.Deserialize(BsonDeserializationContext context, BsonDeserializationArgs args) => Deserialize(context, args);
+
+        public Type Deserialize(BsonDeserializationContext context, BsonDeserializationArgs args)
+        {
+            var typeName = context.Reader.ReadString();
+            return typeName is not null and not "" ? Type.GetType(typeName)! : default!;
+        }
+    }
+}
