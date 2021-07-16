874dca1b0231fdcf72c0fcb21d5ea74cad8bdec4
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2021-07-16 12:34:10 +0200

    Make activityId optional

--- src/core/Elsa.Abstractions/Services/Workflows/IWorkflowLaunchpad.cs
+++ src/core/Elsa.Abstractions/Services/Workflows/IWorkflowLaunchpad.cs
@@ -27,22 +27,22 @@ namespace Elsa.Services
         /// <summary>
         /// Creates a new workflow instance with execution pending for the specified workflow blueprint using the specified starting activity ID.
         /// </summary>
-        Task<StartableWorkflow?> CollectStartableWorkflowAsync(string workflowDefinitionId, string? activityId, string? correlationId = default, string? contextId = default, string? tenantId = default, CancellationToken cancellationToken = default);
+        Task<StartableWorkflow?> CollectStartableWorkflowAsync(string workflowDefinitionId, string? activityId = default, string? correlationId = default, string? contextId = default, string? tenantId = default, CancellationToken cancellationToken = default);
 
         /// <summary>
         /// Creates a new workflow instance with execution pending for the specified workflow blueprint using the specified starting activity ID.
         /// </summary>
-        Task<StartableWorkflow?> CollectStartableWorkflowAsync(IWorkflowBlueprint workflowBlueprint, string? activityId, string? correlationId = default, string? contextId = default, string? tenantId = default, CancellationToken cancellationToken = default);
+        Task<StartableWorkflow?> CollectStartableWorkflowAsync(IWorkflowBlueprint workflowBlueprint, string? activityId = default, string? correlationId = default, string? contextId = default, string? tenantId = default, CancellationToken cancellationToken = default);
         
         /// <summary>
         /// Collects and executes the specified startable workflow.
         /// </summary>
-        Task CollectAndExecuteStartableWorkflowAsync(string workflowDefinitionId, string? activityId, string? correlationId = default, string? contextId = default, object? input = default, string? tenantId = default, CancellationToken cancellationToken = default);
+        Task CollectAndExecuteStartableWorkflowAsync(string workflowDefinitionId, string? activityId = default, string? correlationId = default, string? contextId = default, object? input = default, string? tenantId = default, CancellationToken cancellationToken = default);
         
         /// <summary>
         /// Collects and executes the specified startable workflow.
         /// </summary>
-        Task<RunWorkflowResult> CollectAndExecuteStartableWorkflowAsync(IWorkflowBlueprint workflowBlueprint, string? activityId, string? correlationId = default, string? contextId = default, object? input = default, CancellationToken cancellationToken = default);
+        Task<RunWorkflowResult> CollectAndExecuteStartableWorkflowAsync(IWorkflowBlueprint workflowBlueprint, string? activityId = default, string? correlationId = default, string? contextId = default, object? input = default, CancellationToken cancellationToken = default);
 
         /// <summary>
         /// Executes a list of pending workflows.
@@ -57,7 +57,7 @@ namespace Elsa.Services
         /// <summary>
         /// Executes a pending workflow.
         /// </summary>
-        Task<RunWorkflowResult> ExecutePendingWorkflowAsync(string workflowInstanceId, string? activityId, object? input = default, CancellationToken cancellationToken = default);
+        Task<RunWorkflowResult> ExecutePendingWorkflowAsync(string workflowInstanceId, string? activityId = default, object? input = default, CancellationToken cancellationToken = default);
 
         /// <summary>
         /// Dispatches a list of pending workflows for execution.
@@ -67,27 +67,27 @@ namespace Elsa.Services
         /// <summary>
         /// Dispatches a pending workflow for execution.
         /// </summary>
-        Task DispatchPendingWorkflowAsync(CollectedWorkflow collectedWorkflow, object? input, CancellationToken cancellationToken = default);
+        Task DispatchPendingWorkflowAsync(CollectedWorkflow collectedWorkflow, object? input = default, CancellationToken cancellationToken = default);
         
         /// <summary>
         /// Dispatches a pending workflow for execution.
         /// </summary>
-        Task DispatchPendingWorkflowAsync(string workflowInstanceId, string? activityId, object? input, CancellationToken cancellationToken = default);
+        Task DispatchPendingWorkflowAsync(string workflowInstanceId, string? activityId = default, object? input = default, CancellationToken cancellationToken = default);
 
         /// <summary>
         /// Executes the specified startable workflow.
         /// </summary>
-        Task<RunWorkflowResult> ExecuteStartableWorkflowAsync(StartableWorkflow startableWorkflow, object? input, CancellationToken cancellationToken = default);
+        Task<RunWorkflowResult> ExecuteStartableWorkflowAsync(StartableWorkflow startableWorkflow, object? input = default, CancellationToken cancellationToken = default);
         
         /// <summary>
         /// Dispatches the specified startable workflow.
         /// </summary>
-        Task<CollectedWorkflow> DispatchStartableWorkflowAsync(StartableWorkflow startableWorkflow, object? input, CancellationToken cancellationToken = default);
+        Task<CollectedWorkflow> DispatchStartableWorkflowAsync(StartableWorkflow startableWorkflow, object? input = default, CancellationToken cancellationToken = default);
 
         /// <summary>
         /// Collects and executes workflows that are ready for execution. This takes into account both resumable (suspended) workflows as well as startable workflows.
         /// </summary>
-        Task<IEnumerable<CollectedWorkflow>> CollectAndExecuteWorkflowsAsync(CollectWorkflowsContext context, object? input = null, CancellationToken cancellationToken = default);
+        Task<IEnumerable<CollectedWorkflow>> CollectAndExecuteWorkflowsAsync(CollectWorkflowsContext context, object? input = default, CancellationToken cancellationToken = default);
         
         /// <summary>
         /// Collects and dispatches workflows that are ready for execution. This takes into account both resumable (suspended) workflows as well as startable workflows.
