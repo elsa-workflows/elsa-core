6e73e550edd36eb8b70bceb7868a58b256b02122
Jens Willmer <info@jwillmer.de>
2021-07-20 12:43:19 +0200

    speed up docker build for dashboard

--- src/designer/elsa-workflows-studio/stencil.config.ts
+++ src/designer/elsa-workflows-studio/stencil.config.ts
@@ -11,38 +11,48 @@ const dev: boolean = process.argv && process.argv.indexOf('--dev') > -1;
 const tailwindDev: boolean = process.argv && process.argv.indexOf('--tailwind:dev') > -1;
 
 // @ts-ignore
+const dashboardBuildOnly: boolean = process.argv && process.argv.indexOf('--dasboard') > -1;
+
+// @ts-ignore
 const purgecss = require('@fullhuman/postcss-purgecss')({
   content: ['./src/**/*.tsx', './src/**/*.html'],
   defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
   safelist: ['jtk-connector'],
 });
 
+const targets = [];
+
+targets.push({
+  type: 'dist',
+  esmLoaderPath: '../loader',
+  copy: [
+    { src: 'assets', dest: 'assets' }
+  ]
+});
+
+targets.push({
+  type: 'dist-custom-elements-bundle',
+});
+
+if (!dashboardBuildOnly) {
+  targets.push({
+    type: 'docs-readme'
+  });
+
+  targets.push({
+    type: 'www',
+    serviceWorker: null, // disable service workers,
+    copy: [
+      { src: 'assets', dest: 'build/assets' },
+      { src: '../node_modules/monaco-editor/min', dest: 'build/assets/js/monaco-editor/min' },
+      { src: '../node_modules/monaco-editor/min-maps', dest: 'build/assets/js/monaco-editor/min-maps' }
+    ]
+  });
+}
+
 export const config: Config = {
   namespace: 'elsa-workflows-studio',
-  outputTargets: [
-    {
-      type: 'dist',
-      esmLoaderPath: '../loader',
-      copy: [
-        {src: 'assets', dest: 'assets'}
-      ]
-    },
-    {
-      type: 'dist-custom-elements-bundle',
-    },
-    {
-      type: 'docs-readme',
-    },
-    {
-      type: 'www',
-      serviceWorker: null, // disable service workers,
-      copy: [
-        {src: 'assets', dest: 'build/assets'},
-        {src: '../node_modules/monaco-editor/min', dest: 'build/assets/js/monaco-editor/min'},
-        {src: '../node_modules/monaco-editor/min-maps', dest: 'build/assets/js/monaco-editor/min-maps'}
-      ]
-    },
-  ],
+  outputTargets: targets,
   globalStyle: 'src/globals/tailwind.css',
   plugins: tailwindDev
     ? []
