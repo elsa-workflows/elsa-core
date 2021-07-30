35fd05c913978a36845f92c030c8c6fbb095c568
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2021-07-30 22:09:31 +0200

    Update TriggerEndpoint with support for customizing HTTP response
    Fixes #1304

--- src/activities/Elsa.Activities.Http/Endpoints/Signals/TriggerEndpoint.cs
+++ src/activities/Elsa.Activities.Http/Endpoints/Signals/TriggerEndpoint.cs
@@ -1,5 +1,11 @@
+using System.Net;
+using System.Threading;
 using System.Threading.Tasks;
+using Elsa.Activities.Http.Events;
+using Elsa.Activities.Signaling.Models;
 using Elsa.Activities.Signaling.Services;
+using Elsa.Services;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Open.Linq.AsyncExtensions;
 
@@ -11,16 +17,31 @@ namespace Elsa.Activities.Http.Endpoints.Signals
     public class TriggerEndpoint : ControllerBase
     {
         private readonly ISignaler _signaler;
-        public TriggerEndpoint(ISignaler signaler) => _signaler = signaler;
+        private readonly ITokenService _tokenService;
+        private readonly IMediator _mediator;
+
+        public TriggerEndpoint(ISignaler signaler, ITokenService tokenService, IMediator mediator)
+        {
+            _signaler = signaler;
+            _tokenService = tokenService;
+            _mediator = mediator;
+        }
 
         [HttpGet, HttpPost]
-        public async Task<IActionResult> Handle(string token)
+        public async Task<IActionResult> Handle(string token, CancellationToken cancellationToken)
         {
-            var result = await _signaler.TriggerSignalTokenAsync(token).ToList();
+            if (!_tokenService.TryDecryptToken(token, out SignalModel signal))
+                return BadRequest();
 
-            return HttpContext.Response.HasStarted
+            var affectedWorkflows = await _signaler.TriggerSignalAsync(signal.Name, null, signal.WorkflowInstanceId, cancellationToken: cancellationToken).ToList();
+
+            await _mediator.Publish(new HttpTriggeredSignal(signal, affectedWorkflows), cancellationToken);
+
+            var response = HttpContext.Response;
+
+            return response.HasStarted || response.StatusCode != (int) HttpStatusCode.OK
                 ? new EmptyResult()
-                : Ok(result);
+                : Ok(affectedWorkflows);
         }
     }
 }
