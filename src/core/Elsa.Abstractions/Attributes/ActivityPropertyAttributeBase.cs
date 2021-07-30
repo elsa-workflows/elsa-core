166c06751d75aaab44f73136bc4a6f22b61e405b
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2021-07-30 22:10:02 +0200

    Update ActivityActivator to use default storage provider

---
+++ src/core/Elsa.Abstractions/Attributes/ActivityPropertyAttributeBase.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace Elsa.Attributes
+{
+    public abstract class ActivityPropertyAttributeBase : Attribute
+    {
+        /// <summary>
+        /// The technical name of the activity property.
+        /// </summary>
+        public string? Name { get; set; }
+        
+        /// <summary>
+        /// A brief description about this property for workflow tooling to use when displaying activity editors.
+        /// </summary>
+        public string? Hint { get; set; }
+        
+        /// <summary>
+        /// The workflow storage provider to use by default to store the output value.
+        /// </summary>
+        public string? DefaultWorkflowStorageProvider { get; set; }
+
+        /// <summary>
+        /// A flag indicating whether or not the user is allowed to select a workflow provider.
+        /// </summary>
+        public bool DisableWorkflowProviderSelection { get; set; }
+    }
+}
