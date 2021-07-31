0a5a09302cfb0a2b31a5491c7f07973510c51f89
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2021-07-31 11:37:44 +0200

    Fix workflow registry resources path

--- src/designer/elsa-workflows-studio/src/components/dashboard/pages/elsa-studio-workflow-registry/elsa-studio-workflow-registry.tsx
+++ src/designer/elsa-workflows-studio/src/components/dashboard/pages/elsa-studio-workflow-registry/elsa-studio-workflow-registry.tsx
@@ -1,7 +1,7 @@
 import {Component, h, Prop} from '@stencil/core';
 import {i18n} from "i18next";
 import {loadTranslations} from "../../../i18n/i18n-loader";
-import {resources} from "../elsa-studio-workflow-instances-list/localizations";
+import {resources} from "./localizations";
 import Tunnel from "../../../../data/dashboard";
 
 @Component({
