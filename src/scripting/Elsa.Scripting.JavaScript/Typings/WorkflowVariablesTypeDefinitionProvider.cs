cd43136e746c390ed6f7a4ed4d99c0572d75b6f8
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2021-08-07 18:28:25 +0200

    Fix type definition provider for workflows
    This handles the case when the variable is initialized with a null value.

--- src/scripting/Elsa.Scripting.JavaScript/Typings/WorkflowVariablesTypeDefinitionProvider.cs
+++ src/scripting/Elsa.Scripting.JavaScript/Typings/WorkflowVariablesTypeDefinitionProvider.cs
@@ -14,7 +14,7 @@ namespace Elsa.Scripting.JavaScript.Typings
                 yield break;
             
             foreach (var variable in workflowDefinition.Variables!.Data.Values)
-                yield return variable!.GetType();
+                yield return variable?.GetType() ?? typeof(object);
         }
     }
 }
