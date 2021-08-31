b77e93a4a923bbd74158c1bd97a291bc28e67657
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2021-08-31 13:56:04 +0200

    Merge List with GetMany
    Fixes #1391

--- src/samples/aspnet/Elsa.Samples.SendHttp/Program.cs
+++ src/samples/aspnet/Elsa.Samples.SendHttp/Program.cs
@@ -5,10 +5,7 @@ namespace Elsa.Samples.SendHttp
 {
     public class Program
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+        public static void Main(string[] args) => CreateHostBuilder(args).Build().Run();
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
