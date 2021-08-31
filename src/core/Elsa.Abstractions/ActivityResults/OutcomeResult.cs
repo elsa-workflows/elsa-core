8228053cd939a5afea51143baac1239097628254
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2021-08-31 21:30:37 +0200

    Fix duplicate Done outcome
    Fixes #1455

--- src/core/Elsa.Abstractions/ActivityResults/OutcomeResult.cs
+++ src/core/Elsa.Abstractions/ActivityResults/OutcomeResult.cs
@@ -7,14 +7,11 @@ namespace Elsa.ActivityResults
 {
     public class OutcomeResult : ActivityExecutionResult
     {
-        public OutcomeResult(IEnumerable<string>? outcomes = default, object? input = null, string? parentId = default)
+        public OutcomeResult(IEnumerable<string> outcomes, object? input = null, string? parentId = default)
         {
             Input = input;
             ParentId = parentId;
-            var outcomeList = outcomes?.ToList() ?? new List<string>(1);
-
-            if (!outcomeList.Any())
-                outcomeList.Add(OutcomeNames.Done);
+            var outcomeList = outcomes.ToList();
 
             Outcomes = outcomeList;
         }
