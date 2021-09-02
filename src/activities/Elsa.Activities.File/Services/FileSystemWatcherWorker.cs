dc3b7d62e0e1876075780812192b7eea6a43e8fc
Cameron Cowen <cameronc94@gmail.com>
2021-09-02 23:10:41 +0100

    Added initial FS worker to manage System.IO watcher and hold path and filter

---
+++ src/activities/Elsa.Activities.File/Services/FileSystemWatcherWorker.cs
@@ -0,0 +1,40 @@
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Elsa.Activities.File.Services
+{
+    public class FileSystemWatcherWorker
+    {
+        private readonly ILogger _logger;
+        private readonly FileSystemWatcher _watcher;
+
+        public FileSystemWatcherWorker(string path, string pattern, ILogger<FileSystemWatcherWorker> logger)
+        {
+            Path = path;
+            Pattern = pattern;
+            _watcher = new FileSystemWatcher()
+            {
+                Path = path,
+                Filter = pattern
+            };
+            _watcher.Created += FileCreated;
+            _watcher.Changed += FileChanged;
+            _watcher.EnableRaisingEvents = true;
+        }
+
+        public string Path { get; private set; }
+
+        public string Pattern { get; private set; }
+
+        private void FileChanged(object sender, FileSystemEventArgs e)
+        {
+        }
+
+        private void FileCreated(object sender, FileSystemEventArgs e)
+        {
+        }
+    }
+}
