eae364114a52a454840458e50bd4997cfc13fad6
Cameron Cowen <cameronc94@gmail.com>
2021-09-08 00:20:23 +0100

    Added notifyfilters to watchdiretory

--- src/activities/Elsa.Activities.File/Activities/WatchDirectory/WatchDirectory.cs
+++ src/activities/Elsa.Activities.File/Activities/WatchDirectory/WatchDirectory.cs
@@ -1,10 +1,13 @@
 using Elsa.Activities.File.Models;
 using Elsa.ActivityResults;
 using Elsa.Attributes;
+using Elsa.Design;
 using Elsa.Services;
 using Elsa.Services.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -16,11 +19,25 @@ namespace Elsa.Activities.File
         Outcomes = new[] { OutcomeNames.Done })]
     public class WatchDirectory : Activity
     {
+        [Required]
         [ActivityInput(Hint = "The path of the directory to watch")]
-        public string Path { get; set; }
+        public string? Path { get; set; }
 
         [ActivityInput(Hint = "The file pattern for interested files")]
-        public string Pattern { get; set; }
+        public string? Pattern { get; set; }
+
+        [ActivityInput(Label = "Change Types", Hint = "The types of file system events to subscribe to", UIHint = ActivityInputUIHints.CheckList, DefaultValue = WatcherChangeTypes.All)]
+        public WatcherChangeTypes ChangeTypes { get; set; }
+
+        [ActivityInput(Label = "Notify Filters", UIHint = ActivityInputUIHints.CheckList, DefaultValue = NotifyFilters.Attributes |
+            NotifyFilters.CreationTime |
+            NotifyFilters.DirectoryName |
+            NotifyFilters.FileName |
+            NotifyFilters.LastAccess |
+            NotifyFilters.LastWrite |
+            NotifyFilters.Security |
+            NotifyFilters.Size)]
+        public NotifyFilters NotifyFilters { get; set; }
 
         [ActivityOutput]
         public FileSystemEvent? Output { get; set; }
@@ -29,7 +46,7 @@ namespace Elsa.Activities.File
         {
             var model = (FileSystemEvent)context.Input!;
             Output = model;
-            return Done();
+            return Done(model);
         }
     }
 }
