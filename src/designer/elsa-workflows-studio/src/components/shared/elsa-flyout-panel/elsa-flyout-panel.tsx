ac3ffb607b5f754de3838dc031709db7f4ecb017
simona-taran <88426876+simona-taran@users.noreply.github.com>
2021-10-01 20:45:36 +0300

    Added copy functionality to instance journal. Fixed registry panel issue. Fixed flyout panel scroll issue. (#1593)

--- src/designer/elsa-workflows-studio/src/components/shared/elsa-flyout-panel/elsa-flyout-panel.tsx
+++ src/designer/elsa-workflows-studio/src/components/shared/elsa-flyout-panel/elsa-flyout-panel.tsx
@@ -17,7 +17,9 @@ export class ElsaFlyoutPanel {
   async componentDidLoad() {
     this.headerTabs = Array.from(this.el.querySelectorAll('elsa-tab-header'));
     this.headerTabs.forEach(element => {
-      element.onclick = () => {this.selectTab(element.tab)};
+      element.onclick = () => {
+        this.selectTab(element.tab)
+      };
     })
     this.contentTabs = Array.from(this.el.querySelectorAll('elsa-tab-content'));
     if (this.headerTabs.length > 0) {
@@ -57,7 +59,7 @@ export class ElsaFlyoutPanel {
                 data-transition-leave="elsa-transform elsa-transition elsa-ease-in-out elsa-duration-300 sm:elsa-duration-700"
                 data-transition-leave-start="elsa-translate-x-0"
                 data-transition-leave-end="elsa-translate-x-full"
-                class="elsa-w-screen elsa-max-w-lg elsa-h-full">
+                class="elsa-w-screen elsa-max-w-lg elsa-h-full ">
                 <button type="button"
                         onClick={this.toggle}
                         class="workflow-settings-button elsa-absolute elsa-top-4 elsa-left-2 elsa-inline-flex elsa-items-center elsa-p-2 elsa-rounded-full elsa-border elsa-border-transparent elsa-bg-white shadow elsa-text-gray-400 hover:elsa-text-blue-500 focus:elsa-text-blue-500 hover:elsa-ring-2 hover:elsa-ring-offset-2 hover:elsa-ring-blue-500 focus:elsa-outline-none focus:elsa-ring-2 focus:elsa-ring-offset-2 focus:elsa-ring-blue-500 elsa-z-10">
@@ -68,20 +70,20 @@ export class ElsaFlyoutPanel {
                   </svg>
                 </button>
                 <div
-                  class="elsa-h-full elsa-flex elsa-flex-col elsa-py-6 elsa-bg-white elsa-shadow-xl elsa-overflow-y-scroll elsa-bg-white">
-                  <div class="elsa-h-full">
-                    <div class="elsa-mt-8 elsa-p-6">
+                  class="elsa-h-full elsa-flex elsa-flex-col elsa-py-6 elsa-bg-white elsa-shadow-xl elsa-bg-white">
+                  <div class="elsa-h-full elsa-mt-8 elsa-p-6">
                       <div class="elsa-border-b elsa-border-gray-200">
                         <nav class="-elsa-mb-px elsa-flex elsa-space-x-8" aria-label="Tabs">
                           <slot name="header"/>
                         </nav>
                       </div>
-                      <slot name="content" />
+                      <section class="elsa-overflow-auto elsa-h-full">
+                        <slot name="content"/>
+                      </section>
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
           </div>
         </section>
       </Host>
