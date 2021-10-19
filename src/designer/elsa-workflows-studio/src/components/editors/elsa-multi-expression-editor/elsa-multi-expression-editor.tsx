68d0d7de45ee82d89f95233bbd5b02d12031dd60
Konstantins Vedenins <konstantins.vedenins@gmail.com>
2021-10-19 22:44:31 +0300

    fixed "can't access property "contains", this.contextMenuWidget is undefined"

--- src/designer/elsa-workflows-studio/src/components/editors/elsa-multi-expression-editor/elsa-multi-expression-editor.tsx
+++ src/designer/elsa-workflows-studio/src/components/editors/elsa-multi-expression-editor/elsa-multi-expression-editor.tsx
@@ -40,7 +40,7 @@ export class ElsaMultiExpressionEditor {
   onWindowClicked(event: Event){
     const target = event.target as HTMLElement;
 
-    if (!this.contextMenuWidget.contains(target))
+    if (!this.contextMenuWidget || !this.contextMenuWidget.contains(target))
       this.closeContextMenu();
   }
 
