6ff2d69cb24459d1486e73d033039fe5fc1a33b3
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2021-10-29 14:58:43 +0200

    Fix cleanup service
    Fixes the issue where the job would run only once, instead of peridiocally.

--- src/modules/retention/Elsa.Retention/HostedServices/CleanupService.cs
+++ src/modules/retention/Elsa.Retention/HostedServices/CleanupService.cs
@@ -12,40 +12,27 @@ namespace Elsa.Retention.HostedServices
     /// <summary>
     /// Periodically wipes workflow instances and their execution logs.
     /// </summary>
-    public class CleanupService : IHostedService, IAsyncDisposable
+    public class CleanupService : BackgroundService
     {
         private readonly IServiceScopeFactory _serviceScopeFactory;
-        private readonly CleanupOptions _options;
-        private readonly Timer _timer;
+        private readonly TimeSpan _interval;
 
         public CleanupService(IOptions<CleanupOptions> options, IServiceScopeFactory serviceScopeFactory)
         {
             _serviceScopeFactory = serviceScopeFactory;
-            _options = options.Value;
-            _timer = new Timer(ExecuteAsync, null, Timeout.InfiniteTimeSpan, Timeout.InfiniteTimeSpan);
+            _interval = options.Value.SweepInterval.ToTimeSpan();
         }
 
-        public Task StartAsync(CancellationToken cancellationToken)
-        {
-            _timer.Change(_options.SweepInterval.ToTimeSpan(), Timeout.InfiniteTimeSpan);
-            return Task.CompletedTask;
-        }
-
-        public Task StopAsync(CancellationToken cancellationToken)
-        {
-            _timer.Change(Timeout.InfiniteTimeSpan, Timeout.InfiniteTimeSpan);
-            return Task.CompletedTask;
-        }
-
-        public async ValueTask DisposeAsync() => await _timer.DisposeAsync();
-
-        private async void ExecuteAsync(object state)
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             using var scope = _serviceScopeFactory.CreateScope();
             var job = scope.ServiceProvider.GetRequiredService<CleanupJob>();
-            await job.ExecuteAsync();
-
-            _timer.Change(_options.SweepInterval.ToTimeSpan(), Timeout.InfiniteTimeSpan);
+            
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                await Task.Delay(_interval, stoppingToken);
+                await job.ExecuteAsync(stoppingToken);
+            }
         }
     }
 }
