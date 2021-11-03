f475790c30877664bf634912dcfe33a82f82abca
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2021-11-03 19:31:34 +0100

    Make CleanupService resilient against exceptions
    This ensures the background task keeps running even when a cleanup attempt fails due to e.g. DB timeout exceptions.

--- src/modules/retention/Elsa.Retention/HostedServices/CleanupService.cs
+++ src/modules/retention/Elsa.Retention/HostedServices/CleanupService.cs
@@ -5,6 +5,7 @@ using Elsa.Retention.Jobs;
 using Elsa.Retention.Options;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
 namespace Elsa.Retention.HostedServices
@@ -15,11 +16,13 @@ namespace Elsa.Retention.HostedServices
     public class CleanupService : BackgroundService
     {
         private readonly IServiceScopeFactory _serviceScopeFactory;
+        private readonly ILogger<CleanupService> _logger;
         private readonly TimeSpan _interval;
 
-        public CleanupService(IOptions<CleanupOptions> options, IServiceScopeFactory serviceScopeFactory)
+        public CleanupService(IOptions<CleanupOptions> options, IServiceScopeFactory serviceScopeFactory, ILogger<CleanupService> logger)
         {
             _serviceScopeFactory = serviceScopeFactory;
+            _logger = logger;
             _interval = options.Value.SweepInterval.ToTimeSpan();
         }
 
@@ -27,11 +30,19 @@ namespace Elsa.Retention.HostedServices
         {
             using var scope = _serviceScopeFactory.CreateScope();
             var job = scope.ServiceProvider.GetRequiredService<CleanupJob>();
-            
+
             while (!stoppingToken.IsCancellationRequested)
             {
                 await Task.Delay(_interval, stoppingToken);
-                await job.ExecuteAsync(stoppingToken);
+
+                try
+                {
+                    await job.ExecuteAsync(stoppingToken);
+                }
+                catch (Exception e)
+                {
+                    _logger.LogError(e, "Failed to perform cleanup this time around. Next cleanup attempt will happen in {Interval}", _interval);
+                }
             }
         }
     }
