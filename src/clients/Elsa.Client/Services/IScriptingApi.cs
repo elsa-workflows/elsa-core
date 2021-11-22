b020ed385d91f7441ad5152f9eb029365ebd5022
Dominic Asley <dasley@oexa.co>
2021-11-22 23:34:53 +1000

    Add scripting api to elsa client

---
+++ src/clients/Elsa.Client/Services/IScriptingApi.cs
@@ -0,0 +1,13 @@
+﻿using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Refit;
+
+namespace Elsa.Client.Services
+{
+    public interface IScriptingApi
+    {
+        [Get("/v1/scripting/javascript/type-definitions/{workflowDefinitionId}")]
+        Task<HttpContent> GetTypeScriptDefinitionFileAsync(string workflowDefinitionId, CancellationToken cancellationToken = default);
+    }
+}
