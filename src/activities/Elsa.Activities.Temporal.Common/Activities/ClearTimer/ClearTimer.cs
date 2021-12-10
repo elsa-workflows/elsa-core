8a30bfe66a6a6a1f11167ab5ba6b226f495ca872
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2021-12-10 15:56:14 +0100

    Fix ClearTimer activity
    Fixes #2429

--- src/activities/Elsa.Activities.Temporal.Common/Activities/ClearTimer/ClearTimer.cs
+++ src/activities/Elsa.Activities.Temporal.Common/Activities/ClearTimer/ClearTimer.cs
@@ -1,5 +1,5 @@
+using System.Linq;
 using System.Threading.Tasks;
-using Elsa.Activities.Temporal.Common.Services;
 using Elsa.ActivityResults;
 using Elsa.Attributes;
 using Elsa.Expressions;
@@ -12,19 +12,17 @@ namespace Elsa.Activities.Temporal
     [Trigger(Category = "Timers", Description = "Cancel a timer (Cron, StartAt, Timer) so that it is not executed.")]
     public class ClearTimer : Activity
     {
-        private readonly IWorkflowInstanceScheduler _workflowScheduler;
-
-        public ClearTimer(IWorkflowInstanceScheduler workflowScheduler)
-        {
-            _workflowScheduler = workflowScheduler;
-        }
-
         [ActivityInput(Hint = "The ID of the timer (Cron, StartAt, Timer) activity, which is to be cleared.", SupportedSyntaxes = new[] { SyntaxNames.JavaScript, SyntaxNames.Liquid })]
         public string ActivityId { get; set; } = default!;
 
         protected override async ValueTask<IActivityExecutionResult> OnExecuteAsync(ActivityExecutionContext context)
         {
-            await _workflowScheduler.UnscheduleAsync(context.WorkflowInstance.Id, ActivityId, context.CancellationToken);
+            // Remove blocking activity.
+            var blockingActivity = context.WorkflowInstance.BlockingActivities.FirstOrDefault(x => x.ActivityId == ActivityId);
+
+            if (blockingActivity != null)
+                await context.WorkflowExecutionContext.RemoveBlockingActivityAsync(blockingActivity);
+
             return Done();
         }
     }
