0ef39ae0690bb67c09e9549568f075c087abc885
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2021-12-15 12:19:29 +0100

    Update WorkflowDefinitionVersionIdsSpecification.cs

--- src/core/Elsa.Core/Persistence/Specifications/WorkflowInstances/WorkflowDefinitionVersionIdsSpecification.cs
+++ src/core/Elsa.Core/Persistence/Specifications/WorkflowInstances/WorkflowDefinitionVersionIdsSpecification.cs
@@ -4,11 +4,12 @@ using System.Linq;
 using System.Linq.Expressions;
 using Elsa.Models;
 
-namespace Elsa.Persistence.Specifications.WorkflowInstances;
-
-public class WorkflowDefinitionVersionIdsSpecification : Specification<WorkflowInstance>
+namespace Elsa.Persistence.Specifications.WorkflowInstances
 {
-    public ICollection<string> WorkflowDefinitionVersionIds { get; set; }
-    public WorkflowDefinitionVersionIdsSpecification(IEnumerable<string> workflowDefinitionVersionIds) => WorkflowDefinitionVersionIds = workflowDefinitionVersionIds.ToList();
-    public override Expression<Func<WorkflowInstance, bool>> ToExpression() => x => WorkflowDefinitionVersionIds.Contains(x.DefinitionVersionId);
+    public class WorkflowDefinitionVersionIdsSpecification : Specification<WorkflowInstance>
+    {
+        public ICollection<string> WorkflowDefinitionVersionIds { get; set; }
+        public WorkflowDefinitionVersionIdsSpecification(IEnumerable<string> workflowDefinitionVersionIds) => WorkflowDefinitionVersionIds = workflowDefinitionVersionIds.ToList();
+        public override Expression<Func<WorkflowInstance, bool>> ToExpression() => x => WorkflowDefinitionVersionIds.Contains(x.DefinitionVersionId);
+    }
 }
