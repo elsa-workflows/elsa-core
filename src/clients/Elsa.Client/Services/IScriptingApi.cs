8ab7f6070b68e887252c5894f379a5bf4fc0065a
Dominic Asley <dasley@oexa.co>
2022-01-05 00:26:54 +1000

    Update scripting client to use POST endpoint method

--- src/clients/Elsa.Client/Services/IScriptingApi.cs
+++ src/clients/Elsa.Client/Services/IScriptingApi.cs
@@ -7,7 +7,7 @@ namespace Elsa.Client.Services
 {
     public interface IScriptingApi
     {
-        [Get("/v1/scripting/javascript/type-definitions/{workflowDefinitionId}")]
+        [Post("/v1/scripting/javascript/type-definitions/{workflowDefinitionId}")]
         Task<HttpContent> GetTypeScriptDefinitionFileAsync(string workflowDefinitionId, CancellationToken cancellationToken = default);
     }
 }
