b3c0666a5a712f5eb9b7ef136854e085af7379b2
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2022-01-23 21:46:29 +0100

    Update StartServiceBusQueues to also create workers based on triggers

--- src/activities/Elsa.Activities.AzureServiceBus/StartupTasks/StartServiceBusQueues.cs
+++ src/activities/Elsa.Activities.AzureServiceBus/StartupTasks/StartServiceBusQueues.cs
@@ -21,17 +21,24 @@ namespace Elsa.Activities.AzureServiceBus.StartupTasks
             _serviceBusQueuesStarter = serviceBusQueuesStarter;
             _scopeFactory = scopeFactory;
         }
-        
+
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             using var scope = _scopeFactory.CreateScope();
             var bookmarkFinder = scope.ServiceProvider.GetRequiredService<IBookmarkFinder>();
-            
-            // Load all QueueMessageReceived bookmarks.
-            var bookmarks = await bookmarkFinder.FindBookmarksByTypeAsync<QueueMessageReceivedBookmark>(cancellationToken: stoppingToken).ToList();
+
+            // Load bookmarks.
+            var bookmarks = await bookmarkFinder.FindBookmarksByTypeAsync<QueueMessageReceivedBookmark>(cancellationToken: stoppingToken);
 
             // For each bookmark, start a worker.
             await _serviceBusQueuesStarter.CreateWorkersAsync(bookmarks, stoppingToken);
+
+            // Load triggers.
+            var triggerFinder = scope.ServiceProvider.GetRequiredService<ITriggerFinder>();
+            var triggers = await triggerFinder.FindTriggersByTypeAsync<QueueMessageReceivedBookmark>(cancellationToken: stoppingToken);
+
+            // For each trigger, start a worker.
+            await _serviceBusQueuesStarter.CreateWorkersAsync(triggers, stoppingToken);
         }
     }
 }
