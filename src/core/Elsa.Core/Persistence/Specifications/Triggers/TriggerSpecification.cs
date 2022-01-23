471677ce42caf74c17484162ab5cfd064789714f
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2022-01-23 20:01:04 +0100

    Fix TriggerFinder

--- src/core/Elsa.Core/Persistence/Specifications/Triggers/TriggerSpecification.cs
+++ src/core/Elsa.Core/Persistence/Specifications/Triggers/TriggerSpecification.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
 using Elsa.Models;
 using Elsa.Services;
@@ -8,16 +10,17 @@ namespace Elsa.Persistence.Specifications.Triggers;
 
 public class TriggerSpecification : Specification<Trigger>
 {
-    public TriggerSpecification(string activityType, string? tenantId)
+    public TriggerSpecification(string activityType, IEnumerable<string> hashes, string? tenantId)
     {
         ActivityType = activityType;
+        Hashes = hashes;
         TenantId = tenantId;
     }
 
-    public string? TenantId { get; set; }
-    public string ActivityType { get; set; }
+    public string? TenantId { get; }
+    public string ActivityType { get; }
+    public IEnumerable<string> Hashes { get; }
 
-    public override Expression<Func<Trigger, bool>> ToExpression() => trigger => trigger.TenantId == TenantId && trigger.ActivityType == ActivityType;
-    
-    public static TriggerSpecification For<T>(string? tenantId = default) where T : IActivity => new(typeof(T).GetSimpleAssemblyQualifiedName(), tenantId);
+    public override Expression<Func<Trigger, bool>> ToExpression() => trigger => trigger.TenantId == TenantId && trigger.ActivityType == ActivityType && Hashes.Contains(trigger.Hash);
+    public static TriggerSpecification For<T>(IEnumerable<string> hashes, string? tenantId = default) where T : IActivity => new(typeof(T).GetSimpleAssemblyQualifiedName(), hashes, tenantId);
 }
--- src/core/Elsa.Core/Services/Triggers/TriggerFinder.cs
+++ src/core/Elsa.Core/Services/Triggers/TriggerFinder.cs
@@ -15,37 +15,35 @@ namespace Elsa.Services.Triggers
     {
         private readonly ITriggerStore _triggerStore;
         private readonly IContentSerializer _contentSerializer;
+        private readonly IBookmarkHasher _bookmarkHasher;
 
-        public TriggerFinder(ITriggerStore triggerStore, IContentSerializer contentSerializer)
+        public TriggerFinder(ITriggerStore triggerStore, IContentSerializer contentSerializer, IBookmarkHasher bookmarkHasher)
         {
             _triggerStore = triggerStore;
             _contentSerializer = contentSerializer;
+            _bookmarkHasher = bookmarkHasher;
         }
 
         public async Task<IEnumerable<TriggerFinderResult>> FindTriggersAsync(string activityType, IEnumerable<IBookmark> filters, string? tenantId, CancellationToken cancellationToken = default)
         {
-            var specification = new TriggerSpecification(activityType, tenantId);
-            var records = await _triggerStore.FindManyAsync(specification, cancellationToken: cancellationToken);
             var filterList = filters as ICollection<IBookmark> ?? filters.ToList();
+            var hashes = filterList.Select(x => _bookmarkHasher.Hash(x)).ToList();
+            var specification = new TriggerSpecification(activityType, hashes, tenantId);
+            var records = await _triggerStore.FindManyAsync(specification, cancellationToken: cancellationToken);
+
             var triggerResults = SelectResults(records);
 
             if (!filterList.Any())
                 return triggerResults;
 
-            var filteredTriggers = new List<TriggerFinderResult>();
+            var query =
+                from triggerFinderResult in triggerResults
+                from filter in filterList
+                let result = triggerFinderResult.Bookmark.Compare(filter)
+                where result == null || result.Value
+                select triggerFinderResult;
 
-            foreach (var triggerFinderResult in triggerResults)
-            {
-                foreach (var filter in filterList)
-                {
-                    var result = triggerFinderResult.Bookmark.Compare(filter);
-
-                    if (result == null || result.Value)
-                        filteredTriggers.Add(triggerFinderResult);
-                }
-            }
-
-            return filteredTriggers;
+            return query;
         }
 
         public async Task<IEnumerable<TriggerFinderResult>> FindTriggersByTypeAsync(string modelType, string? tenantId, CancellationToken cancellationToken = default)
