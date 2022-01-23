0f59620489509928eb2e85bda3ebef835792c7a3
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2022-01-23 20:49:13 +0100

    Fix JSON responses for Save, Publish and Retract endpoints

--- src/server/Elsa.Server.Api/Helpers/SerializationHelper.cs
+++ src/server/Elsa.Server.Api/Helpers/SerializationHelper.cs
@@ -1,14 +1,11 @@
-using Elsa.Models;
 using Elsa.Serialization;
 using Elsa.Server.Api.Converters;
 using Newtonsoft.Json;
 
-namespace Elsa.Server.Api
+namespace Elsa.Server.Api.Helpers
 {
     public static class SerializationHelper
     {
-        public static string SerializeWorkflowDefinition(WorkflowDefinition workflowDefinition) => JsonConvert.SerializeObject(workflowDefinition, GetSettingsForWorkflowDefinition());
-
         public static JsonSerializerSettings GetSettingsForWorkflowDefinition()
         {
             // Here we don't want to use the `PreserveReferencesHandling` setting because the model will be used by the designer "as-is" and will not resolve $id references.
