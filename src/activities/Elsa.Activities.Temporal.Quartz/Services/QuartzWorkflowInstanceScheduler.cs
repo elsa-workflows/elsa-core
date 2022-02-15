f0aa734f776a9585962ca7b0a74a07f945736286
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2022-02-15 21:24:42 +0100

    Update Quartz schedule to fail explicitly
    Failing silently would cause workflows to suddenly stop from being triggered when e.g. scheduling a trigger failed for whatever reason. Better to let the workflow fault.

--- src/activities/Elsa.Activities.Temporal.Quartz/Services/QuartzWorkflowInstanceScheduler.cs
+++ src/activities/Elsa.Activities.Temporal.Quartz/Services/QuartzWorkflowInstanceScheduler.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using Elsa.Activities.Temporal.Common.Services;
 using Elsa.Activities.Temporal.Quartz.Jobs;
+using Elsa.Exceptions;
 using Elsa.Options;
 using Microsoft.Extensions.Logging;
 using NodaTime;
@@ -30,7 +31,7 @@ namespace Elsa.Activities.Temporal.Quartz.Services
         public async Task ScheduleAsync(string workflowInstanceId, string activityId, Instant startAt, Duration? interval, CancellationToken cancellationToken = default)
         {
             var triggerBuilder = CreateTrigger(workflowInstanceId, activityId).StartAt(startAt.ToDateTimeOffset());
-            
+
             if (interval != null && interval != Duration.Zero)
                 triggerBuilder.WithSimpleSchedule(x => x.WithInterval(interval.Value.ToTimeSpan()).RepeatForever());
 
@@ -43,30 +44,30 @@ namespace Elsa.Activities.Temporal.Quartz.Services
             var trigger = CreateTrigger(workflowInstanceId, activityId).WithCronSchedule(cronExpression).Build();
             await ScheduleJob(trigger, cancellationToken);
         }
-        
+
         public async Task UnscheduleAsync(string workflowInstanceId, string activityId, CancellationToken cancellationToken)
         {
-            var scheduler = await _schedulerProvider. GetSchedulerAsync(cancellationToken);
+            var scheduler = await _schedulerProvider.GetSchedulerAsync(cancellationToken);
             var trigger = CreateTriggerKey(workflowInstanceId, activityId);
             var existingTrigger = await scheduler.GetTrigger(trigger, cancellationToken);
 
             if (existingTrigger != null)
                 await scheduler.UnscheduleJob(existingTrigger.Key, cancellationToken);
         }
-        
+
         public async Task UnscheduleAsync(string workflowInstanceId, CancellationToken cancellationToken)
         {
-            var scheduler = await _schedulerProvider. GetSchedulerAsync(cancellationToken);
+            var scheduler = await _schedulerProvider.GetSchedulerAsync(cancellationToken);
             var groupName = CreateTriggerGroupKey(workflowInstanceId);
             var existingTriggers = await scheduler.GetTriggerKeys(GroupMatcher<TriggerKey>.GroupEquals(groupName), cancellationToken);
 
-            foreach (var existingTrigger in existingTriggers) 
+            foreach (var existingTrigger in existingTriggers)
                 await scheduler.UnscheduleJob(existingTrigger, cancellationToken);
         }
 
         public async Task UnscheduleAllAsync(CancellationToken cancellationToken)
         {
-            var scheduler = await _schedulerProvider. GetSchedulerAsync(cancellationToken);
+            var scheduler = await _schedulerProvider.GetSchedulerAsync(cancellationToken);
             var jobKeys = await scheduler.GetJobKeys(GroupMatcher<JobKey>.GroupStartsWith("workflow-instance"), cancellationToken);
             await scheduler.DeleteJobs(jobKeys, cancellationToken);
         }
@@ -75,10 +76,11 @@ namespace Elsa.Activities.Temporal.Quartz.Services
         {
             var scheduler = await _schedulerProvider.GetSchedulerAsync(cancellationToken);
             var sharedResource = $"{nameof(QuartzWorkflowInstanceScheduler)}:{trigger.Key}";
-            await using var handle = await _distributedLockProvider.AcquireLockAsync(sharedResource, _elsaOptions.DistributedLockTimeout, cancellationToken);
+            var distributedLockTimeout = _elsaOptions.DistributedLockTimeout;
+            await using var handle = await _distributedLockProvider.AcquireLockAsync(sharedResource, distributedLockTimeout, cancellationToken);
 
             if (handle == null)
-                return;
+                throw new LockAcquisitionException($"Failed to acquire a distributed lock within the configured amount of time ({distributedLockTimeout})");
 
             try
             {
@@ -86,12 +88,13 @@ namespace Elsa.Activities.Temporal.Quartz.Services
 
                 if (existingTrigger != null)
                     await scheduler.UnscheduleJob(existingTrigger.Key, cancellationToken);
-                
+
                 await scheduler.ScheduleJob(trigger, cancellationToken);
             }
             catch (SchedulerException e)
             {
-                _logger.LogWarning(e, "Failed to schedule trigger {TriggerKey}", trigger.Key.ToString());
+                _logger.LogError(e, "Failed to schedule trigger {TriggerKey}", trigger.Key.ToString());
+                throw;
             }
         }
 
@@ -101,7 +104,7 @@ namespace Elsa.Activities.Temporal.Quartz.Services
                 .WithIdentity(CreateTriggerKey(workflowInstanceId, activityId))
                 .UsingJobData("WorkflowInstanceId", workflowInstanceId!)
                 .UsingJobData("ActivityId", activityId);
-        
+
         private TriggerKey CreateTriggerKey(string workflowInstanceId, string activityId)
         {
             var groupName = CreateTriggerGroupKey(workflowInstanceId);
