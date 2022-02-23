fe0257224e8684f94e7df81acdaab5aeaa149d8f
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2022-02-23 12:42:42 +0100

    Add sample that processes Rebus' error queue

---
+++ src/samples/worker/Elsa.Samples.RebusErrorWorker/ProcessErrorQueue.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Elsa.Serialization;
+using Elsa.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Rebus.Config;
+using Rebus.Messages;
+using Rebus.Routing.TransportMessages;
+using Rebus.Routing.TypeBased;
+using Rebus.Serialization.Json;
+using Rebus.ServiceProvider;
+
+namespace Elsa.Samples.RebusErrorWorker;
+
+public class ProcessErrorQueue : BackgroundService
+{
+    private readonly IServiceProvider _serviceProvider;
+    private readonly IConfiguration _configuration;
+
+    public ProcessErrorQueue(IServiceProvider serviceProvider, IConfiguration configuration)
+    {
+        _serviceProvider = serviceProvider;
+        _configuration = configuration;
+    }
+
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        var handlerActivator = new DependencyInjectionHandlerActivator(_serviceProvider);
+        var configurer = Configure.With(handlerActivator);
+
+        configurer
+            .Serialization(serializer => serializer.UseNewtonsoftJson(DefaultContentSerializer.CreateDefaultJsonSerializationSettings()))
+            .Transport(transport => transport.UseAzureServiceBus(_configuration.GetConnectionString("AzureServiceBus"), "error"))
+            .Routing(r =>
+            {
+                r.AddTransportMessageForwarder(transportMessage =>
+                {
+                    var returnAddress = transportMessage.Headers[Headers.SourceQueue];
+
+                    if (returnAddress.Contains("workflow-management-events"))
+                        return Task.FromResult(ForwardAction.Ignore());
+
+                    return Task.FromResult(ForwardAction.ForwardTo(returnAddress));
+                });
+            });
+
+        var bus = configurer.Start();
+
+        return Task.CompletedTask;
+    }
+}
