00f350445c97266cc95cd1f7a7e935f0d10fcb91
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2022-02-24 12:07:35 +0100

    Add sample project showing If activity

---
+++ src/samples/console/Elsa.Samples.IfThenElse/IfThenElseWorkflow.cs
@@ -0,0 +1,27 @@
+﻿using Elsa.Activities.Console;
+using Elsa.Activities.ControlFlow;
+using Elsa.Activities.Primitives;
+using Elsa.Builders;
+
+namespace Elsa.Samples.IfThenElse
+{
+    /// <summary>
+    /// This workflow prompts the user to enter their age. Depending on the value, the user is granted access to an alcoholic beverage.
+    /// </summary>
+    public class IfThenElseWorkflow : IWorkflow
+    {
+        public void Build(IWorkflowBuilder builder)
+        {
+            builder
+                .WriteLine("Please enter your age:")
+                .ReadLine()
+                .SetVariable("Age", context => context.GetInput<int>())
+                .If(context => context.GetVariable<int>("Age") > 21, @if =>
+                {
+                    @if.When(OutcomeNames.True).WriteLine("Enjoy your alcoholic beverage!");
+                    @if.When(OutcomeNames.False).WriteLine("Enjoy your coca cola!");
+                })
+                .WriteLine("Cheers!");
+        }
+    }
+}
