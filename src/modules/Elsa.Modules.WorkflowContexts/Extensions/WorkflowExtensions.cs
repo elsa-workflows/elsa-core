f651c74f4414e90e6a3020bd4278b8fae4be9952
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2022-03-08 19:48:22 +0100

    Update WorkflowExtensions.cs

--- src/modules/Elsa.Modules.WorkflowContexts/Extensions/WorkflowExtensions.cs
+++ src/modules/Elsa.Modules.WorkflowContexts/Extensions/WorkflowExtensions.cs
@@ -10,13 +10,30 @@ public static class WorkflowExtensions
     /// <summary>
     /// Installs the specified workflow context provider type into the specified workflow.
     /// </summary>
-    public static IWorkflowDefinitionBuilder AddWorkflowContext<T, TProvider>(this IWorkflowDefinitionBuilder workflow, WorkflowContext<T, TProvider> workflowContext) where TProvider : IWorkflowContextProvider
+    public static WorkflowContext<T, TProvider> CreateWorkflowContext<T, TProvider>(this IWorkflowDefinitionBuilder workflow) where TProvider : IWorkflowContextProvider
+    {
+        var workflowContext = new WorkflowContext<T, TProvider>();
+        workflow.AddWorkflowContext(workflowContext);
+        return workflowContext;
+    }
+
+    /// <summary>
+    /// Installs the specified workflow context provider type into the specified workflow.
+    /// </summary>
+    public static IWorkflowDefinitionBuilder AddWorkflowContext(this IWorkflowDefinitionBuilder workflow, WorkflowContext workflowContext)
     {
         var providerTypes = workflow.ApplicationProperties!.GetOrAdd("Elsa:WorkflowContexts", () => new List<WorkflowContext>());
 
         providerTypes.Add(workflowContext);
         return workflow;
     }
-    
-    
+
+    /// <summary>
+    /// Installs the specified workflow context provider type into the specified workflow.
+    /// </summary>
+    public static IWorkflowDefinitionBuilder AddWorkflowContexts(this IWorkflowDefinitionBuilder workflow, params WorkflowContext[] workflowContexts)
+    {
+        foreach (var workflowContext in workflowContexts) workflow.AddWorkflowContext(workflowContext);
+        return workflow;
+    }
 }
