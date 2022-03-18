a719f5243201ab55da640a1b1c37e3daf378b570
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2022-03-18 15:03:22 +0100

    Revert "Update MQTT and RabbitMQ consumers"
    This reverts commit d1fc93386f9ba4d34d0408a02972fbac471fc816.

--- src/activities/Elsa.Activities.Mqtt/Consumers/RestartMqttTopicsConsumer.cs
+++ src/activities/Elsa.Activities.Mqtt/Consumers/RestartMqttTopicsConsumer.cs
@@ -1,5 +1,4 @@
-﻿using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Elsa.Activities.Mqtt.Services;
 using Elsa.Events;
 using Rebus.Handlers;
@@ -11,8 +10,8 @@ namespace Elsa.Activities.Mqtt.Consumers
         private readonly IMqttTopicsStarter _mqttTopicsStarter;
         public RestartMqttTopicsConsumer(IMqttTopicsStarter mqttTopicsStarter) => _mqttTopicsStarter = mqttTopicsStarter;
         public Task Handle(TriggerIndexingFinished message) => _mqttTopicsStarter.CreateWorkersAsync();
-        public Task Handle(TriggersDeleted message) => _mqttTopicsStarter.RemoveWorkersAsync(message.Triggers);
+        public Task Handle(TriggersDeleted message) => _mqttTopicsStarter.CreateWorkersAsync();
         public Task Handle(BookmarkIndexingFinished message) => _mqttTopicsStarter.CreateWorkersAsync();
-        public async Task Handle(BookmarksDeleted message) => await _mqttTopicsStarter.RemoveWorkersAsync(message.Bookmarks, ServiceProvider);
+        public Task Handle(BookmarksDeleted message) => _mqttTopicsStarter.CreateWorkersAsync();
     }
 }
--- src/activities/Elsa.Activities.RabbitMq/Consumers/RestartRabbitMqBusConsumer.cs
+++ src/activities/Elsa.Activities.RabbitMq/Consumers/RestartRabbitMqBusConsumer.cs
@@ -10,8 +10,8 @@ namespace Elsa.Activities.RabbitMq.Consumers
         private readonly IRabbitMqQueueStarter _rabbitMqQueueStarter;
         public RestartRabbitMqBusConsumer(IRabbitMqQueueStarter rabbitMqQueueStarter) => _rabbitMqQueueStarter = rabbitMqQueueStarter;
         public Task Handle(TriggerIndexingFinished message) => _rabbitMqQueueStarter.CreateWorkersAsync();
-        public Task Handle(TriggersDeleted message) => _rabbitMqQueueStarter.RemoveWorkersAsync(message.Triggers);
+        public Task Handle(TriggersDeleted message) => _rabbitMqQueueStarter.CreateWorkersAsync();
         public Task Handle(BookmarkIndexingFinished message) => _rabbitMqQueueStarter.CreateWorkersAsync();
-        public async Task Handle(BookmarksDeleted message) => await _rabbitMqQueueStarter.RemoveWorkersAsync(message.Bookmarks);
+        public Task Handle(BookmarksDeleted message) => _rabbitMqQueueStarter.CreateWorkersAsync();
     }
 }
