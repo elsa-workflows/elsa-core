d1fc93386f9ba4d34d0408a02972fbac471fc816
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2022-03-18 14:56:50 +0100

    Update MQTT and RabbitMQ consumers

--- src/activities/Elsa.Activities.Mqtt/Consumers/RestartMqttTopicsConsumer.cs
+++ src/activities/Elsa.Activities.Mqtt/Consumers/RestartMqttTopicsConsumer.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Threading.Tasks;
 using Elsa.Activities.Mqtt.Services;
 using Elsa.Events;
 using Rebus.Handlers;
@@ -10,8 +11,8 @@ namespace Elsa.Activities.Mqtt.Consumers
         private readonly IMqttTopicsStarter _mqttTopicsStarter;
         public RestartMqttTopicsConsumer(IMqttTopicsStarter mqttTopicsStarter) => _mqttTopicsStarter = mqttTopicsStarter;
         public Task Handle(TriggerIndexingFinished message) => _mqttTopicsStarter.CreateWorkersAsync();
-        public Task Handle(TriggersDeleted message) => _mqttTopicsStarter.CreateWorkersAsync();
+        public Task Handle(TriggersDeleted message) => _mqttTopicsStarter.RemoveWorkersAsync(message.Triggers);
         public Task Handle(BookmarkIndexingFinished message) => _mqttTopicsStarter.CreateWorkersAsync();
-        public Task Handle(BookmarksDeleted message) => _mqttTopicsStarter.CreateWorkersAsync();
+        public async Task Handle(BookmarksDeleted message) => await _mqttTopicsStarter.RemoveWorkersAsync(message.Bookmarks, ServiceProvider);
     }
 }
