b9f0521077ca31f4353ebd2bcc000f369a9a2c1a
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2022-04-04 16:00:07 +0200

    Update QueueDefinition.cs

--- src/modules/Elsa.Modules.AzureServiceBus/Models/QueueDefinition.cs
+++ src/modules/Elsa.Modules.AzureServiceBus/Models/QueueDefinition.cs
@@ -3,7 +3,4 @@ namespace Elsa.Modules.AzureServiceBus.Models;
 /// <summary>
 /// Represents a queue that is available to the system.
 /// </summary>
-public class QueueDefinition
-{
-    public string Name { get; set; } = default!;
-}
+public record QueueDefinition(string Name);
