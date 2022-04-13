64058c11d46e26c4d0aaba632eeccad5f68fc778
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2022-04-13 20:07:17 +0200

    Update WorkflowRegistryExtensions.cs

--- src/core/Elsa.Core/Extensions/WorkflowRegistryExtensions.cs
+++ src/core/Elsa.Core/Extensions/WorkflowRegistryExtensions.cs
@@ -1,5 +1,6 @@
 using System.Threading;
 using System.Threading.Tasks;
+using Elsa.Builders;
 using Elsa.Models;
 using Elsa.Services;
 using Elsa.Services.Models;
@@ -11,12 +12,12 @@ namespace Elsa
         public static Task<IWorkflowBlueprint?> GetWorkflowAsync<T>(
             this IWorkflowRegistry workflowRegistry,
             string? tenantId,
-            CancellationToken cancellationToken = default) =>
+            CancellationToken cancellationToken = default) where T : IWorkflow =>
             workflowRegistry.FindAsync(typeof(T).Name, VersionOptions.Latest, tenantId, cancellationToken);
 
         public static Task<IWorkflowBlueprint?> GetWorkflowAsync<T>(
             this IWorkflowRegistry workflowRegistry,
-            CancellationToken cancellationToken = default) =>
+            CancellationToken cancellationToken = default) where T : IWorkflow =>
             workflowRegistry.GetWorkflowAsync<T>(default, cancellationToken);
 
         public static Task<IWorkflowBlueprint?> GetWorkflowAsync(
