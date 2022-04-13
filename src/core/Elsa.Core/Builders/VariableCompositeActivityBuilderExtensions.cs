1865b8c3c3f5616827db5e386b171c9e52c1b24a
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2022-04-13 20:07:13 +0200

    Update VariableCompositeActivityBuilderExtensions.cs

--- src/core/Elsa.Core/Builders/VariableCompositeActivityBuilderExtensions.cs
+++ src/core/Elsa.Core/Builders/VariableCompositeActivityBuilderExtensions.cs
@@ -1,26 +1,65 @@
 using System;
 using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
 using Elsa.Activities.Primitives;
+using Elsa.Services.Models;
 
 // ReSharper disable ExplicitCallerInfoArgument
 namespace Elsa.Builders
 {
     public static class VariableCompositeActivityBuilderExtensions
     {
-        public static IActivityBuilder SetVariable(this ICompositeActivityBuilder builder, string variableName, object? value, [CallerLineNumber] int lineNumber = default, [CallerFilePath] string? sourceFile = default) =>
+        public static IActivityBuilder SetVariable<T>(
+            this ICompositeActivityBuilder builder,
+            string variableName,
+            Func<ActivityExecutionContext, ValueTask<object?>> value,
+            [CallerLineNumber] int lineNumber = default,
+            [CallerFilePath] string? sourceFile = default) =>
             builder.StartWith<SetVariable>(
                 activity =>
                 {
-                    activity.Set(x => x.VariableName, _ => variableName);
-                    activity.Set(x => x.Value, _ => value);
+                    activity.WithVariableName(variableName);
+                    activity.WithValue(value);
                 }, null, lineNumber, sourceFile);
 
-        public static IActivityBuilder SetVariable(
+
+        public static IActivityBuilder SetVariable<T>(
+            this ICompositeActivityBuilder builder,
+            string variableName,
+            Func<ActivityExecutionContext, object?> value,
+            [CallerLineNumber] int lineNumber = default,
+            [CallerFilePath] string? sourceFile = default) =>
+            builder.StartWith<SetVariable>(
+                activity =>
+                {
+                    activity.WithVariableName(variableName);
+                    activity.WithValue(value);
+                }, null, lineNumber, sourceFile);
+
+        public static IActivityBuilder SetVariable<T>(
             this ICompositeActivityBuilder builder,
             string variableName,
             Func<object?> value,
             [CallerLineNumber] int lineNumber = default,
             [CallerFilePath] string? sourceFile = default) =>
-            builder.SetVariable(variableName, value(), lineNumber, sourceFile);
+            builder.StartWith<SetVariable>(
+                activity =>
+                {
+                    activity.WithVariableName(variableName);
+                    activity.WithValue(value);
+                }, null, lineNumber, sourceFile);
+
+        public static IActivityBuilder SetVariable<T>(
+            this ICompositeActivityBuilder builder,
+            string variableName,
+            object? value,
+            [CallerLineNumber] int lineNumber = default,
+            [CallerFilePath] string? sourceFile = default) =>
+            builder.StartWith<SetVariable>(
+                activity =>
+                {
+                    activity.WithVariableName(variableName);
+                    activity.WithValue(value);
+                }, null, lineNumber, sourceFile);
     }
 }
