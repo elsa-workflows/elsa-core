5b91ffd08cad1a396ce3b86d6168b9efc5e1942a
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2022-04-13 20:04:25 +0200

    Add QueueName property to SendRebusMessage activity

--- src/activities/Elsa.Activities.Rebus/Activities/SendRebusMessage.cs
+++ src/activities/Elsa.Activities.Rebus/Activities/SendRebusMessage.cs
@@ -7,28 +7,31 @@ using Elsa.Expressions;
 using Elsa.Services;
 using Elsa.Services.Models;
 
-namespace Elsa.Activities.Rebus
+namespace Elsa.Activities.Rebus;
+
+[Action(Category = "Rebus", Description = "Publishes a message.", Outcomes = new[] { OutcomeNames.Done })]
+public class SendRebusMessage : Activity
 {
-    [Action(Category = "Rebus", Description = "Publishes a message.", Outcomes = new[] { OutcomeNames.Done })]
-    public class SendRebusMessage : Activity
+    private readonly ICommandSender _bus;
+
+    public SendRebusMessage(ICommandSender bus)
     {
-        private readonly ICommandSender _bus;
+        _bus = bus;
+    }
 
-        public SendRebusMessage(ICommandSender bus)
-        {
-            _bus = bus;
-        }
+    [ActivityInput(Hint = "The message to send.", SupportedSyntaxes = new[] { SyntaxNames.JavaScript, SyntaxNames.Liquid })]
+    public object Message { get; set; } = default!;
 
-        [ActivityInput(Hint = "The message to send.", SupportedSyntaxes = new[] { SyntaxNames.JavaScript, SyntaxNames.Liquid })]
-        public object Message { get; set; } = default!;
+    [ActivityInput(Hint = "Optional: the queue name to send the message to.", SupportedSyntaxes = new[] { SyntaxNames.JavaScript, SyntaxNames.Liquid })]
+    public string? QueueName { get; set; } = default!;
 
-        [ActivityInput(Hint = "Optional headers to send along with the message.", UIHint = ActivityInputUIHints.Json)]
-        public IDictionary<string, string>? Headers { get; set; }
+    [ActivityInput(Hint = "Optional headers to send along with the message.", UIHint = ActivityInputUIHints.Json)]
+    public IDictionary<string, string>? Headers { get; set; }
 
-        protected override async ValueTask<IActivityExecutionResult> OnExecuteAsync(ActivityExecutionContext context)
-        {
-            await _bus.SendAsync(Message, default, Headers);
-            return Done();
-        }
+    protected override async ValueTask<IActivityExecutionResult> OnExecuteAsync(ActivityExecutionContext context)
+    {
+        var queueName = string.IsNullOrWhiteSpace(QueueName) ? default : QueueName;
+        await _bus.SendAsync(Message, queueName, Headers);
+        return Done();
     }
 }
