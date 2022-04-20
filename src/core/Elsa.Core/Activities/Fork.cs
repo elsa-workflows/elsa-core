aa7d8f9e3ba0e83cd5e6978a1d3ff4d9a7492e05
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2022-04-20 21:38:13 +0200

    Update Fork.cs

--- src/core/Elsa.Core/Activities/Fork.cs
+++ src/core/Elsa.Core/Activities/Fork.cs
@@ -1,5 +1,6 @@
 using System.Collections.Immutable;
 using Elsa.Attributes;
+using Elsa.Behaviors;
 using Elsa.Contracts;
 using Elsa.Models;
 
@@ -8,25 +9,26 @@ namespace Elsa.Activities;
 [Activity("Elsa", "Control Flow", "Branch execution into multiple branches.")]
 public class Fork : Activity
 {
+    public Fork()
+    {
+        Behaviors.Remove<AutoCompleteBehavior>();
+    }
+
     /// <summary>
     /// Controls when this activity yields control back to its parent activity.
     /// </summary>
-    [Input] public JoinMode JoinMode { get; set; } = JoinMode.WaitAny;
-    
+    [Input]
+    public JoinMode JoinMode { get; set; } = JoinMode.WaitAny;
+
     /// <summary>
     /// The branches to schedule.
     /// </summary>
-    [Outbound] public ICollection<IActivity> Branches { get; set; } = new List<IActivity>();
+    [Outbound]
+    public ICollection<IActivity> Branches { get; set; } = new List<IActivity>();
 
     protected override void Execute(ActivityExecutionContext context) => context.PostActivities(Branches.Reverse(), CompleteChildAsync);
 
-    private ValueTask CompleteChildAsync(ActivityExecutionContext context, ActivityExecutionContext childContext)
-    {
-        OnChildCompleted(context, childContext);
-        return ValueTask.CompletedTask;
-    }
-
-    private void OnChildCompleted(ActivityExecutionContext context, ActivityExecutionContext childContext)
+    private async ValueTask CompleteChildAsync(ActivityExecutionContext context, ActivityExecutionContext childContext)
     {
         var completedChildActivityId = childContext.Activity.Id;
 
@@ -47,22 +49,27 @@ public class Fork : Activity
             {
                 // Remove any and all bookmarks from other branches.
                 RemoveBookmarks(context);
+
+                // Signal activity completion.
+                await CompleteAsync(context);
             }
                 break;
             case JoinMode.WaitAll:
             {
                 var allSet = allChildActivityIds.All(x => completedActivityIds.Contains(x));
 
-                if (!allSet)
-                    context.PreventContinuation();
+                if (allSet)
+                    // Signal activity completion.
+                    await CompleteAsync(context);
             }
                 break;
         }
     }
 
+
     private void RemoveBookmarks(ActivityExecutionContext context)
     {
-        // Find all descendants for each branch and remove any associated bookmarks.
+        // Find all descendants for each branch and remove them as well as any associated bookmarks.
         var workflowExecutionContext = context.WorkflowExecutionContext;
         var forkNode = context.ActivityNode;
         var branchNodes = forkNode.Children;
