65ee8aae605b85f54cbd9aa3a9074326286ddac6
Mohamed Ali <antivirus.cs@gmail.com>
2022-05-21 07:27:15 +0300

    use the recommended mutex syntax

--- src/designer/elsa-workflows-studio/src/components/controls/elsa-monaco/elsa-monaco-utils.ts
+++ src/designer/elsa-workflows-studio/src/components/controls/elsa-monaco/elsa-monaco-utils.ts
@@ -14,100 +14,101 @@ export interface EditorVariable {
   type: string;
 }
 
-export var editorVariables : Array<EditorVariable> = [];
+export var editorVariables: Array<EditorVariable> = [];
 
-let isInitialized : boolean;
+let isInitialized: boolean;
 const mutex = new Mutex();
 
 
 
 export async function initializeMonacoWorker(libPath?: string): Promise<Monaco> {
-  
-  const release = await mutex.acquire();
-  
-  if (isInitialized) {
-    release();
-    return win.monaco;
-  }
 
-  const origin = document.location.origin;
-  const baseUrl = libPath.startsWith('http') ? libPath : `${origin}/${libPath}`;
+  return await mutex.runExclusive(async () => {
 
-  require.config({paths: {'vs': `${baseUrl}/vs`}});
-  win.MonacoEnvironment = {getWorkerUrl: () => proxy};
+    if (isInitialized) {
 
-  let proxy = URL.createObjectURL(new Blob([`
+      return win.monaco;
+    }
+
+    const origin = document.location.origin;
+    const baseUrl = libPath.startsWith('http') ? libPath : `${origin}/${libPath}`;
+
+    require.config({ paths: { 'vs': `${baseUrl}/vs` } });
+    win.MonacoEnvironment = { getWorkerUrl: () => proxy };
+
+    let proxy = URL.createObjectURL(new Blob([`
 	self.MonacoEnvironment = {
 		baseUrl: '${baseUrl}'
 	};
 	importScripts('${baseUrl}/vs/base/worker/workerMain.js');
-`], {type: 'text/javascript'}));
+`], { type: 'text/javascript' }));
 
-  return new Promise(resolve => {
-    require(["vs/editor/editor.main"], () => {
-      isInitialized = true;
-      release();
-      registerLiquid(win.monaco);
-      registerSQL(win.monaco);
-      resolve(win.monaco);
+    return new Promise(resolve => {
+      require(["vs/editor/editor.main"], () => {
+        isInitialized = true;
+        registerLiquid(win.monaco);
+        registerSQL(win.monaco);
+        resolve(win.monaco);
+      });
     });
   });
 
 
-  function registerLiquid(monaco: any) {
-    monaco.languages.register({ id: 'liquid' });
+}
 
-    monaco.languages.registerCompletionItemProvider('liquid', {
-      provideCompletionItems: () => {
-        const autocompleteProviderItems = [];
-        const keywords = ['assign', 'capture', 'endcapture', 'increment', 'decrement',
-          'if', 'else', 'elsif', 'endif', 'for', 'endfor', 'break',
-          'continue', 'limit', 'offset', 'range', 'reversed', 'cols',
-          'case', 'endcase', 'when', 'block', 'endblock', 'true', 'false',
-          'in', 'unless', 'endunless', 'cycle', 'tablerow', 'endtablerow',
-          'contains', 'startswith', 'endswith', 'comment', 'endcomment',
-          'raw', 'endraw', 'editable', 'endentitylist', 'endentityview', 'endinclude',
-          'endmarker', 'entitylist', 'entityview', 'forloop', 'image', 'include',
-          'marker', 'outputcache', 'plugin', 'style', 'text', 'widget',
-          'abs', 'append', 'at_least', 'at_most', 'capitalize', 'ceil', 'compact',
-          'concat', 'date', 'default', 'divided_by', 'downcase', 'escape',
-          'escape_once', 'first', 'floor', 'join', 'last', 'lstrip', 'map',
-          'minus', 'modulo', 'newline_to_br', 'plus', 'prepend', 'remove',
-          'remove_first', 'replace', 'replace_first', 'reverse', 'round',
-          'rstrip', 'size', 'slice', 'sort', 'sort_natural', 'split', 'strip',
-          'strip_html', 'strip_newlines', 'times', 'truncate', 'truncatewords',
-          'uniq', 'upcase', 'url_decode', 'url_encode'];
+function registerLiquid(monaco: any) {
+  monaco.languages.register({ id: 'liquid' });
 
-        for (let i = 0; i < keywords.length; i++) {
-          autocompleteProviderItems.push({ 'label': keywords[i], kind: monaco.languages.CompletionItemKind.Keyword });
-        }
+  monaco.languages.registerCompletionItemProvider('liquid', {
+    provideCompletionItems: () => {
+      const autocompleteProviderItems = [];
+      const keywords = ['assign', 'capture', 'endcapture', 'increment', 'decrement',
+        'if', 'else', 'elsif', 'endif', 'for', 'endfor', 'break',
+        'continue', 'limit', 'offset', 'range', 'reversed', 'cols',
+        'case', 'endcase', 'when', 'block', 'endblock', 'true', 'false',
+        'in', 'unless', 'endunless', 'cycle', 'tablerow', 'endtablerow',
+        'contains', 'startswith', 'endswith', 'comment', 'endcomment',
+        'raw', 'endraw', 'editable', 'endentitylist', 'endentityview', 'endinclude',
+        'endmarker', 'entitylist', 'entityview', 'forloop', 'image', 'include',
+        'marker', 'outputcache', 'plugin', 'style', 'text', 'widget',
+        'abs', 'append', 'at_least', 'at_most', 'capitalize', 'ceil', 'compact',
+        'concat', 'date', 'default', 'divided_by', 'downcase', 'escape',
+        'escape_once', 'first', 'floor', 'join', 'last', 'lstrip', 'map',
+        'minus', 'modulo', 'newline_to_br', 'plus', 'prepend', 'remove',
+        'remove_first', 'replace', 'replace_first', 'reverse', 'round',
+        'rstrip', 'size', 'slice', 'sort', 'sort_natural', 'split', 'strip',
+        'strip_html', 'strip_newlines', 'times', 'truncate', 'truncatewords',
+        'uniq', 'upcase', 'url_decode', 'url_encode'];
 
-        return { suggestions: autocompleteProviderItems };
+      for (let i = 0; i < keywords.length; i++) {
+        autocompleteProviderItems.push({ 'label': keywords[i], kind: monaco.languages.CompletionItemKind.Keyword });
       }
-    });
-  }
 
-  function registerSQL(monaco:any) {
+      return { suggestions: autocompleteProviderItems };
+    }
+  });
+}
 
-    monaco.languages.registerCompletionItemProvider('sql', {
-      triggerCharacters: ["@"],
-      provideCompletionItems: (model, position) => {
+function registerSQL(monaco: any) {
 
-        const word = model.getWordUntilPosition(position)
+  monaco.languages.registerCompletionItemProvider('sql', {
+    triggerCharacters: ["@"],
+    provideCompletionItems: (model, position) => {
 
-        const autocompleteProviderItems = [];
-          for(const varible of editorVariables) {
-            autocompleteProviderItems.push({
-              label: `${varible.variableName}: ${varible.type}`,
-              kind: monaco.languages.CompletionItemKind.Keyword,
-              insertText: varible.variableName
-            });
-          }
+      const word = model.getWordUntilPosition(position)
 
-        return { suggestions: autocompleteProviderItems };
+      const autocompleteProviderItems = [];
+      for (const varible of editorVariables) {
+        autocompleteProviderItems.push({
+          label: `${varible.variableName}: ${varible.type}`,
+          kind: monaco.languages.CompletionItemKind.Variable,
+          insertText: varible.variableName
+        });
       }
-    });
 
-  }
+      return { suggestions: autocompleteProviderItems };
+    }
+  });
+
 }
 
