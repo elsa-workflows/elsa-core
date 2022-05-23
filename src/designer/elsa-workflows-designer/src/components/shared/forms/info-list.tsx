88fcc461483dccd21f9052d1981131f04021f286
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2022-05-23 19:29:38 +0200

    Implement hover effect on info list values

--- src/designer/elsa-workflows-designer/src/components/shared/forms/info-list.tsx
+++ src/designer/elsa-workflows-designer/src/components/shared/forms/info-list.tsx
@@ -1,5 +1,5 @@
-import { FunctionalComponent, h } from '@stencil/core';
-import { copyTextToClipboard } from '../../../utils';
+import {FunctionalComponent, h} from '@stencil/core';
+import {copyTextToClipboard} from '../../../utils';
 
 export interface InfoListProps {
   title: string;
@@ -21,21 +21,23 @@ export const InfoList: FunctionalComponent<InfoListProps> = ({title, dictionary}
                   <dt class="text-sm font-medium text-gray-500">{k}</dt>
                   <dd class="flex justify-between items-center mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
                     {v}
-                    <svg
-                      onClick={() => copyTextToClipboard(v as string)}
-                      class="cursor-pointer inline-block ml-2 h-6 w-6 text-blue-500"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      stroke-width="2"
-                      stroke="currentColor"
-                      fill="none"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    >
-                      <path stroke="none" d="M0 0h24v24H0z" /> <rect x="8" y="8" width="12" height="12" rx="2" />
-                      <path d="M16 8v-2a2 2 0 0 0 -2 -2h-8a2 2 0 0 0 -2 2v8a2 2 0 0 0 2 2h2" />
-                    </svg>
+                    <a href="#" class="ml-2 h-6 w-6 inline-block text-blue-500 hover:text-blue-300" title="Copy value">
+                      <svg
+                        onClick={() => copyTextToClipboard(v as string)}
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        stroke-width="2"
+                        stroke="currentColor"
+                        fill="none"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      >
+                        <path stroke="none" d="M0 0h24v24H0z"/>
+                        <rect x="8" y="8" width="12" height="12" rx="2"/>
+                        <path d="M16 8v-2a2 2 0 0 0 -2 -2h-8a2 2 0 0 0 -2 2v8a2 2 0 0 0 2 2h2"/>
+                      </svg>
+                    </a>
                   </dd>
                 </div>
               ))}
