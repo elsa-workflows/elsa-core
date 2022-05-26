01210932f9ca686806140228c4840b9854c84aaf
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2022-05-26 16:08:57 +0200

    Fix pagination / total count

--- src/common/Elsa.Persistence.EntityFrameworkCore.Common/Extensions/QueryableExtensions.cs
+++ src/common/Elsa.Persistence.EntityFrameworkCore.Common/Extensions/QueryableExtensions.cs
@@ -26,20 +26,18 @@ public static class QueryableExtensions
     
     public static async Task<Page<TTarget>> PaginateAsync<T, TTarget>(this IQueryable<T> queryable, Expression<Func<T, TTarget>> projection, PageArgs? pageArgs = default)
     {
+        var count = await queryable.CountAsync();
         if (pageArgs?.Offset != null) queryable = queryable.Skip(pageArgs.Offset.Value);
         if (pageArgs?.Limit != null) queryable = queryable.Take(pageArgs.Limit.Value);
-        
-        var count = await queryable.CountAsync();
         var results = await queryable.Select(projection).ToListAsync();
         return Page.Of(results, count);
     }
     
     public static async Task<Page<T>> PaginateAsync<T>(this IQueryable<T> queryable, PageArgs? pageArgs = default)
     {
+        var count = await queryable.CountAsync();
         if (pageArgs?.Offset != null) queryable = queryable.Skip(pageArgs.Offset.Value);
         if (pageArgs?.Limit != null) queryable = queryable.Take(pageArgs.Limit.Value);
-        
-        var count = await queryable.CountAsync();
         var results = await queryable.ToListAsync();
         return Page.Of(results, count);
     }
