4b88892befa48082cbd3a5083013ed8eff4eb6ee
Ivan Hrynevytskyi <ivan.hrynevytskyi@IHRYNEVYTSKYIL.local>
2022-06-08 16:59:50 +0300

    fixed incorrect positioning on zoom or graph move

--- src/designer/elsa-workflows-designer/src/components/designer/workflow-definition-editor/workflow-definition-editor.tsx
+++ src/designer/elsa-workflows-designer/src/components/designer/workflow-definition-editor/workflow-definition-editor.tsx
@@ -270,8 +270,8 @@ export class WorkflowDefinitionEditor {
     await this.canvas.addActivity({
       descriptor: activityDescriptor,
       id: newName,
-      x: e.offsetX,
-      y: e.offsetY
+      x: e.pageX,
+      y: e.pageY
     });
   };
 
