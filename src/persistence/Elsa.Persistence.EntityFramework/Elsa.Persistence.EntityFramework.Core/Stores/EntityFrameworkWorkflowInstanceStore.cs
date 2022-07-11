fab7a089b4b0edef9982eae1251199f0b93ab277
Rubén M <ruben.m@smartwaystudio.com>
2022-07-11 19:38:08 +0200

    Restore Fault property and mark it as obsolete. Patched it for less impact.

--- src/persistence/Elsa.Persistence.EntityFramework/Elsa.Persistence.EntityFramework.Core/Stores/EntityFrameworkWorkflowInstanceStore.cs
+++ src/persistence/Elsa.Persistence.EntityFramework/Elsa.Persistence.EntityFramework.Core/Stores/EntityFrameworkWorkflowInstanceStore.cs
@@ -79,6 +79,7 @@ namespace Elsa.Persistence.EntityFramework.Core.Stores
                 entity.BlockingActivities,
                 entity.ScheduledActivities,
                 entity.Scopes,
+                entity.Fault,
                 entity.Faults,
                 entity.CurrentActivity
             };
@@ -100,6 +101,7 @@ namespace Elsa.Persistence.EntityFramework.Core.Stores
                 entity.BlockingActivities,
                 entity.ScheduledActivities,
                 entity.Scopes,
+                entity.Fault,
                 entity.Faults,
                 entity.CurrentActivity
             };
@@ -126,6 +128,7 @@ namespace Elsa.Persistence.EntityFramework.Core.Stores
             entity.BlockingActivities = data.BlockingActivities;
             entity.ScheduledActivities = data.ScheduledActivities;
             entity.Scopes = data.Scopes;
+            entity.Fault = data.Fault;
             entity.Faults = data.Faults;
             entity.CurrentActivity = data.CurrentActivity;
         }
