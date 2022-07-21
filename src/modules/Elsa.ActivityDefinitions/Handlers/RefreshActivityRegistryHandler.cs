bf6ba666ea72ffc45a294af0f49cdc7a64a257c5
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2022-07-21 14:54:36 +0200

    Add activity definition referenced version update handler

--- src/modules/Elsa.ActivityDefinitions/Handlers/RefreshActivityRegistryHandler.cs
+++ src/modules/Elsa.ActivityDefinitions/Handlers/RefreshActivityRegistryHandler.cs
@@ -7,19 +7,19 @@ using Elsa.Workflows.Management.Services;
 namespace Elsa.ActivityDefinitions.Handlers;
 
 /// <summary>
-/// Refreshes the <see cref="IActivityRegistry"/> for the <see cref="ActivityDefinitionActivityProvider"/> provider whenever an <see cref="ActivityDefinition"/> is published.
+/// Refreshes the <see cref="IActivityRegistry"/> for the <see cref="ActivityDefinitionActivityProvider"/> provider whenever an <see cref="ActivityDefinition"/> is published, retracted or deleted.
 /// </summary>
-public class RefreshActivityRegistryHandler : INotificationHandler<ActivityDefinitionPublished>
+public class RefreshActivityRegistryHandler :
+    INotificationHandler<ActivityDefinitionPublished>,
+    INotificationHandler<ActivityDefinitionRetracted>,
+    INotificationHandler<ActivityDefinitionDeleted>,
+    INotificationHandler<ActivityDefinitionsDeleted>
 {
     private readonly IActivityRegistryPopulator _activityRegistryPopulator;
-
-    public RefreshActivityRegistryHandler(IActivityRegistryPopulator activityRegistryPopulator)
-    {
-        _activityRegistryPopulator = activityRegistryPopulator;
-    }
-    
-    public async Task HandleAsync(ActivityDefinitionPublished notification, CancellationToken cancellationToken)
-    {
-        await _activityRegistryPopulator.PopulateRegistryAsync(typeof(ActivityDefinitionActivityProvider), cancellationToken);
-    }
+    public RefreshActivityRegistryHandler(IActivityRegistryPopulator activityRegistryPopulator) => _activityRegistryPopulator = activityRegistryPopulator;
+    public async Task HandleAsync(ActivityDefinitionPublished notification, CancellationToken cancellationToken) => await RefreshAsync(cancellationToken);
+    public async Task HandleAsync(ActivityDefinitionRetracted notification, CancellationToken cancellationToken) => await RefreshAsync(cancellationToken);
+    public async Task HandleAsync(ActivityDefinitionDeleted notification, CancellationToken cancellationToken) => await RefreshAsync(cancellationToken);
+    public async Task HandleAsync(ActivityDefinitionsDeleted notification, CancellationToken cancellationToken) => await RefreshAsync(cancellationToken);
+    private async Task RefreshAsync(CancellationToken cancellationToken) => await _activityRegistryPopulator.PopulateRegistryAsync(typeof(ActivityDefinitionActivityProvider), cancellationToken);
 }
