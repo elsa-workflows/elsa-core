d4fbcef61d06d2199bb785f42cfc89d30974e7cc
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2022-08-14 19:36:24 +0200

    Update migrations

--- src/modules/Elsa.Workflows.Persistence.EntityFrameworkCore.Sqlite/Migrations/20220814124730_Initial.Designer.cs
+++ src/modules/Elsa.Workflows.Persistence.EntityFrameworkCore.Sqlite/Migrations/20220814124730_Initial.Designer.cs
@@ -11,7 +11,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Elsa.Workflows.Persistence.EntityFrameworkCore.Sqlite.Migrations
 {
     [DbContext(typeof(WorkflowsDbContext))]
-    [Migration("20220811151755_Initial")]
+    [Migration("20220814124730_Initial")]
     partial class Initial
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
