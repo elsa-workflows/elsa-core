f66b6531ec754b2752294be1fc1afc8554e4ad0f
cristinamudura <cristina.mudura@nexxbiz.io>
2022-08-15 20:51:49 +0200

    Added proto actor implementation for kubernetes and refactored relevant projects to contain the only needed classes.

---
+++ src/modules/Elsa.ProtoActor.Kubernetes/DependencyInjectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Elsa.ProtoActor.Common;
+using Elsa.ProtoActor.Configuration;
+
+namespace Elsa.ProtoActor.Kubernetes;
+
+public static class DependencyInjectionExtensions
+{
+    public static ProtoActorFeature WithKubernetesProvider(this ProtoActorFeature protoActorFeature, Action<KubernetesProviderOptions> providerOptions)
+    {
+        var options = new KubernetesProviderOptions();
+        providerOptions?.Invoke(options);
+        
+        protoActorFeature.ConfigureProtoActorBuilder(sp =>
+            new ProtoActorBuilder().UseKubernetesProvider(options).Build());
+        return protoActorFeature;
+    }
+}
---
+++ src/modules/Elsa.ProtoActor/Extensions/ProtoActorBuilderExtensions.cs
@@ -0,0 +1,23 @@
+using Elsa.ProtoActor.Common;
+using Proto;
+using Proto.Cluster.Partition;
+using Proto.Cluster.Testing;
+using Proto.Remote.GrpcNet;
+
+namespace Elsa.ProtoActor.Extensions;
+
+public static class ProtoActorBuilderExtensions
+{
+    public static ProtoActorBuilder UseLocalhostProvider(this ProtoActorBuilder builder, string clusterName, bool withDeveloperLogging)
+    {
+        var actorSystemConfig = ActorSystemConfig.Setup();
+        
+        builder.WithClusterProvider(new TestProvider(new TestProviderOptions(), new InMemAgent()))
+            .WithRemoteConfig(GrpcNetRemoteConfig
+                .BindToLocalhost())
+            .WithClusterName(clusterName)
+            .WithIdentity(new PartitionIdentityLookup())
+            .WithActorSystemConfig(withDeveloperLogging ? actorSystemConfig.WithDeveloperLogging() : actorSystemConfig);
+        return builder;
+    }
+}
