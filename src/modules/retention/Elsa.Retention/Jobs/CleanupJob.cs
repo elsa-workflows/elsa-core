6b070519365766330d6ce891566e972db466f428
Jérémie DEVILLARD <jdevillard@users.noreply.github.com>
2022-08-30 21:06:35 +0200

    change logic of deletion
    * Sort Ascending
    * use enhanced WorkflowInstanceStore to delete using only Instance Id
    * optimize query on server side

--- src/modules/retention/Elsa.Retention/Jobs/CleanupJob.cs
+++ src/modules/retention/Elsa.Retention/Jobs/CleanupJob.cs
@@ -3,11 +3,13 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Elsa.Models;
-using Elsa.Persistence;
+using Elsa.Persistence.EntityFramework.Core.Stores;
 using Elsa.Persistence.Specifications;
 using Elsa.Persistence.Specifications.WorkflowInstances;
 using Elsa.Retention.Contracts;
+using Elsa.Retention.Models;
 using Elsa.Retention.Options;
+using Elsa.Retention.Stores;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using NodaTime;
@@ -20,22 +22,25 @@ namespace Elsa.Retention.Jobs
     /// </summary>
     public class CleanupJob
     {
-        private readonly IWorkflowInstanceStore _workflowInstanceStore;
+        private readonly IRetentionWorkflowInstanceStore _workflowInstanceStore;
         private readonly IClock _clock;
         private readonly IRetentionFilterPipeline _retentionFilterPipeline;
+        private readonly IRetentionSpecificationFilter _specificationFilter;
         private readonly CleanupOptions _options;
         private readonly ILogger _logger;
 
         public CleanupJob(
-            IWorkflowInstanceStore workflowInstanceStore,
+            IRetentionWorkflowInstanceStore workflowInstanceStore,
             IClock clock,
             IRetentionFilterPipeline retentionFilterPipeline,
+            IRetentionSpecificationFilter specificationFilter,
             IOptions<CleanupOptions> options,
             ILogger<CleanupJob> logger)
         {
             _workflowInstanceStore = workflowInstanceStore;
             _clock = clock;
             _retentionFilterPipeline = retentionFilterPipeline;
+            _specificationFilter = specificationFilter;
             _options = options.Value;
             _logger = logger;
         }
@@ -43,37 +48,32 @@ namespace Elsa.Retention.Jobs
         public async Task ExecuteAsync(CancellationToken cancellationToken = default)
         {
             var threshold = _clock.GetCurrentInstant().Minus(_options.TimeToLive);
-            var specification = new WorkflowCreatedBeforeSpecification(threshold);
-            var currentPage = 0;
+            ISpecification<WorkflowInstance> specification = new WorkflowCreatedBeforeSpecification(threshold);
             var take = _options.BatchSize;
-            var orderBy = new OrderBy<WorkflowInstance>(x => x.CreatedAt, SortDirection.Descending);
-            var collectedWorkflowInstanceIds = new List<string>();
-
+            var orderBy = new OrderBy<WorkflowInstance>(x => x.CreatedAt, SortDirection.Ascending);
+            
             // Collect workflow instances to be deleted.
             while (true)
             {
-                var paging = Paging.Page(currentPage++, take);
+                var paging = new Paging(0, take);
 
-                var workflowInstances = await _workflowInstanceStore
-                    .FindManyAsync(specification, orderBy, paging, cancellationToken)
-                    .ToList();
+                specification = specification.And(_specificationFilter.GetSpecification());
 
-                var filteredWorkflowInstances = await _retentionFilterPipeline.FilterAsync(workflowInstances, cancellationToken).ToList();
-                collectedWorkflowInstanceIds.AddRange(filteredWorkflowInstances.Select(x => x.Id));
+                var workflowInstanceId = await _workflowInstanceStore.FindManyWithDTOAsync
+                    (specification, orderBy, paging, (wfi) => new RetentionWorkflowId { Id = wfi.Id }, cancellationToken);
 
-                if (workflowInstances.Count < take)
+                // Delete collected workflow instances.
+                await DeleteManyAsync(workflowInstanceId.Select(x=>x.Id).ToArray(), cancellationToken);
+
+                if (workflowInstanceId.Count() < take)
                     break;
-            }
-            
-            // Delete collected workflow instances.
-            await DeleteManyAsync(collectedWorkflowInstanceIds, cancellationToken);
+            }           
         }
 
         private async Task DeleteManyAsync(ICollection<string> workflowInstanceIds, CancellationToken cancellationToken)
         {
             _logger.LogInformation("Deleting {WorkflowInstanceCount} workflow instances", workflowInstanceIds.Count);
-            var specification = new WorkflowInstanceIdsSpecification(workflowInstanceIds);
-            await _workflowInstanceStore.DeleteManyAsync(specification, cancellationToken);
+            await (_workflowInstanceStore as EntityFrameworkWorkflowInstanceStore).DeleteManyByIdsAsync(workflowInstanceIds, cancellationToken);
         }
     }
 }
