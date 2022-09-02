3616db80b7113eb0e97af07f65c634c0b6601687
IBRAHIM NADA <ibrrahim.nada@gmail.com>
2022-09-02 18:00:14 +0300

    revert

--- src/samples/dashboard/aspnetcore/ElsaDashboard.Samples.AspNetCore.Monolith/Startup.cs
+++ src/samples/dashboard/aspnetcore/ElsaDashboard.Samples.AspNetCore.Monolith/Startup.cs
@@ -41,7 +41,6 @@ namespace ElsaDashboard.Samples.AspNetCore.Monolith
                     .AddFeatures(new[] { typeof(Startup) }, Configuration)
                     .WithContainerName(elsaSection.GetSection("Server:ContainerName").Get<string>())
                 );
-
             services
                 .AddElsaSwagger()
                 .AddElsaApiEndpoints()
@@ -76,7 +75,6 @@ namespace ElsaDashboard.Samples.AspNetCore.Monolith
             {
                 // Maps a SignalR hub for the designer to connect to when testing workflows.
                 endpoints.MapWorkflowTestHub();
-
                 // Elsa Server uses ASP.NET Core Controllers.
                 endpoints.MapControllers();
 
