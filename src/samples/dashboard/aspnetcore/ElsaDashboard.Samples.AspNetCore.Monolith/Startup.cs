f1ed5ecca845f80662ee0b2a386bea58e08168a6
IBRAHIM NADA <ibrrahim.nada@gmail.com>
2022-09-02 17:54:15 +0300

    revert

--- src/samples/dashboard/aspnetcore/ElsaDashboard.Samples.AspNetCore.Monolith/Startup.cs
+++ src/samples/dashboard/aspnetcore/ElsaDashboard.Samples.AspNetCore.Monolith/Startup.cs
@@ -2,18 +2,12 @@ using Elsa;
 using Elsa.Activities.UserTask.Extensions;
 using Elsa.Persistence.EntityFramework.Core.Extensions;
 using Elsa.Persistence.EntityFramework.Sqlite;
-using Elsa.Persistence.EntityFramework.SqlServer;
-using Elsa.Server.Authentication.Extensions;
-using Elsa.Services;
 using Elsa.WorkflowTesting.Api.Extensions;
-using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using NetBox.Extensions;
 
 namespace ElsaDashboard.Samples.AspNetCore.Monolith
 {
@@ -33,10 +27,10 @@ namespace ElsaDashboard.Samples.AspNetCore.Monolith
 
             // Elsa Server settings.
             var elsaSection = Configuration.GetSection("Elsa");
-            
+
             services
                 .AddElsa(options => options
-                    .UseEntityFrameworkPersistence(ef => ef.UseSqlServer(Configuration.GetConnectionString("Workflow")))
+                    .UseEntityFrameworkPersistence(ef => ef.UseSqlite())
                     .AddConsoleActivities()
                     .AddHttpActivities(elsaSection.GetSection("Server").Bind)
                     .AddEmailActivities(elsaSection.GetSection("Smtp").Bind)
@@ -53,35 +47,9 @@ namespace ElsaDashboard.Samples.AspNetCore.Monolith
                 .AddElsaApiEndpoints()
                 .AddWorkflowTestingServices();
 
-     
-
             // Allow arbitrary client browser apps to access the API.
             // In a production environment, make sure to allow only origins you trust.
             services.AddCors(cors => cors.AddDefaultPolicy(policy => policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin().WithExposedHeaders("Content-Disposition")));
-
-            services.AddElsaOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, options =>
-            {
-                options.Authority = "https://localhost:44318/";
-                options.ClientId = "ElsaDashboardClientServer";
-                options.ResponseType = "code";
-                options.Scopes.Add("openid");
-                options.Scopes.Add("profile");
-                options.Scopes.Add("address");
-                options.Scopes.Add("TenantId");
-                options.ClientSecret = "Elsa";
-                options.UniqueJsonKeys.Add("TenantId", "TenantId");
-                options.GetClaimsFromUserInfoEndpoint = true;
-            });
-
-            services.AddTenantAccessorFromClaim("TenantId");
-
-          //  services.AddElsaUserEndpoints();
-            services.AddMvc().AddRazorPagesOptions(options =>
-            {
-                options.Conventions.AuthorizeFolder("/pages");
-                options.Conventions.AuthorizePage("/_Host");
-            });
- 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -102,7 +70,6 @@ namespace ElsaDashboard.Samples.AspNetCore.Monolith
             app.UseCors();
             app.UseStaticFiles();
             app.UseRouting();
-           app.UseAuthentication();
             app.UseAuthorization();
             app.UseHttpActivities();
             app.UseEndpoints(endpoints =>
@@ -111,7 +78,8 @@ namespace ElsaDashboard.Samples.AspNetCore.Monolith
                 endpoints.MapWorkflowTestHub();
 
                 // Elsa Server uses ASP.NET Core Controllers.
-                endpoints.MapControllers().RequireAuthorization();
+                endpoints.MapControllers();
+
                 endpoints.MapFallbackToPage("/_Host");
             });
         }
