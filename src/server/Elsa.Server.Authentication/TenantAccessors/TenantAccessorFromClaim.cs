5837ab0b746e8b3d526f8a4e63c5271a32043a8b
IBRAHIM NADA <ibrrahim.nada@gmail.com>
2022-09-08 16:34:56 +0300

    fix spling claimName

--- src/server/Elsa.Server.Authentication/TenantAccessors/TenantAccessorFromClaim.cs
+++ src/server/Elsa.Server.Authentication/TenantAccessors/TenantAccessorFromClaim.cs
@@ -11,17 +11,17 @@ namespace Elsa.Server.Authentication.TenantAccessors
     public class TenantAccessorFromClaim : ITenantAccessor
     {
         private readonly IHttpContextAccessor _httpContextAccessor;
-        private readonly string calimName;
+        private readonly string claimName;
 
-        public TenantAccessorFromClaim(IHttpContextAccessor httpContextAccessor,string calimName)
+        public TenantAccessorFromClaim(IHttpContextAccessor httpContextAccessor,string claimName)
         {
             _httpContextAccessor = httpContextAccessor;
-            this.calimName = calimName;
+            this.claimName = claimName;
         }
 
         public async Task<string> GetTenantIdAsync(CancellationToken cancellationToken = default)
         {
-            var result = _httpContextAccessor.HttpContext.User?.Claims.Where(x => x.Type == calimName).FirstOrDefault() ?? null;
+            var result = _httpContextAccessor.HttpContext.User?.Claims.Where(x => x.Type == claimName).FirstOrDefault() ?? null;
             return result.Value ?? null;
         }
     }
