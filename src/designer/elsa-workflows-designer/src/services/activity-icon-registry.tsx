bd1cb4e3b66ee2888f698c0a61dd0b83340c5c7e
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2022-10-10 15:07:02 +0200

    Set icon for FlowJoin

--- src/designer/elsa-workflows-designer/src/services/activity-icon-registry.tsx
+++ src/designer/elsa-workflows-designer/src/services/activity-icon-registry.tsx
@@ -15,6 +15,7 @@ import {
   WriteLineIcon
 } from "../components/icons/activities";
 import {WriteHttpResponseIcon} from "../components/icons/activities/write-http-response";
+import {FlowJoinIcon} from "../components/icons/activities/flow-join";
 
 export type ActivityType = string;
 export type ActivityIcon = (ActivityIconSettings?) => any;
@@ -37,6 +38,7 @@ export class ActivityIconRegistry {
     this.add('Elsa.FlowDecision', settings => <FlowDecisionIcon size={settings?.size}/>);
     this.add('Elsa.Event', settings => <EventIcon size={settings?.size}/>);
     this.add('Elsa.RunJavaScript', settings => <RunJavaScriptIcon size={settings?.size}/>);
+    this.add('Elsa.FlowJoin', settings => <FlowJoinIcon size={settings?.size}/>);
   }
 
   public add(activityType: ActivityType, icon: ActivityIcon) {
