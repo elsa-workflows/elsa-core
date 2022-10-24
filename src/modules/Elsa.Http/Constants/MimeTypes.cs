a1736b45e1584f5835a17cd2065a2f7bbd0eb7fb
cristinamudura <cristina.mudura@nexxbiz.io>
2022-10-24 15:11:42 +0200

    Updated value of mime types constants

--- src/modules/Elsa.Http/Constants/MimeTypes.cs
+++ src/modules/Elsa.Http/Constants/MimeTypes.cs
@@ -4,5 +4,5 @@ public static class MimeTypes
 {
     public const string ApplicationXml = "application/xml";
     public const string ApplicationJson = "application/json";
-    public const string ApplicationWwwFormUrlEncoded = "VALUE";
+    public const string ApplicationWwwFormUrlEncoded = "application/x-www-form-urlencoded";
 }
