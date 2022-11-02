c800cadbac222e1bf05ad1acf13c68ac9ff7465c
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2022-11-02 20:49:35 +0100

    Configurable HTTP content parser (#3403)
    * Add default content parser option
    
    Co-authored-by: Sipke Schoorstra <sschoorstra@interfirst.com>

--- src/activities/Elsa.Activities.Http/Parsers/Response/JTokenHttpResponseContentReader.cs
+++ src/activities/Elsa.Activities.Http/Parsers/Response/JTokenHttpResponseContentReader.cs
@@ -12,20 +12,20 @@ namespace Elsa.Activities.Http.Parsers.Response
     public class JTokenHttpResponseContentReader : IHttpResponseContentReader
     {
         public string Name => "JToken";
-        public int Priority => 0;
+        public int Priority => 5;
         public bool GetSupportsContentType(string contentType) => GetIsJsonContentType(contentType) || GetIsXmlContentType(contentType);
 
         public async Task<object> ReadAsync(HttpResponseMessage response, object context, CancellationToken cancellationToken)
         {
-            var contentType = response.Content.Headers.ContentType.MediaType;
-
+            var contentType = response.Content.Headers.ContentType?.MediaType;
             var responseText = (await response.Content.ReadAsStringAsync()).Trim();
-            if (GetIsXmlContentType(contentType))
-            {
-                var doc = new XmlDocument();
-                doc.LoadXml(responseText);
-                responseText = JsonConvert.SerializeXmlNode(doc);
-            }
+
+            if (!GetIsXmlContentType(contentType)) 
+                return GetJTokenValue(responseText);
+            
+            var doc = new XmlDocument();
+            doc.LoadXml(responseText);
+            responseText = JsonConvert.SerializeXmlNode(doc);
             return GetJTokenValue(responseText);
         }
 
@@ -40,8 +40,8 @@ namespace Elsa.Activities.Http.Parsers.Response
                 return JToken.FromObject(value);
             }
         }
-        private bool GetIsJsonContentType(string contentType) => GetIsContentType(contentType, "json");
-        private bool GetIsXmlContentType(string contentType) => GetIsContentType(contentType, "xml");
-        private bool GetIsContentType(string contentType, string match) => contentType.Contains($"/{match}", StringComparison.OrdinalIgnoreCase);
+        private bool GetIsJsonContentType(string? contentType) => GetIsContentType(contentType, "json");
+        private bool GetIsXmlContentType(string? contentType) => GetIsContentType(contentType, "xml");
+        private bool GetIsContentType(string? contentType, string match) => contentType?.Contains($"/{match}", StringComparison.OrdinalIgnoreCase) ?? false;
     }
 }
