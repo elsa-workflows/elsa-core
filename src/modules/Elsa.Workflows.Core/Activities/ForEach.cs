7dd7e14745daaf4eefd4a850152eb46bb2751130
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2022-11-12 22:03:05 +0100

    Remove unused namespaces

--- src/modules/Elsa.Workflows.Core/Activities/ForEach.cs
+++ src/modules/Elsa.Workflows.Core/Activities/ForEach.cs
@@ -1,8 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text.Json.Serialization;
-using System.Threading.Tasks;
 using Elsa.Expressions.Models;
 using Elsa.Workflows.Core.Attributes;
 using Elsa.Workflows.Core.Behaviors;
