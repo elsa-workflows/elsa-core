7591a2f37b857babbae5ad023a2f38d547a494c0
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2022-11-27 13:28:43 +0100

    Register HTTP models for variable types by default

--- src/modules/Elsa.Http/Features/HttpFeature.cs
+++ src/modules/Elsa.Http/Features/HttpFeature.cs
@@ -5,10 +5,12 @@ using Elsa.Features.Services;
 using Elsa.Http.ContentWriters;
 using Elsa.Http.Handlers;
 using Elsa.Http.Implementations;
+using Elsa.Http.Models;
 using Elsa.Http.Options;
 using Elsa.Http.Parsers;
 using Elsa.Http.Services;
 using Elsa.Mediator.Extensions;
+using Elsa.Workflows.Management.Extensions;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Elsa.Http.Features;
@@ -23,25 +25,19 @@ public class HttpFeature : FeatureBase
     public Action<HttpActivityOptions>? ConfigureHttpOptions { get; set; } 
     public Func<IServiceProvider, IHttpEndpointAuthorizationHandler> HttpEndpointAuthorizationHandlerFactory { get; set; } = ActivatorUtilities.GetServiceOrCreateInstance<AllowAnonymousHttpEndpointAuthorizationHandler>;
     public Func<IServiceProvider, IHttpEndpointWorkflowFaultHandler> HttpEndpointWorkflowFaultHandlerFactory { get; set; } = ActivatorUtilities.GetServiceOrCreateInstance<DefaultHttpEndpointWorkflowFaultHandler>;
-    
-    public HttpFeature WithHttpOptions(Action<HttpActivityOptions> value)
+
+    /// <inheritdoc />
+    public override void Configure()
     {
-        ConfigureHttpOptions = value;
-        return this;
-    }
-    
-    public HttpFeature WithAuthorizationHandlerFactory(Func<IServiceProvider, IHttpEndpointAuthorizationHandler> value)
-    {
-        HttpEndpointAuthorizationHandlerFactory = value;
-        return this;
+        Module.UseWorkflowManagement(management => management.AddVariableTypes(new[]
+        {
+            typeof(HttpRequestHeaders),
+            typeof(HttpRequestModel),
+            typeof(HttpResponseModel)
+        }, "HTTP"));
     }
 
-    public HttpFeature WithWorkflowFaultHandlerFactory(Func<IServiceProvider, IHttpEndpointWorkflowFaultHandler> value)
-    {
-        HttpEndpointWorkflowFaultHandlerFactory = value;
-        return this;
-    }
-
+    /// <inheritdoc />
     public override void Apply()
     {
         var configureOptions = ConfigureHttpOptions ?? (options =>
