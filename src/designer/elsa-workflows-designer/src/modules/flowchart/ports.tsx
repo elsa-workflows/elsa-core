810962af42806bdcf673af58a4262bd8a980b6c5
Gürkan Güran <grkngrn@gmail.com>
2022-11-29 01:20:24 +0100

    Added arrow for targeting activities

--- src/designer/elsa-workflows-designer/src/modules/flowchart/ports.tsx
+++ src/designer/elsa-workflows-designer/src/modules/flowchart/ports.tsx
@@ -29,7 +29,7 @@ Graph.registerEdge(
       line: {
         stroke: '#C2C8D5',
         strokeWidth: 1,
-        targetMarker: null,
+        targetMarker: 'classic', size: 6,
       },
     },
   },
