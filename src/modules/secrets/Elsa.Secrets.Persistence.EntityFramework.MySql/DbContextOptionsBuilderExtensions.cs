5c0480b8c912b1681303e2cf4db03377d7527dfb
John Carew <johnwc@users.noreply.github.com>
2022-12-02 10:28:06 -0600

    Add MySql secrets provider (#3487)
    * Add MySql secrets provider
    
    * Update mysql initial db id default value
    
    * Fix spelling of Persistence

---
+++ src/modules/secrets/Elsa.Secrets.Persistence.EntityFramework.MySql/DbContextOptionsBuilderExtensions.cs
@@ -0,0 +1,19 @@
+using Elsa.Persistence.EntityFramework.MySql;
+using Elsa.Secrets.Persistence.EntityFramework.Core;
+using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
+
+namespace Elsa.Secrets.Persistence.EntityFramework.MySql
+{
+    public static class DbContextOptionsBuilderExtensions
+    {
+        /// <summary>
+        /// Configures the context to use MySql 
+        /// </summary>
+        public static DbContextOptionsBuilder UseSecretsMySql(this DbContextOptionsBuilder builder, string connectionString) => 
+            builder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString), db => db
+                .MigrationsAssembly(typeof(MySqlContextFactory).Assembly.GetName().Name)
+                .MigrationsHistoryTable(SecretsContext.MigrationsHistoryTable)
+                .SchemaBehavior(MySqlSchemaBehavior.Ignore));
+    }
+}
---
+++ src/modules/secrets/Elsa.Secrets.Persistence.EntityFramework.MySql/Startup.cs
@@ -0,0 +1,26 @@
+using Elsa.Attributes;
+using Elsa.Options;
+using Elsa.Secrets.Persistence.EntityFramework.Core;
+using Elsa.Secrets.Persistence.EntityFramework.MySql.ValueFormatters;
+using Elsa.Secrets.ValueFormatters;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Elsa.Secrets.Persistence.EntityFramework.MySql
+{
+    [Feature("Secrets:EntityFrameworkCore:MySql")]
+    public class Startup : EntityFrameworkSecretsStartupBase
+    {
+        protected override string ProviderName => "MySql";
+
+        public override void ConfigureElsa(ElsaOptionsBuilder elsa, IConfiguration configuration)
+        {
+            base.ConfigureElsa(elsa, configuration);
+            elsa.Services
+                .AddSingleton<ISecretValueFormatter, MySqlSecretValueFormatter>();
+
+        }
+        protected override void Configure(DbContextOptionsBuilder options, string connectionString) => options.UseSecretsMySql(connectionString);
+    }
+}
