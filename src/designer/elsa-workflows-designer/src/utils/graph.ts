6ada301b1fe77ca20d8c5a2393dd737217acc2c2
Gürkan Güran <grkngrn@gmail.com>
2022-12-02 19:46:48 +0100

    Finished inflexible port implementation for cases

--- src/designer/elsa-workflows-designer/src/utils/graph.ts
+++ src/designer/elsa-workflows-designer/src/utils/graph.ts
@@ -3,7 +3,6 @@ import { PortManager } from "@antv/x6/lib/model/port";
 import { Connection } from "../modules/flowchart/models";
 import { v4 as uuid } from 'uuid';
 import { Activity } from "../models";
-import {ActivityNode} from "../services";
 
 export function rebuildGraph(graph: Graph) {
   graph.getNodes().forEach((node: Node<Node.Properties>) => {
