0e9961e36fcd417305b9f66c2059f22e0901df73
John Carew <johnwc@users.noreply.github.com>
2022-12-09 01:43:39 -0600

    Separate MySql persistence store feature from MySql value formatter feature.

--- src/modules/secrets/Elsa.Secrets.Persistence.EntityFramework.MySql/StoreStartup.cs
+++ src/modules/secrets/Elsa.Secrets.Persistence.EntityFramework.MySql/StoreStartup.cs
@@ -3,21 +3,23 @@ using Elsa.Options;
 using Elsa.Secrets.Persistence.EntityFramework.Core;
 using Elsa.Secrets.Persistence.EntityFramework.MySql.ValueFormatters;
 using Elsa.Secrets.ValueFormatters;
-using Elsa.Services.Startup;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Elsa.Secrets.MySql
+namespace Elsa.Secrets.Persistence.EntityFramework.MySql
 {
-    [Feature("Secrets:MySql")]
-    public class Startup : StartupBase
+    [Feature("Secrets:EntityFrameworkCore:MySql")]
+    public class StoreStartup : EntityFrameworkSecretsStartupBase
     {
+        protected override string ProviderName => "MySql";
+
         public override void ConfigureElsa(ElsaOptionsBuilder elsa, IConfiguration configuration)
         {
             base.ConfigureElsa(elsa, configuration);
-            elsa.Services
-                .AddSingleton<ISecretValueFormatter, MySqlSecretValueFormatter>();
+            elsa.AddFeatures(new[] { typeof(Elsa.Secrets.MySql.Startup) }, configuration);
+
         }
+        protected override void Configure(DbContextOptionsBuilder options, string connectionString) => options.UseSecretsMySql(connectionString);
     }
 }
