29a35e1cb1016f7d47eab4d6b1128f4a2ffac71e
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2022-12-09 12:24:05 +0100

    Update All In One bundle

--- src/bundles/Elsa.AllInOne.Web/Program.cs
+++ src/bundles/Elsa.AllInOne.Web/Program.cs
@@ -2,22 +2,40 @@ using Elsa.Extensions;
 using Elsa.Jobs.Extensions;
 using Elsa.Http;
 using Elsa.Http.Extensions;
+using Elsa.Identity;
+using Elsa.Identity.Extensions;
+using Elsa.Identity.Features;
+using Elsa.Identity.Options;
 using Elsa.JavaScript.Activities;
 using Elsa.JavaScript.Extensions;
+using Elsa.Labels.Extensions;
 using Elsa.Scheduling.Activities;
 using Elsa.Liquid.Extensions;
+using Elsa.Persistence.EntityFrameworkCore.Modules.ActivityDefinitions;
+using Elsa.Persistence.EntityFrameworkCore.Modules.Labels;
+using Elsa.Persistence.EntityFrameworkCore.Modules.Runtime;
+using Elsa.Persistence.EntityFrameworkCore.Sqlite.Modules.ActivityDefinitions;
+using Elsa.Persistence.EntityFrameworkCore.Sqlite.Modules.Labels;
+using Elsa.Persistence.EntityFrameworkCore.Sqlite.Modules.Runtime;
+using Elsa.Requirements;
 using Elsa.Scheduling.Extensions;
+using Elsa.Workflows.Api.Endpoints.WorkflowDefinitions.Post;
+using Elsa.Workflows.Api.Features;
 using Elsa.Workflows.Core;
 using Elsa.Workflows.Core.Activities;
-using Elsa.Workflows.Core.Activities.Flowchart.Activities;
-using Elsa.Workflows.Core.Serialization;
-using Elsa.Workflows.Core.Services;
 using Elsa.Workflows.Management.Extensions;
-using Elsa.Workflows.Management.Serialization;
 using Elsa.Workflows.Runtime.Extensions;
+using FastEndpoints;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 
 var builder = WebApplication.CreateBuilder(args);
 var services = builder.Services;
+var configuration = builder.Configuration;
+var sqliteConnectionString = configuration.GetConnectionString("Sqlite");
+var identityOptions = new IdentityOptions();
+var identitySection = configuration.GetSection("Identity");
+identitySection.Bind(identityOptions);
 
 // Add Elsa services.
 services
@@ -25,29 +43,40 @@ services
         .UseWorkflows()
         .UseWorkflowRuntime()
         .UseWorkflowManagement(management => management
-            .AddActivity<Sequence>()
-            .AddActivity<WriteLine>()
-            .AddActivity<ReadLine>()
-            .AddActivity<If>()
-            .AddActivity<HttpEndpoint>()
-            .AddActivity<Flowchart>()
-            .AddActivity<Delay>()
-            .AddActivity<Elsa.Scheduling.Activities.Timer>()
-            .AddActivity<ForEach>()
-            .AddActivity<Switch>()
-            .AddActivity<RunJavaScript>()
+            .AddActivitiesFrom<WriteLine>()
+            .AddActivitiesFrom<HttpEndpoint>()
+            .AddActivitiesFrom<Delay>()
+            .AddActivitiesFrom<RunJavaScript>()
         )
+        .UseWorkflowApiEndpoints()
+        .UseIdentity(identity =>
+        {
+            identity.CreateDefaultUser = true;
+            identity.IdentityOptions = options => identitySection.Bind(options);
+        })
+        .UseWorkflowRuntime(runtime => { runtime.UseEntityFrameworkCore(ef => ef.UseSqlite(sqliteConnectionString)); })
+        .UseLabels(labels => labels.UseEntityFrameworkCore(ef => ef.UseSqlite(sqliteConnectionString)))
+        .UseActivityDefinitions(feature => feature.UseEntityFrameworkCore(ef => ef.UseSqlite(sqliteConnectionString)))
         .UseJobs()
         .UseScheduling()
         .UseJavaScript()
         .UseLiquid()
         .UseHttp()
         .UseActivityDefinitions()
-    );
+    )
+    // Register FastEndpoints using collected assemblies from features. 
+    .AddFastEndpointsFromModule();
 
-// Register serialization configurator for configuring what types to allow to be serialized.
-services.AddSingleton<ISerializationOptionsConfigurator, CustomSerializationOptionConfigurator>();
-services.AddSingleton<ISerializationOptionsConfigurator, SerializationOptionsConfigurator>();
+services.AddHealthChecks();
+
+// Authentication & Authorization.
+services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, identityOptions.ConfigureJwtBearerOptions);
+
+services.AddHttpContextAccessor();
+services.AddSingleton<IAuthorizationHandler, LocalHostRequirementHandler>();
+services.AddAuthorization(options => options.AddPolicy(IdentityPolicyNames.SecurityRoot, policy => policy.AddRequirements(new LocalHostRequirement())));
 
 // Razor Pages.
 services.AddRazorPages();
@@ -66,6 +95,7 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
+app.UseAuthentication();
 app.UseAuthorization();
 app.UseElsaFastEndpoints();
 app.UseHttpActivities();
