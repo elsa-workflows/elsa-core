4f7e82a9dc578d958a7c9276403697f403ffa2a3
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2022-12-26 18:38:30 +0100

    Implement exception handling middleware

---
+++ src/modules/Elsa.Workflows.Core/Middleware/Activities/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,52 @@
+using Elsa.Workflows.Core.Models;
+using Elsa.Workflows.Core.Pipelines.ActivityExecution;
+using Elsa.Workflows.Core.Services;
+using Microsoft.Extensions.Logging;
+
+namespace Elsa.Workflows.Core.Middleware.Activities;
+
+/// <summary>
+/// Adds extension methods to <see cref="ExceptionHandlingMiddleware"/>.
+/// </summary>
+public static class ExceptionHandlingMiddlewareExtensions
+{
+    /// <summary>
+    /// Installs the <see cref="ExceptionHandlingMiddleware"/> component in the activity execution pipeline.
+    /// </summary>
+    public static IActivityExecutionBuilder UseExceptionHandling(this IActivityExecutionBuilder builder) => builder.UseMiddleware<ExceptionHandlingMiddleware>();
+}
+
+/// <summary>
+/// Catches any exceptions thrown by downstream components and transitions the workflow into the faulted state.
+/// </summary>
+public class ExceptionHandlingMiddleware : IActivityExecutionMiddleware
+{
+    private readonly ActivityMiddlewareDelegate _next;
+    private readonly ILogger<ExceptionHandlingMiddleware> _logger;
+
+    /// <summary>
+    /// Constructor.
+    /// </summary>
+    public ExceptionHandlingMiddleware(ActivityMiddlewareDelegate next, ILogger<ExceptionHandlingMiddleware> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    /// <inheritdoc />
+    public async ValueTask InvokeAsync(ActivityExecutionContext context)
+    {
+        var workflowExecutionContext = context.WorkflowExecutionContext;
+        
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception e)
+        {
+            _logger.LogWarning(e, "An exception was caught from a downstream middleware component. Transitioning workflow instance {WorkflowInstanceId} into the Faulted state", workflowExecutionContext.Id);
+            workflowExecutionContext.Fault = new WorkflowFault(e, e.Message, context.Id);
+            workflowExecutionContext.TransitionTo(WorkflowSubStatus.Faulted);
+        }
+    }
+}
--- src/modules/Elsa.Workflows.Core/Pipelines/ActivityExecution/ActivityExecutionPipeline.cs
+++ src/modules/Elsa.Workflows.Core/Pipelines/ActivityExecution/ActivityExecutionPipeline.cs
@@ -4,16 +4,21 @@ using Elsa.Workflows.Core.Services;
 
 namespace Elsa.Workflows.Core.Pipelines.ActivityExecution;
 
+/// <inheritdoc />
 public class ActivityExecutionPipeline : IActivityExecutionPipeline
 {
     private readonly IServiceProvider _serviceProvider;
     private ActivityMiddlewareDelegate? _pipeline;
 
+    /// <summary>
+    /// Constructor.
+    /// </summary>
     public ActivityExecutionPipeline(IServiceProvider serviceProvider)
     {
         _serviceProvider = serviceProvider;
     }
 
+    /// <inheritdoc />
     public ActivityMiddlewareDelegate Setup(Action<IActivityExecutionBuilder> setup)
     {
         var builder = new ActivityExecutionPipelineBuilder(_serviceProvider);
@@ -22,10 +27,14 @@ public class ActivityExecutionPipeline : IActivityExecutionPipeline
         return _pipeline;
     }
 
+    /// <inheritdoc />
     public ActivityMiddlewareDelegate Pipeline => _pipeline ??= CreateDefaultPipeline();
+
+    /// <inheritdoc />
     public async Task ExecuteAsync(ActivityExecutionContext context) => await Pipeline(context);
         
     private ActivityMiddlewareDelegate CreateDefaultPipeline() => Setup(x => x
+        .UseExceptionHandling()
         .UseDefaultActivityInvoker()
     );
 }
