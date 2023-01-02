0cadc9a712a84753922e4decf6ed19ea01bbbd1d
gurkanguran <54795879+gurkanguran@users.noreply.github.com>
2023-01-02 22:10:17 +0100

    Implemented PostgreSql provider for data persistence (#3581)
    * Implemented PostgreSql provider for data persistence
    
    * Refactor
    
    Co-authored-by: Gürkan Güran <grkngrn@gmail.com>

---
+++ src/modules/Elsa.EntityFrameworkCore.PostgreSql/Modules/Management/DesignTimeDbContextFactory.cs
@@ -0,0 +1,9 @@
+﻿using Elsa.EntityFrameworkCore.Modules.Management;
+using Elsa.EntityFrameworkCore.PostgreSql.Abstractions;
+
+namespace Elsa.EntityFrameworkCore.PostgreSql.Modules.Management;
+
+// ReSharper disable once UnusedType.Global
+public class DesignTimeDbContextFactory : PostgreSqlDesignTimeDbContextFactoryBase<ManagementElsaDbContext>
+{
+}
---
+++ src/modules/Elsa.EntityFrameworkCore.PostgreSql/Modules/Management/Extensions.cs
@@ -0,0 +1,13 @@
+﻿using Elsa.EntityFrameworkCore.Modules.Management;
+
+// ReSharper disable once CheckNamespace
+namespace Elsa.EntityFrameworkCore.Extensions;
+
+public static partial class Extensions
+{
+    public static EFCoreManagementPersistenceFeature UsePostgreSql(this EFCoreManagementPersistenceFeature feature, string connectionString)
+    {
+        feature.DbContextOptionsBuilder = (_, db) => db.UseElsaPostgreSql(connectionString);
+        return feature;
+    }
+}
