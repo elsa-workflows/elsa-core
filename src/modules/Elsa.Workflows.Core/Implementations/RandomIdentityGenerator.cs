e64f2364f65e08e4534a6e02232a5d3c65adbf65
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2023-01-02 14:19:41 +0100

    Update identity generator to use hyphen format

--- src/modules/Elsa.Workflows.Core/Implementations/RandomIdentityGenerator.cs
+++ src/modules/Elsa.Workflows.Core/Implementations/RandomIdentityGenerator.cs
@@ -2,7 +2,11 @@ using Elsa.Workflows.Core.Services;
 
 namespace Elsa.Workflows.Core.Implementations;
 
+/// <summary>
+/// Generates a unique identifier using <see cref="Guid"/>.
+/// </summary>
 public class RandomIdentityGenerator : IIdentityGenerator
 {
-    public string GenerateId() => Guid.NewGuid().ToString("N");
+    /// <inheritdoc />
+    public string GenerateId() => Guid.NewGuid().ToString("D");
 }
--- src/modules/Elsa.Workflows.Core/Services/IIdentityGenerator.cs
+++ src/modules/Elsa.Workflows.Core/Services/IIdentityGenerator.cs
@@ -1,6 +1,12 @@
 namespace Elsa.Workflows.Core.Services;
 
+/// <summary>
+/// Represents a unique identity generator.
+/// </summary>
 public interface IIdentityGenerator
 {
+    /// <summary>
+    /// Generates a unique identifier.
+    /// </summary>
     string GenerateId();
 }
