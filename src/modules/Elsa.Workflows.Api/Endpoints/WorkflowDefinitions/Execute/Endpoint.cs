894cd00218eaf8509dcd7f35294172802180550f
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2023-01-14 12:24:32 +0100

    Add XML comments to Execute endpoint

--- src/modules/Elsa.Workflows.Api/Endpoints/WorkflowDefinitions/Execute/Endpoint.cs
+++ src/modules/Elsa.Workflows.Api/Endpoints/WorkflowDefinitions/Execute/Endpoint.cs
@@ -2,26 +2,34 @@ using Elsa.Abstractions;
 using Elsa.Common.Models;
 using Elsa.Workflows.Management.Services;
 using Elsa.Workflows.Runtime.Services;
+using JetBrains.Annotations;
 
 namespace Elsa.Workflows.Api.Endpoints.WorkflowDefinitions.Execute;
 
+/// <summary>
+/// An API endpoint that executes a given workflow definition.
+/// </summary>
+[PublicAPI]
 public class Execute : ElsaEndpoint<Request, Response>
 {
     private readonly IWorkflowDefinitionStore _store;
     private readonly IWorkflowRuntime _workflowRuntime;
 
+    /// <inheritdoc />
     public Execute(IWorkflowDefinitionStore store, IWorkflowRuntime workflowRuntime)
     {
         _store = store;
         _workflowRuntime = workflowRuntime;
     }
 
+    /// <inheritdoc />
     public override void Configure()
     {
         Post("/workflow-definitions/{definitionId}/execute");
         ConfigurePermissions("exec:workflow-definitions");
     }
 
+    /// <inheritdoc />
     public override async Task HandleAsync(Request request, CancellationToken cancellationToken)
     {
         var definitionId = request.DefinitionId;
