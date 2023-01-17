9351671b0608033b03055b39c2632f730ef9780b
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2023-01-17 12:21:49 +0100

    Fix workflow fault state deserialization

--- src/modules/Elsa.Workflows.Core/State/WorkflowFaultState.cs
+++ src/modules/Elsa.Workflows.Core/State/WorkflowFaultState.cs
@@ -1,4 +1,7 @@
-﻿namespace Elsa.Workflows.Core.State;
+﻿using System.Text.Json.Serialization;
+using JetBrains.Annotations;
+
+namespace Elsa.Workflows.Core.State;
 
 /// <summary>
 /// Holds information about a workflow fault.
@@ -6,4 +9,15 @@
 /// <param name="Exception">The exception that occurred</param>
 /// <param name="Message">A description about the fault. Usually the exception message, if there waa an exception.</param>
 /// <param name="FaultedActivityId">The ID of the activity that caused the workflow to fault.</param>
-public record WorkflowFaultState(ExceptionState? Exception, string Message, string? FaultedActivityId);
+[PublicAPI]
+public record WorkflowFaultState(ExceptionState? Exception, string Message, string? FaultedActivityId)
+{
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    [JsonConstructor]
+    public WorkflowFaultState() : this(default, default!, default)
+    {
+        var y = "";
+    }
+}
