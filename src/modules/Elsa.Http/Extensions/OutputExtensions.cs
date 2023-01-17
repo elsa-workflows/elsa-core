9fc01875d2fc3f336c46dded62987f1a55667c20
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2023-01-17 12:22:44 +0100

    Fix potential NRE in HTTP activities when attempting to determine target type

--- src/modules/Elsa.Http/Extensions/OutputExtensions.cs
+++ src/modules/Elsa.Http/Extensions/OutputExtensions.cs
@@ -8,9 +8,9 @@ internal static class OutputExtensions
     /// <summary>
     /// Gets the target type of the specified variable type.
     /// </summary>
-    public static Type? GetTargetType(this Output output, ActivityExecutionContext context)
+    public static Type? GetTargetType(this Output? output, ActivityExecutionContext context)
     {
-        var memoryBlock = output.MemoryBlockReference() is Variable variable
+        var memoryBlock = output?.MemoryBlockReference() is Variable variable
             ? context.WorkflowExecutionContext.MemoryRegister.TryGetBlock(variable.Id, out var block)
                 ? block
                 : default
