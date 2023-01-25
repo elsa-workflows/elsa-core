fe845e65e766bf249f9b04412e8b50d041626c06
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2023-01-25 11:46:07 +0100

    Fix JsonObject type handling

--- src/modules/Elsa.JavaScript/Providers/VariableTypeDefinitionProvider.cs
+++ src/modules/Elsa.JavaScript/Providers/VariableTypeDefinitionProvider.cs
@@ -20,16 +20,21 @@ internal class VariableTypeDefinitionProvider : TypeDefinitionProvider
 
     protected override IEnumerable<TypeDefinition> GetTypeDefinitions(TypeDefinitionContext context)
     {
-        var excludedTypes = new[] { typeof(ExpandoObject), typeof(IDictionary<string, object>) };
+        var excludedTypes = new Func<Type, bool>[]
+        {
+            type => type == typeof(ExpandoObject),
+            type => typeof(IDictionary<string, object>).IsAssignableFrom(type),
+            type => type == typeof(object)
+        };
 
         var variableTypeQuery =
             from variable in context.Variables
             let variableType = variable.GetVariableType()
-            where (variableType.IsClass || variableType.IsInterface) && !variableType.IsPrimitive && !excludedTypes.Any(x => x.IsAssignableFrom(variableType))
+            where (variableType.IsClass || variableType.IsInterface) && !variableType.IsPrimitive && !excludedTypes.Any(x => x(variableType))
             select variableType;
 
         var variableTypes = variableTypeQuery.Distinct();
-        
+
         foreach (var variableType in variableTypes)
         {
             yield return _typeDescriber.DescribeType(variableType);
--- src/modules/Elsa.Workflows.Core/Models/Input.cs
+++ src/modules/Elsa.Workflows.Core/Models/Input.cs
@@ -6,8 +6,12 @@ using Elsa.Workflows.Core.Expressions;
 
 namespace Elsa.Workflows.Core.Models;
 
+/// <summary>
+/// A base type for the <see cref="Input{T}"/> type.
+/// </summary>
 public abstract class Input : Argument
 {
+    /// <inheritdoc />
     protected Input(IExpression expression, MemoryBlockReference memoryBlockReference, Type type) : base(memoryBlockReference)
     {
         Expression = expression;
@@ -19,60 +23,77 @@ public abstract class Input : Argument
     [JsonPropertyName("typeName")] public Type Type { get; set; }
 }
 
+/// <summary>
+/// Represents activity input that is evaluated at runtime.
+/// </summary>
 public class Input<T> : Input
 {
+    /// <inheritdoc />
     public Input(T literal, string? id = default) : this(new Literal<T>(literal) { Id = id! })
     {
     }
 
+    /// <inheritdoc />
     public Input(Func<T> @delegate, string? id = default) : this(new DelegateBlockReference(() => @delegate()){ Id = id!})
     {
     }
 
+    /// <inheritdoc />
     public Input(Func<ExpressionExecutionContext, ValueTask<T?>> @delegate) : this(new DelegateBlockReference<T>(@delegate))
     {
     }
 
+    /// <inheritdoc />
     public Input(Func<ValueTask<T?>> @delegate) : this(new DelegateBlockReference<T>(@delegate))
     {
     }
 
+    /// <inheritdoc />
     public Input(Func<ExpressionExecutionContext, T> @delegate) : this(new DelegateBlockReference<T>(@delegate))
     {
     }
 
+    /// <inheritdoc />
     public Input(Variable variable) : base(new VariableExpression(variable), variable, typeof(T))
     {
     }
 
+    /// <inheritdoc />
     public Input(Output output) : base(new OutputExpression(output), output.MemoryBlockReference(), typeof(T))
     {
     }
 
+    /// <inheritdoc />
     public Input(Literal<T> literal) : base(new LiteralExpression(literal.Value), literal, typeof(T))
     {
     }
 
+    /// <inheritdoc />
     public Input(Literal literal) : base(new LiteralExpression(literal.Value), literal, typeof(T))
     {
     }
     
+    /// <inheritdoc />
     public Input(JsonLiteral<T> literal) : base(new JsonExpression(literal.Value), literal, typeof(T))
     {
     }
 
+    /// <inheritdoc />
     public Input(JsonLiteral literal) : base(new JsonExpression(literal.Value), literal, typeof(T))
     {
     }
 
+    /// <inheritdoc />
     public Input(DelegateBlockReference delegateBlockReference) : base(new DelegateExpression(delegateBlockReference), delegateBlockReference, typeof(T))
     {
     }
 
+    /// <inheritdoc />
     public Input(ElsaExpression expression) : this(new ElsaExpressionBlockReference(expression))
     {
     }
 
+    /// <inheritdoc />
     public Input(IExpression expression, MemoryBlockReference memoryBlockReference) : base(expression, memoryBlockReference, typeof(T))
     {
     }
