71b0696192d1e9acf8fff3bffd9107e1648cb35f
brunomartinspro <hello@brunomartins.pro>
2023-02-03 12:09:43 +0000

    Added postman collection and support for enums as strings in swagger ui, and openapi generator support

--- src/server/Elsa.Server.Api/Extensions/SchemaFilters/XEnumNamesSchemaFilter.cs
+++ src/server/Elsa.Server.Api/Extensions/SchemaFilters/XEnumNamesSchemaFilter.cs
@@ -3,23 +3,49 @@ using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.SwaggerGen;
 using System;
 using System.Linq;
+using System.Reflection;
 
 namespace Elsa.Server.Api.Extensions.SchemaFilters
 {
     public class XEnumNamesSchemaFilter : ISchemaFilter
     {
-        private const string NAME = "x-enumNames";
+        private const string nswag = "x-enumNames";
+        private const string openapiGenerator = "x-enum-varnames";
+
 
         public void Apply(OpenApiSchema model, SchemaFilterContext context)
         {
-            var typeInfo = context.Type;
-            if (typeInfo.IsEnum && !model.Extensions.ContainsKey(NAME))
+            if (context.Type.IsEnum)
+            {
+                AddGeneratorSupport(model, context, nswag);
+                AddGeneratorSupport(model, context, openapiGenerator);
+                AddSwaggerUiSupport(model, context);
+            }
+
+        }
+
+        private void AddGeneratorSupport(OpenApiSchema model, SchemaFilterContext context, string generatorType)
+        {
+            if (!model.Extensions.ContainsKey(generatorType))
             {
                 var names = Enum.GetNames(context.Type);
                 var arr = new OpenApiArray();
                 arr.AddRange(names.Select(name => new OpenApiString(name)));
-                model.Extensions.Add(NAME, arr);
+                model.Extensions.Add(generatorType, arr);
             }
+        }
+
+        private void AddSwaggerUiSupport(OpenApiSchema model, SchemaFilterContext context)
+        {
+            model.Enum.Clear();
+            Enum.GetNames(context.Type)
+                .ToList()
+                .ForEach(n =>
+                {
+                    model.Enum.Add(new OpenApiString(n));
+                    model.Type = "string";
+                    model.Format = null;
+                });
         }
     }
 }
