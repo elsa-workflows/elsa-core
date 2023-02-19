3b93feaadbbd8c0651404c0a67500f9aa98de3ec
Martin Hjulström <martin.hjulstrom@gmail.com>
2023-02-19 21:08:20 +0100

    Changed so that DeleteAsync uses batch delete and removed psql and mysql work around

--- src/persistence/Elsa.Persistence.EntityFramework/Elsa.Persistence.EntityFramework.Core/Extensions/QueryableBulkExtensions.cs
+++ src/persistence/Elsa.Persistence.EntityFramework/Elsa.Persistence.EntityFramework.Core/Extensions/QueryableBulkExtensions.cs
@@ -10,10 +10,9 @@ namespace Elsa.Persistence.EntityFramework.Core.Extensions
     {
         public static async Task<int> BatchDeleteWithWorkAroundAsync<T>(this IQueryable<T> queryable, DbContext elsaContext, CancellationToken cancellationToken = default) where T : class
         {
-            if (elsaContext.Database.IsPostgres() || elsaContext.Database.IsMySql() || elsaContext.Database.IsOracle())
+            if (elsaContext.Database.IsOracle())
             {
-                // Need this workaround https://github.com/borisdj/EFCore.BulkExtensions/issues/553 is solved.
-                // Oracle also https://github.com/borisdj/EFCore.BulkExtensions/issues/375
+                // Oracle need this workaround also https://github.com/borisdj/EFCore.BulkExtensions/issues/375 is solved.
                 var records = await queryable.ToListAsync(cancellationToken);
 
                 foreach (var record in records) 
