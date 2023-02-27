0ff3841de7aacbb99edf5103bda037a614fb245f
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2023-02-27 13:36:39 +0100

    Add XML comments

--- src/modules/Elsa.Labels/Implementations/InMemoryLabelStore.cs
+++ src/modules/Elsa.Labels/Implementations/InMemoryLabelStore.cs
@@ -6,29 +6,38 @@ using Elsa.Labels.Services;
 
 namespace Elsa.Labels.Implementations;
 
+/// <summary>
+/// An in-memory store of labels.
+/// </summary>
 public class InMemoryLabelStore : ILabelStore
 {
     private readonly MemoryStore<Label> _labelStore;
     private readonly MemoryStore<WorkflowDefinitionLabel> _workflowDefinitionLabelStore;
 
+    /// <summary>
+    /// Constructor.
+    /// </summary>
     public InMemoryLabelStore(MemoryStore<Label> labelStore, MemoryStore<WorkflowDefinitionLabel> workflowDefinitionLabelStore)
     {
         _labelStore = labelStore;
         _workflowDefinitionLabelStore = workflowDefinitionLabelStore;
     }
 
+    /// <inheritdoc />
     public Task SaveAsync(Label record, CancellationToken cancellationToken = default)
     {
         _labelStore.Save(record, x => x.Id);
         return Task.CompletedTask;
     }
 
+    /// <inheritdoc />
     public Task SaveManyAsync(IEnumerable<Label> records, CancellationToken cancellationToken = default)
     {
         _labelStore.SaveMany(records, x => x.Id);
         return Task.CompletedTask;
     }
 
+    /// <inheritdoc />
     public Task<bool> DeleteAsync(string id, CancellationToken cancellationToken = default)
     {
         _workflowDefinitionLabelStore.DeleteWhere(x => x.LabelId == id);
@@ -36,6 +45,7 @@ public class InMemoryLabelStore : ILabelStore
         return Task.FromResult(result);
     }
 
+    /// <inheritdoc />
     public Task<int> DeleteManyAsync(IEnumerable<string> ids, CancellationToken cancellationToken = default)
     {
         var idList = ids.ToList();
@@ -44,12 +54,14 @@ public class InMemoryLabelStore : ILabelStore
         return Task.FromResult(result);
     }
 
+    /// <inheritdoc />
     public Task<Label?> FindByIdAsync(string id, CancellationToken cancellationToken = default)
     {
         var record = _labelStore.Find(x => x.Id == id);
         return Task.FromResult(record);
     }
 
+    /// <inheritdoc />
     public Task<Page<Label>> ListAsync(PageArgs? pageArgs = default, CancellationToken cancellationToken = default)
     {
         var query = _labelStore.List().AsQueryable().OrderBy(x => x.Name);
@@ -57,6 +69,7 @@ public class InMemoryLabelStore : ILabelStore
         return Task.FromResult(page);
     }
 
+    /// <inheritdoc />
     public Task<IEnumerable<Label>> FindManyByIdAsync(IEnumerable<string> ids, CancellationToken cancellationToken)
     {
         var idList = ids.ToList();
