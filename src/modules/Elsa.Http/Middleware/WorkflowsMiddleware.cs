c554f5c55f513a7a37473147b5428d887803c341
MariusVuscanNx <96233009+MariusVuscanNx@users.noreply.github.com>
2023-03-02 11:27:11 +0200

    Feature/workflow execution status codes (#3750)
    * Returned 404 when path is not found and 500 when there are multiple workflows with the same path
    
    * Return failure response in case of workflow fault

--- src/modules/Elsa.Http/Middleware/WorkflowsMiddleware.cs
+++ src/modules/Elsa.Http/Middleware/WorkflowsMiddleware.cs
@@ -1,13 +1,16 @@
-using System.Net.Mime;
-using System.Text.Json;
 using Elsa.Http.Models;
 using Elsa.Http.Options;
 using Elsa.Http.Services;
 using Elsa.Workflows.Core.Helpers;
+using Elsa.Workflows.Core.Models;
+using Elsa.Workflows.Management.Services;
 using Elsa.Workflows.Runtime.Services;
 using JetBrains.Annotations;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Options;
+using System.Net;
+using System.Net.Mime;
+using System.Text.Json;
 
 namespace Elsa.Http.Middleware;
 
@@ -22,6 +25,8 @@ public class WorkflowsMiddleware
     private readonly IWorkflowHostFactory _workflowHostFactory;
     private readonly IWorkflowDefinitionService _workflowDefinitionService;
     private readonly IHttpBookmarkProcessor _httpBookmarkProcessor;
+    private readonly IWorkflowInstanceStore _workflowInstanceStore;
+    private readonly IHttpEndpointWorkflowFaultHandler _httpEndpointWorkflowFaultHandler;
     private readonly HttpActivityOptions _options;
     private readonly string _activityTypeName = ActivityTypeNameHelper.GenerateTypeName<HttpEndpoint>();
 
@@ -34,17 +39,21 @@ public class WorkflowsMiddleware
         IWorkflowHostFactory workflowHostFactory,
         IWorkflowDefinitionService workflowDefinitionService,
         IHttpBookmarkProcessor httpBookmarkProcessor,
-        IOptions<HttpActivityOptions> options)
+        IWorkflowInstanceStore workflowInstanceStore,
+        IOptions<HttpActivityOptions> options,
+        IHttpEndpointWorkflowFaultHandler httpEndpointWorkflowFaultHandler)
     {
         _next = next;
         _workflowRuntime = workflowRuntime;
         _workflowHostFactory = workflowHostFactory;
         _workflowDefinitionService = workflowDefinitionService;
         _httpBookmarkProcessor = httpBookmarkProcessor;
+        _workflowInstanceStore = workflowInstanceStore;
+        _httpEndpointWorkflowFaultHandler = httpEndpointWorkflowFaultHandler;
         _options = options.Value;
     }
 
-    
+
     /// <summary>
     /// Attempts to matches the inbound request path to an associated workflow and then run that workflow.
     /// </summary>
@@ -79,10 +88,19 @@ public class WorkflowsMiddleware
         var bookmarkPayload = new HttpEndpointBookmarkPayload(path, method);
         var triggerOptions = new TriggerWorkflowsRuntimeOptions(correlationId, input);
         var cancellationToken = httpContext.RequestAborted;
-        
+
         // Trigger the workflow.
         var triggerResult = await _workflowRuntime.TriggerWorkflowsAsync(_activityTypeName, bookmarkPayload, triggerOptions, cancellationToken);
-        
+
+        if (await HandleNoWorkflowsFoundAsync(httpContext, triggerResult.TriggeredWorkflows, basePath))
+            return;
+
+        if (await HandleMultipleWorkflowsFoundAsync(httpContext, triggerResult.TriggeredWorkflows, cancellationToken))
+            return;
+
+        if (await HandleWorkflowFaultAsync(httpContext, triggerResult, cancellationToken))
+            return;
+
         // Process the trigger result by resuming each HTTP bookmark, if any.
         await _httpBookmarkProcessor.ProcessBookmarks(triggerResult.TriggeredWorkflows, correlationId, input, cancellationToken);
     }
@@ -105,6 +123,59 @@ public class WorkflowsMiddleware
             await response.WriteAsync(json, cancellationToken);
         }
     }
-    
+
     private string GetPath(HttpContext httpContext) => httpContext.Request.Path.Value.ToLowerInvariant();
+
+    private async Task<bool> HandleNoWorkflowsFoundAsync(HttpContext httpContext, ICollection<WorkflowExecutionResult> triggeredWorkflows, PathString? basePath)
+    {
+        if (triggeredWorkflows.Any())
+            return false;
+
+        // If a base path was configured, we are sure the requester tried to execute a workflow that doesn't exist.
+        // Therefore, sending a 404 response seems appropriate instead of continuing with any subsequent middlewares.
+        if (basePath != null)
+        {
+            httpContext.Response.StatusCode = (int)HttpStatusCode.NotFound;
+            return true;
+        }
+
+        // If no base path was configured on the other hand, the request could be targeting anything else and should be handled by subsequent middlewares. 
+        await _next(httpContext);
+
+        return true;
+    }
+
+    private async Task<bool> HandleMultipleWorkflowsFoundAsync(HttpContext httpContext, ICollection<WorkflowExecutionResult> triggeredWorkflows, CancellationToken cancellationToken)
+    {
+        if (triggeredWorkflows.Count <= 1)
+            return false;
+
+        httpContext.Response.ContentType = "application/json";
+        httpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+        var responseContent = JsonSerializer.Serialize(new
+        {
+            errorMessage = "The call is ambiguous and matches multiple workflows.",
+            workflows = triggeredWorkflows
+        });
+
+        await httpContext.Response.WriteAsync(responseContent, cancellationToken);
+        return true;
+    }
+
+    private async Task<bool> HandleWorkflowFaultAsync(HttpContext httpContext, TriggerWorkflowsResult triggerResult, CancellationToken cancellationToken)
+    {
+        var instanceFilter = new WorkflowInstanceFilter { Id = triggerResult.TriggeredWorkflows.Single().InstanceId };
+        var workflowInstance = await _workflowInstanceStore.FindAsync(instanceFilter, cancellationToken);
+
+        if (workflowInstance is not null
+            && workflowInstance.SubStatus == WorkflowSubStatus.Faulted
+            && !httpContext.Response.HasStarted)
+        {
+            await _httpEndpointWorkflowFaultHandler.HandleAsync(new HttpEndpointFaultedWorkflowContext(httpContext, workflowInstance, null, cancellationToken));
+            return true;
+        }
+
+        return false;
+    }
 }
