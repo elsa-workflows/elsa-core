d45f6f2f858859075ebd6872d47333b23b817f5a
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2023-03-12 20:59:11 +0100

    Fix delete workflow definition version

--- src/modules/Elsa.Workflows.Api/Endpoints/WorkflowDefinitions/Version/Delete.cs
+++ src/modules/Elsa.Workflows.Api/Endpoints/WorkflowDefinitions/Version/Delete.cs
@@ -1,29 +1,38 @@
 ﻿using Elsa.Workflows.Management.Contracts;
 using FastEndpoints;
+using JetBrains.Annotations;
 
 namespace Elsa.Workflows.Api.Endpoints.WorkflowDefinitions.Version;
 
+/// <summary>
+/// Deletes a specific version of a workflow definition.
+/// </summary>
+[PublicAPI]
 public class DeleteVersion : EndpointWithoutRequest
 {
     private readonly IWorkflowDefinitionManager _workflowDefinitionManager;
-    
+
+    /// <inheritdoc />
     public DeleteVersion(IWorkflowDefinitionManager workflowDefinitionManager)
     {
         _workflowDefinitionManager = workflowDefinitionManager;
     }
 
+    /// <inheritdoc />
     public override void Configure()
     {
         Delete("workflow-definitions/{definitionId}/version/{version}");
         AllowAnonymous();
     }
 
+    /// <inheritdoc />
     public override async Task HandleAsync(CancellationToken ct)
     {
         var definitionId = Route<string>("definitionId")!;
         var version = Route<int>("version");
         
         var result = await _workflowDefinitionManager.DeleteVersionAsync(definitionId, version, ct);
+        
         if (!result)
         {
             await SendNotFoundAsync(ct);
