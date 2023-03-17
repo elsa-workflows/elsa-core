a9ad5d70cf624a18da2271d4b278972b95f5d53e
Jérémie DEVILLARD <jdevillard@users.noreply.github.com>
2023-03-17 15:56:01 +0100

    add class for angular wrapper

--- src/designer/designer_packages/elsa-workflows-designer/src/index.ts
+++ src/designer/designer_packages/elsa-workflows-designer/src/index.ts
@@ -4,4 +4,8 @@ export {Components, JSX} from './components';
 export {Container} from 'typedi';
 export {ElsaClientProvider} from './services/api-client/elsa-client';
 export {stripActivityNameSpace} from './utils';
-export {PluginRegistry, ActivityNameFormatter} from './services';
+export {PluginRegistry, ActivityNameFormatter,StudioService,ActivityDescriptorManager, ServerSettings, AuthContext} from './services';
+
+export {LoginApi, } from './modules/login/services';
+export { WorkflowDefinitionsPlugin} from './modules/workflow-definitions/plugins/workflow-definitions-plugin'
+export { WorkflowDefinitionsApi } from './modules/workflow-definitions/services/api'
