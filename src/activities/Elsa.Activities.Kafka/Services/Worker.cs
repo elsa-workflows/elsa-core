cc611fe2638d6074d62a405b677bf87428f16779
Yannick Laubscher <yannick.laubscher@swissteach.ch>
2023-03-23 08:49:46 +0100

    Tested

--- src/activities/Elsa.Activities.Kafka/Services/Worker.cs
+++ src/activities/Elsa.Activities.Kafka/Services/Worker.cs
@@ -1,13 +1,9 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using Avro.Specific;
 using Confluent.Kafka;
-using Confluent.SchemaRegistry;
 using Elsa.Activities.Kafka.Activities.KafkaMessageReceived;
 using Elsa.Activities.Kafka.Bookmarks;
 using Elsa.Activities.Kafka.Configuration;
