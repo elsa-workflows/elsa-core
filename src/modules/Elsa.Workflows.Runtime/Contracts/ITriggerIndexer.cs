87878ded8aa798ce6432d267a9d9506a8b146435
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2023-04-17 15:22:09 +0200

    Cleanup

--- src/modules/Elsa.Workflows.Runtime/Contracts/ITriggerIndexer.cs
+++ src/modules/Elsa.Workflows.Runtime/Contracts/ITriggerIndexer.cs
@@ -4,6 +4,9 @@ using Elsa.Workflows.Runtime.Models.Notifications;
 
 namespace Elsa.Workflows.Runtime.Contracts;
 
+/// <summary>
+/// Extracts triggers from workflow definitions.
+/// </summary>
 public interface ITriggerIndexer
 {
     /// <summary>
@@ -20,9 +23,4 @@ public interface ITriggerIndexer
     /// Indexes triggers of the specified workflow.
     /// </summary>
     Task<IndexedWorkflowTriggers> IndexTriggersAsync(Workflow workflow, CancellationToken cancellationToken = default);
-
-    /// <summary>
-    /// Send message of all triggers in the trigger store to populate the routetable on startup
-    /// </summary>
-    Task<IndexedWorkflowTriggers> IndexAllTriggersAsync(CancellationToken cancellationToken = default);
 }
