7a33ce806ba929029b6cbdf986c63e09ab0b9a0f
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2023-04-18 22:30:13 +0200

    Incremental work on context activity

---
+++ src/modules/Elsa.WorkflowContexts/Activities/SetWorkflowContextParameter.cs
@@ -0,0 +1,50 @@
+using System.Runtime.CompilerServices;
+using System.Text.Json.Serialization;
+using Elsa.Extensions;
+using Elsa.Workflows.Core.Attributes;
+using Elsa.Workflows.Core.Models;
+
+namespace Elsa.WorkflowContexts.Activities;
+
+/// <summary>
+/// Sets a workflow context parameter for a given workflow context provider.
+/// </summary>
+public class SetWorkflowContextParameter : CodeActivity
+{
+    /// <inheritdoc />
+    [JsonConstructor]
+    public SetWorkflowContextParameter([CallerFilePath] string? source = default, [CallerLineNumber] int? line = default) : base(source, line)
+    {
+    }
+    
+    /// <summary>
+    /// The type of the workflow context provider.
+    /// </summary>
+    [Input(
+        UIHint = "workflow-context-provider-picker",
+        Description = "The type of the workflow context provider."
+    )]
+    public Input<Type> ProviderType { get; set; } = default!;
+    
+    /// <summary>
+    /// The name of the parameter to set. If not specified, the parameter name will be inferred from the workflow context provider.
+    /// </summary>
+    [Input(Description = "The name of the parameter to set. If not specified, the parameter name will be inferred from the workflow context provider.")]
+    public Input<string?> ParameterName { get; set; } = default!;
+
+    /// <summary>
+    /// The value of the parameter to set.
+    /// </summary>
+    [Input(Description = "The value of the parameter to set.")]
+    public Input<object> ParameterValue { get; set; } = default!;
+
+    /// <inheritdoc />
+    protected override void Execute(ActivityExecutionContext context)
+    {
+        var providerType = ProviderType.Get(context);
+        var parameterName = ParameterName.GetOrDefault(context) ?? providerType.Name.Replace("WorkflowContextProvider", "");
+        var parameterValue = ParameterValue.Get(context);
+
+        context.WorkflowExecutionContext.SetProperty(parameterName, parameterValue);
+    }
+}
