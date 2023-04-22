deaa6f334824f43e5df2289ba7c69a0b69eacb66
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2023-04-23 00:25:39 +0200

    Add Parallel activity

---
+++ src/modules/Elsa.Workflows.Core/Activities/Parallel.cs
@@ -0,0 +1,46 @@
+using System.Runtime.CompilerServices;
+using System.Text.Json.Serialization;
+using Elsa.Extensions;
+using Elsa.Workflows.Core.Attributes;
+using Elsa.Workflows.Core.Contracts;
+using Elsa.Workflows.Core.Models;
+
+namespace Elsa.Workflows.Core.Activities;
+
+/// <summary>
+/// Execute a set of activities in parallel.
+/// </summary>
+[Activity("Elsa", "Workflows", "Execute a set of activities in parallel.")]
+public class Parallel : Container
+{
+    private const string ScheduledChildrenProperty = "ScheduledChildren";
+    
+    /// <inheritdoc />
+    [JsonConstructor]
+    public Parallel([CallerFilePath] string? source = default, [CallerLineNumber] int? line = default) : base(source, line)
+    {
+    }
+    
+    /// <inheritdoc />
+    public Parallel(params IActivity[] activities) : this()
+    {
+        Activities = activities;
+    }
+
+    /// <inheritdoc />
+    protected override async ValueTask ScheduleChildrenAsync(ActivityExecutionContext context)
+    {
+        context.SetProperty(ScheduledChildrenProperty, Activities.Count);
+        
+        foreach (var activity in Activities) 
+            await context.ScheduleActivityAsync(activity, OnChildCompleted);
+    }
+    
+    private static async ValueTask OnChildCompleted(ActivityExecutionContext context, ActivityExecutionContext childContext)
+    {
+        var remainingChildren = context.UpdateProperty<int>(ScheduledChildrenProperty, scheduledChildren => scheduledChildren - 1);
+        
+        if (remainingChildren == 0)
+            await context.CompleteActivityAsync();
+    }
+}
