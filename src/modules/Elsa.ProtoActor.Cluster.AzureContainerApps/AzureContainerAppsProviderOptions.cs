20722bed7ea7bee14fd7af8d3c46f70a1dd57264
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2023-04-30 22:23:53 +0200

    Remove whitespace

--- src/modules/Elsa.ProtoActor.Cluster.AzureContainerApps/AzureContainerAppsProviderOptions.cs
+++ src/modules/Elsa.ProtoActor.Cluster.AzureContainerApps/AzureContainerAppsProviderOptions.cs
@@ -11,13 +11,13 @@ public class AzureContainerAppsProviderOptions
     /// <summary>
     /// The subscription ID to use. If not set, the default subscription will be used.
     /// </summary>
-    public string? SubscriptionId { get; set; } = default!;
-    
+    public string? SubscriptionId { get; set; }
+
     /// <summary>
     /// The name of the resource group to use.
     /// </summary>
     public string ResourceGroupName { get; set; } = default!;
-    
+
     /// <summary>
     /// The name of the container app to use.
     /// </summary>
