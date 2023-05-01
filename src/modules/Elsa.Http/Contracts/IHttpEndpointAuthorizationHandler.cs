23b98d128fa7ff4daee35300857d547bc7d2c1c6
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2023-05-01 12:02:45 +0200

    XML comments and minor DI changes

--- src/modules/Elsa.Http/Contracts/IHttpEndpointAuthorizationHandler.cs
+++ src/modules/Elsa.Http/Contracts/IHttpEndpointAuthorizationHandler.cs
@@ -2,7 +2,15 @@ using Elsa.Http.Models;
 
 namespace Elsa.Http.Contracts;
 
+/// <summary>
+/// A handler that is invoked when authorizing an inbound HTTP request.
+/// </summary>
 public interface IHttpEndpointAuthorizationHandler
 {
+    /// <summary>
+    /// Authorizes an inbound HTTP request.
+    /// </summary>
+    /// <param name="context">The context.</param>
+    /// <returns>True if the request is authorized, otherwise false.</returns>
     ValueTask<bool> AuthorizeAsync(AuthorizeHttpEndpointContext context);
 }
--- src/modules/Elsa.Http/Handlers/AuthenticationBasedHttpEndpointAuthorizationHandler.cs
+++ src/modules/Elsa.Http/Handlers/AuthenticationBasedHttpEndpointAuthorizationHandler.cs
@@ -1,14 +1,24 @@
 using Elsa.Http.Contracts;
 using Elsa.Http.Models;
+using JetBrains.Annotations;
 using Microsoft.AspNetCore.Authorization;
 
 namespace Elsa.Http.Handlers;
 
+/// <summary>
+/// An <see cref="IHttpEndpointAuthorizationHandler"/> that uses the <see cref="IAuthorizationService"/> to authorize an inbound HTTP request.
+/// </summary>
+[PublicAPI]
 public class AuthenticationBasedHttpEndpointAuthorizationHandler : IHttpEndpointAuthorizationHandler
 {
     private readonly IAuthorizationService _authorizationService;
+    
+    /// <summary>
+    /// Initializes a new instance of the <see cref="AuthenticationBasedHttpEndpointAuthorizationHandler"/> class.
+    /// </summary>
     public AuthenticationBasedHttpEndpointAuthorizationHandler(IAuthorizationService authorizationService) => _authorizationService = authorizationService;
 
+    /// <inheritdoc />
     public async ValueTask<bool> AuthorizeAsync(AuthorizeHttpEndpointContext context)
     {
         var httpContext = context.HttpContext;
