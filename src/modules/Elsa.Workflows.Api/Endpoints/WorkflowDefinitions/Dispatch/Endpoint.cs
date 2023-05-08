c02800a2fc4cb07ee73848354d5559de2d74f8a9
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2023-05-08 22:35:50 +0200

    Pre-create workflow instance ID when dispatching workflows

--- src/modules/Elsa.Workflows.Api/Endpoints/WorkflowDefinitions/Dispatch/Endpoint.cs
+++ src/modules/Elsa.Workflows.Api/Endpoints/WorkflowDefinitions/Dispatch/Endpoint.cs
@@ -1,5 +1,6 @@
 using Elsa.Abstractions;
 using Elsa.Common.Models;
+using Elsa.Workflows.Core.Contracts;
 using Elsa.Workflows.Management.Contracts;
 using Elsa.Workflows.Management.Filters;
 using Elsa.Workflows.Runtime.Contracts;
@@ -13,11 +14,13 @@ internal class Endpoint : ElsaEndpoint<Request, Response>
 {
     private readonly IWorkflowDefinitionStore _store;
     private readonly IWorkflowDispatcher _workflowDispatcher;
+    private readonly IIdentityGenerator _identityGenerator;
 
-    public Endpoint(IWorkflowDefinitionStore store, IWorkflowDispatcher workflowDispatcher)
+    public Endpoint(IWorkflowDefinitionStore store, IWorkflowDispatcher workflowDispatcher, IIdentityGenerator identityGenerator)
     {
         _store = store;
         _workflowDispatcher = workflowDispatcher;
+        _identityGenerator = identityGenerator;
     }
 
     public override void Configure()
@@ -36,6 +39,7 @@ internal class Endpoint : ElsaEndpoint<Request, Response>
             return;
         }
 
+        var instanceId = request.InstanceId ?? _identityGenerator.GenerateId();
         var correlationId = request.CorrelationId;
         var input = (IDictionary<string, object>?)request.Input;
         var dispatchRequest = new DispatchWorkflowDefinitionRequest
@@ -43,11 +47,12 @@ internal class Endpoint : ElsaEndpoint<Request, Response>
             DefinitionId = request.DefinitionId,
             VersionOptions = VersionOptions.Published,
             Input = input,
+            InstanceId = instanceId,
             CorrelationId = correlationId
         };
 
         await _workflowDispatcher.DispatchAsync(dispatchRequest, cancellationToken);
 
-        await SendOkAsync(new Response(), cancellationToken);
+        await SendOkAsync(new Response(instanceId), cancellationToken);
     }
 }
