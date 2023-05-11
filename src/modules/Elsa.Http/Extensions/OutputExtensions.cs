4e99f37b7745c813c5d1769b54997c0e8347395c
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2023-05-11 16:53:33 +0200

    Fix target type discovery
    When trying to locate a memory block from a reference, we need to search the hierarchy.

--- src/modules/Elsa.Http/Extensions/OutputExtensions.cs
+++ src/modules/Elsa.Http/Extensions/OutputExtensions.cs
@@ -10,13 +10,15 @@ internal static class OutputExtensions
     /// </summary>
     public static Type? GetTargetType(this Output? output, ActivityExecutionContext context)
     {
-        var memoryBlock = output?.MemoryBlockReference() is Variable variable
-            ? context.WorkflowExecutionContext.MemoryRegister.TryGetBlock(variable.Id, out var block)
-                ? block
-                : default
-            : default;
+        var memoryBlockReference = output?.MemoryBlockReference();
+        
+        if (memoryBlockReference is null)
+            return default;
 
-        var parsedContentVariableType = (memoryBlock?.Metadata as VariableBlockMetadata)?.Variable.GetType();
+        if(!context.ExpressionExecutionContext.TryGetBlock(memoryBlockReference, out var memoryBlock))
+            return default;
+        
+        var parsedContentVariableType = (memoryBlock.Metadata as VariableBlockMetadata)?.Variable.GetType();
         return parsedContentVariableType?.GenericTypeArguments.FirstOrDefault();
     }
 }
