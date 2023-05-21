577e8fa30de937ba09eda54523d4c82827ad0cc1
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2023-05-21 10:15:56 +0200

    Fix activity of T result

--- src/modules/Elsa.Workflows.Core/Models/CodeActivity.cs
+++ src/modules/Elsa.Workflows.Core/Models/CodeActivity.cs
@@ -97,36 +97,3 @@ public abstract class CodeActivity<T> : CodeActivity, IActivityWithResult<T>
         set => Result = (Output<T>?)value;
     }
 }
-
-/// <summary>
-/// Base class for custom activities that return a result.
-/// </summary>
-public abstract class Activity<T> : Activity
-{
-    /// <inheritdoc />
-    protected Activity(string? source = default, int? line = default) : base(source, line)
-    {
-    }
-
-    /// <inheritdoc />
-    protected Activity(string activityType, int version = 1, string? source = default, int? line = default) : base(activityType, version, source, line)
-    {
-    }
-
-    /// <inheritdoc />
-    protected Activity(MemoryBlockReference? output, string? source = default, int? line = default) : this(source, line)
-    {
-        if (output != null) Result = new Output<T>(output);
-    }
-
-    /// <inheritdoc />
-    protected Activity(Output<T>? output, string? source = default, int? line = default) : this(source, line)
-    {
-        Result = output;
-    }
-
-    /// <summary>
-    /// The result of the activity.
-    /// </summary>
-    public Output<T>? Result { get; set; }
-}
