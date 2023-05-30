005c0eb6ec37918c941fdca249f8cf377541d72b
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2023-05-30 20:14:18 +0200

    Update HttpEndpoints sample project

---
+++ src/samples/aspnet/Elsa.Samples.AspNet.HttpEndpoints/Activities/MapTo.cs
@@ -0,0 +1,18 @@
+using AutoMapper;
+using Elsa.Extensions;
+using Elsa.Workflows.Core.Models;
+
+namespace Elsa.Samples.AspNet.HttpEndpoints.Activities;
+
+public class MapTo<TSource, TDestination> : CodeActivity<TDestination>
+{
+    public Input<TSource> Source { get; set; } = default!;
+
+    protected override void Execute(ActivityExecutionContext context)
+    {
+        var mapper = context.GetRequiredService<IMapper>();
+        var source = Source.Get(context);
+        var destination = mapper.Map<TDestination>(source);
+        context.SetResult(destination);
+    }
+}
