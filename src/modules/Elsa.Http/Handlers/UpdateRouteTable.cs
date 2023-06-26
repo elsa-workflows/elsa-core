f66941a39e406c9197810626526bcb9ac35a91f7
Gürkan Güran <gurkanguran@gmail.com>
2023-06-26 12:49:03 +0200

    Ensure routes in use wont be deleted from route table

--- src/modules/Elsa.Http/Handlers/UpdateRouteTable.cs
+++ src/modules/Elsa.Http/Handlers/UpdateRouteTable.cs
@@ -1,6 +1,13 @@
-﻿using Elsa.Extensions;
+﻿using Elsa.Common.Models;
+using Elsa.Extensions;
 using Elsa.Http.Contracts;
+using Elsa.Http.Models;
 using Elsa.Mediator.Contracts;
+using Elsa.Workflows.Core.Helpers;
+using Elsa.Workflows.Management.Contracts;
+using Elsa.Workflows.Management.Filters;
+using Elsa.Workflows.Runtime.Contracts;
+using Elsa.Workflows.Runtime.Entities;
 using Elsa.Workflows.Runtime.Notifications;
 
 namespace Elsa.Http.Handlers;
@@ -13,19 +20,35 @@ public class UpdateRouteTable :
     INotificationHandler<WorkflowBookmarksIndexed>
 {
     private readonly IRouteTable _routeTable;
+    private readonly IWorkflowDefinitionStore _workflowDefinitionStore;
+    private readonly ITriggerStore _triggerStore;
+    private readonly IBookmarkStore _bookmarkStore;
     
     /// <summary>
     /// Initializes a new instance of the <see cref="UpdateRouteTable"/> class.
     /// </summary>
-    public UpdateRouteTable(IRouteTable routeTable) => _routeTable = routeTable;
+    public UpdateRouteTable(
+        IRouteTable routeTable, 
+        IWorkflowDefinitionStore workflowDefinitionStore,
+        ITriggerStore triggerStore,
+        IBookmarkStore bookmarkStore)
+    {
+        _routeTable = routeTable;
+        _workflowDefinitionStore = workflowDefinitionStore;
+        _triggerStore = triggerStore;
+        _bookmarkStore = bookmarkStore;
+    }
 
     /// <inheritdoc />
-    public Task HandleAsync(WorkflowTriggersIndexed notification, CancellationToken cancellationToken)
+    public async Task HandleAsync(WorkflowTriggersIndexed notification, CancellationToken cancellationToken)
     {
-        _routeTable.RemoveRoutes(notification.IndexedWorkflowTriggers.RemovedTriggers);
-        _routeTable.AddRoutes(notification.IndexedWorkflowTriggers.AddedTriggers);
-        _routeTable.AddRoutes(notification.IndexedWorkflowTriggers.UnchangedTriggers);
-        return Task.CompletedTask;
+        var triggers = notification.IndexedWorkflowTriggers;
+
+        await EnsureRouteDataIntegrityAsync(triggers.Workflow.Id, triggers.RemovedTriggers, cancellationToken);
+        
+        _routeTable.RemoveRoutes(triggers.RemovedTriggers);
+        _routeTable.AddRoutes(triggers.AddedTriggers);
+        _routeTable.AddRoutes(triggers.UnchangedTriggers);
     }
 
     /// <inheritdoc />
@@ -33,6 +56,30 @@ public class UpdateRouteTable :
     {
         _routeTable.RemoveRoutes(notification.IndexedWorkflowBookmarks.RemovedBookmarks);
         _routeTable.AddRoutes(notification.IndexedWorkflowBookmarks.AddedBookmarks);
+        
         return Task.CompletedTask;
+    }
+    
+    private async Task EnsureRouteDataIntegrityAsync(string workflowId, ICollection<StoredTrigger> removedTriggers, CancellationToken cancellationToken)
+    {
+        // Filter out triggers with routes that are still used by other workflows.
+        
+        var triggerName = ActivityTypeNameHelper.GenerateTypeName<HttpEndpoint>();
+        var paths = removedTriggers
+            .Where(x => x.Name == triggerName && x.Payload != null)
+            .Select(x => x.GetPayload<HttpEndpointBookmarkPayload>().Path).ToList();
+        
+        var workflowDefinitions = (await _workflowDefinitionStore.FindManyAsync(new WorkflowDefinitionFilter { VersionOptions = VersionOptions.Published }, cancellationToken)).ToList();
+        var triggers = await _triggerStore.FindManyAsync(new TriggerFilter { WorkflowDefinitionIds = workflowDefinitions.Select(x => x.DefinitionId).ToList() }, cancellationToken);
+
+        foreach (var path in paths)
+        {
+            if (workflowDefinitions.Any(x => x.Id != workflowId && triggers
+                    .Where(t => t.WorkflowDefinitionId == x.DefinitionId)
+                    .Any(t => t.Payload != null && t.GetPayload<HttpEndpointBookmarkPayload>().Path == path)))
+            {
+                removedTriggers.RemoveWhere(x => x.Name == triggerName && x.Payload != null && x.GetPayload<HttpEndpointBookmarkPayload>().Path == path);
+            }
+        }
     }
 }
