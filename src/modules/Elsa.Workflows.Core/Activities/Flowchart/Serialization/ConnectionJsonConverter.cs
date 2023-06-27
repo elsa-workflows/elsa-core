f84617a2592a1e0f1c83f3570ab7be8cf7bbf6f6
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2023-06-27 10:29:32 +0200

    Make connection JON converter more resilient

--- src/modules/Elsa.Workflows.Core/Activities/Flowchart/Serialization/ConnectionJsonConverter.cs
+++ src/modules/Elsa.Workflows.Core/Activities/Flowchart/Serialization/ConnectionJsonConverter.cs
@@ -1,5 +1,6 @@
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using Elsa.Extensions;
 using Elsa.Workflows.Core.Activities.Flowchart.Models;
 using Elsa.Workflows.Core.Contracts;
 
@@ -30,12 +31,12 @@ public class ConnectionJsonConverter : JsonConverter<Connection>
         var sourceElement = doc.RootElement.GetProperty("source");
         var targetElement = doc.RootElement.GetProperty("target");
         var sourceId = sourceElement.GetProperty("activity").GetString()!;
-        var targetId = targetElement.GetProperty("activity").GetString()!;
+        var targetId = targetElement.TryGetProperty("activity", out var targetIdValue) ? targetIdValue.GetString() : default;
         var sourcePort = sourceElement.GetProperty("port").GetString()!;
-        var targetPort = targetElement.GetProperty("port").GetString()!;
+        var targetPort = targetElement.TryGetProperty("port", out var targetPortValue) ? targetPortValue.GetString() : default;
 
         var sourceActivity = _activities.TryGetValue(sourceId, out var s) ? s : default!;
-        var targetActivity = _activities.TryGetValue(targetId, out var t) ? t : default!;
+        var targetActivity = targetId != null ? _activities.TryGetValue(targetId, out var t) ? t : default! : default!;
         var source = new Endpoint(sourceActivity, sourcePort);
         var target = new Endpoint(targetActivity, targetPort);
         return new Connection(source, target);
