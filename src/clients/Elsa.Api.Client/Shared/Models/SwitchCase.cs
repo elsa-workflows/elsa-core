e3797ab9acedb211b225bf328119be12ac923e6b
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2023-07-17 15:39:38 +0200

    Rename FlowSwitchCase to SwitchCase

--- src/clients/Elsa.Api.Client/Shared/Models/SwitchCase.cs
+++ src/clients/Elsa.Api.Client/Shared/Models/SwitchCase.cs
@@ -1,12 +1,14 @@
+using System.Diagnostics;
+using System.Text.Json.Nodes;
 using Elsa.Api.Client.Contracts;
 using Elsa.Api.Client.Expressions;
 
 namespace Elsa.Api.Client.Shared.Models;
 
 /// <summary>
-/// Represents a case in a flow switch expression.
+/// Represents a case in a switch statement.
 /// </summary>
-public class FlowSwitchCase
+public class SwitchCase
 {
     /// <summary>
     /// Gets or sets the label of the case.
@@ -17,4 +19,9 @@ public class FlowSwitchCase
     /// Gets or sets the condition of the case.
     /// </summary>
     public IExpression Condition { get; set; } = new JavaScriptExpression("");
+
+    /// <summary>
+    /// When used in a <see cref="Switch"/> activity, specifies the activity to schedule when the condition evaluates to true.
+    /// </summary>
+    public JsonObject? Activity { get; set; }
 }
