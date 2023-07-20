58c4e020f45e7a33129ded5c9f576f75d3fd8764
Michel van den Berg <mvb@promontis.nl>
2023-07-20 15:36:33 +0200

    Create WriteWorkflowSuspendExecutionLog.cs

---
+++ src/core/Elsa.Core/Handlers/WriteWorkflowSuspendExecutionLog.cs
@@ -0,0 +1,16 @@
+using Elsa.Events;
+using Elsa.Models;
+using MediatR;
+
+namespace Elsa.Handlers;
+
+public class WriteWorkflowSuspendExecutionLog : INotificationHandler<WorkflowSuspended>
+{
+    public Task Handle(WorkflowSuspended notification, CancellationToken cancellationToken)
+    {
+        var context = notification.WorkflowExecutionContext;
+        var blockingActivity = context.WorkflowInstance.BlockingActivities.First();
+        context.WorkflowExecutionLog.AddEntry(context.WorkflowInstance.Id, blockingActivity.ActivityId, blockingActivity.ActivityType, nameof(WorkflowStatus.Suspended), null, null, null, null);
+        return Task.CompletedTask;
+    }
+}
