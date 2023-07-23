78aa011df8f5e8ef2cea30cd2e8d6c2cd3fa4d65
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2023-07-23 19:38:52 +0200

    Generate default instance ID

--- src/modules/Elsa.Workflows.Runtime/Services/WorkflowHostFactory.cs
+++ src/modules/Elsa.Workflows.Runtime/Services/WorkflowHostFactory.cs
@@ -1,29 +1,39 @@
 using Elsa.Workflows.Core.Activities;
+using Elsa.Workflows.Core.Contracts;
 using Elsa.Workflows.Core.State;
 using Elsa.Workflows.Runtime.Contracts;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Elsa.Workflows.Runtime.Services;
 
+/// <inheritdoc />
 public class WorkflowHostFactory : IWorkflowHostFactory
 {
+    private readonly IIdentityGenerator _identityGenerator;
     private readonly IServiceProvider _serviceProvider;
 
-    public WorkflowHostFactory(IServiceProvider serviceProvider)
+    /// <summary>
+    /// Initializes a new instance of the <see cref="WorkflowHostFactory"/> class.
+    /// </summary>
+    public WorkflowHostFactory(IIdentityGenerator identityGenerator, IServiceProvider serviceProvider)
     {
+        _identityGenerator = identityGenerator;
         _serviceProvider = serviceProvider;
     }
-    
+
+    /// <inheritdoc />
     public Task<IWorkflowHost> CreateAsync(Workflow workflow, WorkflowState workflowState, CancellationToken cancellationToken = default)
     {
         var workflowHost = (IWorkflowHost)ActivatorUtilities.CreateInstance<WorkflowHost>(_serviceProvider, workflow, workflowState);
         return Task.FromResult(workflowHost);
     }
 
+    /// <inheritdoc />
     public Task<IWorkflowHost> CreateAsync(Workflow workflow, CancellationToken cancellationToken = default)
     {
         var workflowState = new WorkflowState
         {
+            Id = _identityGenerator.GenerateId(),
             DefinitionId = workflow.Identity.DefinitionId,
             DefinitionVersion = workflow.Identity.Version
         };
