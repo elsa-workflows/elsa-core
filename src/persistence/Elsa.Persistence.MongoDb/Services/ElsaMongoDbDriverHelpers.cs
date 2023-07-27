559829c62fc04a4af1781643566bfb22c9db814f
Gian Maria <ricci.gianmaria@gmail.com>
2023-07-27 12:49:06 +0200

    Added ability to externalize MongoClient creation (#4262)
    This is needed so the caller program can implement
    a system to have a single MongoClient for the entire
    application. Actually the driver already uses an
    internal cache to have only one MongoClient but
    with this commit we can let the user decide how
    to create the MongoClient.

--- src/persistence/Elsa.Persistence.MongoDb/Services/ElsaMongoDbDriverHelpers.cs
+++ src/persistence/Elsa.Persistence.MongoDb/Services/ElsaMongoDbDriverHelpers.cs
@@ -8,22 +8,29 @@ namespace Elsa.Persistence.MongoDb.Services
     public static class ElsaMongoDbDriverHelpers
     {
         /// <summary>
-        /// Avoid creating too much instances for MongoClient.
+        /// Avoid creating too much instances for MongoClient, usually caller program should
+        /// implmenent a singleton to avoid creating too much Client instance, but actually we
+        /// implement a simple cache to avoid this problem.
         /// </summary>
-        private static readonly ConcurrentDictionary<string, MongoClient> _clients = new();
+        private static readonly ConcurrentDictionary<string, IMongoClient> _clients = new();
 
-        public static MongoClient CreateClient(ElsaMongoDbOptions options)
+        public static IMongoClient CreateClient(ElsaMongoDbOptions options)
         {
             if (string.IsNullOrEmpty(options.ConnectionString)) throw new ArgumentException("Connection string is required.", nameof(options.ConnectionString));
-            if (!_clients.TryGetValue(options.ConnectionString, out MongoClient client))
+            if (!_clients.TryGetValue(options.ConnectionString, out IMongoClient client))
             {
                 var connectionString = options.ConnectionString;
+                var mongoUrl = new MongoUrl(connectionString);
                 var clientSettings = MongoClientSettings.FromConnectionString(connectionString);
-                if (options.UseNewLinq3Provider == false)
+
+                //This will be removed in a future version, we started deprecation
+                if (!options.UseNewLinq3Provider)
                 {
                     clientSettings.LinqProvider = MongoDB.Driver.Linq.LinqProvider.V2;
                 }
-                client = new MongoClient(clientSettings);
+
+                options.ConfigureMongoClientSettings(clientSettings);
+                client = options.MongoClientFactory(clientSettings, mongoUrl);
                 _clients[options.ConnectionString] = client;
             }
 
