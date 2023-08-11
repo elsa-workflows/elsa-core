0a42a296c21a2eecaaedb3d0058beda10e2ffb4d
RuslanSay <rus.sayfetdinov@gmail.com>
2023-08-11 23:00:02 +0300

    add versionOptions to Dispatch endpoint

--- src/modules/Elsa.Workflows.Api/Endpoints/WorkflowDefinitions/Dispatch/Models.cs
+++ src/modules/Elsa.Workflows.Api/Endpoints/WorkflowDefinitions/Dispatch/Models.cs
@@ -1,4 +1,5 @@
 using System.Text.Json.Serialization;
+using Elsa.Common.Models;
 using Elsa.Workflows.Core.Serialization.Converters;
 
 namespace Elsa.Workflows.Api.Endpoints.WorkflowDefinitions.Dispatch;
@@ -9,6 +10,8 @@ internal class Request
     public string? InstanceId { get; set; }
     public string? CorrelationId { get; set; }
     public string? TriggerActivityId { get; set; }
+    
+    public VersionOptions? VersionOptions { get; set; }
 
     [JsonConverter(typeof(ExpandoObjectConverterFactory))]
     public object? Input { get; set; }
