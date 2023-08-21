68a20de07f810a5f7bce65fe3d97151bca095124
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2023-08-22 00:21:02 +0200

    Ignore MemoryBlockReference from Argument when serializing to JSON

--- src/modules/Elsa.Workflows.Core/Models/Argument.cs
+++ src/modules/Elsa.Workflows.Core/Models/Argument.cs
@@ -1,21 +1,36 @@
+using System.Text.Json.Serialization;
 using Elsa.Expressions.Models;
 
 namespace Elsa.Workflows.Core.Models;
 
+/// <summary>
+/// A base type for the <see cref="Input{T}"/> type.
+/// </summary>
 public abstract class Argument
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Argument"/> class.
+    /// </summary>
     protected Argument()
     {
     }
 
+    /// <inheritdoc />
     protected Argument(MemoryBlockReference memoryBlockReference) : this(() => memoryBlockReference)
     {
     }
     
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Argument"/> class.
+    /// </summary>
+    /// <param name="memoryBlockReference"></param>
     protected Argument(Func<MemoryBlockReference> memoryBlockReference)
     {
         MemoryBlockReference = memoryBlockReference;
     }
 
-    public Func<MemoryBlockReference> MemoryBlockReference { get; set; } = default!;
+    /// <summary>
+    /// Gets or sets the memory block reference.
+    /// </summary>
+    [JsonIgnore] public Func<MemoryBlockReference> MemoryBlockReference { get; set; } = default!;
 }
