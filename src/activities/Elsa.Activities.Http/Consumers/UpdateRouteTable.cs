e3f66a84b0194df39d126fd37f4bae3961d9e645
Alessandro Chiavegato <alessandro.chiavegato@dgroove.it>
2023-08-30 16:59:13 +0200

    fix issue 4308

--- src/activities/Elsa.Activities.Http/Consumers/UpdateRouteTable.cs
+++ src/activities/Elsa.Activities.Http/Consumers/UpdateRouteTable.cs
@@ -1,7 +1,17 @@
-﻿using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Elsa.Activities.Http.Bookmarks;
 using Elsa.Activities.Http.Contracts;
 using Elsa.Activities.Http.Extensions;
 using Elsa.Events;
+using Elsa.Models;
+using Elsa.Persistence;
+using Elsa.Persistence.Specifications.Bookmarks;
+using Elsa.Persistence.Specifications.Triggers;
+using Elsa.Services;
+using Elsa.Services.Bookmarks;
+using Rebus.Extensions;
 using Rebus.Handlers;
 
 namespace Elsa.Activities.Http.Consumers
@@ -13,7 +23,23 @@ namespace Elsa.Activities.Http.Consumers
         IHandleMessages<BookmarksDeleted>
     {
         private readonly IRouteTable _routeTable;
-        public UpdateRouteTable(IRouteTable routeTable) => _routeTable = routeTable;
+        private readonly ITriggerStore _triggerStore;
+        private readonly IBookmarkStore _bookmarkStore;
+        private readonly IBookmarkHasher _bookmarkHasher;
+        private readonly BookmarkSerializer _bookmarkSerializer;
+
+        public UpdateRouteTable(
+            IRouteTable routeTable,
+            ITriggerStore triggerStore,
+            IBookmarkStore bookmarkStore,
+            IBookmarkHasher bookmarkHasher)
+        {
+            _routeTable = routeTable;
+            _triggerStore = triggerStore;
+            _bookmarkStore = bookmarkStore;
+            _bookmarkHasher = bookmarkHasher;
+            _bookmarkSerializer = new();
+        }
 
         public Task Handle(TriggerIndexingFinished message)
         {
@@ -21,10 +47,19 @@ namespace Elsa.Activities.Http.Consumers
             return Task.CompletedTask;
         }
 
-        public Task Handle(TriggersDeleted message)
+        public async Task Handle(TriggersDeleted message)
         {
-            _routeTable.RemoveRoutes(message.Triggers);
-            return Task.CompletedTask;
+            WorkflowDefinitionIdSpecification specification = new(message.WorkflowDefinitionId);
+            ICollection<Trigger> currentDefinitionTriggers = (await _triggerStore.FindManyAsync(specification)).ToList();
+
+            bool Exists(Trigger trigger) => currentDefinitionTriggers.Any(x => x.ActivityId == trigger.ActivityId 
+                && x.WorkflowDefinitionId == trigger.WorkflowDefinitionId
+                && x.ModelType == typeof(HttpEndpointBookmark).GetSimpleAssemblyQualifiedName()
+                && trigger.ModelType == typeof(HttpEndpointBookmark).GetSimpleAssemblyQualifiedName()
+                && _bookmarkHasher.Hash(Deserialize(x)) == _bookmarkHasher.Hash(Deserialize(trigger)));
+
+            ICollection<Trigger> triggers = message.Triggers.Where(x => !Exists(x)).ToList();
+            _routeTable.RemoveRoutes(triggers);
         }
 
         public Task Handle(BookmarkIndexingFinished message)
@@ -33,10 +68,23 @@ namespace Elsa.Activities.Http.Consumers
             return Task.CompletedTask;
         }
 
-        public Task Handle(BookmarksDeleted message)
+        public async Task Handle(BookmarksDeleted message)
         {
-            _routeTable.RemoveRoutes (message.Bookmarks);
-            return Task.CompletedTask;
+            WorkflowInstanceIdSpecification specification = new(message.WorkflowInstanceId);
+            ICollection<Bookmark> currentInstanceBookmarks = (await _bookmarkStore.FindManyAsync(specification)).ToList();
+
+            bool Exists(Bookmark bookmark) => currentInstanceBookmarks.Any(x => x.ActivityId == bookmark.ActivityId 
+                && x.WorkflowInstanceId == bookmark.WorkflowInstanceId
+                && x.ModelType == typeof(HttpEndpointBookmark).GetSimpleAssemblyQualifiedName()
+                && bookmark.ModelType == typeof(HttpEndpointBookmark).GetSimpleAssemblyQualifiedName()
+                && _bookmarkHasher.Hash(Deserialize(x)) == _bookmarkHasher.Hash(Deserialize(bookmark)));
+
+            ICollection<Bookmark> bookmarks = message.Bookmarks.Where(x => !Exists(x)).ToList();
+            _routeTable.RemoveRoutes(bookmarks);
         }
+
+        private HttpEndpointBookmark Deserialize(Trigger trigger) => Deserialize(trigger.Model);
+        private HttpEndpointBookmark Deserialize(Bookmark bookmark) => Deserialize(bookmark.Model);
+        private HttpEndpointBookmark Deserialize(string model) => _bookmarkSerializer.Deserialize<HttpEndpointBookmark>(model);
     }
 }
