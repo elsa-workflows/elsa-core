72d8550907196ab49e63881b9c3f154d97c68d26
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2023-09-06 10:25:31 +0200

    Update PostgreSQL for Dapper Upsert dialect

--- src/modules/Elsa.Dapper/Dialects/PostgreSqlDialect.cs
+++ src/modules/Elsa.Dapper/Dialects/PostgreSqlDialect.cs
@@ -7,14 +7,14 @@ namespace Elsa.Dapper.Dialects;
 /// </summary>
 public class PostgreSqlDialect : SqlDialectBase
 {
-
     /// <inheritdoc />
-    public override string Upsert(string table, string primaryKeyField, string[] fields)
+    public override string Upsert(string table, string primaryKeyField, string[] fields, Func<string, string>? getParamName = default)
     {
+        getParamName ??= x => x;
         var fieldList = string.Join(", ", fields);
-        var fieldParamNames = fields.Select(x => $"@{x}");
+        var fieldParamNames = fields.Select(x => $"@{getParamName(x)}");
         var fieldParamList = string.Join(", ", fieldParamNames);
-        var updateList = string.Join(", ", fields.Select(x => $"{x} = @{x}"));
-       return $"insert into {table} ({fieldList}) values ({fieldParamList}) on conflict({primaryKeyField}) do update set {updateList}";
+        var updateList = string.Join(", ", fields.Select(x => $"{x} = @{getParamName(x)}"));
+        return $"insert into {table} ({fieldList}) values ({fieldParamList}) on conflict({primaryKeyField}) do update set {updateList}";
     }
 }
