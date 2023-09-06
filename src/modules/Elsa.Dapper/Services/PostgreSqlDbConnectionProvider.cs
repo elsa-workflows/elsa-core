0ad3418db7aa9dc0d9f46ee9ae2ddf8000a5dbe8
Herbert J Lindner <herbert.lindner@nexxbiz.io>
2023-09-06 10:44:48 +0200

    update provider to match sql lite

--- src/modules/Elsa.Dapper/Services/PostgreSqlDbConnectionProvider.cs
+++ src/modules/Elsa.Dapper/Services/PostgreSqlDbConnectionProvider.cs
@@ -12,8 +12,27 @@ namespace Elsa.Dapper.Services;
 [PublicAPI]
 public class PostgreSqlDbConnectionProvider : IDbConnectionProvider
 {
+    private readonly string _connectionString = "Data Source=elsa.dapper.db";
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PostgreSqlDbConnectionProvider"/> class.
+    /// </summary>
+    public PostgreSqlDbConnectionProvider()
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PostgreSqlDbConnectionProvider"/> class.
+    /// </summary>
+    /// <param name="connectionString">The connection string to use.</param>
+    public PostgreSqlDbConnectionProvider(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+
+
     /// <inheritdoc />
-    public string GetConnectionString() => "Data Source=elsa.dapper.db";
+    public string GetConnectionString() => _connectionString;
 
     /// <inheritdoc />
     public IDbConnection GetConnection()
