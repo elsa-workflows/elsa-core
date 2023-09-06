e9f439ccd224cd0622ab32d33e88e94c581c6c81
Herbert J Lindner <herbert.lindner@nexxbiz.io>
2023-09-06 10:29:30 +0200

    base was changed

--- src/modules/Elsa.Dapper/Dialects/PostgreSqlDialect.cs
+++ src/modules/Elsa.Dapper/Dialects/PostgreSqlDialect.cs
@@ -9,10 +9,11 @@ public class PostgreSqlDialect : SqlDialectBase
 {
 
     /// <inheritdoc />
-    public override string Upsert(string table, string primaryKeyField, string[] fields)
+    public override string Upsert(string table, string primaryKeyField, string[] fields, Func<string, string>? getParamName = default)
     {
+        getParamName ??= x => x;
         var fieldList = string.Join(", ", fields);
-        var fieldParamNames = fields.Select(x => $"@{x}");
+        var fieldParamNames = fields.Select(x => $"@{getParamName(x)}");
         var fieldParamList = string.Join(", ", fieldParamNames);
         var updateList = string.Join(", ", fields.Select(x => $"{x} = @{x}"));
        return $"insert into {table} ({fieldList}) values ({fieldParamList}) on conflict({primaryKeyField}) do update set {updateList}";
