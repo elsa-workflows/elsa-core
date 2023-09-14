8a97a190cc1cc839cc28041c0373865c7cf489e9
kirill <44907112+polferov@users.noreply.github.com>
2023-09-14 09:28:51 +0200

    fix issues with double slashes (#4416)
    * fix issues with double slashes
    
    * implement double slashes fix in RouteTable.cs instead of RouteTableExtensions.cs to make the fix universal.
    
    * add logging to RouteTable.cs to warn about ignored input

--- src/activities/Elsa.Activities.Http/Services/RouteTable.cs
+++ src/activities/Elsa.Activities.Http/Services/RouteTable.cs
@@ -3,6 +3,7 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using Elsa.Activities.Http.Contracts;
 using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Logging;
 
 namespace Elsa.Activities.Http.Services;
 
@@ -10,10 +11,25 @@ public class RouteTable : IRouteTable
 {
     private static readonly object Key = new();
     private readonly IMemoryCache _cache;
-    
-    public RouteTable(IMemoryCache cache) => _cache = cache;
+    private readonly ILogger<RouteTable> _logger;
+
+    public RouteTable(IMemoryCache cache, ILogger<RouteTable> logger)
+    {
+        _cache = cache;
+        _logger = logger;
+    }
+
     private ConcurrentDictionary<string, string> Routes => _cache.GetOrCreate(Key, _ => new ConcurrentDictionary<string, string>());
-    public void Add(string path) => Routes.TryAdd(path, path);
+    public void Add(string path)
+    {
+        if (path.Contains("//"))
+        {
+            _logger.LogWarning("Path {Path} contains double slashes. This is not allowed. The path will be ignored", path);
+            return;
+        }
+        Routes.TryAdd(path, path);
+    }
+
     public void Remove(string path) => Routes.TryRemove(path, out _);
 
     public void AddRange(IEnumerable<string> paths)
