0d06424059e9673b5b30121b857e08570d90d53e
Jérémie DEVILLARD <jdevillard@users.noreply.github.com>
2023-10-05 17:02:33 +0200

    change filename

---
+++ test/integration/Elsa.ServiceBusIntegrationTests/Helpers/ServiceBusProcessorManager.cs
@@ -0,0 +1,53 @@
+﻿using Azure.Messaging.ServiceBus;
+using Elsa.ServiceBusIntegrationTests.Contracts;
+using Moq;
+using Xunit.Abstractions;
+
+namespace Elsa.ServiceBusIntegrationTests.Helpers
+{
+    public class ServiceBusProcessorManager : IServiceBusProcessorManager
+    {
+        private readonly IMock<ServiceBusClient> _serviceBusClient;
+        private readonly ITestOutputHelper _testOutputHelper;
+        private readonly IDictionary<(string topicName, string subscriptionName), ServiceBusProcessorTest> _serviceBusProcessors
+            = new Dictionary<(string topicName, string subscriptionName), ServiceBusProcessorTest>();
+        public ServiceBusProcessorManager(IMock<ServiceBusClient> serviceBusClient, ITestOutputHelper testOutputHelper)
+        {
+            _serviceBusClient = serviceBusClient;
+            _testOutputHelper = testOutputHelper;
+        }
+
+        public ServiceBusProcessorTest Init(string topic ,string subscription)
+        {
+            _serviceBusProcessors.TryGetValue((topic, subscription), out var processor);
+            if (processor != null)
+                throw new ArgumentException($"ServiceBusProcessor with topic:{topic}, subscription:{subscription} already initialized");
+
+            processor = new ServiceBusProcessorTest(_testOutputHelper);
+            (_serviceBusClient as Mock<ServiceBusClient>)
+                .Setup(sb =>
+                    sb.CreateProcessor(It.Is<string>(s => s == topic),
+                    It.IsAny<string>(),
+                    It.IsAny<ServiceBusProcessorOptions>())
+                    )
+                .Callback<string, string, ServiceBusProcessorOptions>(
+                    (t, s, sbO) =>
+                    {
+                        _testOutputHelper.WriteLine("ServiceBusClient");
+                    })
+                .Returns(processor);
+
+            _serviceBusProcessors.Add((topic,subscription), processor);
+            return processor;
+
+        }
+        public ServiceBusProcessorTest Get(string topic, string subscription)
+        {
+            _serviceBusProcessors.TryGetValue((topic, subscription), out var processor);
+            if (processor == null)
+                throw new ArgumentException($"ServiceBusProcessor with topic:{topic}, subscription:{subscription} not exist");
+
+            return processor;
+        }
+    }
+}
---
+++ test/integration/Elsa.ServiceBusIntegrationTests/Helpers/ServiceBusProcessorTest.cs
@@ -0,0 +1,44 @@
+﻿using Azure.Messaging.ServiceBus;
+using System.Text.Json;
+using Xunit.Abstractions;
+
+namespace Elsa.ServiceBusIntegrationTests.Helpers
+{
+    public class ServiceBusProcessorTest : ServiceBusProcessor
+    {
+        private readonly ITestOutputHelper _testOutputHelper;
+
+        public ServiceBusProcessorTest(ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper;
+        }
+        public async Task SendMessage<T>(T payload, string correlationId, int attempt = 1)
+        {
+            var args = CreateMessageArgs(payload, correlationId, attempt);
+            await base.OnProcessMessageAsync(args);
+        }
+
+        public ProcessMessageEventArgs CreateMessageArgs<T>(T payload, string correlationId, int deliveryCount = 1)
+        {
+            var payloadJson = JsonSerializer.Serialize(payload);
+            var props = new Dictionary<string, object>() { };
+         
+            var message = ServiceBusModelFactory.ServiceBusReceivedMessage(
+                body: BinaryData.FromString(payloadJson),
+                deliveryCount: deliveryCount,
+                correlationId: correlationId,
+                properties: props
+                );
+            
+
+            var args = new ProcessMessageEventArgs(message, null, new CancellationToken());
+
+            return args;
+        }
+        public override async Task StartProcessingAsync(CancellationToken cancellationToken = default)
+        {
+            _testOutputHelper.WriteLine("Receiving Service Bus Message");
+        }
+
+    }
+}
