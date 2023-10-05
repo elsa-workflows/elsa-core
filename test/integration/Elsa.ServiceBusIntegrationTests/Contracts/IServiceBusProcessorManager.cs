07b72a351f45bd3795f4047cdfd932925f7fb53a
Jérémie DEVILLARD <jdevillard@users.noreply.github.com>
2023-10-05 17:03:01 +0200

    add workflow test

--- test/integration/Elsa.ServiceBusIntegrationTests/Contracts/IServiceBusProcessorManager.cs
+++ test/integration/Elsa.ServiceBusIntegrationTests/Contracts/IServiceBusProcessorManager.cs
@@ -5,6 +5,6 @@ namespace Elsa.ServiceBusIntegrationTests.Contracts
     public interface IServiceBusProcessorManager
     {
         public ServiceBusProcessorTest Get(string topic, string subscription);
-        ServiceBusProcessorTest Init(string topic, string subscription);
+        public ServiceBusProcessorTest Init(string topic, string subscription);
     }
 }
