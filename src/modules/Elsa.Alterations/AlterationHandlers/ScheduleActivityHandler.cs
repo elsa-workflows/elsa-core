450cc4c0baa6ea2d77b7b6aba1024ee8a351315a
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2023-10-07 10:49:04 +0200

    Update ScheduleActivity alteration with support for activity instance ID

--- src/modules/Elsa.Alterations/AlterationHandlers/ScheduleActivityHandler.cs
+++ src/modules/Elsa.Alterations/AlterationHandlers/ScheduleActivityHandler.cs
@@ -15,47 +15,70 @@ public class ScheduleActivityHandler : AlterationHandlerBase<ScheduleActivity>
     /// <inheritdoc />
     protected override async ValueTask HandleAsync(AlterationHandlerContext context, ScheduleActivity alteration)
     {
-        var workflowExecutionContext = context.WorkflowExecutionContext;
-        var activityId = alteration.ActivityId;
-        var activity = workflowExecutionContext.FindActivityById(activityId);
-        
-        if (activity == null)
+        if (alteration.ActivityInstanceId == null && alteration.ActivityId == null)
         {
-            context.Fail($"Activity with ID {activityId} not found");
+            context.Fail("Either ActivityInstanceId or ActivityId must be specified");
             return;
         }
-        
-        // Check to see if there's an existing activity execution context for the activity.
-        var existingActivityExecutionContext = workflowExecutionContext.ActiveActivityExecutionContexts.FirstOrDefault(x => x.Activity.Id == activityId);
-        
+
+        var workflowExecutionContext = context.WorkflowExecutionContext;
+        var existingActivityExecutionContext = GetActivityExecutionContext(context, alteration);
+
         if (existingActivityExecutionContext != null)
         {
             // If the activity is in a faulted state, reset it to Running.
             if (existingActivityExecutionContext.Status == ActivityStatus.Faulted)
                 existingActivityExecutionContext.Status = ActivityStatus.Running;
-            
+
             // Schedule the activity execution context.
             workflowExecutionContext.ScheduleActivityExecutionContext(existingActivityExecutionContext);
             context.Succeed();
             return;
         }
-        
-        var activityNode = workflowExecutionContext.FindNodeByActivity(activity)!;
+
+        var activityId = alteration.ActivityId;
+
+        if (activityId == null)
+        {
+            context.Fail("No existing activity execution context was found and no activity ID was specified");
+            return;
+        }
+
+        var activityNode = workflowExecutionContext.FindNodeByActivityId(activityId);
+
+        if (activityNode == null)
+        {
+            context.Fail($"Activity with ID {activityId} not found");
+            return;
+        }
+
+        // Find the parent activity execution context within which to schedule the activity.
         var parentActivityContexts = workflowExecutionContext.ActiveActivityExecutionContexts.Reverse().ToList();
 
         var parentExecutionContext =
             (from ancestorNode in activityNode.Ancestors()
-            from parentActivityContext in parentActivityContexts
-            where parentActivityContext.Activity.Id == ancestorNode.Activity.Id
-            select parentActivityContext).FirstOrDefault();
-        
+                from parentActivityContext in parentActivityContexts
+                where parentActivityContext.Activity.Id == ancestorNode.Activity.Id
+                select parentActivityContext).FirstOrDefault();
+
         if (parentExecutionContext == null)
         {
             context.Fail($"Could not find parent activity execution context for activity with ID {activityId}");
             return;
         }
 
-        await parentExecutionContext.SendSignalAsync(new ScheduleChildActivity(activity));
+        await parentExecutionContext.SendSignalAsync(new ScheduleChildActivity(activityNode.Activity));
         context.Succeed();
+    }
+
+    private static ActivityExecutionContext? GetActivityExecutionContext(AlterationHandlerContext context, ScheduleActivity alteration)
+    {
+        if (alteration.ActivityInstanceId != null)
+            return context.WorkflowExecutionContext.ActivityExecutionContexts.FirstOrDefault(x => x.Id == alteration.ActivityInstanceId);
+
+        if (alteration.ActivityId != null)
+            return context.WorkflowExecutionContext.ActiveActivityExecutionContexts.FirstOrDefault(x => x.Activity.Id == alteration.ActivityId);
+
+        return null;
     }
 }
--- src/modules/Elsa.Alterations/AlterationTypes/ScheduleActivity.cs
+++ src/modules/Elsa.Alterations/AlterationTypes/ScheduleActivity.cs
@@ -8,7 +8,12 @@ namespace Elsa.Alterations.AlterationTypes;
 public class ScheduleActivity : AlterationBase
 {
     /// <summary>
-    /// The ID of the next activity to be scheduled.
+    /// The ID of the next activity to be scheduled. If not specified, the ActivityInstanceId will be used.
     /// </summary>
-    public string ActivityId { get; set; } = default!;
+    public string? ActivityId { get; set; }
+
+    /// <summary>
+    /// The ID of the activity instance to be scheduled. If not specified, the ActivityId will be used.
+    /// </summary>
+    public string? ActivityInstanceId { get; set; }
 }
