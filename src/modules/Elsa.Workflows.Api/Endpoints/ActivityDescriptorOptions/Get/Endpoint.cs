e36b5ae41bfb3414334f1eb94dafd64f4cc0127d
Jérémie DEVILLARD <jdevillard@users.noreply.github.com>
2023-11-09 10:06:12 +0100

    add Endpoint to fetch data from options provider

---
+++ src/modules/Elsa.Workflows.Api/Endpoints/ActivityDescriptorOptions/Get/Endpoint.cs
@@ -0,0 +1,49 @@
+using Elsa.Abstractions;
+using Elsa.Workflows.Core.Contracts;
+using Elsa.Workflows.Core.Models;
+using Elsa.Workflows.Core.Services;
+using JetBrains.Annotations;
+
+namespace Elsa.Workflows.Api.Endpoints.ActivityDescriptorOptions.Get;
+
+[PublicAPI]
+internal class Get : ElsaEndpoint<Request, Response>
+{
+    private readonly IServiceProvider _serviceProvider;
+    private readonly IActivityRegistry _registry;
+    private readonly IPropertyOptionsResolver _optionsResolver;
+
+    /// <inheritdoc />
+    public Get(IServiceProvider serviceProvider, IActivityRegistry registry, IPropertyOptionsResolver optionsResolver)
+    {
+        _serviceProvider = serviceProvider;
+        _registry = registry;
+        _optionsResolver = optionsResolver;
+    }
+
+    /// <inheritdoc />
+    public override void Configure()
+    {
+        Get("/descriptors/activities/{typeName}/options/{propertyName}");
+        ConfigurePermissions("read:*", "read:activity-descriptors-options");
+    }
+
+    /// <inheritdoc />
+    public override async Task HandleAsync(Request request, CancellationToken cancellationToken)
+    {
+        var descriptor = request.Version == null ? _registry.Find(request.TypeName) : _registry.Find(request.TypeName, request.Version.Value);
+        if (descriptor == null)
+            await SendNotFoundAsync(cancellationToken);
+
+        var propertyDescriptor =  descriptor!.Inputs.FirstOrDefault(x => x.Name == request.PropertyName);
+
+        if(propertyDescriptor == null)
+            await SendNotFoundAsync(cancellationToken);
+
+        var optionsResolver = new PropertyOptionsResolver(_serviceProvider);
+
+        var inputOptions = await optionsResolver.GetOptionsAsync(propertyDescriptor!.PropertyInfo, cancellationToken);
+
+        await SendOkAsync(new Response(inputOptions), cancellationToken);
+    }
+}
---
+++ src/modules/Elsa.Workflows.Api/Endpoints/ActivityDescriptorOptions/Get/Models.cs
@@ -0,0 +1,21 @@
+using JetBrains.Annotations;
+
+namespace Elsa.Workflows.Api.Endpoints.ActivityDescriptorOptions.Get;
+
+internal class Request
+{
+    public string TypeName { get; set; }
+    public int? Version { get; set; }
+    public string PropertyName { get; set; }
+}
+
+[PublicAPI]
+internal class Response
+{
+    public Response(IDictionary<string,object> items)
+    {
+        Items = items;
+    }
+
+    public IDictionary<string, object> Items { get; set; }
+}
