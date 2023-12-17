916256e9f603601839ebf436051cde2aa22c2fed
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2023-12-17 21:26:58 +0100

    Change ActivityKind from Job to Task in BulkDispatchWorkflows
    The ActivityKind property in BulkDispatchWorkflows class has been changed from Job to Task. This updates the type of activity associated with the BulkDispatchWorkflows process within the Elsa workflow runtime module.

--- src/modules/Elsa.Workflows.Runtime/Activities/BulkDispatchWorkflows.cs
+++ src/modules/Elsa.Workflows.Runtime/Activities/BulkDispatchWorkflows.cs
@@ -22,7 +22,7 @@ namespace Elsa.Workflows.Runtime.Activities;
 /// <summary>
 /// Creates new workflow instances of the specified workflow for each item in the data source and dispatches them for execution.
 /// </summary>
-[Activity("Elsa", "Composition", "Create new workflow instances for each item in the data source and dispatch them for execution.", Kind = ActivityKind.Job)]
+[Activity("Elsa", "Composition", "Create new workflow instances for each item in the data source and dispatch them for execution.", Kind = ActivityKind.Task)]
 [FlowNode("Finished", "Canceled", "Done")]
 [UsedImplicitly]
 public class BulkDispatchWorkflows : Activity
