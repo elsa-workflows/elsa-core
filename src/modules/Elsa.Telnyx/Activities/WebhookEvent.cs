5e95daaa7c251b296b47936610e1d9f991f0a3be
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2023-12-18 20:20:20 +0100

    Refactor code for readability and improved logging
    Code in several activity files and Worker.cs that involve creating bookmarks and handling messages received was refactored to improve readability and clarity. The instantiated "CreateBookmarkArgs" parameter was split into its individual arguments for improved readability. Logging in the Worker.cs file was also fine-tuned, with explicit count of workflows triggered by service bus messages being included in the debug log.

--- src/modules/Elsa.Telnyx/Activities/WebhookEvent.cs
+++ src/modules/Elsa.Telnyx/Activities/WebhookEvent.cs
@@ -20,18 +20,18 @@ namespace Elsa.Telnyx.Activities;
 public class WebhookEvent : Activity<Payload>
 {
     /// <inheritdoc />
-    public WebhookEvent([CallerFilePath]string? source = default, [CallerLineNumber]int? line = default) : base(source, line)
+    public WebhookEvent([CallerFilePath] string? source = default, [CallerLineNumber] int? line = default) : base(source, line)
     {
     }
 
     /// <inheritdoc />
-    public WebhookEvent(string eventType, string activityTypeName, Variable<Payload> result, int version = 1, [CallerFilePath]string? source = default, [CallerLineNumber]int? line = default) 
+    public WebhookEvent(string eventType, string activityTypeName, Variable<Payload> result, int version = 1, [CallerFilePath] string? source = default, [CallerLineNumber] int? line = default)
         : base(activityTypeName, version, source, line)
     {
         EventType = eventType;
         Result = new(result);
     }
-    
+
     /// <summary>
     /// The Telnyx webhook event type to listen for.
     /// </summary>
@@ -47,8 +47,13 @@ public class WebhookEvent : Activity<Payload>
         {
             var eventType = EventType;
             var payload = new WebhookEventBookmarkPayload(eventType);
-            
-            context.CreateBookmark(new CreateBookmarkArgs(payload, Resume, Type, false));
+
+            context.CreateBookmark(new CreateBookmarkArgs
+            {
+                Payload = payload,
+                Callback = Resume,
+                BookmarkName = Type
+            });
         }
     }
 
