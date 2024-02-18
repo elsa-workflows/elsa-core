8d24311e9518d621c3ba3884abbeaaa16915c9ce
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2024-02-18 23:30:21 +0100

    Update src/modules/Elsa.Workflows.Management/Models/TimestampFilter.cs
    Co-authored-by: Béchir BEN AMEUR <32399944+bbenameur@users.noreply.github.com>

--- src/modules/Elsa.Workflows.Management/Models/TimestampFilter.cs
+++ src/modules/Elsa.Workflows.Management/Models/TimestampFilter.cs
@@ -12,7 +12,7 @@ public class TimestampFilter
     /// <summary>
     /// Gets or sets the column to filter by.
     /// </summary>
-    public string Column { get; set; } = default!;
+    public required string Column { get; set; } = default!;
 
     /// <summary>
     /// Gets or sets the operator to use for filtering.
