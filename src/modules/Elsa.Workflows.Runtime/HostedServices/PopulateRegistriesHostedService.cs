0c18cb62ba8785f1201c9f0b56e0bc1a88f6982e
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2024-02-22 11:52:33 +0100

    Implement distributed lock for registry population
    Added distributed lock in 'PopulateRegistriesHostedService' to prevent concurrent registry updates. Also implemented a semaphore in 'DefaultWorkflowDefinitionStorePopulator' to control access to shared resources during add or update operations. This change helps to ensure the integrity and consistency of the workflow registries.

--- src/modules/Elsa.Workflows.Runtime/HostedServices/PopulateRegistriesHostedService.cs
+++ src/modules/Elsa.Workflows.Runtime/HostedServices/PopulateRegistriesHostedService.cs
@@ -1,5 +1,7 @@
 using Elsa.Workflows.Contracts;
 using Elsa.Workflows.Runtime.Contracts;
+using JetBrains.Annotations;
+using Medallion.Threading;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -8,24 +10,30 @@ namespace Elsa.Workflows.Runtime.HostedServices;
 /// <summary>
 /// Updates the workflow store from <see cref="IWorkflowProvider"/> implementations, creates triggers and updates the <see cref="IActivityRegistry"/>.
 /// </summary>
+[UsedImplicitly]
 public class PopulateRegistriesHostedService : IHostedService
 {
+    private readonly IDistributedLockProvider _distributedLockProvider;
     private readonly IServiceScopeFactory _scopeFactory;
 
     /// <summary>
     /// Initializes a new instance of the <see cref="PopulateRegistriesHostedService"/> class.
     /// </summary>
-    public PopulateRegistriesHostedService(IServiceScopeFactory scopeFactory)
+    public PopulateRegistriesHostedService(IDistributedLockProvider distributedLockProvider, IServiceScopeFactory scopeFactory)
     {
+        _distributedLockProvider = distributedLockProvider;
         _scopeFactory = scopeFactory;
     }
 
     /// <inheritdoc />
     public async Task StartAsync(CancellationToken cancellationToken)
     {
-        using var scope = _scopeFactory.CreateScope();
-        var registriesPopulator = scope.ServiceProvider.GetRequiredService<IRegistriesPopulator>();
-        await registriesPopulator.PopulateAsync(cancellationToken);
+        await using (await _distributedLockProvider.AcquireLockAsync("PopulateRegistries", TimeSpan.FromMinutes(10), cancellationToken))
+        {
+            using var scope = _scopeFactory.CreateScope();
+            var registriesPopulator = scope.ServiceProvider.GetRequiredService<IRegistriesPopulator>();
+            await registriesPopulator.PopulateAsync(cancellationToken);
+        }
     }
 
     /// <inheritdoc />
