1a112ff4a885084b63a4d8cd6d7de132946e25ee
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2024-03-01 16:56:00 +0100

    Add V3_1 migration for Elsa
    This commit introduces the V3_1 migration for the Elsa module. Changes involve the introduction of new tables and alteration of existing tables. Specifically, a new 'KeyValuePairs' table was added and several columns on various tables were renamed or altered.

---
+++ src/modules/Elsa.EntityFrameworkCore.Sqlite/Migrations/Management/20240229205101_V3_1.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Elsa.EntityFrameworkCore.Sqlite.Migrations.Management
+{
+    /// <inheritdoc />
+    public partial class V3_1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DataCompressionAlgorithm",
+                table: "WorkflowInstances",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DataCompressionAlgorithm",
+                table: "WorkflowInstances");
+        }
+    }
+}
