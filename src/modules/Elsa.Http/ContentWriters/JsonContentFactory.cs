0146d427eb369ecfd9268627cef96d21f8a96a7d
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2024-03-08 14:20:48 +0100

    Add Unicode encoding support to serializers
    The update enriches the serializers with Unicode encoding to handle a wider range of characters. Classes such as ConfigurableSerializer, ObjectConverter, and JsonContentFactory have been modified to use JavaScriptEncoder with UnicodeRanges.All, supporting serialization and deserialization of all Unicode characters. Furthermore, any unreferenced code now carries a note for JSON serializer stating that it's incompatible with .NET's trimming feature.

--- src/modules/Elsa.Http/ContentWriters/JsonContentFactory.cs
+++ src/modules/Elsa.Http/ContentWriters/JsonContentFactory.cs
@@ -1,6 +1,9 @@
+using System.Diagnostics.CodeAnalysis;
 using System.Net.Mime;
 using System.Text;
+using System.Text.Encodings.Web;
 using System.Text.Json;
+using System.Text.Unicode;
 
 namespace Elsa.Http.ContentWriters;
 
@@ -13,6 +16,7 @@ public class JsonContentFactory : IHttpContentFactory
     public IEnumerable<string> SupportedContentTypes => new[] { MediaTypeNames.Application.Json, "text/json" };
 
     /// <inheritdoc />
+    [RequiresUnreferencedCode("The JsonSerializer type is not trim-compatible.")]
     public HttpContent CreateHttpContent(object content, string contentType)
     {
         var text = content as string ?? JsonSerializer.Serialize(content);
