d137e4dc72f050d51b838989814fcc3a4631e69d
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2024-03-12 17:43:49 +0100

    Remove outdated workflow activities
    Removed several deprecated workflow activities pertaining to lead and deal updates in preparation for new workflow structure implementation.

--- src/modules/Elsa.JavaScript/Services/JintJavaScriptEvaluator.cs
+++ src/modules/Elsa.JavaScript/Services/JintJavaScriptEvaluator.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics.CodeAnalysis;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Text.RegularExpressions;
@@ -36,6 +37,7 @@ public class JintJavaScriptEvaluator : IJavaScriptEvaluator
     }
 
     /// <inheritdoc />
+    [RequiresUnreferencedCode("The Jint library uses reflection and can't be statically analyzed.")]
     public async Task<object?> EvaluateAsync(string expression,
         Type returnType,
         ExpressionExecutionContext context,
@@ -53,22 +55,24 @@ public class JintJavaScriptEvaluator : IJavaScriptEvaluator
     {
         options ??= new ExpressionEvaluatorOptions();
 
-        var engine = new Engine(opts =>
+        var engineOptions = new Jint.Options();;
+        
+        if (_jintOptions.AllowClrAccess)
+            engineOptions.AllowClr();
+
+        // Wrap objects in ObjectWrapper instances and set their prototype to Array.prototype if they are array-like.
+        engineOptions.SetWrapObjectHandler((engine, target, type) =>
         {
-            if (_jintOptions.AllowClrAccess)
-                opts.AllowClr();
+            var instance = new ObjectWrapper(engine, target);
 
-            // Wrap objects in ObjectWrapper instances and set their prototype to Array.prototype if they are array-like.
-            opts.SetWrapObjectHandler((engine, target, type) =>
-            {
-                var instance = new ObjectWrapper(engine, target);
+            if (ObjectArrayHelper.DetermineIfObjectIsArrayLikeClrCollection(target.GetType()))
+                instance.Prototype = engine.Intrinsics.Array.PrototypeObject;
 
-                if (ObjectArrayHelper.DetermineIfObjectIsArrayLikeClrCollection(target.GetType()))
-                    instance.Prototype = engine.Intrinsics.Array.PrototypeObject;
-
-                return instance;
-            });
+            return instance;
         });
+            
+        await _mediator.SendAsync(new CreatingJavaScriptEngine(engineOptions, context), cancellationToken);
+        var engine = new Engine(engineOptions);
 
         configureEngine?.Invoke(engine);
 
