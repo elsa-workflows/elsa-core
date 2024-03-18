d1f547255798c9c42c0b21400d0586692a38a54a
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2024-03-18 19:17:30 +0100

    Add multi-tenancy support to Dapper provider
    Introduced TenantId to various tables and records to enable multi-tenancy. Migration scripts have been added across modules for managing TenantId columns effectively.

---
+++ src/modules/Elsa.Dapper.Migrations/Management/V3_2.cs
@@ -0,0 +1,27 @@
+using System.Diagnostics.CodeAnalysis;
+using FluentMigrator;
+using JetBrains.Annotations;
+using static System.Int32;
+
+namespace Elsa.Dapper.Migrations.Management;
+
+/// <inheritdoc />
+[Migration(10003, "Elsa:Management:V3.2")]
+[PublicAPI]
+[SuppressMessage("ReSharper", "InconsistentNaming")]
+public class V3_2 : Migration
+{
+    /// <inheritdoc />
+    public override void Up()
+    {
+        Alter.Table("WorkflowDefinitions").AddColumn("TenantId").AsString().Nullable();
+        Alter.Table("WorkflowInstances").AddColumn("TenantId").AsString().Nullable();
+    }
+
+    /// <inheritdoc />
+    public override void Down()
+    {
+        Delete.Column("TenantId").FromTable("WorkflowDefinitions");
+        Delete.Column("TenantId").FromTable("WorkflowInstances");
+    }
+}
---
+++ src/modules/Elsa.Dapper.Migrations/Runtime/V3_2.cs
@@ -0,0 +1,37 @@
+using System.Diagnostics.CodeAnalysis;
+using FluentMigrator;
+using JetBrains.Annotations;
+using static System.Int32;
+
+namespace Elsa.Dapper.Migrations.Runtime;
+
+/// <inheritdoc />
+[Migration(20003, "Elsa:Runtime:V3.2")]
+[PublicAPI]
+[SuppressMessage("ReSharper", "InconsistentNaming")]
+public class V3_2 : Migration
+{
+    /// <inheritdoc />
+    public override void Up()
+    {
+        Alter.Table("WorkflowInstances").AddColumn("TenantId").AsString().Nullable();
+        Alter.Table("Triggers").AddColumn("TenantId").AsString().Nullable();
+        Alter.Table("Bookmarks").AddColumn("TenantId").AsString().Nullable();
+        Alter.Table("WorkflowExecutionLogRecords").AddColumn("TenantId").AsString().Nullable();
+        Alter.Table("ActivityExecutionRecords").AddColumn("TenantId").AsString().Nullable();
+        Alter.Table("WorkflowInboxMessages").AddColumn("TenantId").AsString().Nullable();
+        Alter.Table("KeyValuePairs").AddColumn("TenantId").AsString().Nullable();
+    }
+
+    /// <inheritdoc />
+    public override void Down()
+    {
+        Delete.Column("TenantId").FromTable("WorkflowInstances");
+        Delete.Column("TenantId").FromTable("Triggers");
+        Delete.Column("TenantId").FromTable("Bookmarks");
+        Delete.Column("TenantId").FromTable("WorkflowExecutionLogRecords");
+        Delete.Column("TenantId").FromTable("ActivityExecutionRecords");
+        Delete.Column("TenantId").FromTable("WorkflowInboxMessages");
+        Delete.Column("TenantId").FromTable("KeyValuePairs");
+    }
+}
