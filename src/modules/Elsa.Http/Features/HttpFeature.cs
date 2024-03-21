7848c1475b43a996ca08284a740f8f180a0c4e4f
Alexis Delarre <alexis.delarre@stereograph.fr>
2024-03-21 10:08:51 +0100

    issue 5112: Allows string support for HttpFileResponse activity

--- src/modules/Elsa.Http/Features/HttpFeature.cs
+++ src/modules/Elsa.Http/Features/HttpFeature.cs
@@ -58,7 +58,7 @@ public class HttpFeature : FeatureBase
     public Func<IServiceProvider, IHttpEndpointAuthorizationHandler> HttpEndpointAuthorizationHandler { get; set; } = sp => sp.GetRequiredService<AuthenticationBasedHttpEndpointAuthorizationHandler>();
 
     /// <summary>
-    /// A delegate that is invoked when an HTTP workflow faults. 
+    /// A delegate that is invoked when an HTTP workflow faults.
     /// </summary>
     public Func<IServiceProvider, IHttpEndpointFaultHandler> HttpEndpointWorkflowFaultHandler { get; set; } = sp => sp.GetRequiredService<DefaultHttpEndpointFaultHandler>();
 
@@ -160,7 +160,7 @@ public class HttpFeature : FeatureBase
             .AddRequestHandler<ValidateWorkflowRequestHandler, ValidateWorkflowRequest, ValidateWorkflowResponse>()
             .AddNotificationHandler<UpdateRouteTable>()
 
-            // Content parsers.            
+            // Content parsers.
             .AddSingleton<IHttpContentParser, JsonHttpContentParser>()
             .AddSingleton<IHttpContentParser, XmlHttpContentParser>()
             .AddSingleton<IHttpContentParser, PlainTextHttpContentParser>()
@@ -192,6 +192,7 @@ public class HttpFeature : FeatureBase
             .AddScoped<IDownloadableContentHandler, FormFileDownloadableContentHandler>()
             .AddScoped<IDownloadableContentHandler, DownloadableDownloadableContentHandler>()
             .AddScoped<IDownloadableContentHandler, UrlDownloadableContentHandler>()
+            .AddScoped<IDownloadableContentHandler, StringDownloadableContentHandler>()
 
             // File caches.
             .AddScoped(FileCache)
