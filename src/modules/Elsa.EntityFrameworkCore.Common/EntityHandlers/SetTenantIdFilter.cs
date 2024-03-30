2f5b2ed1b284333332680ee8c279ee92b5133264
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2024-03-30 20:15:21 +0100

    Refactor TenantId filter application in Elsa module
    The SetTenantIdFilter class in the Elsa.EntityFrameworkCore.Common module has been refactored to improve readability and performance. Flattening of control blocks has been done for better comprehensibility. Additionally, the logic has been revised to apply a TenantId filter only if an entity is assignable to the shared Entity base class.

--- src/modules/Elsa.EntityFrameworkCore.Common/EntityHandlers/SetTenantIdFilter.cs
+++ src/modules/Elsa.EntityFrameworkCore.Common/EntityHandlers/SetTenantIdFilter.cs
@@ -8,21 +8,21 @@ using Microsoft.EntityFrameworkCore.Query;
 namespace Elsa.EntityFrameworkCore.Common.EntityHandlers;
 
 /// <summary>
-/// 
+/// Represents a class that applies a filter to set the TenantId for entities.
 /// </summary>
 public class SetTenantIdFilter : IEntityModelCreatingHandler
 {
     /// <inheritdoc />
     public void Handle(ElsaDbContextBase dbContext, ModelBuilder modelBuilder, IMutableEntityType entityType)
     {
-        if (entityType.ClrType.IsAssignableTo(typeof(Entity)))
-        {
-            var parameter = Expression.Parameter(entityType.ClrType);
-            Expression<Func<Entity, bool>> filterExpr = entity => dbContext.TenantId == entity.TenantId;
-            var body = ReplacingExpressionVisitor.Replace(filterExpr.Parameters[0], parameter, filterExpr.Body);
-            var lambdaExpression = Expression.Lambda(body, parameter);
+        if (!entityType.ClrType.IsAssignableTo(typeof(Entity)))
+            return;
 
-            entityType.SetQueryFilter(lambdaExpression);
-        }
+        var parameter = Expression.Parameter(entityType.ClrType);
+        Expression<Func<Entity, bool>> filterExpr = entity => dbContext.TenantId == entity.TenantId;
+        var body = ReplacingExpressionVisitor.Replace(filterExpr.Parameters[0], parameter, filterExpr.Body);
+        var lambdaExpression = Expression.Lambda(body, parameter);
+
+        entityType.SetQueryFilter(lambdaExpression);
     }
 }
