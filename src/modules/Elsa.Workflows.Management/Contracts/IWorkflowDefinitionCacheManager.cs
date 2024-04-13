3287d0a50de41bb3e7354eff41a8b78c684310c2
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2024-04-13 10:53:39 +0200

    Update workflow interfaces to support filter queries
    The update extends `IWorkflowDefinitionCacheManager` and `IWorkflowDefinitionService` interfaces. Functions are added to allow creating filter cache keys and finding workflow definitions and workflows using a new `WorkflowDefinitionFilter`. This enhances querying flexibility by enabling filtered searches.

--- src/modules/Elsa.Workflows.Management/Contracts/IWorkflowDefinitionCacheManager.cs
+++ src/modules/Elsa.Workflows.Management/Contracts/IWorkflowDefinitionCacheManager.cs
@@ -1,12 +1,15 @@
 using Elsa.Common.Models;
+using Elsa.Workflows.Management.Filters;
 
 namespace Elsa.Workflows.Management.Contracts;
 
-public interface IWorkflowDefinitionCacheManager
+internal interface IWorkflowDefinitionCacheManager
 {
     string CreateWorkflowDefinitionVersionCacheKey(string definitionId, VersionOptions versionOptions);
+    string CreateWorkflowDefinitionFilterCacheKey(WorkflowDefinitionFilter filter);
     string CreateWorkflowVersionCacheKey(string definitionId, VersionOptions versionOptions);
     string CreateWorkflowVersionCacheKey(string definitionVersionId);
+    string CreateWorkflowFilterCacheKey(WorkflowDefinitionFilter filter);
     string CreateWorkflowDefinitionVersionCacheKey(string definitionVersionId);
     string CreateWorkflowDefinitionChangeTokenKey(string definitionId);
     Task EvictWorkflowDefinitionAsync(string definitionId, CancellationToken cancellationToken = default);
--- src/modules/Elsa.Workflows.Management/Contracts/IWorkflowDefinitionService.cs
+++ src/modules/Elsa.Workflows.Management/Contracts/IWorkflowDefinitionService.cs
@@ -1,6 +1,7 @@
 using Elsa.Common.Models;
 using Elsa.Workflows.Activities;
 using Elsa.Workflows.Management.Entities;
+using Elsa.Workflows.Management.Filters;
 
 namespace Elsa.Workflows.Management.Contracts;
 
@@ -25,6 +26,11 @@ public interface IWorkflowDefinitionService
     Task<WorkflowDefinition?> FindWorkflowDefinitionAsync(string definitionVersionId, CancellationToken cancellationToken = default);
     
     /// <summary>
+    /// Looks for a <see cref="WorkflowDefinition"/> by the specified <see cref="WorkflowDefinitionFilter"/>.
+    /// </summary>
+    Task<WorkflowDefinition?> FindWorkflowDefinitionAsync(WorkflowDefinitionFilter filter, CancellationToken cancellationToken = default);
+    
+    /// <summary>
     /// Looks for a <see cref="Workflow"/> by the specified definition ID and <see cref="VersionOptions"/>.
     /// </summary>
     Task<Workflow?> FindWorkflowAsync(string definitionId, VersionOptions versionOptions, CancellationToken cancellationToken = default);
@@ -33,4 +39,9 @@ public interface IWorkflowDefinitionService
     /// Looks for a <see cref="Workflow"/> by the specified version ID.
     /// </summary>
     Task<Workflow?> FindWorkflowAsync(string definitionVersionId, CancellationToken cancellationToken = default);
+    
+    /// <summary>
+    /// Looks for a <see cref="Workflow"/> by the specified <see cref="WorkflowDefinitionFilter"/>.
+    /// </summary>
+    Task<Workflow?> FindWorkflowAsync(WorkflowDefinitionFilter filter, CancellationToken cancellationToken = default);
 }
