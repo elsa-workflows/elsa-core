2abe58fa10dfe238f534f206df92c7aeee12f9c3
Raymond den Haan <raymond.den.haan@nexxbiz.io>
2024-05-13 16:16:45 +0200

    Update activity registry & workflow definitions
    This commit removes unnecessary calls related to populating activity registry and reinitializing workflow definitions.

--- src/modules/Elsa.Workflows.Management/Contracts/IActivityRegistryPopulator.cs
+++ src/modules/Elsa.Workflows.Management/Contracts/IActivityRegistryPopulator.cs
@@ -12,13 +12,6 @@ public interface IActivityRegistryPopulator
     /// </summary>
     /// <param name="cancellationToken">The cancellation token.</param>
     Task PopulateRegistryAsync(CancellationToken cancellationToken = default);
-    
-    /// <summary>
-    /// Populates the <see cref="IActivityRegistry"/> with activities for the specified provider.
-    /// </summary>
-    /// <param name="providerType">The type of the provider.</param>
-    /// <param name="cancellationToken">The cancellation token.</param>
-    Task PopulateRegistryAsync(Type providerType, CancellationToken cancellationToken = default);
 
     /// <summary>
     /// Tries to add a workflow as an activity to the registry.
