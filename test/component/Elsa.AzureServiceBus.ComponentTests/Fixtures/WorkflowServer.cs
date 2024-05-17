61ab247afdb7ab4fc7d4fd8391735c062c722807
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2024-05-17 23:25:50 +0100

    Refactor Azure service bus testing setup to separate extension
    This commit abstracts the setup for Azure service bus testing into a separate extension named AzureServiceBusServiceCollectionExtensions. The code has been removed from the WorkflowServer class, contributing to a cleaner and more modular codebase. This enhancement will facilitate better unit testing and reduce redundancy in test setup.

--- test/component/Elsa.AzureServiceBus.ComponentTests/Fixtures/WorkflowServer.cs
+++ test/component/Elsa.AzureServiceBus.ComponentTests/Fixtures/WorkflowServer.cs
@@ -1,7 +1,5 @@
 using System.Net.Http.Headers;
-using System.Text.Json;
-using Azure.Messaging.ServiceBus;
-using Azure.Messaging.ServiceBus.Administration;
+using Elsa.AzureServiceBus.ComponentTests.Extensions;
 using Elsa.EntityFrameworkCore.Extensions;
 using Elsa.EntityFrameworkCore.Modules.Management;
 using Elsa.EntityFrameworkCore.Modules.Runtime;
@@ -11,38 +9,18 @@ using Elsa.MassTransit.Extensions;
 using Elsa.Testing.Shared;
 using Elsa.Testing.Shared.Handlers;
 using Elsa.Testing.Shared.Services;
-using Elsa.Workflows.ComponentTests;
 using FluentStorage;
 using JetBrains.Annotations;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
-using NSubstitute;
-using Refit;
-using static Elsa.Api.Client.RefitSettingsHelper;
 
 namespace Elsa.AzureServiceBus.ComponentTests;
 
 [UsedImplicitly]
 public class WorkflowServer(Infrastructure infrastructure, string url) : WebApplicationFactory<Program>
 {
-    public TClient CreateApiClient<TClient>()
-    {
-        var client = CreateClient();
-        client.BaseAddress = new Uri(client.BaseAddress!, "/elsa/api");
-        client.Timeout = TimeSpan.FromMinutes(1);
-        return RestService.For<TClient>(client, CreateRefitSettings());
-    }
-
-    public HttpClient CreateHttpWorkflowClient()
-    {
-        var client = CreateClient();
-        client.BaseAddress = new Uri(client.BaseAddress!, "/workflows/");
-        client.Timeout = TimeSpan.FromMinutes(1);
-        return client;
-    }
-
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
         var dbConnectionString = infrastructure.DbContainer.GetConnectionString();
@@ -95,76 +73,12 @@ public class WorkflowServer(Infrastructure infrastructure, string url) : WebAppl
 
         builder.ConfigureTestServices(services =>
         {
-            var serviceBusClient = Substitute.For<ServiceBusClient>();
-            var senders = new Dictionary<string, ServiceBusSender>();
-            var processors = new Dictionary<string, MockServiceBusProcessor>();
-            serviceBusClient.CreateSender(Arg.Any<string>()).Returns(createSenderCall =>
-            {
-                var queueOrTopicName = createSenderCall.Arg<string>();
-
-                return senders.GetOrAdd(queueOrTopicName, () =>
-                {
-                    var serviceBusSender = Substitute.For<ServiceBusSender>();
-                    serviceBusSender.SendMessageAsync(Arg.Any<ServiceBusMessage>(), Arg.Any<CancellationToken>()).Returns(sendMessageCall =>
-                    {
-                        var message = sendMessageCall.Arg<ServiceBusMessage>();
-                        var args = CreateMessageArgs(message);
-                        var serviceBusProcessor = processors.Where(x => x.Key.StartsWith(queueOrTopicName, StringComparison.OrdinalIgnoreCase)).ToList();
-                        foreach (var (_, processor) in serviceBusProcessor)
-                            processor.RaiseProcessMessageAsync(args);
-                        return Task.CompletedTask;
-                    });
-                    return serviceBusSender;
-                });
-            });
-            serviceBusClient.CreateProcessor(Arg.Any<string>(), Arg.Any<ServiceBusProcessorOptions>()).Returns(createProcessorCall =>
-            {
-                var queueOrTopicName = createProcessorCall.Arg<string>();
-                var key = queueOrTopicName;
-                return processors.GetOrAdd(key, () =>
-                {
-                    return new MockServiceBusProcessor(() =>
-                    {
-                        processors.Remove(key);
-                    });
-                });
-            });
-            serviceBusClient.CreateProcessor(Arg.Any<string>(), Arg.Any<string>(), Arg.Any<ServiceBusProcessorOptions>()).Returns(createProcessorCall =>
-            {
-                var queueOrTopicName = createProcessorCall.ArgAt<string>(0);
-                var subscription = createProcessorCall.ArgAt<string>(1);
-                var key = $"{queueOrTopicName}:{subscription}";
-                return processors.GetOrAdd(key, () =>
-                {
-                    return new MockServiceBusProcessor(() =>
-                    {
-                        processors.Remove(key);
-                    });
-                });
-            });
-
             services.AddSingleton<ISignalManager, SignalManager>();
             services.AddSingleton<IWorkflowEvents, WorkflowEvents>();
             services.AddNotificationHandlersFrom<WorkflowServer>();
             services.AddNotificationHandlersFrom<WorkflowEventHandlers>();
-            services.AddSingleton(serviceBusClient);
-            services.AddSingleton(Substitute.For<ServiceBusAdministrationClient>());
+            services.AddAzureServiceBusTestServices();
         });
-    }
-
-    private ProcessMessageEventArgs CreateMessageArgs(ServiceBusMessage transportMessage, int deliveryCount = 1)
-    {
-        var payloadJson = JsonSerializer.Serialize(transportMessage);
-        var props = new Dictionary<string, object>();
-
-        var message = ServiceBusModelFactory.ServiceBusReceivedMessage(
-            body: BinaryData.FromString(payloadJson),
-            deliveryCount: deliveryCount,
-            correlationId: transportMessage.CorrelationId,
-            properties: props
-        );
-
-        return new ProcessMessageEventArgs(message, null, new CancellationToken());
     }
 
     protected override void ConfigureClient(HttpClient client)
