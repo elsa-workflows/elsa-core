d9aaad97980a95e1bb1fba2b85c45aa03acb6ea0
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2024-05-24 21:35:17 +0200

    Reduce default timeout in ISignalManager interface
    The default millisecond timeout for the WaitAsync functions in the ISignalManager interface has been reduced from 5000ms to 1000ms. This change will speed up signal wait times in our testing framework.

--- src/common/Elsa.Testing.Shared.Component/Contracts/ISignalManager.cs
+++ src/common/Elsa.Testing.Shared.Component/Contracts/ISignalManager.cs
@@ -2,7 +2,7 @@ namespace Elsa.Testing.Shared;
 
 public interface ISignalManager
 {
-    Task<T> WaitAsync<T>(object signal, int millisecondsTimeout = 5000);
-    Task<object?> WaitAsync(object signal, int millisecondsTimeout = 5000);
+    Task<T> WaitAsync<T>(object signal, int millisecondsTimeout = 1000);
+    Task<object?> WaitAsync(object signal, int millisecondsTimeout = 1000);
     void Trigger(object signal, object? result = null);
 }
