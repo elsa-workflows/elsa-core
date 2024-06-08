b4acb304fd73b0a8d67c9d5073c3d7324d1bc701
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2024-06-08 12:16:13 +0200

    Add CountdownStep activity and CountdownWorkflow for testing
    This commit introduces new component tests for simulations involving counters. It includes a new CountdownStep activity that decrements a counter variable, as well as a CountdownWorkflow which consists of a loop based on the aforementioned activity. It also involves a CountdownWorkflowTests class for testing counter persistence across workflow runs.

---
+++ test/component/Elsa.Workflows.ComponentTests/Scenarios/Variables/CountdownWorkflowTests.cs
@@ -0,0 +1,60 @@
+using Elsa.Expressions.Helpers;
+using Elsa.Extensions;
+using Elsa.Workflows.ComponentTests.Scenarios.Variables.Workflows;
+using Elsa.Workflows.Management.Contracts;
+using Elsa.Workflows.Models;
+using Elsa.Workflows.Runtime.Contracts;
+using Elsa.Workflows.Runtime.Parameters;
+using Elsa.Workflows.Services;
+using Elsa.Workflows.State;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Elsa.Workflows.ComponentTests.Scenarios.Variables;
+
+public class CountdownWorkflowTests : AppComponentTest
+{
+    /// <inheritdoc />
+    public CountdownWorkflowTests(App app) : base(app)
+    {
+    }
+
+    [Fact(DisplayName = "Variable is persisted across workflow runs")]
+    public async Task VariableIsPersistedAcrossWorkflowRuns()
+    {
+        var workflowRuntime = Scope.ServiceProvider.GetRequiredService<IWorkflowRuntime>();
+        var workflowInstanceStore = Scope.ServiceProvider.GetRequiredService<IWorkflowInstanceStore>();
+        var startParams = new StartWorkflowRuntimeParams();
+        var result = await workflowRuntime.StartWorkflowAsync(CountdownWorkflow.DefinitionId, startParams);
+        var workflowInstanceId = result.WorkflowInstanceId;
+        var bookmarks = new Stack<Bookmark>(result.Bookmarks);
+        var expectedCounter = 3;
+
+        while (bookmarks.Any())
+        {
+            var workflowInstance = await workflowInstanceStore.FindAsync(workflowInstanceId);
+            var workflowState = workflowInstance!.WorkflowState;
+            var rootWorkflowActivityExecutionContext = workflowState.ActivityExecutionContexts.Single(x => x.ParentContextId == null);
+            var variables = GetVariablesDictionary(rootWorkflowActivityExecutionContext);
+            var actualCounter = variables["Workflow1:variable-1"].ConvertTo<int>();
+            Assert.Equal(--expectedCounter, actualCounter);
+            
+            var bookmark = bookmarks.Pop();
+            var resumeWorkflowRuntimeOptions = new ResumeWorkflowRuntimeParams
+            {
+                BookmarkId = bookmark?.Id,
+            };
+
+            result = await workflowRuntime.ResumeWorkflowAsync(workflowInstanceId, resumeWorkflowRuntimeOptions);
+
+            if (result == null)
+                break;
+
+            foreach (var newBookmark in result.Bookmarks) bookmarks.Push(newBookmark);
+
+            
+        }
+    }
+
+    private IDictionary<string, object> GetVariablesDictionary(ActivityExecutionContextState context) =>
+        context.Properties.GetOrAdd(WorkflowStorageDriver.VariablesDictionaryStateKey, () => new Dictionary<string, object>());
+}
