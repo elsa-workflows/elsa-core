da7bff83b7a48101050dedbb17143145c2a3fee2
rosca-sabina <43028000+rosca-sabina@users.noreply.github.com>
2024-06-09 16:18:30 +0300

    Added MongoDB implementation for alteration storage (#5555)

---
+++ src/modules/Elsa.MongoDb/Modules/Alterations/CreateIndices.cs
@@ -0,0 +1,62 @@
+﻿using Elsa.Alterations.Core.Entities;
+using Elsa.MongoDb.Helpers;
+using Elsa.MongoDb.Modules.Alterations.Documents;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using MongoDB.Driver;
+
+namespace Elsa.MongoDb.Modules.Alterations;
+internal class CreateIndices(IServiceProvider serviceProvider) : IHostedService
+{
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        using var scope = serviceProvider.CreateScope();
+
+        return Task.WhenAll(
+            CreateAlterationPlanIndices(scope, cancellationToken),
+            CreateAlterationJobIndices(scope, cancellationToken));
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+
+    private static Task CreateAlterationPlanIndices(IServiceScope serviceScope, CancellationToken cancellationToken)
+    {
+        var alterationPlanCollection = serviceScope.ServiceProvider.GetService<IMongoCollection<AlterationPlanDocument>>();
+        if (alterationPlanCollection == null) return Task.CompletedTask;
+
+        return IndexHelpers.CreateAsync(
+            alterationPlanCollection,
+            async (collection, indexBuilder) =>
+                await collection.Indexes.CreateManyAsync(
+                    [
+                        new(indexBuilder.Ascending(x => x.Status)),
+                        new(indexBuilder.Ascending(x => x.CreatedAt)),
+                        new(indexBuilder.Ascending(x => x.StartedAt)),
+                        new(indexBuilder.Ascending(x => x.CompletedAt))
+                    ],
+                    cancellationToken));
+    }
+
+    private static Task CreateAlterationJobIndices(IServiceScope serviceScope, CancellationToken cancellationToken)
+    {
+        var alterationJobCollection = serviceScope.ServiceProvider.GetService<IMongoCollection<AlterationJob>>();
+        if (alterationJobCollection == null) return Task.CompletedTask;
+
+        return IndexHelpers.CreateAsync(
+            alterationJobCollection,
+            async (collection, indexBuilder) =>
+                await collection.Indexes.CreateManyAsync(
+                    [
+                        new(indexBuilder.Ascending(x => x.PlanId)),
+                        new(indexBuilder.Ascending(x => x.WorkflowInstanceId)),
+                        new(indexBuilder.Ascending(x => x.Status)),
+                        new(indexBuilder.Ascending(x => x.CreatedAt)),
+                        new(indexBuilder.Ascending(x => x.StartedAt)),
+                        new(indexBuilder.Ascending(x => x.CompletedAt))
+                    ],
+                    cancellationToken));
+    }
+}
---
+++ src/modules/Elsa.MongoDb/Modules/Alterations/Feature.cs
@@ -0,0 +1,31 @@
+﻿using Elsa.Alterations.Core.Entities;
+using Elsa.Alterations.Features;
+using Elsa.Features.Attributes;
+using Elsa.Features.Services;
+using Elsa.MongoDb.Common;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Elsa.MongoDb.Modules.Alterations;
+
+[DependsOn(typeof(AlterationsFeature))]
+public class MongoAlterationsPersistenceFeature(IModule module) : PersistenceFeatureBase(module)
+{
+    public override void Configure()
+    {
+        Module.Configure<AlterationsFeature>(feature =>
+        {
+            feature.AlterationPlanStoreFactory = sp => sp.GetRequiredService<MongoAlterationPlanStore>();
+            feature.AlterationJobStoreFactory = sp => sp.GetRequiredService<MongoAlterationJobStore>();
+        });
+    }
+
+    public override void Apply()
+    {
+        base.Apply();
+
+        AddCollection<AlterationJob>("alteration_jobs");
+        AddStore<AlterationJob, MongoAlterationJobStore>();
+
+        Services.AddHostedService<CreateIndices>();
+    }
+}
