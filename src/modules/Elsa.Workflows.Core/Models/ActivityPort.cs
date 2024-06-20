735f5bd5720a0ef835fe1349ac3557776ee40c42
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2024-06-20 18:59:19 +0200

    Refactor ActivityPort in Elsa.Workflows module
    This commit simplifies the code in the ActivityPort class, part of the Elsa.Workflows module. It includes a significant rework of the comments for better clarity and understanding. Effort has been made to make the code more readable and easier to maintain in this refactor.

--- src/modules/Elsa.Workflows.Core/Models/ActivityPort.cs
+++ src/modules/Elsa.Workflows.Core/Models/ActivityPort.cs
@@ -2,14 +2,16 @@ using Elsa.Workflows.Contracts;
 
 namespace Elsa.Workflows.Models;
 
-/// <summary>
-/// Represents a binding between an owner activity's port to a child activity.
-/// </summary>
+/// Represents a binding between an owner activity's port and a child activity.
 public record ActivityPort(IActivity? Activity, ICollection<IActivity>? Activities, string PortName)
 {
-    public static ActivityPort FromActivity(IActivity activity, string portName) => new ActivityPort(activity, null, portName);
-    public static ActivityPort FromActivities(IEnumerable<IActivity> activities, string portName) => new ActivityPort(null, activities.ToList(), portName);
+    /// Creates a new ActivityPort from the specified activity and port name.
+    public static ActivityPort FromActivity(IActivity activity, string portName) => new(activity, null, portName);
 
+    /// Creates a new ActivityPort from the specified activities and port name.
+    public static ActivityPort FromActivities(IEnumerable<IActivity> activities, string portName) => new(null, activities.ToList(), portName);
+
+    /// Returns the activities bound to the port. Returns a list even if the Activity is set and not Activities.
     public IEnumerable<IActivity> GetActivities()
     {
         return Activity != null ? [Activity] : Activities!.ToList();
