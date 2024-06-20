384ae65ef0d4d88aa21908cc964eb5a67d33c61b
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2024-06-20 18:49:49 +0200

    Remove IncludeCompositeRoot property from Models
    The IncludeCompositeRoot property has been removed from the Models in the GetByDefinitionId, GetById, and GetManyById endpoints. These changes simplify the code as the property was commented out and not used in current workflow logic.

--- src/modules/Elsa.Workflows.Api/Endpoints/WorkflowDefinitions/GetById/Models.cs
+++ src/modules/Elsa.Workflows.Api/Endpoints/WorkflowDefinitions/GetById/Models.cs
@@ -3,9 +3,4 @@ namespace Elsa.Workflows.Api.Endpoints.WorkflowDefinitions.GetById;
 internal class Request
 {
     public string Id { get; set; } = default!;
-    
-    // /// <summary>
-    // /// True if the response should include the root activity of composite activities.
-    // /// </summary>
-    // public bool IncludeCompositeRoot { get; set; }
 }
