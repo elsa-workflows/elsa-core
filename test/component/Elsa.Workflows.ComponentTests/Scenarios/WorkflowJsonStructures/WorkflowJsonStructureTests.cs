7ee444f4c34feab5d7ff9a33ee926dc478205f11
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2024-06-21 20:19:44 +0200

    Add WorkflowJsonStructureTests and related JSON files
    This commit introduces the WorkflowJsonStructureTests class which includes tests for the structure of the workflow definition in JSON format. In addition, main.json and sub.json files, containing mock data for testing, were added to the `WorkflowJsonStructures` scenario within the Elsa Workflows Component Tests. Updates to the project file ensure these new files are included correctly.

---
+++ test/component/Elsa.Workflows.ComponentTests/Scenarios/WorkflowJsonStructures/WorkflowJsonStructureTests.cs
@@ -0,0 +1,17 @@
+﻿using Elsa.Api.Client.Resources.WorkflowDefinitions.Contracts;
+
+namespace Elsa.Workflows.ComponentTests.Scenarios.WorkflowJsonStructures;
+
+public class WorkflowJsonStructureTests(App app) : AppComponentTest(app)
+{
+    [Fact]
+    public async Task Workflow_ContainingWorkflowActivity_ShouldNotIncludeChildrenOfWorkflowActivity()
+    {
+        var client = WorkflowServer.CreateApiClient<IWorkflowDefinitionsApi>();
+        var workflowDefinition = (await client.GetByIdAsync("a417fadbbb7c417e"))!;
+        var root = workflowDefinition.Root;
+        var subActivity = root["activities"]![0]!;
+
+        Assert.Null(subActivity["root"]);
+    }
+}
