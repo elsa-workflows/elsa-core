51ee5549f4ac69297ac9d734f99806400b3d73e5
Marius Vasile Vușcan <marius.vuscan@nexxbiz.io>
2024-07-04 17:13:17 +0300

    Added workflow definition refresh component test

---
+++ test/component/Elsa.Workflows.ComponentTests/Scenarios/WorkflowDefinitionRefresh/DynamicEndpointTests.cs
@@ -0,0 +1,36 @@
+﻿using Elsa.Workflows.ComponentTests.Helpers.Services;
+using Elsa.Workflows.Runtime.Contracts;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Elsa.Workflows.ComponentTests.Scenarios.WorkflowDefinitionRefresh;
+
+public class DynamicEndpointTests : AppComponentTest
+{
+    private readonly IWorkflowDefinitionsRefresher _workflowDefinitionsRefresher;
+
+    public DynamicEndpointTests(App app) : base(app)
+    {
+        StaticValueHolder.Value = "firstvalue";
+        _workflowDefinitionsRefresher = Scope.ServiceProvider.GetRequiredService<IWorkflowDefinitionsRefresher>();
+    }
+
+    [Fact]
+    public async Task HelloWorldWorkflow_ShouldRespondWithHelloWorld()
+    {
+        var client = WorkflowServer.CreateHttpWorkflowClient();
+
+        var firstResponse = await client.GetStringAsync("firstvalue");
+
+        StaticValueHolder.Value = "secondvalue";
+        await _workflowDefinitionsRefresher.RefreshWorkflowDefinitionsAsync(new Runtime.Requests.RefreshWorkflowDefinitionsRequest
+        {
+            BatchSize = 10,
+            DefinitionIds = ["f926beb0afc8505e"]
+        }, CancellationToken.None);
+
+        var secondResponse = await client.GetStringAsync("secondvalue");
+
+        Assert.Equal("Hello World!", firstResponse);
+        Assert.Equal("Hello World!", secondResponse);
+    }
+}
