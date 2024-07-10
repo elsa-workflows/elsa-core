4d41cd743bb248dce8b557e9ccb3cd76751c7da8
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2024-07-10 15:32:01 +0200

    Add DistributedCachingTransport enum
    Introduced the DistributedCachingTransport enum to define caching transport options. Also cleaned up the code by removing an unnecessary using directive and a commented summary in the MassTransitBroker enum.

--- src/bundles/Elsa.ServerAndStudio.Web/Enums/MassTransitBroker.cs
+++ src/bundles/Elsa.ServerAndStudio.Web/Enums/MassTransitBroker.cs
@@ -1,8 +1,6 @@
 namespace Elsa.ServerAndStudio.Web.Enums;
 
-/// <summary>
 /// Represents the type of messaging broker used in MassTransit.
-/// </summary>
 public enum MassTransitBroker
 {
     Memory,
