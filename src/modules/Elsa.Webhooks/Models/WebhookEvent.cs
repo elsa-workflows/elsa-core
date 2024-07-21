bf998659ad1fe1cdb7c393095e9f8f33c8f2e204
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2024-07-21 16:40:14 +0200

    Replace custom webhooks implementation with WebhooksCore
    Replaced the existing custom webhooks registration and dispatching system with the WebhooksCore library. This involved removing all previous custom webhook classes and interfaces, updating project dependencies, and modifying appsettings and program files to use the new configuration. The update simplifies the webhook handling architecture and leverages the features provided by WebhooksCore.

--- src/modules/Elsa.Webhooks/Models/WebhookEvent.cs
+++
@@ -1,6 +0,0 @@
-namespace Elsa.Webhooks.Models;
-
-/// <summary>
-/// A payload sent to a webhook url.
-/// </summary>
-public record WebhookEvent(string EventType, object? Payload, DateTimeOffset Timestamp);
