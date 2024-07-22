02dd5a9413107a94a421beffd8662c7e9c58ef24
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2024-07-22 23:01:32 +0200

    Add LocalNodeStrategy and integrate it in actor provider
    Introduced `LocalNodeStrategy` to handle member placement on the current node. Integrated the new strategy in `LocalCacheVirtualActorProvider`, ensuring it uses `LocalNodeStrategy` for member management.

--- src/modules/Elsa.Caching.Distributed.ProtoActor/Providers/LocalCacheVirtualActorProvider.cs
+++ src/modules/Elsa.Caching.Distributed.ProtoActor/Providers/LocalCacheVirtualActorProvider.cs
@@ -1,5 +1,6 @@
 using Elsa.Caching.Distributed.ProtoActor.ProtoBuf;
 using Elsa.ProtoActor;
+using Elsa.ProtoActor.MemberStrategies;
 using Google.Protobuf.Reflection;
 using Proto;
 using Proto.Cluster;
@@ -7,14 +8,14 @@ using Proto.DependencyInjection;
 
 namespace Elsa.Caching.Distributed.ProtoActor.Providers;
 
-public class LocalCacheVirtualActorProvider  : VirtualActorsProviderBase
+public class LocalCacheVirtualActorProvider : VirtualActorsProviderBase
 {
     public override IEnumerable<ClusterKind> GetClusterKinds(ActorSystem system)
     {
         var props = system.DI().PropsFor<LocalCacheActor>();
-        yield return new ClusterKind(LocalCacheActor.Kind, props);
+        yield return new ClusterKind(LocalCacheActor.Kind, props).WithMemberStrategy(cluster => new LocalNodeStrategy(cluster));
     }
-    
+
     public override IEnumerable<FileDescriptor> GetFileDescriptors()
     {
         yield return SharedReflection.Descriptor;
