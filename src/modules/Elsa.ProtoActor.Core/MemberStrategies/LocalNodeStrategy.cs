259fffbce59c827ff0020314cc149f2fcb453758
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2024-07-22 23:05:38 +0200

    Prevent duplicate member additions based on ID.
    Updated the member checking logic to include member IDs. In addition, this change improves the robustness of the member management in `LocalNodeStrategy.cs`.

--- src/modules/Elsa.ProtoActor.Core/MemberStrategies/LocalNodeStrategy.cs
+++ src/modules/Elsa.ProtoActor.Core/MemberStrategies/LocalNodeStrategy.cs
@@ -17,7 +17,7 @@ public class LocalNodeStrategy(Cluster cluster) : IMemberStrategy
     public void AddMember(Member member)
     {
         // Avoid adding the same member twice
-        if (_members.Any(x => x.Address == member.Address))
+        if (_members.Any(x => x.Address == member.Address || x.Id == member.Id))
             return;
 
         if (member.Address.Equals(cluster.System.Address, StringComparison.InvariantCulture))
@@ -28,7 +28,7 @@ public class LocalNodeStrategy(Cluster cluster) : IMemberStrategy
 
     public void RemoveMember(Member member)
     {
-        _members = _members.RemoveAll(x => x.Address == member.Address);
+        _members = _members.RemoveAll(x => x.Address == member.Address || x.Id == member.Id);
     }
 
     public Member? GetActivator(string senderAddress)
