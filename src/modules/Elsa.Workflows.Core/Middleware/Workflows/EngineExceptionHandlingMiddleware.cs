de4a4b3505d7e9adba3ca8001db48d0e450c6211
Raymond den Haan <raymond.den.haan@nexxbiz.io>
2024-08-06 13:54:02 +0200

    Add engine exception handling middleware
    Integrate new EngineExceptionHandlingMiddleware into the workflow execution pipeline to catch and log exceptions outside of running workflow activity.

---
+++ src/modules/Elsa.Workflows.Core/Middleware/Workflows/EngineExceptionHandlingMiddleware.cs
@@ -0,0 +1,61 @@
+using Elsa.Common.Contracts;
+using Elsa.Workflows.Contracts;
+using Elsa.Workflows.Models;
+using Elsa.Workflows.Pipelines.WorkflowExecution;
+using Elsa.Workflows.State;
+using Microsoft.Extensions.Logging;
+
+namespace Elsa.Workflows.Middleware.Workflows;
+
+/// <summary>
+/// Adds extension methods to <see cref="ExceptionHandlingMiddleware"/>.
+/// </summary>
+public static class EngineExceptionHandlingMiddlewareExtensions
+{
+    /// <summary>
+    /// Installs the <see cref="ExceptionHandlingMiddleware"/> component in the activity execution pipeline.
+    /// </summary>
+    public static IWorkflowExecutionPipelineBuilder UseEngineExceptionHandling(this IWorkflowExecutionPipelineBuilder pipelineBuilder) => pipelineBuilder.UseMiddleware<EngineExceptionHandlingMiddleware>();
+}
+
+/// <summary>
+/// Catches any exceptions thrown by downstream components and transitions the workflow into the faulted state.
+/// </summary>
+public class EngineExceptionHandlingMiddleware : IWorkflowExecutionMiddleware
+{
+    private readonly WorkflowMiddlewareDelegate _next;
+    private readonly ISystemClock _systemClock;
+    private readonly ILogger<ExceptionHandlingMiddleware> _logger;
+
+    /// <summary>
+    /// Constructor.
+    /// </summary>
+    public EngineExceptionHandlingMiddleware(WorkflowMiddlewareDelegate next, ISystemClock systemClock, ILogger<ExceptionHandlingMiddleware> logger)
+    {
+        _next = next;
+        _systemClock = systemClock;
+        _logger = logger;
+    }
+
+    /// <inheritdoc />
+    public async ValueTask InvokeAsync(WorkflowExecutionContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception e)
+        {
+            _logger.LogWarning(e, "An exception was caught from a downstream middleware component");
+            var exceptionState = ExceptionState.FromException(e);
+            var now = _systemClock.UtcNow;
+            var activity = context.Workflow;
+            var incident = new ActivityIncident(activity.Id, activity.Type, e.Message, exceptionState, now);
+            
+            // No state change as the workflow / activities status should be leading.
+            // We will however be adding an incident to make the issue visible.
+            context.Incidents.Add(incident);
+            context.AddExecutionLogEntry("Faulted", e.Message, exceptionState);
+        }
+    }
+}
