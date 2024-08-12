c8669d1bc170b1e45077582537740fe8f477459d
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2024-08-12 09:42:25 +0200

    Add optional refresh parameter (#5876)
    * Add optional refresh parameter
    
    Renamed several `RefreshDescriptors` methods to `RefreshDescriptorsAsync` to reflect their asynchronous nature consistently. Added an optional refresh parameter in the `ListActivityDescriptorsRequest` class and updated related endpoints to handle this parameter, triggering a registry refresh if necessary.
    
    * Update version to 3.2.0-rc5 in GitHub Actions
    
    Modified the `packages.yml` workflow to change the version from `3.2.0-rc4` to `3.2.0-rc5` for non-tagged releases. This ensures version consistency with the new release candidate.

--- src/modules/Elsa.Workflows.Api/Endpoints/ActivityDescriptors/List/Endpoint.cs
+++ src/modules/Elsa.Workflows.Api/Endpoints/ActivityDescriptors/List/Endpoint.cs
@@ -1,11 +1,13 @@
 using Elsa.Abstractions;
 using Elsa.Workflows.Contracts;
+using Elsa.Workflows.Management.Activities.WorkflowDefinitionActivity;
+using Elsa.Workflows.Management.Contracts;
 using JetBrains.Annotations;
 
 namespace Elsa.Workflows.Api.Endpoints.ActivityDescriptors.List;
 
 [PublicAPI]
-internal class List(IActivityRegistry registry) : ElsaEndpointWithoutRequest<Response>
+internal class List(IActivityRegistry registry, IActivityRegistryPopulator registryPopulator, WorkflowDefinitionActivityProvider workflowDefinitionActivityProvider) : ElsaEndpointWithoutRequest<Response>
 {
     public override void Configure()
     {
@@ -13,11 +15,16 @@ internal class List(IActivityRegistry registry) : ElsaEndpointWithoutRequest<Res
         ConfigurePermissions("read:*", "read:activity-descriptors");
     }
 
-    public override Task<Response> ExecuteAsync(CancellationToken cancellationToken)
+    public override async Task<Response> ExecuteAsync(CancellationToken cancellationToken)
     {
+        var forceRefresh = Query<bool>("refresh", false);
+
+        if (forceRefresh)
+            await registryPopulator.PopulateRegistryAsync(cancellationToken);
+
         var descriptors = registry.ListAll().ToList();
         var response = new Response(descriptors);
 
-        return Task.FromResult(response);
+        return response;
     }
 }
