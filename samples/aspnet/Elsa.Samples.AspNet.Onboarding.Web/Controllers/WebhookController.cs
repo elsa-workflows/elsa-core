426b9bcde3d6359e0327d2f0a685ff904dfd6797
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2024-08-14 21:42:46 +0200

    Move build and deployment scripts
    This commit moves various build scripts, Docker-related files, Kubernetes deployment configurations, and migration scripts.

--- samples/aspnet/Elsa.Samples.AspNet.Onboarding.Web/Controllers/WebhookController.cs
+++ samples/aspnet/Elsa.Samples.AspNet.Onboarding.Web/Controllers/WebhookController.cs
@@ -1,41 +1,42 @@
 using Elsa.Samples.AspNet.Onboarding.Web.Data;
 using Elsa.Samples.AspNet.Onboarding.Web.Entities;
 using Elsa.Samples.AspNet.Onboarding.Web.Models;
+using Elsa.Samples.AspNet.Onboarding.Web.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Elsa.Samples.AspNet.Onboarding.Web.Controllers;
 
 [ApiController]
 [Route("api/webhooks")]
-public class WebhookController : Controller
+public class WebhookController(OnboardingDbContext dbContext, ElsaClient elsaClient) : Controller
 {
-    private readonly OnboardingDbContext _dbContext;
-
-    public WebhookController(OnboardingDbContext dbContext)
-    {
-        _dbContext = dbContext;
-    }
-    
     [HttpPost("run-task")]
-    public async Task<IActionResult> RunTask(WebhookEvent webhookEvent)
+    public async Task<IActionResult> RunTask(WebhookEvent webhookEvent, CancellationToken cancellationToken)
     {
         var payload = webhookEvent.Payload;
         var taskPayload = payload.TaskPayload;
-        var employee = taskPayload.Employee;
         
-        var task = new OnboardingTask
+        var result = new
         {
-            ProcessId = payload.WorkflowInstanceId,
-            ExternalId = payload.TaskId,
-            Name = payload.TaskName,
-            Description = taskPayload.Description,
-            EmployeeEmail = employee.Email,
-            EmployeeName = employee.Name,
-            CreatedAt = DateTimeOffset.Now
+            Magic = Random.Shared.NextDouble()
         };
-
-        await _dbContext.Tasks.AddAsync(task);
-        await _dbContext.SaveChangesAsync();
+        await elsaClient.ReportTaskCompletedAsync(webhookEvent.Payload.TaskId, result, cancellationToken);
+        
+        // var employee = taskPayload.Employee;
+        //
+        // var task = new OnboardingTask
+        // {
+        //     ProcessId = payload.WorkflowInstanceId,
+        //     ExternalId = payload.TaskId,
+        //     Name = payload.TaskName,
+        //     Description = taskPayload.Description,
+        //     EmployeeEmail = employee.Email,
+        //     EmployeeName = employee.Name,
+        //     CreatedAt = DateTimeOffset.Now
+        // };
+        //
+        // await dbContext.Tasks.AddAsync(task);
+        // await dbContext.SaveChangesAsync();
 
         return Ok();
     }
