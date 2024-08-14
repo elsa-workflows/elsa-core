5f11026ee7c97c9c51e8443ebde17ecab0ddbfec
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2024-08-14 17:43:35 +0200

    Add JS functions for encoding/decoding byte arrays from and to strings (#5902)
    * Add handlers and improve JavaScript engine configuration
    
    Added several new handlers for configuring the JavaScript engine with common types, functions, variable accessors, and input/output accessors. Refactored the JintJavaScriptEvaluator for better clarity and modularity by breaking down configuration steps into separate methods. Also replaced `IsInsideCompositeActivity` with `IsContainedWithinCompositeActivity` for better semantic consistency.
    
    * Refactor to use primary constructor syntax for TypeDefinitionProviders
    
    Updated CommonTypeDefinitionProvider, VariableTypeDefinitionProvider, and ActivityOutputFunctionsDefinitionProvider to use primary constructors for dependency injection. This change reduces redundancy and simplifies the code structure for better readability and maintainability.
    
    * Add byte conversion functions to JavaScript engine
    
    Introduced functions for converting bytes to/from strings and Base64. Updated CommonFunctionsDefinitionProvider and ConfigureEngineWithCommonFunctions to incorporate these new functions. This enhances the JavaScript engine's ability to handle byte array manipulations.
    
    * Add tests for JavaScript byte and string conversions.
    
    Introduce integration tests to verify byte array to string, string to byte array, byte array to Base64, and Base64 to byte array conversions using JavaScript functions. Ensure accurate transformation of data within different encoding scenarios.
    
    * Add meaningful summaries to Jint engine configuration handlers
    
    Updated comment summaries in four handler classes to provide clear and concise descriptions of their purpose. This helps improve code readability and understanding for future developers.
    
    * Rename and merge variable and input/output handlers
    
    Merged the variable accessor logic into the input/output handler and renamed the class to reflect its broader functionality. This consolidation ensures the accessors are registered in the right order.
    
    * Add string base64 conversion functions
    
    Introduced `stringToBase64` and `stringFromBase64` functions to handle base64 encoding and decoding of strings. Updated corresponding provider, handler, and added tests to ensure functionality.

--- src/modules/Elsa.JavaScript/TypeDefinitions/Providers/ActivityOutputFunctionsDefinitionProvider.cs
+++ src/modules/Elsa.JavaScript/TypeDefinitions/Providers/ActivityOutputFunctionsDefinitionProvider.cs
@@ -9,29 +9,21 @@ namespace Elsa.JavaScript.TypeDefinitions.Providers;
 /// <summary>
 /// Produces <see cref="FunctionDefinition"/>s for common functions.
 /// </summary>
-internal class ActivityOutputFunctionsDefinitionProvider : FunctionDefinitionProvider
+internal class ActivityOutputFunctionsDefinitionProvider(
+    IActivityVisitor activityVisitor, 
+    IActivityRegistryLookupService activityRegistryLookup, 
+    IIdentityGraphService identityGraphService) : FunctionDefinitionProvider
 {
-    private readonly IActivityVisitor _activityVisitor;
-    private readonly IActivityRegistryLookupService _activityRegistryLookup;
-    private readonly IIdentityGraphService _identityGraphService;
-
-    public ActivityOutputFunctionsDefinitionProvider(IActivityVisitor activityVisitor, IActivityRegistryLookupService activityRegistryLookup, IIdentityGraphService identityGraphService)
-    {
-        _activityVisitor = activityVisitor;
-        _activityRegistryLookup = activityRegistryLookup;
-        _identityGraphService = identityGraphService;
-    }
-
     protected override async ValueTask<IEnumerable<FunctionDefinition>> GetFunctionDefinitionsAsync(TypeDefinitionContext context)
     {
         // Output getters.
         var workflow = context.Workflow;
-        var nodes = (await _activityVisitor.VisitAsync(workflow.Root, context.CancellationToken)).Flatten().Distinct().ToList();
+        var nodes = (await activityVisitor.VisitAsync(workflow.Root, context.CancellationToken)).Flatten().Distinct().ToList();
         
         // Ensure identities.
-        await _identityGraphService.AssignIdentitiesAsync(nodes);
+        await identityGraphService.AssignIdentitiesAsync(nodes);
         
-        var activitiesWithOutputs = nodes.GetActivitiesWithOutputs(_activityRegistryLookup);
+        var activitiesWithOutputs = nodes.GetActivitiesWithOutputs(activityRegistryLookup);
         var definitions = new List<FunctionDefinition>();
 
         await foreach (var (activity, activityDescriptor) in activitiesWithOutputs)
--- src/modules/Elsa.Workflows.Core/Extensions/ExpressionExecutionContextExtensions.cs
+++ src/modules/Elsa.Workflows.Core/Extensions/ExpressionExecutionContextExtensions.cs
@@ -394,7 +394,7 @@ public static class ExpressionExecutionContextExtensions
     /// <returns>The value of the specified input.</returns>
     public static object? GetInput(this ExpressionExecutionContext context, string name)
     {
-        if (context.IsInsideCompositeActivity())
+        if (context.IsContainedWithinCompositeActivity())
         {
             // If there's a variable in the current scope with the specified name, return that.
             var variable = context.GetVariable(name);
@@ -469,7 +469,7 @@ public static class ExpressionExecutionContextExtensions
     /// <summary>
     /// Returns a value indicating whether the current activity is inside a composite activity.
     /// </summary>
-    public static bool IsInsideCompositeActivity(this ExpressionExecutionContext context)
+    public static bool IsContainedWithinCompositeActivity(this ExpressionExecutionContext context)
     {
         if (!context.TryGetActivityExecutionContext(out var activityExecutionContext))
             return false;
