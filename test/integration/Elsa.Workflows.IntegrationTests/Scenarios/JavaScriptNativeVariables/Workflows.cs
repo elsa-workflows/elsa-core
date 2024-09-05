17b80ed6d8f386ae072a8e0b7278ce59370e0bcd
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2024-09-05 22:03:29 +0200

    Add JavaScript variable handling integration test
    Introduced integration tests for JavaScript activities to verify they can access and modify native variables. Added classes for data setup, test execution, and workflow definition with corresponding NUnit tests.

---
+++ test/integration/Elsa.Workflows.IntegrationTests/Scenarios/JavaScriptNativeVariables/Workflows.cs
@@ -0,0 +1,34 @@
+using Elsa.JavaScript.Activities;
+using Elsa.JavaScript.Models;
+using Elsa.Workflows.Activities;
+using Elsa.Workflows.Contracts;
+
+namespace Elsa.Workflows.IntegrationTests.Scenarios.JavaScriptNativeVariables;
+
+public class JavaScriptWorkflow : WorkflowBase
+{
+    protected override void Build(IWorkflowBuilder workflow)
+    {
+        workflow.WithDefinitionId(Guid.NewGuid().ToString());
+        var customerVariable = workflow.WithVariable("customer", Data.CreateCustomerModel());
+        
+        workflow.Root = new Sequence
+        {
+            Activities =
+            {
+                new RunJavaScript
+                {
+                    Script = new("""
+                                 const customer = variables.customer;
+                                 customer.Name = 'Jane Doe';
+                                 customer.Orders.sort((a, b) => a.Product.localeCompare(b.Product));
+                                 variables.customer = {...customer, Email: 'jane.doe@acme.com'};
+                                 """)
+                },
+                new WriteLine(JavaScriptExpression.Create("variables.customer.Name")),
+                new WriteLine(JavaScriptExpression.Create("variables.customer.Orders.map(x => x.Product).join(', ')")),
+                new WriteLine(JavaScriptExpression.Create("variables.customer.Email"))
+            }
+        };
+    }
+}
