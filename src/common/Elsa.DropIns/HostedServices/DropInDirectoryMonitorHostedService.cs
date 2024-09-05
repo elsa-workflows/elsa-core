f958f7ef19929ec3daebd2112bc1d0ae19c9e86e
Renato Capelo <rcapelo@mainhub.pt>
2024-09-05 10:38:19 +0100

    Add drop-in unloading on file deletion
    Updated DropInDirectoryMonitorHostedService to handle unloading of drop-ins when files are deleted. Added `_debouncedUnloader` and `_installedDropIns` fields. Modified constructor to initialize these fields. Updated `ExecuteAsync` to call `LoadDropInAssemblyAsync` initially and handle file deletion events. Added `OnDeleted` method to manage file deletions and invoke the debounced unloader. Enhanced `LoadDropInAssemblyAsync` to track loaded drop-ins. Introduced `UnloadDropInAssemblyAsync` to remove drop-ins on file deletion.

--- src/common/Elsa.DropIns/HostedServices/DropInDirectoryMonitorHostedService.cs
+++ src/common/Elsa.DropIns/HostedServices/DropInDirectoryMonitorHostedService.cs
@@ -15,7 +15,11 @@ public class DropInDirectoryMonitorHostedService : BackgroundService
     private readonly IOptions<DropInOptions> _options;
     private readonly IServiceProvider _serviceProvider;
     private readonly RateLimitedFunc<string, Task> _debouncedLoader;
+    private readonly RateLimitedFunc<string, Task> _debouncedUnloader;
     private readonly FileSystemWatcher _watcher;
+
+    private readonly Dictionary<string, List<IDropIn>> _installedDropIns;
+
 
     /// <inheritdoc />
     public DropInDirectoryMonitorHostedService(IOptions<DropInOptions> options, IServiceProvider serviceProvider)
@@ -23,6 +27,9 @@ public class DropInDirectoryMonitorHostedService : BackgroundService
         _options = options;
         _serviceProvider = serviceProvider;
         _debouncedLoader = Debouncer.Debounce<string, Task>(LoadDropInAssemblyAsync, TimeSpan.FromSeconds(2));
+        _debouncedUnloader = Debouncer.Debounce<string, Task>(UnloadDropInAssemblyAsync, TimeSpan.FromSeconds(2));
+
+        _installedDropIns = [];
 
         var rootDirectoryPath = _options.Value.DropInRootDirectory;
 
@@ -38,15 +45,27 @@ public class DropInDirectoryMonitorHostedService : BackgroundService
     }
 
     /// <inheritdoc />
-    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
+        await LoadDropInAssemblyAsync(_options.Value.DropInRootDirectory);
+
         _watcher.Changed += OnChanged;
-        return Task.CompletedTask;
+        _watcher.Deleted += OnDeleted;
     }
 
     private async void OnChanged(object sender, FileSystemEventArgs e)
     {
         var task = _debouncedLoader.Invoke(e.FullPath);
+
+        if (task == null)
+            return;
+
+        await task;
+    }
+
+    private async void OnDeleted(object sender, FileSystemEventArgs e)
+    {
+        var task = _debouncedUnloader.Invoke(e.FullPath);
 
         if (task == null)
             return;
@@ -63,7 +82,25 @@ public class DropInDirectoryMonitorHostedService : BackgroundService
         foreach (var dropInDescriptor in dropInDescriptors)
         {
             var dropIn = (IDropIn)Activator.CreateInstance(dropInDescriptor.Type)!;
+            if(_installedDropIns.TryGetValue(fullPath, out var installedDropIns))
+            {
+                installedDropIns.Add(dropIn);
+            }
+            else
+            {
+                _installedDropIns[fullPath] = [dropIn];
+            }
             await dropIn.ConfigureAsync(_serviceProvider, CancellationToken.None);
         }
+    }
+
+    private Task UnloadDropInAssemblyAsync(string fullPath)
+    {
+        if (_installedDropIns.TryGetValue(fullPath, out var installedDropIn))
+        {
+            installedDropIn.ForEach(dropIn => dropIn.Unconfigure(_serviceProvider));
+            _installedDropIns.Remove(fullPath);
+        }
+        return Task.CompletedTask;
     }
 }
