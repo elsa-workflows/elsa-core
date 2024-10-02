88d09457d7f3eea6da509bd75407745fde1f0eee
Carmine Francesco Giugliano <74872779+cfgiugliano@users.noreply.github.com>
2024-10-02 19:17:47 +0200

    feat: add DomainEvent activity

---
+++ src/modules/Elsa.Workflows.Runtime/Activities/DomainEvent.cs
@@ -0,0 +1,44 @@
+using Elsa.Extensions;
+using Elsa.Mediator.Contracts;
+using Elsa.Workflows.Attributes;
+using Elsa.Workflows.Contracts;
+using Elsa.Workflows.Models;
+using Elsa.Workflows.Runtime.Notifications;
+using JetBrains.Annotations;
+
+namespace Elsa.Workflows.Runtime.Activities;
+
+/// <summary>
+/// Notifies the application that a domain event with a given name is requested to be published.
+/// </summary>
+[Activity("Elsa", "Primitives", "Requests a given domain event to be published. ", Kind = ActivityKind.Action)]
+[UsedImplicitly]
+public class DomainEvent: Activity
+{
+    /// <summary>
+    /// The name of the domain event being published.
+    /// </summary>
+    [Input(Description = "The name of the domain event being published.")]
+    public Input<string> DomainEventName { get; set; } = default!;
+
+    /// <summary>
+    /// The payload of the domain event being published.
+    /// </summary>
+    [Input(Description = "Any additional parameters to send to the domain event.")]
+    public Input<object?> Payload { get; set; } = default!;
+
+    protected override async ValueTask ExecuteAsync(ActivityExecutionContext context)
+    {
+        var domainEventName = DomainEventName.Get(context);
+        var identityGenerator = context.GetRequiredService<IIdentityGenerator>();
+        var domainEventId = identityGenerator.GenerateId();
+
+        // Publish the domain event
+        var domainEventPayload = Payload.GetOrDefault(context);
+        var domainEventNotification = new DomainEventNotification(context, domainEventId, domainEventName, domainEventPayload);
+        var dispatcher = context.GetRequiredService<INotificationSender>();
+
+        await dispatcher.SendAsync(domainEventNotification, context.CancellationToken);
+        await context.CompleteActivityAsync();
+    }
+}
