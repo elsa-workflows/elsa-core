99f9de36b05813d97860dd7c55c9835b966ac7d3
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2024-10-02 23:45:59 +0200

    Remove obsolete Content Definition related classes
    Deleted old content definition records and related classes from the Elsa.ResourceManagement.Core module. These classes were no longer in use and their removal simplifies code maintenance and reduces potential confusion.

--- src/modules/Elsa.ResourceManagement.Core/Metadata/Records/ContentFieldDefinitionRecord.cs
+++
@@ -1,9 +0,0 @@
-namespace Elsa.ResourceManagement.Metadata.Records;
-
-/// <summary>
-/// Represents a field name.
-/// </summary>
-public class ContentFieldDefinitionRecord
-{
-    public string Name { get; set; }
-}
