19bca063daa11800c5448a1a68a24e051441e123
Carmine Francesco Giugliano <74872779+cfgiugliano@users.noreply.github.com>
2024-10-03 19:33:29 +0200

    feat: add ITasksApi to Elsa.Api.Client (#5995)
    * feat: add ITasksApi to Elsa.Api.Client
    
    * fix ReportTaskCompleted method name
    
    ---------
    
    Co-authored-by: Sipke Schoorstra <sipkeschoorstra@outlook.com>

---
+++ src/clients/Elsa.Api.Client/Resources/Tasks/Contracts/ITasksApi.cs
@@ -0,0 +1,19 @@
+using Elsa.Api.Client.Resources.Tasks.Requests;
+using Refit;
+
+namespace Elsa.Api.Client.Resources.Tasks.Contracts;
+
+/// <summary>
+/// Represents a client for the tasks API.
+/// </summary>
+public interface ITasksApi
+{
+    /// <summary>
+    /// Sends the specified request to the tasks API.
+    /// </summary>
+    /// <param name="request">The request.</param>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>The response.</returns>
+    [Post("/tasks/{taskId}/complete")]
+    Task ReportTaskCompletedAsync([Body] ReportTaskCompletedRequest request, CancellationToken cancellationToken = default);
+}
