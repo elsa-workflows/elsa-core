b4a50a1c26e3b7aa7ddc303071fa7ceb3eee7476
Sabina Rosca <rosca_sabina@yahoo.com>
2024-10-04 21:54:55 +0300

    Add MongoDB collection naming strategy

---
+++ src/modules/Elsa.MongoDb/Contracts/ICollectionNamingStrategy.cs
@@ -0,0 +1,12 @@
+﻿namespace Elsa.MongoDb.Contracts;
+
+/// <summary>
+/// Represents a naming strategy to use when creating the name of a MongoDB collection.
+/// </summary>
+public interface ICollectionNamingStrategy
+{
+    /// <summary>
+    /// Returns a collection name from the specified base collection name.
+    /// </summary>
+    string GetCollectionName(string collectionName);
+}
