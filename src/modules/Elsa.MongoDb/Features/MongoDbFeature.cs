5507ff5b1e7587158109f904897295f369dd7bc2
Sabina Rosca <rosca_sabina@yahoo.com>
2024-10-09 19:53:21 +0300

    Register MongoDb client as singleton

--- src/modules/Elsa.MongoDb/Features/MongoDbFeature.cs
+++ src/modules/Elsa.MongoDb/Features/MongoDbFeature.cs
@@ -41,7 +41,9 @@ public class MongoDbFeature : FeatureBase
     {
         Services.Configure(Options);
 
-        Services.AddScoped(sp => CreateDatabase(sp, ConnectionString));
+        var mongoUrl = new MongoUrl(ConnectionString);
+        Services.AddSingleton(sp => CreateMongoClient(sp, mongoUrl));
+        Services.AddScoped(sp => CreateDatabase(sp, mongoUrl));
 
         RegisterSerializers();
         RegisterClassMaps();
@@ -84,11 +86,10 @@ public class MongoDbFeature : FeatureBase
        }
     }
     
-    private static IMongoDatabase CreateDatabase(IServiceProvider sp, string connectionString)
+    private static IMongoClient CreateMongoClient(IServiceProvider sp, MongoUrl mongoUrl)
     {
         var options = sp.GetRequiredService<IOptions<MongoDbOptions>>().Value;
 
-        var mongoUrl = new MongoUrl(connectionString);
         var settings = MongoClientSettings.FromUrl(mongoUrl);
 
         settings.ClusterConfigurator = cb => cb.Subscribe(new DiagnosticsActivityEventSubscriber());
@@ -100,8 +101,13 @@ public class MongoDbFeature : FeatureBase
         settings.RetryWrites = options.RetryWrites;
         settings.SslSettings = options.SslSettings;
 
-        var mongoClient = new MongoClient(settings);
-        return mongoClient.GetDatabase(mongoUrl.DatabaseName);
+        return new MongoClient(settings);
+    }
+
+    private static IMongoDatabase CreateDatabase(IServiceProvider sp, MongoUrl mongoUrl)
+    {
+        var client = sp.GetRequiredService<IMongoClient>();
+        return client.GetDatabase(mongoUrl.DatabaseName);
     }
 
     private static string GetApplicationName(MongoClientSettings settings) =>
