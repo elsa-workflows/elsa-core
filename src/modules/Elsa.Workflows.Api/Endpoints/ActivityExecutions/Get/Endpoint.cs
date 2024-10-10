9164239c235feccd4fde069fb041bd2156a6aa3a
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2024-10-10 21:17:55 +0200

    Add endpoints for activity execution records and summaries
    Created new classes and endpoints to handle activity execution records and summaries retrieval. Added related models and updated relevant interfaces.

---
+++ src/modules/Elsa.Workflows.Api/Endpoints/ActivityExecutions/Get/Endpoint.cs
@@ -0,0 +1,41 @@
+using Elsa.Abstractions;
+using Elsa.Workflows.Runtime.Contracts;
+using Elsa.Workflows.Runtime.Entities;
+using Elsa.Workflows.Runtime.Filters;
+using JetBrains.Annotations;
+
+namespace Elsa.Workflows.Api.Endpoints.ActivityExecutions.Get;
+
+/// <summary>
+/// Gets an individual execution for a given activity.
+/// </summary>
+[PublicAPI]
+internal class List(IActivityExecutionStore store) : ElsaEndpointWithoutRequest<ActivityExecutionRecord>
+{
+    /// <inheritdoc />
+    public override void Configure()
+    {
+        Get("/activity-executions/{id}");
+        ConfigurePermissions("read:activity-execution");
+    }
+
+    /// <inheritdoc />
+    public override async Task HandleAsync(CancellationToken cancellationToken)
+    {
+        var id = Route<string>("id");
+        var filter = new ActivityExecutionRecordFilter
+        {
+            Id = id
+        };
+
+        var record = await store.FindAsync(filter, cancellationToken);
+
+        if (record == null)
+        {
+            await SendNotFoundAsync(cancellationToken);
+            return;
+        }
+
+        await SendOkAsync(record, cancellationToken);
+    }
+}
