ab5bebcdb4754e2adf3e9773f852a05c5c67db87
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2024-10-27 21:48:02 +0100

    Refactor scheduling service to be a background task
    Renamed `CreateSchedulesHostedService` to `CreateSchedulesBackgroundTask` and refactored it to inherit from `BackgroundTask` instead of `BackgroundService`. Simplified the constructor by injecting the required dependencies directly, eliminating the need for a scoped factory.

--- src/modules/Elsa.Scheduling/HostedServices/CreateSchedulesBackgroundTask.cs
+++ src/modules/Elsa.Scheduling/HostedServices/CreateSchedulesBackgroundTask.cs
@@ -1,9 +1,8 @@
+using Elsa.Common;
 using Elsa.Scheduling.Activities;
 using Elsa.Workflows.Helpers;
 using Elsa.Workflows.Runtime;
 using Elsa.Workflows.Runtime.Filters;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Timer = Elsa.Scheduling.Activities.Timer;
 
 namespace Elsa.Scheduling.HostedServices;
@@ -11,28 +10,11 @@ namespace Elsa.Scheduling.HostedServices;
 /// <summary>
 /// Creates new schedules when using the default scheduler (which doesn't have its own persistence layer like Quartz or Hangfire).
 /// </summary>
-public class CreateSchedulesHostedService : BackgroundService
+public class CreateSchedulesBackgroundTask(ITriggerStore triggerStore, IBookmarkStore bookmarkStore, ITriggerScheduler triggerScheduler, IBookmarkScheduler bookmarkScheduler) : BackgroundTask
 {
-    private readonly IServiceScopeFactory _scopeFactory;
-
-    /// <summary>
-    /// Initializes a new instance of the <see cref="CreateSchedulesHostedService"/> class.
-    /// </summary>
-    public CreateSchedulesHostedService(IServiceScopeFactory scopeFactory
-)
-    {
-        _scopeFactory = scopeFactory;
-    }
-
     /// <inheritdoc />
-    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    public override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        using var scope = _scopeFactory.CreateScope();
-        var triggerStore = scope.ServiceProvider.GetRequiredService<ITriggerStore>();
-        var bookmarkStore = scope.ServiceProvider.GetRequiredService<IBookmarkStore>();
-        var triggerScheduler = scope.ServiceProvider.GetRequiredService<ITriggerScheduler>();
-        var bookmarkScheduler = scope.ServiceProvider.GetRequiredService<IBookmarkScheduler>();
-
         var activityTypes = new[] { typeof(Cron), typeof(Timer), typeof(StartAt), typeof(Delay) };
         var activityTypeNames = activityTypes.Select(ActivityTypeNameHelper.GenerateTypeName).ToList();
         var triggerFilter = new TriggerFilter { Names = activityTypeNames };
