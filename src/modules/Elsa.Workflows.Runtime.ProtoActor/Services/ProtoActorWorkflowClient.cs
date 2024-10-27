a73a17235bad5ffe1c9e6d52345cb93defb9dbce
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2024-10-27 09:41:26 +0100

    Add support for custom headers in ProtoActor grain methods
    Introduced a T4 template to generate grain methods with custom headers, enabling the use of tenant ID in requests. Updated `ProtoActorWorkflowClient` to employ these methods, removing redundant code and directly utilizing the client for various workflow operations.

--- src/modules/Elsa.Workflows.Runtime.ProtoActor/Services/ProtoActorWorkflowClient.cs
+++ src/modules/Elsa.Workflows.Runtime.ProtoActor/Services/ProtoActorWorkflowClient.cs
@@ -2,9 +2,7 @@ using Elsa.Common.Multitenancy;
 using Elsa.Extensions;
 using Elsa.Workflows.Runtime.ProtoActor.ProtoBuf;
 using Elsa.Workflows.State;
-using Google.Protobuf;
 using JetBrains.Annotations;
-using Proto;
 using Proto.Cluster;
 using CreateAndRunWorkflowInstanceRequest = Elsa.Workflows.Runtime.Messages.CreateAndRunWorkflowInstanceRequest;
 using CreateWorkflowInstanceRequest = Elsa.Workflows.Runtime.Messages.CreateWorkflowInstanceRequest;
@@ -20,7 +18,6 @@ namespace Elsa.Workflows.Runtime.ProtoActor.Services;
 [UsedImplicitly]
 public class ProtoActorWorkflowClient : IWorkflowClient
 {
-    private readonly Cluster _cluster;
     private readonly Mappers.Mappers _mappers;
     private readonly ITenantAccessor _tenantAccessor;
     private readonly WorkflowInstanceClient _actorClient;
@@ -31,7 +28,6 @@ public class ProtoActorWorkflowClient : IWorkflowClient
     public ProtoActorWorkflowClient(string workflowInstanceId, Cluster cluster, Mappers.Mappers mappers, ITenantAccessor tenantAccessor)
     {
         WorkflowInstanceId = workflowInstanceId;
-        _cluster = cluster;
         _mappers = mappers;
         _tenantAccessor = tenantAccessor;
         _actorClient = cluster.GetNamedWorkflowInstanceClient(WorkflowInstanceId);
@@ -44,7 +40,7 @@ public class ProtoActorWorkflowClient : IWorkflowClient
     public async Task<CreateWorkflowInstanceResponse> CreateInstanceAsync(CreateWorkflowInstanceRequest request, CancellationToken cancellationToken = default)
     {
         var protoRequest = _mappers.CreateWorkflowInstanceRequestMapper.Map(WorkflowInstanceId, request);
-        var response = await Create(protoRequest, cancellationToken);
+        var response = await _actorClient.Create(protoRequest, CreateHeaders(), cancellationToken);
         return _mappers.CreateWorkflowInstanceResponseMapper.Map(response!);
     }
 
@@ -52,7 +48,7 @@ public class ProtoActorWorkflowClient : IWorkflowClient
     public async Task<RunWorkflowInstanceResponse> RunInstanceAsync(RunWorkflowInstanceRequest request, CancellationToken cancellationToken = default)
     {
         var protoRequest = _mappers.RunWorkflowInstanceRequestMapper.Map(request);
-        var response = await Run(protoRequest, cancellationToken);
+        var response = await _actorClient.Run(protoRequest, CreateHeaders(), cancellationToken);
         return _mappers.RunWorkflowInstanceResponseMapper.Map(WorkflowInstanceId, response!);
     }
 
@@ -60,21 +56,20 @@ public class ProtoActorWorkflowClient : IWorkflowClient
     public async Task<RunWorkflowInstanceResponse> CreateAndRunInstanceAsync(CreateAndRunWorkflowInstanceRequest request, CancellationToken cancellationToken = default)
     {
         var protoRequest = _mappers.CreateAndRunWorkflowInstanceRequestMapper.Map(WorkflowInstanceId, request);
-        var response = await CreateAndRun(protoRequest, cancellationToken);
+        var response = await _actorClient.CreateAndRun(protoRequest, CreateHeaders(), cancellationToken);
         return _mappers.RunWorkflowInstanceResponseMapper.Map(WorkflowInstanceId, response!);
     }
-   
-
+    
     /// <inheritdoc />
     public async Task CancelAsync(CancellationToken cancellationToken = default)
     {
-        await Cancel(cancellationToken);
+        await _actorClient.Cancel(CreateHeaders(), cancellationToken);
     }
 
     /// <inheritdoc />
     public async Task<WorkflowState> ExportStateAsync(CancellationToken cancellationToken = default)
     {
-        var response = await ExportState(cancellationToken);
+        var response = await _actorClient.ExportState(CreateHeaders(), cancellationToken);
         return _mappers.WorkflowStateJsonMapper.Map(response!.SerializedWorkflowState);
     }
 
@@ -86,57 +81,13 @@ public class ProtoActorWorkflowClient : IWorkflowClient
         {
             SerializedWorkflowState = protoJson
         };
-        await _actorClient.ImportState(request, cancellationToken);
+        await _actorClient.ImportState(request, CreateHeaders(), cancellationToken);
     }
-    
-    // Copied and adapted from generated grains in order to allow for custom headers.
-    private async Task<Elsa.Workflows.Runtime.ProtoActor.ProtoBuf.CreateWorkflowInstanceResponse?> Create(Elsa.Workflows.Runtime.ProtoActor.ProtoBuf.CreateWorkflowInstanceRequest request, CancellationToken cancellationToken)
-    {
-        return await RequestAsync<Elsa.Workflows.Runtime.ProtoActor.ProtoBuf.CreateWorkflowInstanceRequest, Elsa.Workflows.Runtime.ProtoActor.ProtoBuf.CreateWorkflowInstanceResponse>(request, 0, cancellationToken);
-    }
-    
-    private async Task<Elsa.Workflows.Runtime.ProtoActor.ProtoBuf.RunWorkflowInstanceResponse?> Run(Elsa.Workflows.Runtime.ProtoActor.ProtoBuf.RunWorkflowInstanceRequest request, CancellationToken cancellationToken)
-    {
-        return await RequestAsync<Elsa.Workflows.Runtime.ProtoActor.ProtoBuf.RunWorkflowInstanceRequest, Elsa.Workflows.Runtime.ProtoActor.ProtoBuf.RunWorkflowInstanceResponse>(request, 1, cancellationToken);
-    }
-    
-    private async Task<Elsa.Workflows.Runtime.ProtoActor.ProtoBuf.RunWorkflowInstanceResponse?> CreateAndRun(Elsa.Workflows.Runtime.ProtoActor.ProtoBuf.CreateAndRunWorkflowInstanceRequest request, CancellationToken cancellationToken)
-    {
-        return await RequestAsync<Elsa.Workflows.Runtime.ProtoActor.ProtoBuf.CreateAndRunWorkflowInstanceRequest, Elsa.Workflows.Runtime.ProtoActor.ProtoBuf.RunWorkflowInstanceResponse>(request, 2, cancellationToken);
-    }
-    
-    public async Task Cancel(CancellationToken cancellationToken)
-    {
-        await RequestAsync<Google.Protobuf.WellKnownTypes.Empty, Google.Protobuf.WellKnownTypes.Empty>(null, 4, cancellationToken);
-    }
-    
-    public async Task<ExportWorkflowStateResponse?> ExportState(CancellationToken cancellationToken)
-    {
-        return await RequestAsync<Google.Protobuf.WellKnownTypes.Empty, ExportWorkflowStateResponse>(null, 5, cancellationToken);
-    }
-    
-    private string GrainIdentity => $"WorkflowInstanceActor-{WorkflowInstanceId}";
 
-    private MessageEnvelope CreateEnvelope(GrainRequestMessage gr)
+    private IDictionary<string, string> CreateHeaders()
     {
         var headers = new Dictionary<string, string>();
-        if(_tenantAccessor.Tenant != null) headers["TenantId"] = _tenantAccessor.Tenant.Id;
-        return new MessageEnvelope(gr, _cluster.System.Root.Self, new MessageHeader(headers));
-    }
-    
-    private async Task<TResponse?> RequestAsync<TRequest, TResponse>(TRequest? request, int methodIndex, CancellationToken cancellationToken) where TRequest : IMessage
-    {
-        var gr = new GrainRequestMessage(methodIndex, request);
-        var envelope = CreateEnvelope(gr);
-        var res = await _cluster.RequestAsync<object>(GrainIdentity, WorkflowInstanceActor.Kind, envelope, cancellationToken);
-
-        return res switch
-        {
-            TResponse message => message,
-            GrainResponseMessage grainResponse => (TResponse?)grainResponse.ResponseMessage,
-            GrainErrorResponse grainErrorResponse => throw new Exception(grainErrorResponse.Err),
-            null => default,
-            _ => throw new NotSupportedException($"Unknown response type {res.GetType().FullName}")
-        };
+        if (_tenantAccessor.Tenant != null) headers["TenantId"] = _tenantAccessor.Tenant.Id;
+        return headers;
     }
 }
