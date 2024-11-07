d2608cf6ad2cf07d766895e4f6ef61c4245ba364
Robin Sue <robinsue@live.de>
2024-11-07 23:01:31 +0100

    Undo Migrations reset, fixes #6103

---
+++ src/modules/Elsa.EntityFrameworkCore.PostgreSql/Migrations/Alterations/20240610184626_V3_2.cs
@@ -0,0 +1,28 @@
+#nullable disable
+
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Elsa.EntityFrameworkCore.PostgreSql.Migrations.Alterations
+{
+    /// <inheritdoc />
+    public partial class V3_2 : Migration
+    {
+        private readonly IElsaDbContextSchema _schema;
+
+        /// <inheritdoc />
+        public V3_2(IElsaDbContextSchema schema)
+        {
+            _schema = schema;
+        }
+
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
---
+++ src/modules/Elsa.EntityFrameworkCore.SqlServer/Migrations/Management/20240610184814_V3_2.cs
@@ -0,0 +1,30 @@
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Elsa.EntityFrameworkCore.SqlServer.Migrations.Management
+{
+    /// <inheritdoc />
+    public partial class V3_2 : Migration
+    {
+        private readonly IElsaDbContextSchema _schema;
+
+        /// <inheritdoc />
+        public V3_2(IElsaDbContextSchema schema)
+        {
+            _schema = schema;
+        }
+
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
