6c9579ee2729d39fa5f3994c6b49aec14b59947c
Robin Sue <robinsue@live.de>
2024-11-07 21:48:40 +0100

    Remove unused classes

--- src/modules/Elsa.Workflows.Runtime/Matches/ResumableWorkflowMatch.cs
+++
@@ -1,6 +0,0 @@
-using Elsa.Workflows.Management.Entities;
-
-namespace Elsa.Workflows.Runtime.Matches;
-
-public record ResumableWorkflowMatch(string WorkflowInstanceId, WorkflowInstance? WorkflowInstance, string? CorrelationId, string? BookmarkId, object? Payload)
-    : WorkflowMatch(WorkflowInstanceId, WorkflowInstance, CorrelationId, Payload);
--- src/modules/Elsa.Workflows.Runtime/Matches/WorkflowMatch.cs
+++
@@ -1,5 +0,0 @@
-using Elsa.Workflows.Management.Entities;
-
-namespace Elsa.Workflows.Runtime.Matches;
-
-public record WorkflowMatch(string WorkflowInstanceId, WorkflowInstance? WorkflowInstance, string? CorrelationId, object? Payload);
