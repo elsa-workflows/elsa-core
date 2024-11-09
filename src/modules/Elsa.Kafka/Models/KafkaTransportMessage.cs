7831f4f95bf7faa73e09c36601ff34123fa5eef6
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2024-11-09 21:32:37 +0100

    Refactor KafkaTransportMessage to inline Timestamp namespace
    Simplify the namespace usage for the Timestamp type within the KafkaTransportMessage record. This change eliminates the need for a separate using directive for Timestamp, enhancing code readability and maintainability.

--- src/modules/Elsa.Kafka/Models/KafkaTransportMessage.cs
+++ src/modules/Elsa.Kafka/Models/KafkaTransportMessage.cs
@@ -1,6 +1,5 @@
 using Confluent.Kafka;
-using Timestamp = Confluent.Kafka.Timestamp;
 
 namespace Elsa.Kafka;
 
-public record KafkaTransportMessage(object? Key, string Value, Headers Headers, Timestamp Timestamp);
+public record KafkaTransportMessage(object? Key, string Value, Headers Headers, Confluent.Kafka.Timestamp Timestamp);
