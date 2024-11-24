afdabfd66b437c09a7b5192da25dbfc0dad25642
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2024-11-24 23:20:30 +0100

    Refactor tenant middleware and consumer implementations
    Update `TenantConsumeMiddleware` to utilize `ITenantFinder` and `ITenantScopeFactory` for tenant context management. Refactor `WorkflowMessageConsumer` to use constructor parameters directly for improved dependency injection. Additionally, replace private scope variable in `TenantScope` with a public property to enhance code clarity.

--- src/modules/Elsa.MassTransit/Consumers/WorkflowMessageConsumer.cs
+++ src/modules/Elsa.MassTransit/Consumers/WorkflowMessageConsumer.cs
@@ -1,3 +1,4 @@
+using Elsa.Common.Multitenancy;
 using Elsa.MassTransit.Activities;
 using Elsa.Workflows.Helpers;
 using Elsa.Workflows.Runtime;
@@ -10,18 +11,9 @@ namespace Elsa.MassTransit.Consumers;
 /// <summary>
 /// A consumer of various dispatch message types to asynchronously execute workflows.
 /// </summary>
-public class WorkflowMessageConsumer<T> : IConsumer<T> where T : class
+public class WorkflowMessageConsumer<T>(IWorkflowDispatcher workflowRuntime) : IConsumer<T>
+    where T : class
 {
-    private readonly IWorkflowDispatcher _workflowRuntime;
-
-    /// <summary>
-    /// Constructor.
-    /// </summary>
-    public WorkflowMessageConsumer(IWorkflowDispatcher workflowRuntime)
-    {
-        _workflowRuntime = workflowRuntime;
-    }
-
     /// <inheritdoc />
     public async Task Consume(ConsumeContext<T> context)
     {
@@ -31,12 +23,15 @@ public class WorkflowMessageConsumer<T> : IConsumer<T> where T : class
         var activityTypeName = ActivityTypeNameHelper.GenerateTypeName(messageType);
         var bookmark = new MessageReceivedBookmarkPayload(messageType);
         var correlationId = context.CorrelationId?.ToString();
-        var input = new Dictionary<string, object> { [MessageReceived.InputKey] = message };
+        var input = new Dictionary<string, object>
+        {
+            [MessageReceived.InputKey] = message
+        };
         var request = new DispatchTriggerWorkflowsRequest(activityTypeName, bookmark)
         {
             CorrelationId = correlationId,
             Input = input
         };
-        await _workflowRuntime.DispatchAsync(request, cancellationToken: cancellationToken);
+        await workflowRuntime.DispatchAsync(request, cancellationToken: cancellationToken);
     }
 }
