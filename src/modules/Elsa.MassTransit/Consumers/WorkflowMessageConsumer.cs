bbf9398475ecc6f15f70b23638b99f0502248235
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2024-11-24 23:26:08 +0100

    Refactor to replace IWorkflowDispatcher with IStimulusSender
    Updated WorkflowMessageConsumer to utilize IStimulusSender for handling message-triggered activities. Simplified dependencies and method calls to align with the new interface, improving the code's clarity and maintainability.

--- src/modules/Elsa.MassTransit/Consumers/WorkflowMessageConsumer.cs
+++ src/modules/Elsa.MassTransit/Consumers/WorkflowMessageConsumer.cs
@@ -1,17 +1,14 @@
-using Elsa.Common.Multitenancy;
 using Elsa.MassTransit.Activities;
 using Elsa.Workflows.Helpers;
 using Elsa.Workflows.Runtime;
-using Elsa.Workflows.Runtime.Contracts;
-using Elsa.Workflows.Runtime.Requests;
 using MassTransit;
 
 namespace Elsa.MassTransit.Consumers;
 
 /// <summary>
-/// A consumer of various dispatch message types to asynchronously execute workflows.
+/// A consumer of various message types to trigger activities derived from these messages.
 /// </summary>
-public class WorkflowMessageConsumer<T>(IWorkflowDispatcher workflowRuntime) : IConsumer<T>
+public class WorkflowMessageConsumer<T>(IStimulusSender stimulusSender) : IConsumer<T>
     where T : class
 {
     /// <inheritdoc />
@@ -21,17 +18,17 @@ public class WorkflowMessageConsumer<T>(IWorkflowDispatcher workflowRuntime) : I
         var messageType = typeof(T);
         var message = context.Message;
         var activityTypeName = ActivityTypeNameHelper.GenerateTypeName(messageType);
-        var bookmark = new MessageReceivedBookmarkPayload(messageType);
+        var stimulus = new MessageReceivedBookmarkPayload(messageType);
         var correlationId = context.CorrelationId?.ToString();
         var input = new Dictionary<string, object>
         {
             [MessageReceived.InputKey] = message
         };
-        var request = new DispatchTriggerWorkflowsRequest(activityTypeName, bookmark)
+        var stimulusMetadata = new StimulusMetadata
         {
             CorrelationId = correlationId,
             Input = input
         };
-        await workflowRuntime.DispatchAsync(request, cancellationToken: cancellationToken);
+        await stimulusSender.SendAsync(activityTypeName, stimulus, stimulusMetadata, cancellationToken);
     }
 }
