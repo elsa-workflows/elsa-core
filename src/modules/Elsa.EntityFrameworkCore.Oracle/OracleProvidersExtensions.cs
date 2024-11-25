8502165dcca709daab57bc1f64b890f72f4b5fc5
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2024-11-25 19:57:03 +0100

    Rename UseMySql methods to UseOracle
    Renamed all instances of the `UseMySql` method to `UseOracle` in the `OracleProvidersExtensions` class. This aligns the method names with Oracle-specific functionality. Additionally, marked the class with `PublicAPI` attribute for better code documentation.

--- src/modules/Elsa.EntityFrameworkCore.Oracle/OracleProvidersExtensions.cs
+++ src/modules/Elsa.EntityFrameworkCore.Oracle/OracleProvidersExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection;
+using JetBrains.Annotations;
 using Oracle.EntityFrameworkCore.Infrastructure;
 
 // ReSharper disable once CheckNamespace
@@ -7,21 +8,22 @@ namespace Elsa.EntityFrameworkCore.Extensions;
 /// <summary>
 /// Provides extensions to configure EF Core to use Oracle.
 /// </summary>
+[PublicAPI]
 public static class OracleProvidersExtensions
 {
     private static Assembly Assembly => typeof(OracleProvidersExtensions).Assembly;
     
-    public static TFeature UseMySql<TFeature, TDbContext>(this PersistenceFeatureBase<TFeature, TDbContext> feature, 
+    public static TFeature UseOracle<TFeature, TDbContext>(this PersistenceFeatureBase<TFeature, TDbContext> feature, 
         string connectionString, 
         ElsaDbContextOptions? options = null, 
         Action<OracleDbContextOptionsBuilder>? configure = null) 
         where TDbContext : ElsaDbContextBase
         where TFeature : PersistenceFeatureBase<TFeature, TDbContext>
     {
-        return feature.UseMySql(Assembly, connectionString, options, configure);
+        return feature.UseOracle(Assembly, connectionString, options, configure);
     }
     
-    public static TFeature UseMySql<TFeature, TDbContext>(this PersistenceFeatureBase<TFeature, TDbContext> feature, 
+    public static TFeature UseOracle<TFeature, TDbContext>(this PersistenceFeatureBase<TFeature, TDbContext> feature, 
         Func<IServiceProvider, string> connectionStringFunc,
         ElsaDbContextOptions? options = null,
         Action<OracleDbContextOptionsBuilder>? configure = null
@@ -29,10 +31,10 @@ public static class OracleProvidersExtensions
         where TDbContext : ElsaDbContextBase
         where TFeature : PersistenceFeatureBase<TFeature, TDbContext>
     {
-        return feature.UseMySql(Assembly, connectionStringFunc, options, configure);
+        return feature.UseOracle(Assembly, connectionStringFunc, options, configure);
     }
     
-    public static TFeature UseMySql<TFeature, TDbContext>(this PersistenceFeatureBase<TFeature, TDbContext> feature, 
+    public static TFeature UseOracle<TFeature, TDbContext>(this PersistenceFeatureBase<TFeature, TDbContext> feature, 
         Assembly migrationsAssembly, 
         string connectionString,
         ElsaDbContextOptions? options = null,
@@ -41,10 +43,10 @@ public static class OracleProvidersExtensions
         where TDbContext : ElsaDbContextBase
         where TFeature : PersistenceFeatureBase<TFeature, TDbContext>
     {
-        return feature.UseMySql(migrationsAssembly, _ => connectionString, options, configure);
+        return feature.UseOracle(migrationsAssembly, _ => connectionString, options, configure);
     }
     
-    public static TFeature UseMySql<TFeature, TDbContext>(this PersistenceFeatureBase<TFeature, TDbContext> feature, 
+    public static TFeature UseOracle<TFeature, TDbContext>(this PersistenceFeatureBase<TFeature, TDbContext> feature, 
         Assembly migrationsAssembly, 
         Func<IServiceProvider, string> connectionStringFunc,
         ElsaDbContextOptions? options = null,
