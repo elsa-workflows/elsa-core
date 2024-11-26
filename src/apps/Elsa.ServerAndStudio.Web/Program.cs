e2388eba0603931e5af932ebe3980f432e55e710
Amir h Aghajani <amirhaghajani@gmail.com>
2024-11-26 18:14:04 +0330

    solve error Python.Runtime.BadPythonDllException: Runtime.PythonDLL was not set or does not point to a supported Python runtime DLL on Elsa.ServerAndStudio.Web solution

--- src/apps/Elsa.ServerAndStudio.Web/Program.cs
+++ src/apps/Elsa.ServerAndStudio.Web/Program.cs
@@ -108,7 +108,15 @@ services
             })
             .UseLiquid()
             .UseCSharp()
-            .UsePython()
+            .UsePython(python =>
+            {
+                python.PythonOptions += options =>
+                {
+                    // Make sure to configure the path to the python DLL. E.g. /opt/homebrew/Cellar/python@3.11/3.11.6_1/Frameworks/Python.framework/Versions/3.11/bin/python3.11
+                    // alternatively, you can set the PYTHONNET_PYDLL environment variable.
+                    configuration.GetSection("Scripting:Python").Bind(options);
+                };
+            })
             .UseHttp(http =>
             {
                 if (useCaching)
