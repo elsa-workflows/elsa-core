df250d13643d7f908a89ab142696dd0ba74dc1ce
funshow.liu <funshow.liu@didatravel.com>
2024-12-02 10:38:09 +0800

    test: add test for BigIntegerJsonConverter

---
+++ test/integration/Elsa.JavaScript.IntegrationTests/JsonConverterTest.cs
@@ -0,0 +1,48 @@
+﻿using System.Dynamic;
+using System.Text.Json;
+using Elsa.Common.Converters;
+using Elsa.Expressions.Models;
+using Elsa.JavaScript.Contracts;
+using Elsa.Testing.Shared;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Elsa.JavaScript.IntegrationTests;
+public class JsonConverterTest(ITestOutputHelper testOutputHelper)
+{
+    private readonly IServiceProvider _serviceProvider = new TestApplicationBuilder(testOutputHelper).Build();
+
+    [Fact(DisplayName = "JavaScript BigInt mapping to BigInteger serialization")]
+    public async Task Test1()
+    {
+        var javaScriptEvaluator = _serviceProvider.GetRequiredService<IJavaScriptEvaluator>();
+        var script = @"return {
+    'BigNumber': BigInt('7239948466988781569') 
+}";
+        var expressionExecutionContext = new ExpressionExecutionContext(_serviceProvider, new MemoryRegister());
+        var result = (await javaScriptEvaluator.EvaluateAsync(script, typeof(ExpandoObject), expressionExecutionContext))!;
+        var options = new JsonSerializerOptions()
+        {
+            Converters = { new BigIntegerJsonConverter() }
+        };
+        var serializedText1 = JsonSerializer.Serialize(result);
+        var serializedText = JsonSerializer.Serialize(result, options);
+
+        Assert.Equal("{\"BigNumber\":7239948466988781569}", serializedText);
+    }
+
+    [Fact(DisplayName = "JavaScript BigInt mapping to BigInteger serialization")]
+    public async Task Test2()
+    {
+        var javaScriptEvaluator = _serviceProvider.GetRequiredService<IJavaScriptEvaluator>();
+        var script = @"return {
+    'BigNumber': BigInt('7239948466988781569') 
+}";
+        var expressionExecutionContext = new ExpressionExecutionContext(_serviceProvider, new MemoryRegister());
+        var result = (await javaScriptEvaluator.EvaluateAsync(script, typeof(ExpandoObject), expressionExecutionContext))!;
+        var serializedText = JsonSerializer.Serialize(result);
+
+        Assert.Equal("{\"BigNumber\":{\"IsPowerOfTwo\":false,\"IsZero\":false,\"IsOne\":false,\"IsEven\":false,\"Sign\":1}}", serializedText);
+    }
+}
