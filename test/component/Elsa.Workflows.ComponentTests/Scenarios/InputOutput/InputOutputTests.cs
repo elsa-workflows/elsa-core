c8ae6ce56d6084b2ec08e3e30502d78647271a37
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2024-12-04 18:04:52 +0100

    Add InputOutput workflows and tests for data exchange
    Introduce new Consumer and Provider JSON workflows to facilitate testing of input and output functionalities, ensuring seamless data exchange between processes. Implement corresponding tests to verify the correct reception and transmission of input data within the Provider workflow back to the Consumer, validating the setup with an assertion for expected output. Update the project configuration to include these workflows in the build output.

---
+++ test/component/Elsa.Workflows.ComponentTests/Scenarios/InputOutput/InputOutputTests.cs
@@ -0,0 +1,25 @@
+﻿using Elsa.Workflows.ComponentTests.Abstractions;
+using Elsa.Workflows.ComponentTests.Fixtures;
+using Elsa.Workflows.Management;
+using Elsa.Workflows.Runtime;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Elsa.Workflows.ComponentTests.Scenarios.InputOutput;
+
+public class InputOutputTests(App app) : AppComponentTest(app)
+{
+    [Fact(DisplayName = "Input should be received by the Provider and should in turn be able to send it back to the Consumer.")]
+    public async Task InputShouldBeOutput()
+    {
+        const string workflowDefinitionVersionId = "4e993ab1b3d4e4a1";
+        var workflowDefinitionService = Scope.ServiceProvider.GetRequiredService<IWorkflowDefinitionService>();
+        var workflowInvoker = Scope.ServiceProvider.GetRequiredService<IWorkflowInvoker>();
+        var variableManager = Scope.ServiceProvider.GetRequiredService<IWorkflowInstanceVariableManager>();
+        var workflowGraph = await workflowDefinitionService.FindWorkflowGraphAsync(workflowDefinitionVersionId);
+        var result = await workflowInvoker.InvokeAsync(workflowGraph!);
+        var variables = await variableManager.GetVariablesAsync(result.WorkflowState.Id);
+        var variableValue = variables.First().Value;
+        
+        Assert.Equal("Hello World!", variableValue);
+    }
+}
