bd75f20a5807ca01dc20e24ae9eb200e95a9867d
tulrichtrimble <tyler_ulrich@trimble.com>
2024-12-10 16:55:35 -0800

    cleanup anf fix nugets

--- src/_tyler/Trimble.Elsa.Activities/Activities/Expressions/VariableMatchCondition.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Trimble.Elsa.Activities.Activities.Expressions;
-
-/// <summary>
-/// Defines the variable to be matched in a regex pattern 
-/// for use in the <see cref="RegexVariableExpressionHandler"/>
-/// </summary>
-public record VariableMatchCondition(string variableName, string matchPattern);
