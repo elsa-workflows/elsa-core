a7a1b8b66024169f281a2c0fdf3387f96e0e75a8
Bob Hauser <rhauser@kinaxis.com>
2024-12-16 14:42:30 -0500

    Add support for collections and dictionaries of input

---
+++ test/integration/Elsa.Workflows.IntegrationTests/Activities/CollectionsOfInput/WriteMultiLine.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.CompilerServices;
+using Elsa.Workflows.Models;
+
+namespace Elsa.Workflows.IntegrationTests.Activities.CollectionInputs;
+
+public class WriteMultiLine : CodeActivity
+{
+    public WriteMultiLine([CallerFilePath] string? source = default, [CallerLineNumber] int? line = default) : base(source, line) { }
+
+
+    public WriteMultiLine(List<Input<string>> text, [CallerFilePath] string? source = default, [CallerLineNumber] int? line = default) : this(source, line) => Text = text;
+
+    public List<Input<string>>? Text { get; set; } = default;
+
+    protected override void Execute(ActivityExecutionContext context)
+    {
+        var provider = context.GetService<IStandardOutStreamProvider>() ?? new StandardOutStreamProvider(Console.Out);
+        var textWriter = provider.GetTextWriter();
+
+        Text?.ForEach(t => {
+            var text = context.Get(t);
+            textWriter.WriteLine(text);
+        });
+    }
+}
+
