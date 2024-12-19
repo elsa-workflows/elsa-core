6b11115743652ee61ef600a373ae749e73ce5572
Bob Hauser <rhauser@kinaxis.com>
2024-12-19 15:47:27 -0500

    Updated output handling of asynchronously run workflows to be the same as when run synchronously

---
+++ src/common/Elsa.Testing.Shared.Integration/TestWorkflow.cs
@@ -0,0 +1,18 @@
+﻿using Elsa.Workflows;
+
+namespace Elsa.Testing.Shared;
+
+public class TestWorkflow : WorkflowBase
+{
+    private readonly Action<IWorkflowBuilder> _buildWorkflow;
+
+    public TestWorkflow(Action<IWorkflowBuilder> buildWorkflow)
+    {
+        _buildWorkflow = buildWorkflow;
+    }
+
+    protected override void Build(IWorkflowBuilder workflowBuilder)
+    {
+        _buildWorkflow(workflowBuilder);
+    }
+}
