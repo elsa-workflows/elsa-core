3e76e3efb01a769016a1805e33b10c608fbbe37a
sergergood <serzher@inbox.ru>
2024-12-27 18:57:22 +0300

    Added example of perfomance test

---
+++ test/performance/Elsa.Workflows.PerformanceTests/ConsoleActivitiesBenchmark.cs
@@ -0,0 +1,32 @@
+﻿using BenchmarkDotNet.Attributes;
+using Elsa.Extensions;
+using Elsa.Workflows.Activities;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Elsa.Workflows.PerformanceTests;
+
+[Config(typeof(Config))]
+public class ConsoleActivitiesBenchmark
+{
+    private WriteLine _writeLineWorkflow;
+    private IWorkflowRunner _workflowRunner;
+    private ServiceProvider _serviceProvider;
+
+    [GlobalSetup]
+    public void GlobalSetup()
+    {
+        var services = new ServiceCollection();
+        services.AddElsa();
+
+        _serviceProvider = services.BuildServiceProvider();
+        _workflowRunner = _serviceProvider.GetRequiredService<IWorkflowRunner>();
+        
+        _writeLineWorkflow = new WriteLine("Hello, World!");
+    }
+    
+    [Benchmark]
+    public async Task WriteLine() => await _workflowRunner.RunAsync(_writeLineWorkflow);
+
+    [GlobalCleanup]
+    public void GlobalCleanup() => _serviceProvider.Dispose();
+}
--- test/performance/Elsa.Workflows.PerformanceTests/UnitTest1.cs
+++
@@ -1,9 +0,0 @@
-namespace Elsa.Workflows.PerformanceTests;
-
-public class UnitTest1
-{
-    [Fact]
-    public void Test1()
-    {
-    }
-}
