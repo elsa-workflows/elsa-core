afc14fab4be9e0a9389f3d022f1be028561d8c9a
Jérémie DEVILLARD <jdevillard@users.noreply.github.com>
2025-01-13 15:52:03 +0100

    add virtual method to DropDownOptions provider to allow customization of options in inherited class.
    clean ConnectionOptionsProvider class

--- src/modules/Elsa.Connections.Core/ServiceProvider/ConnectionOptionsProvider.cs
+++ src/modules/Elsa.Connections.Core/ServiceProvider/ConnectionOptionsProvider.cs
@@ -25,8 +25,6 @@ public class ConnectionOptionsProvider : DropDownOptionsProviderBase
 
         var connectionType = propertyInfo.GetCustomAttribute<ConnectionTypeAttribute>()?.Type;
 
-        
-
         if (connectionType == null)
             return connection;
 
@@ -34,13 +32,14 @@ public class ConnectionOptionsProvider : DropDownOptionsProviderBase
 
         foreach(var conn in connections)
             connection.Add(new SelectListItem(conn.Name, conn.Name));
-
-
-
-
-        //connection.Add(new SelectListItem("connection 2", "idConnection2"));
-        //connection.Add(new SelectListItem("connection 3", "idConnection3"));
         return connection;
-        //throw new NotImplementedException();
+    }
+
+    protected override IDictionary<string, object> GetUIPropertyAdditionalOptions()
+    {
+        return new Dictionary<string, object>
+        {
+            ["Refresh"] = true
+        };
     }
 }
