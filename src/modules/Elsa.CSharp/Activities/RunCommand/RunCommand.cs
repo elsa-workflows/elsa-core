2184b68540e31f0abb4d0d2321bcc450aba650ce
IBRAHIM NADA <ibrrahim.nada@gmail.com>
2025-01-21 15:09:34 +0300

    Create class Library

---
+++ src/modules/Elsa.CSharp/Activities/RunCommand/RunCommand.cs
@@ -0,0 +1,11 @@
+﻿
+using Elsa.Workflows;
+using Elsa.Workflows.Attributes;
+
+namespace Elsa.CSharp.Activities.RunCommand;
+
+[Activity("Elsa", "Scripting", "Runs a command predefined from the code base", DisplayName = "Run C# Command")]
+public class RunCommand : Activity
+{
+
+}
