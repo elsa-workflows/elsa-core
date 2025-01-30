2402bacabab5a6a1c2811958285ef840682c8eef
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2025-01-30 18:11:35 +0100

    Remove custom JSON converter for workflow commit strategies.
    Eliminated `WorkflowCommitStateStrategyJsonConverter` and its usage in the `IWorkflowCommitStrategy` interface. This simplifies serialization logic and reduces unnecessary complexity in the codebase.

--- src/modules/Elsa.Workflows.Core/CommitStates/Contracts/IWorkflowCommitStrategy.cs
+++ src/modules/Elsa.Workflows.Core/CommitStates/Contracts/IWorkflowCommitStrategy.cs
@@ -1,9 +1,5 @@
-using System.Text.Json.Serialization;
-using Elsa.Workflows.CommitStates.Serialization;
-
 namespace Elsa.Workflows.CommitStates;
 
-[JsonConverter(typeof(WorkflowCommitStateStrategyJsonConverter))]
 public interface IWorkflowCommitStrategy
 {
     CommitAction ShouldCommit(WorkflowCommitStateStrategyContext context);
