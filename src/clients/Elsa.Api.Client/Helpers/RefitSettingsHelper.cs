bec0e559b221e2cd54582071877ec540b94d9d92
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2025-02-08 10:10:40 +0100

    Refactor RefitSettingsHelper for improved readability
    Reformatted object initialization in CreateRefitSettings to improve structure and code clarity. Also cleaned up unnecessary whitespace for consistency and better readability.

--- src/clients/Elsa.Api.Client/Helpers/RefitSettingsHelper.cs
+++ src/clients/Elsa.Api.Client/Helpers/RefitSettingsHelper.cs
@@ -15,7 +15,10 @@ public static class RefitSettingsHelper
     /// </summary>
     public static RefitSettings CreateRefitSettings(IServiceProvider serviceProvider, Action<IServiceProvider, JsonSerializerOptions>? configureJsonSerializerOptions = null)
     {
-        var settings = new RefitSettings { ContentSerializer = new SystemTextJsonContentSerializer(CreateJsonSerializerOptions(serviceProvider, configureJsonSerializerOptions)) };
+        var settings = new RefitSettings
+        {
+            ContentSerializer = new SystemTextJsonContentSerializer(CreateJsonSerializerOptions(serviceProvider, configureJsonSerializerOptions))
+        };
 
         return settings;
     }
@@ -33,9 +36,9 @@ public static class RefitSettingsHelper
         options.Converters.Add(new JsonStringEnumConverter());
         options.Converters.Add(new VersionOptionsJsonConverter());
         options.Converters.Add(new TypeJsonConverter());
-        
+
         configureJsonSerializerOptions?.Invoke(serviceProvider, options);
-        
+
         return options;
     }
 }
