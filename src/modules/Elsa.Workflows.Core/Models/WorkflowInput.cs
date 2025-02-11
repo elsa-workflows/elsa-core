3fb4bb18f1cd27e916c921405b9ab74b4f47a82d
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2025-02-11 15:44:48 +0100

    Rename ActivityInput to WorkflowInput and replace 'default' with 'null'.
    The record class was renamed to better reflect its purpose as representing workflow input. Additionally, parameters with a default value of 'default' were updated to 'null' for increased clarity and consistency. These changes improve code readability and maintainability.

--- src/modules/Elsa.Workflows.Core/Models/WorkflowInput.cs
+++ src/modules/Elsa.Workflows.Core/Models/WorkflowInput.cs
@@ -1,6 +1,6 @@
 namespace Elsa.Workflows.Models;
 
 /// <summary>
-/// Represents an activity input name and value.
+/// Represents a workflow input name and value.
 /// </summary>
 public record WorkflowInput(string Name, object? Value);
