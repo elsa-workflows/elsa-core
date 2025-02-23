7aabfb6d0bc1d62f12f05dd45d10eb663c1ea217
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2025-02-23 20:10:16 +0100

    Remove Elsa.OrchardCore module.
    Deleted the entire Elsa.OrchardCore module, including all its associated activities, handlers, services, and extensions. This removes support for integrations with Orchard Core application instances.

--- src/modules/Elsa.OrchardCore/Client/Contracts/ISecurityTokenService.cs
+++
@@ -1,7 +0,0 @@
-namespace Elsa.OrchardCore.Client;
-
-public interface ISecurityTokenService
-{
-    Task<SecurityToken> GetTokenAsync(CancellationToken cancellationToken = default);
-    Task<SecurityToken> RefreshTokenAsync(CancellationToken cancellationToken = default);
-}
--- src/modules/Elsa.OrchardCore/Options/OrchardCoreOptions.cs
+++
@@ -1,6 +0,0 @@
-namespace Elsa.OrchardCore;
-
-public class OrchardCoreOptions
-{
-    public ISet<string> ContentTypes { get; set; } = new HashSet<string>();
-}
