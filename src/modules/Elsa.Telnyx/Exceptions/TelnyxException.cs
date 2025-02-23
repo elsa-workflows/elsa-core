b9e9459e94960e634495418b61065b552b4913d1
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2025-02-23 14:47:19 +0100

    Remove Telnyx module from the codebase
    Eliminated all source files related to the Telnyx module, including activities, payloads, extensions, and models. This effectively deprecates Telnyx-related functionalities from the system in favor of the new Elsa.Integrations.Telnyx module in the [elsa-integrations](https://github.com/elsa-workflows/elsa-integrations) repo.

--- src/modules/Elsa.Telnyx/Exceptions/TelnyxException.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Elsa.Telnyx.Exceptions;
-
-public class TelnyxException : Exception
-{
-    public TelnyxException(string message, Exception? innerException = default) : base(message, innerException)
-    {
-    }
-}
--- src/modules/Elsa.Telnyx/Payloads/Call/CallMachineGreetingEndedBase.cs
+++
@@ -1,8 +0,0 @@
-using Elsa.Telnyx.Payloads.Abstractions;
-
-namespace Elsa.Telnyx.Payloads.Call;
-
-public record CallMachineGreetingEndedBase : CallPayload
-{
-    public string Result { get; set; } = default!;
-}
