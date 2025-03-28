6b7f156149d86709628b4f99cd09fa0ae5b8bd0e
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2025-03-28 22:36:18 +0100

    Simplify XML doc comment in IStimulusDispatcher interface
    Refined the XML documentation for the `SendAsync` method by shortening the description and clarifying the statement. This improves readability and maintains the intent of the comment.

--- src/modules/Elsa.Workflows.Runtime/Contracts/IStimulusDispatcher.cs
+++ src/modules/Elsa.Workflows.Runtime/Contracts/IStimulusDispatcher.cs
@@ -6,7 +6,7 @@ namespace Elsa.Workflows.Runtime;
 public interface IStimulusDispatcher
 {
     /// <summary>
-    /// Dispatches a stimulus to an activity. This could result in new workflow instances as well as existing workflow instances being resumed.
+    /// Dispatches stimulus. This could result in new workflow instances being started as well as existing workflow instances being resumed.
     /// </summary>
     Task<DispatchStimulusResponse> SendAsync(DispatchStimulusRequest request, CancellationToken cancellationToken = default);
 }
