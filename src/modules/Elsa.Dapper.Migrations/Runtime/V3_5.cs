8c7f617050609bb7ad69024e46631a2bb3670f9a
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2025-04-02 00:03:27 +0200

    Add migration to track fault counts in runtime (V3.5)
    This migration adds a new column, "AggregatedFaultCount," to the "ActivityExecutionRecords" table. The column is an integer, non-nullable, with a default value of 0, and enables tracking aggregated fault occurrences. The migration also includes a rollback to remove this column if needed.

---
+++ src/modules/Elsa.Dapper.Migrations/Runtime/V3_5.cs
@@ -0,0 +1,25 @@
+using System.Diagnostics.CodeAnalysis;
+using FluentMigrator;
+using JetBrains.Annotations;
+using static System.Int32;
+
+namespace Elsa.Dapper.Migrations.Runtime;
+
+/// <inheritdoc />
+[Migration(20006, "Elsa:Runtime:V3.5")]
+[PublicAPI]
+[SuppressMessage("ReSharper", "InconsistentNaming")]
+public class V3_5 : Migration
+{
+    /// <inheritdoc />
+    public override void Up()
+    {
+        Alter.Table("ActivityExecutionRecords").AddColumn("AggregatedFaultCount").AsInt32().NotNullable().WithDefault(0);
+    }
+
+    /// <inheritdoc />
+    public override void Down()
+    {
+        Delete.Column("AggregatedFaultCount").FromTable("ActivityExecutionRecords");
+    }
+}
