8c52acece504d6cab491e2476c2943ceef264427
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2025-04-10 15:02:39 +0200

    Fix MessageReceived not setting Result (#6562)
    * Fix MessageReceived not setting Result
    
    Fixes #6540
    
    * Add custom equality comparer for VariableDescriptor
    
    Introduced a `VariableDescriptorComparer` to ensure proper equality checks within `HashSet<VariableDescriptor>`. Updated related code to use the comparer and added a new message type `OrderReceived` for MassTransit integration.

--- src/modules/Elsa.MassTransit/Activities/MessageReceived.cs
+++ src/modules/Elsa.MassTransit/Activities/MessageReceived.cs
@@ -16,46 +16,61 @@ public class MessageReceived : Trigger<object>
     internal const string InputKey = "Message";
 
     /// <inheritdoc />
-    public MessageReceived([CallerFilePath] string? source = default, [CallerLineNumber] int? line = default) : base(source, line)
+    public MessageReceived([CallerFilePath] string? source = null, [CallerLineNumber] int? line = null) : base(source, line)
     {
     }
 
     /// <summary>
     /// The message type to receive.
     /// </summary>
-    public Type MessageType { get; set; } = default!;
+    public Type MessageType { get; set; } = null!;
 
     /// <inheritdoc />
     protected override object GetTriggerPayload(TriggerIndexingContext context) => GetBookmarkPayload(context.ExpressionExecutionContext);
 
     /// <inheritdoc />
-    protected override async ValueTask ExecuteAsync(ActivityExecutionContext context)
+    protected override ValueTask ExecuteAsync(ActivityExecutionContext context)
     {
-        // If we did not receive external input, it means we are just now encountering this activity and we need to block execution by creating a bookmark.
-        if (!context.TryGetWorkflowInput<object>(InputKey, out var message) || message.GetType() != MessageType)
+        // If we did not receive external input, it means we are just now encountering this activity, and we need to block execution by creating a bookmark.
+        if (!TryGetMessage(context, out var message))
         {
             // Create bookmarks for when we receive the expected HTTP request.
             context.CreateBookmark(GetBookmarkPayload(context.ExpressionExecutionContext), ResumeAsync, includeActivityInstanceId: false);
-            return;
+            return default;
         }
 
-        // Provide the received message as output.
-        context.Set(Result, message);
-        
-        // Remove the input to prevent it from being passed to the next activity.
-        context.WorkflowInput.Remove(InputKey);
-        
-        // Complete.
-        await context.CompleteActivityAsync();
+        return ExecuteInternalAsync(context, message);
     }
 
     private ValueTask ResumeAsync(ActivityExecutionContext context)
     {
+        if(!TryGetMessage(context, out var message))
+            throw new InvalidOperationException("Message was not received.");
+        return ExecuteInternalAsync(context, message);
+    }
+
+    private ValueTask ExecuteInternalAsync(ActivityExecutionContext context, object message)
+    {
+        // Provide the received message as output.
+        context.Set(Result, message);
+
         // Remove the input to prevent it from being passed to the next activity.
         context.WorkflowInput.Remove(InputKey);
-        
+
         // Complete.
         return context.CompleteActivityAsync();
+    }
+
+    private bool TryGetMessage(ActivityExecutionContext context, out object message)
+    {
+        if (!context.TryGetWorkflowInput(InputKey, out message))
+            return false;
+
+        if (message.GetType() == MessageType)
+            return true;
+
+        message = null!;
+        return false;
     }
 
     private object GetBookmarkPayload(ExpressionExecutionContext context)
--- src/modules/Elsa.Workflows.Management/Models/VariableDescriptor.cs
+++ src/modules/Elsa.Workflows.Management/Models/VariableDescriptor.cs
@@ -3,4 +3,21 @@ namespace Elsa.Workflows.Management.Models;
 /// <summary>
 /// Represents a description of a .NET type that can be used as a workflow variable.
 /// </summary>
-public record VariableDescriptor(Type Type, string Category, string? Description);
+public record VariableDescriptor(Type Type, string Category, string? Description)
+{
+    public class VariableDescriptorComparer : IEqualityComparer<VariableDescriptor>
+    {
+        public bool Equals(VariableDescriptor? x, VariableDescriptor? y)
+        {
+            if (ReferenceEquals(x, y)) return true;
+            if (x is null || y is null) return false;
+
+            return x.Type == y.Type;
+        }
+
+        public int GetHashCode(VariableDescriptor obj)
+        {
+            return obj.Type.GetHashCode();
+        }
+    }
+}
