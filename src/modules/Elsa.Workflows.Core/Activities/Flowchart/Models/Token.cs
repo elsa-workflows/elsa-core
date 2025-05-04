8ecbd4498060115a0ce8bb3ebe0b1bc5be9dc8f4
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2025-05-04 22:08:44 +0200

    Refactor Token model to add scheduling functionality
    Replaced the `consumed` parameter with a `Scheduled` property and added a `Schedule` method for mark scheduling. This enhances the token model's flexibility, improving flowchart activity management.

--- src/modules/Elsa.Workflows.Core/Activities/Flowchart/Models/Token.cs
+++ src/modules/Elsa.Workflows.Core/Activities/Flowchart/Models/Token.cs
@@ -1,15 +1,22 @@
 namespace Elsa.Workflows.Activities.Flowchart.Models;
 
-internal class Token(string fromActivityId, string? fromActivityName, string toActivityId, string? toActivityName, bool consumed)
+internal class Token(string fromActivityId, string? fromActivityName, string toActivityId, string? toActivityName)
 {
-    public static Token Create(IActivity from, IActivity to) => new(from.Id, from.Name, to.Id, to.Name, false);
+    public static Token Create(IActivity from, IActivity to) => new(from.Id, from.Name, to.Id, to.Name);
 
     public string FromActivityId { get; } = fromActivityId;
     public string? FromActivityName { get; } = fromActivityName;
     public string ToActivityId { get; } = toActivityId;
     public string? ToActivityName { get; } = toActivityName;
-    public bool Consumed { get; private set; } = consumed;
-        
+    public bool Scheduled { get; set; }
+    public bool Consumed { get; private set; }
+
+    public Token Schedule()
+    {
+        Scheduled = true;
+        return this;
+    }
+
     public Token Consume()
     {
         Consumed = true;
