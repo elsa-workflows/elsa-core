0906277ea18019dccf675d1981cb4984b6b417d0
Sipke Schoorstra <sipke.schoorstra@nexxbiz.io>
2025-05-19 14:54:35 +0200

    Add logging to DefaultActivitySchedulerMiddleware
    Introduce ILogger to enable detailed logging of workflow state transitions, cancellations, and activity execution. This enhances observability and helps in debugging workflow execution by providing insights into the internal process.

--- src/modules/Elsa.Workflows.Core/Middleware/Workflows/DefaultActivitySchedulerMiddleware.cs
+++ src/modules/Elsa.Workflows.Core/Middleware/Workflows/DefaultActivitySchedulerMiddleware.cs
@@ -3,6 +3,7 @@ using Elsa.Workflows.CommitStates;
 using Elsa.Workflows.Models;
 using Elsa.Workflows.Options;
 using Elsa.Workflows.Pipelines.WorkflowExecution;
+using Microsoft.Extensions.Logging;
 
 namespace Elsa.Workflows.Middleware.Workflows;
 
@@ -20,13 +21,14 @@ public static class UseActivitySchedulerMiddlewareExtensions
 /// <summary>
 /// A workflow execution middleware component that executes scheduled work items.
 /// </summary>
-public class DefaultActivitySchedulerMiddleware(WorkflowMiddlewareDelegate next, IActivityInvoker activityInvoker, ICommitStrategyRegistry commitStrategyRegistry) : WorkflowExecutionMiddleware(next)
+public class DefaultActivitySchedulerMiddleware(WorkflowMiddlewareDelegate next, IActivityInvoker activityInvoker, ICommitStrategyRegistry commitStrategyRegistry, ILogger<DefaultActivitySchedulerMiddleware> logger) : WorkflowExecutionMiddleware(next)
 {
     /// <inheritdoc />
     public override async ValueTask InvokeAsync(WorkflowExecutionContext context)
     {
         var scheduler = context.Scheduler;
 
+        logger.LogDebug("Transitioning workflow context to Executing state.");
         context.TransitionTo(WorkflowSubStatus.Executing);
         await ConditionallyCommitStateAsync(context, WorkflowLifetimeEvent.WorkflowExecuting);
         
@@ -34,7 +36,10 @@ public class DefaultActivitySchedulerMiddleware(WorkflowMiddlewareDelegate next,
         {
             // Do not start a workflow if cancellation has been requested.
             if (context.CancellationToken.IsCancellationRequested)
+            {
+                logger.LogDebug("Cancellation requested. Transitioning workflow context to Cancelled state.");
                 break;
+            }
             
             var currentWorkItem = scheduler.Take();
             await ExecuteWorkItemAsync(context, currentWorkItem);
@@ -43,7 +48,24 @@ public class DefaultActivitySchedulerMiddleware(WorkflowMiddlewareDelegate next,
         await Next(context);
         
         if (context.Status == WorkflowStatus.Running)
-            context.TransitionTo(context.AllActivitiesCompleted() ? WorkflowSubStatus.Finished : WorkflowSubStatus.Suspended);
+        {
+            var allActivitiesCompleted = context.AllActivitiesCompleted();
+            logger.LogDebug("All activities completed: {AllActivitiesCompleted}.", allActivitiesCompleted);
+            if (!allActivitiesCompleted)
+            {
+                var incompleteActivities = context.ActivityExecutionContexts.Where(x => !x.IsCompleted).ToList();
+                logger.LogDebug("Incomplete activities: {IncompleteActivitiesCount}.", incompleteActivities.Count);
+                foreach (var activityExecutionContext in incompleteActivities)
+                {
+                    var activity = activityExecutionContext.Activity;
+                    var activityId = activity.Id;
+                    var activityType = activity.Type;
+                    var activityStatus = activityExecutionContext.Status;
+                    logger.LogDebug("Activity {ActivityType}: {ActivityId} is in status {ActivityStatus}.", activityType, activityId, activityStatus);
+                }
+            }
+            context.TransitionTo(allActivitiesCompleted ? WorkflowSubStatus.Finished : WorkflowSubStatus.Suspended);
+        }
     }
 
     private async Task ExecuteWorkItemAsync(WorkflowExecutionContext context, ActivityWorkItem workItem)
@@ -57,6 +79,7 @@ public class DefaultActivitySchedulerMiddleware(WorkflowMiddlewareDelegate next,
             Input = workItem.Input
         };
 
+        logger.LogDebug("Executing scheduled activity {ActivityType}: {ActivityId}.", workItem.Activity.Type, workItem.Activity.Id);;
         await activityInvoker.InvokeAsync(context, workItem.Activity, options);
     }
     
@@ -72,6 +95,9 @@ public class DefaultActivitySchedulerMiddleware(WorkflowMiddlewareDelegate next,
         var commitAction = strategy.ShouldCommit(strategyContext);
         
         if (commitAction is CommitAction.Commit)
+        {
+            logger.LogDebug("Committing workflow context.");
             await context.CommitAsync();
+        }
     }
 }
