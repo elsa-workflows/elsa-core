10f92bf16b8577fb834c373c72cd0b7309a6a7e8
Matt <knibbsy10@live.com>
2025-05-20 12:11:27 +0100

    Return Caching to Core.

---
+++ src/modules/Elsa.Caching/Contracts/IChangeTokenSignaler.cs
@@ -0,0 +1,19 @@
+using Microsoft.Extensions.Primitives;
+
+namespace Elsa.Caching;
+
+/// <summary>
+/// Provides change tokens for memory caches, allowing code to evict cache entries by triggering a signal.
+/// </summary>
+public interface IChangeTokenSignaler
+{
+    /// <summary>
+    /// Gets a change token for the specified key.
+    /// </summary>
+    IChangeToken GetToken(string key);
+    
+    /// <summary>
+    /// Triggers the change token for the specified key.
+    /// </summary>
+    ValueTask TriggerTokenAsync(string key, CancellationToken cancellationToken = default);
+}
---
+++ src/modules/Elsa.Caching/Services/ChangeTokenSignaler.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Primitives;
+
+namespace Elsa.Caching.Services;
+
+/// <inheritdoc />
+public class ChangeTokenSignaler(IChangeTokenSignalInvoker invoker) : IChangeTokenSignaler
+{
+    /// <inheritdoc />
+    public IChangeToken GetToken(string key)
+    {
+        return invoker.GetToken(key);
+    }
+
+    /// <inheritdoc />
+    public ValueTask TriggerTokenAsync(string key, CancellationToken cancellationToken = default)
+    {
+        return invoker.TriggerTokenAsync(key, cancellationToken);
+    }
+}
