f8968b5cfcb5e722b3eeb6b6baf3f609b6dd9de1
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2025-05-25 20:38:02 +0200

    Simplify CheckListOptions logic
    Refactor `StaticCheckListOptionsProvider` to inherit from the new `CheckListOptionsProviderBase` and streamline checklist item generation logic.

--- src/modules/Elsa.Workflows.Core/UIHints/CheckList/StaticCheckListOptionsProvider.cs
+++ src/modules/Elsa.Workflows.Core/UIHints/CheckList/StaticCheckListOptionsProvider.cs
@@ -6,32 +6,22 @@ namespace Elsa.Workflows.UIHints.CheckList;
 /// <summary>
 /// Provides static drop-down options for a given property.
 /// </summary>
-public class StaticCheckListOptionsProvider : IPropertyUIHandler
+public class StaticCheckListOptionsProvider : CheckListOptionsProviderBase
 {
     /// <inheritdoc />
-    public ValueTask<IDictionary<string, object>> GetUIPropertiesAsync(PropertyInfo propertyInfo, object? context, CancellationToken cancellationToken = default)
+    protected override ValueTask<ICollection<CheckListItem>> GetItemsAsync(PropertyInfo propertyInfo, object? context, CancellationToken cancellationToken)
     {
         var inputAttribute = propertyInfo.GetCustomAttribute<InputAttribute>();
         var inputOptions = inputAttribute?.Options;
-        var dictionary = new Dictionary<string, object>();
 
         if (inputOptions == null)
-            return new(dictionary);
+            return new([]);
 
         var selectListItems = (inputOptions as ICollection<string>)?.Select(x => new CheckListItem(x, x)).ToList();
 
         if (selectListItems == null)
-            return new(dictionary);
-
-        var props = new CheckListProps
-        {
-            CheckList = new()
-            {
-                Items = selectListItems.ToList() 
-            }
-        };
-
-        dictionary[InputUIHints.CheckList] = props;
-        return new(dictionary);
+            return new([]);
+        
+        return new(selectListItems);
     }
 }
