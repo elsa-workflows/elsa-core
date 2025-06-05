b4fd26d52376037e89a424d140251d99947f2aca
Matt <knibbsy10@live.com>
2025-06-05 01:56:44 +0100

    Add radio list support and related UI components
    - Renamed `CheckList` to `RadioList` in `RadioListProps`.
    - Updated `WorkflowsFeature` to include `RadioListUIHintHandler` and `StaticRadioListOptionsProvider`.
    - Introduced `TestRadioList` class for executing radio list functionality.
    - Created `RadioList` and `RadioListItem` classes for managing radio list items.
    - Added `RadioListOptionsProviderBase` for custom radio list data logic.
    - Implemented `StaticRadioListOptionsProvider` for static radio list options.

---
+++ src/apps/Elsa.Server.Web/RadioListActivity.cs
@@ -0,0 +1,42 @@
+﻿using System.Runtime.CompilerServices;
+using Elsa.Workflows;
+using Elsa.Workflows.Attributes;
+using Elsa.Workflows.UIHints;
+using Elsa.Workflows.Models;
+
+// ReSharper disable once CheckNamespace
+namespace Elsa.Server.Web;
+
+/// <summary>
+/// Executes C# code.
+/// </summary>
+[Activity("Elsa", "TESTS", "Tests Radio List Functionality", DisplayName = "TEST")]
+public class TestRadioList : CodeActivity<object?>
+{
+    /// <inheritdoc />
+    public TestRadioList([CallerFilePath] string? source = null, [CallerLineNumber] int? line = null) : base(source, line)
+    {
+    }
+
+    /// <inheritdoc />
+    public TestRadioList(string script, [CallerFilePath] string? source = null, [CallerLineNumber] int? line = null) : this(source, line)
+    {
+    }
+
+    /// <summary>
+    /// The script to run.
+    /// </summary>
+    [Input(
+        Description = "Choose to download one file or entire folder",
+        DefaultValue = "File",
+        Options = new[] { "File", "Folder" },
+        UIHint = InputUIHints.RadioList
+        )]
+    public Input<string> SelectedRadioOption { get; set; } = default!;
+
+    /// <inheritdoc />
+    protected override async ValueTask ExecuteAsync(ActivityExecutionContext context)
+    {
+        
+    }
+}
