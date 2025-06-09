9e9cd918169f8d797e9077d62097891bade4c672
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2025-06-09 14:36:42 +0200

    Add support for clearing test values of variables
    Introduced a "Clear Test Value" button in the EditVariableTestValueDialog to allow clearing variable test values. Extended the WorkflowDefinition logic to handle clearing variable test values and updated related UI elements for consistency.

--- src/clients/Elsa.Api.Client/Resources/WorkflowDefinitions/Extensions/WorkflowDefinitionExtensions.cs
+++ src/clients/Elsa.Api.Client/Resources/WorkflowDefinitions/Extensions/WorkflowDefinitionExtensions.cs
@@ -5,18 +5,20 @@ namespace Elsa.Api.Client.Resources.WorkflowDefinitions.Extensions;
 
 public static class WorkflowDefinitionExtensions
 {
+    private const string VariableTestValuesKey = "VariableTestValues";
+
     public static IDictionary<string, object?> GetVariableTestValues(this WorkflowDefinition workflowDefinition)
     {
-        if (!workflowDefinition.CustomProperties.TryGetValue("VariableTestValues", out var dictionary))
+        if (!workflowDefinition.CustomProperties.TryGetValue(VariableTestValuesKey, out var dictionary))
         {
             var testValues = new Dictionary<string, object?>();
-            workflowDefinition.CustomProperties["VariableTestValues"] = testValues;
+            workflowDefinition.CustomProperties[VariableTestValuesKey] = testValues;
             return testValues;
         }
-        
-        if(dictionary is JsonElement jsonElement)
+
+        if (dictionary is JsonElement jsonElement)
             dictionary = JsonSerializer.Deserialize<Dictionary<string, object?>>(jsonElement.GetRawText());
-        
+
         return dictionary as IDictionary<string, object?> ?? throw new InvalidOperationException("Invalid variable test values.");
     }
 
@@ -25,11 +27,18 @@ public static class WorkflowDefinitionExtensions
         var testValues = workflowDefinition.GetVariableTestValues();
         return testValues.TryGetValue(variableId, out var value) ? value : null;
     }
-    
+
     public static void SetVariableTestValue(this WorkflowDefinition workflowDefinition, string variableId, object? value)
     {
         var testValues = workflowDefinition.GetVariableTestValues();
         testValues[variableId] = value;
-        workflowDefinition.CustomProperties["VariableTestValues"] = testValues;       
+        workflowDefinition.CustomProperties[VariableTestValuesKey] = testValues;
+    }
+
+    public static void ClearVariableTestValue(this WorkflowDefinition workflowDefinition, string variableId)
+    {
+        var testValues = workflowDefinition.GetVariableTestValues();
+        testValues.Remove(variableId);
+        workflowDefinition.CustomProperties[VariableTestValuesKey] = testValues;
     }
 }
