8ce30480a0c0b10c2d89930188e434f4c4c59ced
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2025-06-09 13:50:42 +0200

    Refactor test run logic and enhance activity response handling
    Introduced new properties in `TestActivityResponse` to better manage activity state and payload. Updated the test run UI to display detailed status, state, outcomes, and outputs using separate data panels. Refactored backend logic to improve data organization and ensure a clear separation of responsibility within the `UpdateDataModels` method.

--- src/clients/Elsa.Api.Client/Resources/Tests/TestActivityResponse.cs
+++ src/clients/Elsa.Api.Client/Resources/Tests/TestActivityResponse.cs
@@ -5,8 +5,9 @@ namespace Elsa.Api.Client.Resources.Tests;
 
 public class TestActivityResponse
 {
+    public IDictionary<string, object?> ActivityState { get; set; } = new Dictionary<string, object?>();
+    public IDictionary<string, object>? Payload { get; set; }
     public IDictionary<string, object?>? Outputs { get; set; }
-    public ICollection<string>? Outcomes { get; set; }
     public ExceptionState? Exception { get; set; }
     public ActivityStatus Status { get; set; }
 }
