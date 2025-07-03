51b0ddd4adebe3019ae114ba816adc283d5e883e
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2025-07-03 22:32:56 +0200

    Add backward compatibility for `WorkflowInstance.Name` mapping in `WorkflowStateMapper`
    - Introduced constant `WorkflowInstanceNameKey` to handle legacy workflow instance name properties.
    - Updated `MapWorkflowStateToWorkflowInstance` method to set `Name` property for older instances.

--- src/modules/Elsa.Workflows.Management/Mappers/WorkflowStateMapper.cs
+++ src/modules/Elsa.Workflows.Management/Mappers/WorkflowStateMapper.cs
@@ -1,3 +1,5 @@
+using Elsa.Extensions;
+using Elsa.Workflows.Activities;
 using Elsa.Workflows.Management.Entities;
 using Elsa.Workflows.State;
 
@@ -8,6 +10,11 @@ namespace Elsa.Workflows.Management.Mappers;
 /// </summary>
 public class WorkflowStateMapper
 {
+    /// <summary>
+    /// [Obsolete] The property key name used to store the workflow instance name.
+    /// </summary>
+    private const string WorkflowInstanceNameKey = "WorkflowInstanceName";
+    
     /// <summary>
     /// Maps a workflow state to a workflow instance.
     /// </summary>
@@ -43,6 +50,10 @@ public class WorkflowStateMapper
         target.UpdatedAt = source.UpdatedAt;
         target.FinishedAt = source.FinishedAt;
         target.WorkflowState = source;
+        
+        // Keep for backward compatibility with workflow instances created before the introduction of the Name property.
+        if (source.Properties.TryGetValue<string>(WorkflowInstanceNameKey, out var name))
+            target.Name = name;
     }
 
     /// <summary>
