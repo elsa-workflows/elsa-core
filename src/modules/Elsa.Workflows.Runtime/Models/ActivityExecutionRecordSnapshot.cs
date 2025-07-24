9b1a76d047eef7f5244e61527399e12f396e2d04
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2025-07-24 21:38:18 +0200

    Extend `ActivityExecutionRecordSnapshot` and update `DefaultActivityExecutionMapper` to include additional activity execution details.

--- src/modules/Elsa.Workflows.Runtime/Models/ActivityExecutionRecordSnapshot.cs
+++ src/modules/Elsa.Workflows.Runtime/Models/ActivityExecutionRecordSnapshot.cs
@@ -2,6 +2,19 @@ namespace Elsa.Workflows.Runtime;
 
 public class ActivityExecutionRecordSnapshot
 {
+    public string Id { get; set; } = null!;
+    public string? TenantId { get; set; }
+    public string WorkflowInstanceId { get; set; } = null!;
+    public string ActivityId { get; set; } = null!;
+    public string ActivityNodeId { get; set; } = null!;
+    public string ActivityType { get; set; } = null!;
+    public int ActivityTypeVersion { get; set; }
+    public string? ActivityName { get; set; }
+    public DateTimeOffset StartedAt { get; set; }
+    public bool HasBookmarks { get; set; }
+    public ActivityStatus Status { get; set; }
+    public int AggregateFaultCount { get; set; }
+    public DateTimeOffset? CompletedAt { get; set; }
     public string? SerializedActivityState { get; set; }
     public string? SerializedOutputs { get; set; }
     public string? SerializedProperties { get; set; }
