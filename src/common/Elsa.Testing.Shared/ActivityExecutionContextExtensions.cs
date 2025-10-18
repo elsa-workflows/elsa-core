92b54f1fee7c07583df3fdba1fb2c50f64de9075
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2025-10-18 11:32:12 +0200

    Add XML documentation for `ActivityExecutionContextExtensions`, detailing methods and parameters.

--- src/common/Elsa.Testing.Shared/ActivityExecutionContextExtensions.cs
+++ src/common/Elsa.Testing.Shared/ActivityExecutionContextExtensions.cs
@@ -2,8 +2,17 @@ using Elsa.Workflows;
 
 namespace Elsa.Testing.Shared;
 
+/// <summary>
+/// Provides extension methods for <see cref="ActivityExecutionContext"/> to enhance its functionality.
+/// </summary>
 public static class ActivityExecutionContextExtensions
 {
+    /// <summary>
+    /// Determines whether the specified activity is scheduled in the workflow execution context.
+    /// </summary>
+    /// <param name="activityExecutionContext">The activity execution context instance used for checking the scheduled activity.</param>
+    /// <param name="activity">The activity to check for scheduling.</param>
+    /// <returns>True if the specified activity is scheduled; otherwise, false.</returns>
     public static bool HasScheduledActivity(this ActivityExecutionContext activityExecutionContext, IActivity activity)
     {
         return activityExecutionContext.WorkflowExecutionContext.Scheduler.Find(x => x.Activity == activity) != null;
