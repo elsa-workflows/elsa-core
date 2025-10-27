460875994ed1e9a86c29414bff082ac8de6fed94
lucas.hipolito <lukhipolito@yahoo.com.br>
2025-10-27 11:35:32 +0100

    Small refactoring of while unit tests

--- test/unit/Elsa.Activities.UnitTests/Looping/WhileTests.cs
+++ test/unit/Elsa.Activities.UnitTests/Looping/WhileTests.cs
@@ -6,35 +6,26 @@ namespace Elsa.Activities.UnitTests.Looping;
 
 public class WhileTests
 {
-    [Fact]
-    public async Task Should_Not_Schedule_Body_When_Condition_Is_False()
+    [Theory]
+    [InlineData(false, 0)]
+    [InlineData(true, 1)]
+    public async Task Should_Schedule_Body_Based_On_Condition(bool conditionValue, int expectedScheduledCount)
     {
         // Arrange
         var bodyActivity = Substitute.For<IActivity>();
-        var whileActivity = new While(new Input<bool>(false), bodyActivity);
+        var whileActivity = new While(new Input<bool>(conditionValue), bodyActivity);
 
         // Act
         var context = await ExecuteAsync(whileActivity);
 
         // Assert
         var scheduledActivities = context.WorkflowExecutionContext.Scheduler.List().ToList();
-        Assert.Empty(scheduledActivities);
-    }
+        Assert.Equal(expectedScheduledCount, scheduledActivities.Count);
 
-    [Fact]
-    public async Task Should_Schedule_Body_When_Condition_Is_True()
-    {
-        // Arrange
-        var bodyActivity = Substitute.For<IActivity>();
-        var whileActivity = new While(new Input<bool>(true), bodyActivity);
-
-        // Act
-        var context = await ExecuteAsync(whileActivity);
-
-        // Assert
-        var scheduledActivities = context.WorkflowExecutionContext.Scheduler.List().ToList();
-        Assert.Single(scheduledActivities);
-        Assert.Equal(bodyActivity, scheduledActivities.First().Activity);
+        if (expectedScheduledCount > 0)
+        {
+            Assert.Equal(bodyActivity, scheduledActivities.First().Activity);
+        }
     }
 
     [Fact]
