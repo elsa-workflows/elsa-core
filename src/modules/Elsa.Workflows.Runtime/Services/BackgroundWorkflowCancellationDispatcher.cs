372948b54287bd033b98e8c4ecbdb23ac597caf7
Sverre Winkelmans <sverre.winkelmans@gmail.com>
2025-11-07 08:02:13 +0800

    Add tenant headers support to BackgroundWorkflowCancellationDispatcher

--- src/modules/Elsa.Workflows.Runtime/Services/BackgroundWorkflowCancellationDispatcher.cs
+++ src/modules/Elsa.Workflows.Runtime/Services/BackgroundWorkflowCancellationDispatcher.cs
@@ -1,5 +1,7 @@
+using Elsa.Common.Multitenancy;
 using Elsa.Mediator;
 using Elsa.Mediator.Contracts;
+using Elsa.Tenants.Mediator;
 using Elsa.Workflows.Runtime.Commands;
 using Elsa.Workflows.Runtime.Requests;
 using Elsa.Workflows.Runtime.Responses;
@@ -9,7 +11,7 @@ namespace Elsa.Workflows.Runtime;
 /// <summary>
 /// Dispatches workflow cancellation requests to a local background worker.
 /// </summary>
-public class BackgroundWorkflowCancellationDispatcher(ICommandSender commandSender) : IWorkflowCancellationDispatcher
+public class BackgroundWorkflowCancellationDispatcher(ICommandSender commandSender, ITenantAccessor tenantAccessor) : IWorkflowCancellationDispatcher
 {
     /// <inheritdoc />
     public async Task<DispatchCancelWorkflowsResponse> DispatchAsync(DispatchCancelWorkflowRequest request, CancellationToken cancellationToken = default)
@@ -17,5 +19,11 @@ public class BackgroundWorkflowCancellationDispatcher(ICommandSender commandSend
         var command = new CancelWorkflowsCommand(request);
         await commandSender.SendAsync(command, CommandStrategy.Background, cancellationToken);
         return new DispatchCancelWorkflowsResponse();
+    }
+    
+    
+    private IDictionary<object, object> CreateHeaders()
+    {
+        return TenantHeaders.CreateHeaders(tenantAccessor.Tenant?.Id);
     }
 }
