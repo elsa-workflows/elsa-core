8aadbc336cd861371d5e176db23b5219c3a9cffa
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2025-11-20 20:12:49 +0100

    Remove `EnumerableTypeConverter` and update related usages for serialization.
    - Deleted the `EnumerableTypeConverter` class and its JSON serialization logic.
    - Removed associated type descriptor attribute in `DefaultFormattersFeature`.
    - Updated `ObjectFormatter` to handle collection serialization directly with JSON.

--- src/apps/Elsa.Server.Web/Program.cs
+++ src/apps/Elsa.Server.Web/Program.cs
@@ -4,14 +4,10 @@ using Elsa.Common.RecurringTasks;
 using Elsa.Expressions.Helpers;
 using Elsa.Extensions;
 using Elsa.Features.Services;
-using Elsa.Identity.Multitenancy;
 using Elsa.Persistence.EFCore.Extensions;
 using Elsa.Persistence.EFCore.Modules.Management;
 using Elsa.Persistence.EFCore.Modules.Runtime;
 using Elsa.Server.Web.Filters;
-using Elsa.Tenants.AspNetCore;
-using Elsa.Tenants.Extensions;
-using Elsa.Workflows;
 using Elsa.Workflows.Api;
 using Elsa.Workflows.CommitStates.Strategies;
 using Elsa.Workflows.IncidentStrategies;
@@ -46,7 +42,7 @@ services
             .AddWorkflowsFrom<Program>()
             .UseIdentity(identity =>
             {
-                identity.TokenOptions = options => identityTokenSection.Bind(options);
+                identity.TokenOptions += options => identityTokenSection.Bind(options);
                 identity.UseConfigurationBasedUserProvider(options => identitySection.Bind(options));
                 identity.UseConfigurationBasedApplicationProvider(options => identitySection.Bind(options));
                 identity.UseConfigurationBasedRoleProvider(options => identitySection.Bind(options));
@@ -135,6 +131,7 @@ services.AddCors(cors => cors.AddDefaultPolicy(policy => policy.AllowAnyHeader()
 
 // Build the web application.
 var app = builder.Build();
+
 
 // Configure the pipeline.
 if (app.Environment.IsDevelopment())
