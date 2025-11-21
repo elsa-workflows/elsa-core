f0c6c44194f60ff2dc6449785fd392c1832481dc
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2025-11-21 12:10:54 +0100

    Refactor extension methods to use instance methods for improved encapsulation and readability in core workflow modules

--- src/modules/Elsa.Workflows.Core/Extensions/ActivityDescriptorExtensions.cs
+++ src/modules/Elsa.Workflows.Core/Extensions/ActivityDescriptorExtensions.cs
@@ -9,54 +9,57 @@ namespace Elsa.Extensions;
 /// </summary>
 public static class ActivityDescriptorExtensions
 {
-    /// <summary>
-    /// Returns the specified attribute from the activity descriptor.
-    /// </summary>
-    public static T? GetAttribute<T>(this ActivityDescriptor activityDescriptor) where T : Attribute
+    extension(ActivityDescriptor activityDescriptor)
     {
-        return (T?)activityDescriptor.Attributes.FirstOrDefault(x => x.GetType() == typeof(T));
-    }
+        /// <summary>
+        /// Returns the specified attribute from the activity descriptor.
+        /// </summary>
+        public T? GetAttribute<T>() where T : Attribute
+        {
+            return (T?)activityDescriptor.Attributes.FirstOrDefault(x => x.GetType() == typeof(T));
+        }
 
-    /// <summary>
-    /// Returns each wrapped input from the specified activity.
-    /// </summary>
-    public static IEnumerable<InputDescriptor> GetWrappedInputPropertyDescriptors(this ActivityDescriptor activityDescriptor, IActivity activity)
-    {
-        return activityDescriptor.Inputs.Where(x => x.IsWrapped);
-    }
-    
-    /// <summary>
-    /// Returns each naked input from the specified activity.
-    /// </summary>
-    public static IEnumerable<InputDescriptor> GetNakedInputPropertyDescriptors(this ActivityDescriptor activityDescriptor, IActivity activity)
-    {
-        return activityDescriptor.Inputs.Where(x => !x.IsWrapped);
-    }
-    
-    /// <summary>
-    /// Returns each input from the specified activity.
-    /// </summary>
-    public static IDictionary<string, Input?> GetWrappedInputProperties(this ActivityDescriptor activityDescriptor, IActivity activity)
-    {
-        var wrappedInputDescriptors = activityDescriptor.Inputs.Where(x => x.IsWrapped).ToList();
-        var inputLookup = wrappedInputDescriptors.ToDictionary(x => x.Name, x => (Input?)x.ValueGetter(activity));
-        return inputLookup;
-    }
-    
-    /// <summary>
-    /// Returns each input descriptor from the specified activity.
-    /// </summary>
-    public static InputDescriptor? GetWrappedInputPropertyDescriptor(this ActivityDescriptor activityDescriptor, IActivity activity, string name)
-    {
-        return activityDescriptor.Inputs.FirstOrDefault(x => x.Name == name && x.IsWrapped);
-    }
-    
-    /// <summary>
-    /// Returns each input from the specified activity.
-    /// </summary>
-    public static Input? GetWrappedInputProperty(this ActivityDescriptor activityDescriptor, IActivity activity, string name)
-    {
-        var inputDescriptor = activityDescriptor.Inputs.FirstOrDefault(x => x.Name == name && x.IsWrapped);
-        return inputDescriptor?.ValueGetter(activity) as Input;
+        /// <summary>
+        /// Returns each wrapped input from the specified activity.
+        /// </summary>
+        public IEnumerable<InputDescriptor> GetWrappedInputPropertyDescriptors(IActivity activity)
+        {
+            return activityDescriptor.Inputs.Where(x => x.IsWrapped);
+        }
+
+        /// <summary>
+        /// Returns each naked input from the specified activity.
+        /// </summary>
+        public IEnumerable<InputDescriptor> GetNakedInputPropertyDescriptors(IActivity activity)
+        {
+            return activityDescriptor.Inputs.Where(x => !x.IsWrapped);
+        }
+
+        /// <summary>
+        /// Returns each input from the specified activity.
+        /// </summary>
+        public IDictionary<string, Input?> GetWrappedInputProperties(IActivity activity)
+        {
+            var wrappedInputDescriptors = activityDescriptor.Inputs.Where(x => x.IsWrapped).ToList();
+            var inputLookup = wrappedInputDescriptors.ToDictionary(x => x.Name, x => (Input?)x.ValueGetter(activity));
+            return inputLookup;
+        }
+
+        /// <summary>
+        /// Returns each input descriptor from the specified activity.
+        /// </summary>
+        public InputDescriptor? GetWrappedInputPropertyDescriptor(IActivity activity, string name)
+        {
+            return activityDescriptor.Inputs.FirstOrDefault(x => x.Name == name && x.IsWrapped);
+        }
+
+        /// <summary>
+        /// Returns each input from the specified activity.
+        /// </summary>
+        public Input? GetWrappedInputProperty(IActivity activity, string name)
+        {
+            var inputDescriptor = activityDescriptor.Inputs.FirstOrDefault(x => x.Name == name && x.IsWrapped);
+            return inputDescriptor?.ValueGetter(activity) as Input;
+        }
     }
 }
