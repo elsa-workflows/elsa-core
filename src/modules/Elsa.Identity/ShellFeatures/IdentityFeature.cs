331b6052a2f1188eb03361aef9275d1282dde64f
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2026-01-15 21:08:20 +0100

    Add `Identity` and `DefaultAuthentication` shell features with enhanced authentication and authorization support

---
+++ src/modules/Elsa.Identity/ShellFeatures/IdentityFeature.cs
@@ -0,0 +1,119 @@
+using AspNetCore.Authentication.ApiKey;
+using CShells.FastEndpoints.Features;
+using CShells.Features;
+using Elsa.Common.Multitenancy;
+using Elsa.Extensions;
+using Elsa.Identity.Contracts;
+using Elsa.Identity.Entities;
+using Elsa.Identity.Multitenancy;
+using Elsa.Identity.Options;
+using Elsa.Identity.Providers;
+using Elsa.Identity.Services;
+using JetBrains.Annotations;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Elsa.Identity.ShellFeatures;
+
+/// <summary>
+/// Provides identity feature to authenticate &amp; authorize API requests.
+/// </summary>
+[ShellFeature(
+    DisplayName = "Identity",
+    Description = "Provides identity management, authentication and authorization capabilities",
+    DependsOn = ["SystemClock"])]
+[UsedImplicitly]
+public class IdentityFeature : IFastEndpointsShellFeature
+{
+    /// <summary>
+    /// A delegate that creates an instance of an implementation of <see cref="IUserStore"/>.
+    /// </summary>
+    public Func<IServiceProvider, IUserStore> UserStore { get; set; } = sp => sp.GetRequiredService<MemoryUserStore>();
+
+    /// <summary>
+    /// A delegate that creates an instance of an implementation of <see cref="IApplicationStore"/>.
+    /// </summary>
+    public Func<IServiceProvider, IApplicationStore> ApplicationStore { get; set; } = sp => sp.GetRequiredService<MemoryApplicationStore>();
+
+    /// <summary>
+    /// A delegate that creates an instance of an implementation of <see cref="IRoleStore"/>.
+    /// </summary>
+    public Func<IServiceProvider, IRoleStore> RoleStore { get; set; } = sp => sp.GetRequiredService<MemoryRoleStore>();
+
+    /// <summary>
+    /// A delegate that creates an instance of an implementation of <see cref="IUserProvider"/>.
+    /// </summary>
+    public Func<IServiceProvider, IUserProvider> UserProvider { get; set; } = sp => sp.GetRequiredService<AdminUserProvider>();
+
+    /// <summary>
+    /// A delegate that creates an instance of an implementation of <see cref="IApplicationProvider"/>.
+    /// </summary>
+    public Func<IServiceProvider, IApplicationProvider> ApplicationProvider { get; set; } = sp => sp.GetRequiredService<StoreBasedApplicationProvider>();
+
+    /// <summary>
+    /// A delegate that creates an instance of an implementation of <see cref="IRoleProvider"/>.
+    /// </summary>
+    public Func<IServiceProvider, IRoleProvider> RoleProvider { get; set; } = sp => sp.GetRequiredService<AdminRoleProvider>();
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        // Configure options with defaults
+        services.Configure<IdentityTokenOptions>(_ => { });
+        services.Configure<ApiKeyOptions>(ApiKeyDefaults.AuthenticationScheme, options =>
+        {
+            options.Realm = "Elsa Workflows";
+            options.KeyName = "ApiKey";
+        });
+        services.Configure<UsersOptions>(_ => { });
+        services.Configure<ApplicationsOptions>(_ => { });
+        services.Configure<RolesOptions>(_ => { });
+
+        // Memory stores.
+        services
+            .AddMemoryStore<User, MemoryUserStore>()
+            .AddMemoryStore<Application, MemoryApplicationStore>()
+            .AddMemoryStore<Role, MemoryRoleStore>();
+
+        // User providers.
+        services
+            .AddScoped<AdminUserProvider>()
+            .AddScoped<StoreBasedUserProvider>()
+            .AddScoped<ConfigurationBasedUserProvider>();
+
+        // Application providers.
+        services
+            .AddScoped<StoreBasedApplicationProvider>()
+            .AddScoped<ConfigurationBasedApplicationProvider>();
+
+        // Role providers.
+        services
+            .AddScoped<AdminRoleProvider>()
+            .AddScoped<StoreBasedRoleProvider>()
+            .AddScoped<ConfigurationBasedRoleProvider>();
+
+        // Tenant resolution strategies.
+        services
+            .AddScoped<ITenantResolver, ClaimsTenantResolver>()
+            .AddScoped<ITenantResolver, CurrentUserTenantResolver>();
+
+        // Services.
+        services
+            .AddScoped(UserStore)
+            .AddScoped(ApplicationStore)
+            .AddScoped(RoleStore)
+            .AddScoped(UserProvider)
+            .AddScoped(ApplicationProvider)
+            .AddScoped(RoleProvider)
+            .AddScoped<ISecretHasher, DefaultSecretHasher>()
+            .AddScoped<IAccessTokenIssuer, DefaultAccessTokenIssuer>()
+            .AddScoped<IUserCredentialsValidator, DefaultUserCredentialsValidator>()
+            .AddScoped<IApplicationCredentialsValidator, DefaultApplicationCredentialsValidator>()
+            .AddScoped<IApiKeyGenerator>(sp => sp.GetRequiredService<DefaultApiKeyGeneratorAndParser>())
+            .AddScoped<IApiKeyParser>(sp => sp.GetRequiredService<DefaultApiKeyGeneratorAndParser>())
+            .AddScoped<IClientIdGenerator, DefaultClientIdGenerator>()
+            .AddScoped<ISecretGenerator, DefaultSecretGenerator>()
+            .AddScoped<IRandomStringGenerator, DefaultRandomStringGenerator>()
+            .AddScoped<DefaultApiKeyGeneratorAndParser>()
+            .AddHttpContextAccessor()
+            ;
+    }
+}
