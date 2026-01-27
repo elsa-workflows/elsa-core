15fd780bb6aa0e84aef1f89a5ea24ceea7359894
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2026-01-27 14:30:09 +0100

    Enable multitenancy support and normalize tenant ID handling.
    - Activate multitenancy in `Program.cs`.
    - Introduce `NormalizeTenantId` method for consistent tenant ID usage.
    - Update tenant-related classes and features to support normalization logic.

--- src/modules/Elsa.Tenants/Extensions/ModuleExtensions.cs
+++ src/modules/Elsa.Tenants/Extensions/ModuleExtensions.cs
@@ -1,5 +1,6 @@
 using Elsa.Features.Services;
 using Elsa.Tenants.Features;
+using JetBrains.Annotations;
 
 // ReSharper disable once CheckNamespace
 namespace Elsa.Tenants.Extensions;
@@ -7,32 +8,39 @@ namespace Elsa.Tenants.Extensions;
 /// <summary>
 /// Extensions for <see cref="IModule"/> that installs the <see cref="TenantsFeature"/> feature.
 /// </summary>
+[UsedImplicitly]
 public static class ModuleExtensions
 {
     /// <summary>
     /// Installs and configures the <see cref="TenantsFeature"/> feature.
     /// </summary>
-    public static IModule UseTenants(this IModule module, Action<TenantsFeature>? configure = default)
+    [UsedImplicitly]
+    public static IModule UseTenants(this IModule module, Action<TenantsFeature>? configure = null)
     {
         module.Configure(configure);
         return module;
     }
 
-    /// <summary>
-    /// Installs and configures the <see cref="TenantManagementEndpointsFeature"/> feature.
-    /// </summary>
-    public static TenantsFeature UseTenantManagementEndpoints(this TenantsFeature feature, Action<TenantManagementEndpointsFeature>? configure = default)
+    extension(TenantsFeature feature)
     {
-        feature.Module.Configure(configure);
-        return feature;
-    }
+        /// <summary>
+        /// Installs and configures the <see cref="TenantManagementEndpointsFeature"/> feature.
+        /// </summary>
+        [UsedImplicitly]
+        public TenantsFeature UseTenantManagementEndpoints(Action<TenantManagementEndpointsFeature>? configure = null)
+        {
+            feature.Module.Configure(configure);
+            return feature;
+        }
 
-    /// <summary>
-    /// Installs and configures the <see cref="TenantManagementFeature"/> feature.
-    /// </summary>
-    public static TenantsFeature UseTenantManagement(this TenantsFeature feature, Action<TenantManagementFeature>? configure = default)
-    {
-        feature.Module.Configure(configure);
-        return feature;
+        /// <summary>
+        /// Installs and configures the <see cref="TenantManagementFeature"/> feature.
+        /// </summary>
+        [UsedImplicitly]
+        public TenantsFeature UseTenantManagement(Action<TenantManagementFeature>? configure = null)
+        {
+            feature.Module.Configure(configure);
+            return feature;
+        }
     }
 }
