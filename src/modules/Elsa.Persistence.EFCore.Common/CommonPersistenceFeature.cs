6c1e6fc14c93f750501ba26b6165f3fd4d0823ca
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2026-01-27 15:26:31 +0100

    Enable configuration-based multitenancy with tenant-specific settings
    - Introduced a configuration-based tenant provider to streamline tenant initialization and customization.
    - Added tenant ID handling filters to ensure tenant ID is applied and filtered automatically.
    - Deprecated the `CommonPersistenceFeature` in favor of modular persistence feature extension.

--- src/modules/Elsa.Persistence.EFCore.Common/CommonPersistenceFeature.cs
+++ src/modules/Elsa.Persistence.EFCore.Common/CommonPersistenceFeature.cs
@@ -1,17 +1,17 @@
-using Elsa.Persistence.EFCore.EntityHandlers;
-using Elsa.Features.Abstractions;
-using Elsa.Features.Services;
-using Microsoft.Extensions.DependencyInjection;
-
-namespace Elsa.Persistence.EFCore;
-
-/// <inheritdoc />
-public class CommonPersistenceFeature(IModule module) : FeatureBase(module)
-{
-    /// <inheritdoc />
-    public override void Apply()
-    {
-        Services.AddScoped<IEntitySavingHandler, ApplyTenantId>();
-        Services.AddScoped<IEntityModelCreatingHandler, SetTenantIdFilter>();
-    }
-}
+// using Elsa.Persistence.EFCore.EntityHandlers;
+// using Elsa.Features.Abstractions;
+// using Elsa.Features.Services;
+// using Microsoft.Extensions.DependencyInjection;
+//
+// namespace Elsa.Persistence.EFCore;
+//
+// /// <inheritdoc />
+// public class CommonPersistenceFeature(IModule module) : FeatureBase(module)
+// {
+//     /// <inheritdoc />
+//     public override void Apply()
+//     {
+//         Services.AddScoped<IEntitySavingHandler, ApplyTenantId>();
+//         Services.AddScoped<IEntityModelCreatingHandler, SetTenantIdFilter>();
+//     }
+// }
