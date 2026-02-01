b71d18db42c4f1afeaf68a16306112f39b857526
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2026-02-01 14:18:06 +0100

    Improve async handling in `CommandHandlerInvokerMiddleware` to await tasks without blocking

--- src/common/Elsa.Mediator/Middleware/Command/Components/CommandHandlerInvokerMiddleware.cs
+++ src/common/Elsa.Mediator/Middleware/Command/Components/CommandHandlerInvokerMiddleware.cs
@@ -41,8 +41,10 @@ public class CommandHandlerInvokerMiddleware(CommandMiddlewareDelegate next) : I
         // Execute command.
         var task = executeMethodWithReturnType.Invoke(strategy, [strategyContext]);
 
-        // Get the result of the task.
+        // Await the task to get the result without blocking.
         var taskWithReturnType = typeof(Task<>).MakeGenericType(resultType);
+        var taskInstance = (Task)task!;
+        await taskInstance.ConfigureAwait(false);
         var resultProperty = taskWithReturnType.GetProperty(nameof(Task<object>.Result))!;
         context.Result = resultProperty.GetValue(task);
 
