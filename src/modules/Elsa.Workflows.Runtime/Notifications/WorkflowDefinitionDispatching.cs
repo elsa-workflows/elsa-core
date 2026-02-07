bc94071b1c47e6da091689e8c2ca5865379aa0ba
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2026-02-07 11:46:58 +0100

    Cleanup: Remove workflow dispatch notifications

--- src/modules/Elsa.Workflows.Runtime/Notifications/WorkflowDefinitionDispatching.cs
+++
@@ -1,9 +0,0 @@
-using Elsa.Mediator.Contracts;
-using Elsa.Workflows.Runtime.Requests;
-
-namespace Elsa.Workflows.Runtime.Notifications;
-
-/// <summary>
-/// A notification that is published when a workflow definition is being dispatched.
-/// </summary>
-public record WorkflowDefinitionDispatching(DispatchWorkflowDefinitionRequest Request) : INotification;
--- src/modules/Elsa.Workflows.Runtime/Notifications/WorkflowInstanceDispatching.cs
+++
@@ -1,9 +0,0 @@
-using Elsa.Mediator.Contracts;
-using Elsa.Workflows.Runtime.Requests;
-
-namespace Elsa.Workflows.Runtime.Notifications;
-
-/// <summary>
-/// A notification that is published when a workflow instance is being dispatched.
-/// </summary>
-public record WorkflowInstanceDispatching(DispatchWorkflowInstanceRequest Request) : INotification;
