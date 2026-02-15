f58417cd7bf9d27b342e0840aef8f738db079761
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2026-02-15 18:47:50 +0100

    Remove IRoleStore dependency from Create endpoint

--- src/modules/Elsa.Identity/Endpoints/Users/Create/Endpoint.cs
+++ src/modules/Elsa.Identity/Endpoints/Users/Create/Endpoint.cs
@@ -8,11 +8,8 @@ namespace Elsa.Identity.Endpoints.Users.Create;
 /// An endpoint that creates a new user. Requires the <code>SecurityRoot</code> policy.
 /// </summary>
 [PublicAPI]
-internal class Create(IUserManager userManager, IRoleStore roleStore)
-    : ElsaEndpoint<Request, Response>
+internal class Create(IUserManager userManager) : ElsaEndpoint<Request, Response>
 {
-    private readonly IRoleStore _roleStore = roleStore;
-
     /// <inheritdoc />
     public override void Configure()
     {
