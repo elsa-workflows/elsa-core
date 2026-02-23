5bf753882584c12dad802511cec5eb510b9727b1
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2026-02-23 20:26:25 +0100

    Refactor `RefreshWorkflowDefinitionsAsync` for lock acquisition logic simplification.

--- src/modules/Elsa.Workflows.Runtime.Distributed/Services/DistributedWorkflowDefinitionsRefresher.cs
+++ src/modules/Elsa.Workflows.Runtime.Distributed/Services/DistributedWorkflowDefinitionsRefresher.cs
@@ -14,44 +14,33 @@ public class DistributedWorkflowDefinitionsRefresher(IWorkflowDefinitionsRefresh
     IDistributedLockProvider distributedLockProvider,
     ILogger<DistributedWorkflowDefinitionsRefresher> logger) : IWorkflowDefinitionsRefresher
 {
-    
-    
     /// <summary>
     /// This ensures that only one instance of the application can refresh a set of workflow definitions at a time, preventing potential conflicts and ensuring consistency across distributed environments.
     /// </summary>
     public async Task<RefreshWorkflowDefinitionsResponse> RefreshWorkflowDefinitionsAsync(RefreshWorkflowDefinitionsRequest request, CancellationToken cancellationToken = default)
     {
-        if (request.DefinitionIds == null 
-            || request.DefinitionIds.Count == 0)
+        var isRefreshingAll = request.DefinitionIds == null || request.DefinitionIds.Count == 0;
+        var lockKey = isRefreshingAll
+            ? "WorkflowDefinitionsRefresher:All"
+            : $"WorkflowDefinitionsRefresher:{string.Join(",", request.DefinitionIds!.OrderBy(x => x))}";
+
+        await using var distributedLock = await distributedLockProvider.TryAcquireLockAsync(
+            lockKey,
+            TimeSpan.Zero,
+            cancellationToken);
+
+        if (distributedLock == null)
         {
-            const string refreshAllLockKey = "WorkflowDefinitionsRefresher:All";
-            await using var distributedAllLock = await distributedLockProvider.TryAcquireLockAsync(
-                refreshAllLockKey,
-                TimeSpan.Zero,
-                cancellationToken);
-            
-            if (distributedAllLock == null)
-            {
-                logger.LogInformation("Could not acquire lock for refreshing all workflow definitions. Another instance is already refreshing all workflow definitions");
-                return new RefreshWorkflowDefinitionsResponse(Array.Empty<string>(), Array.Empty<string>(), RefreshWorkflowDefinitionsStatus.AlreadyInProgress);
-            }
+            var logMessage = isRefreshingAll
+                ? "Could not acquire lock for refreshing all workflow definitions. Another instance is already refreshing all workflow definitions"
+                : "Could not acquire lock for refreshing workflow definitions. Another instance is already refreshing these workflow definitions";
+
+            logger.LogInformation(logMessage);
+
+            var failedDefinitionIds = isRefreshingAll ? Array.Empty<string>() : request.DefinitionIds!;
+            return new(Array.Empty<string>(), failedDefinitionIds, RefreshWorkflowDefinitionsStatus.AlreadyInProgress);
         }
-        else
-        {
-            var definitionIdsKey = string.Join(",", request.DefinitionIds!.OrderBy(x => x));
-            var lockKey = $"WorkflowDefinitionsRefresher:{definitionIdsKey}";
-            await using var distributedLock = await distributedLockProvider.TryAcquireLockAsync(
-                lockKey,
-                TimeSpan.Zero,
-                cancellationToken);
-            
-            if (distributedLock == null)
-            {
-                logger.LogInformation("Could not acquire lock for refreshing workflow definitions. Another instance is already refreshing these workflow definitions");
-                return new RefreshWorkflowDefinitionsResponse(Array.Empty<string>(), request.DefinitionIds!, RefreshWorkflowDefinitionsStatus.AlreadyInProgress);
-            }
-        }
-        
+
         return await inner.RefreshWorkflowDefinitionsAsync(request, cancellationToken);
     }
 }
