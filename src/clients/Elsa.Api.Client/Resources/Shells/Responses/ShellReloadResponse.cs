b60dd5c2942cfbf3edfd1738cfdc8bed2fad9e75
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2026-04-18 14:26:36 +0200

    Refactor ShellReload models: remove ShellReloadItemResult, update ShellReloadResponse properties.

--- src/clients/Elsa.Api.Client/Resources/Shells/Responses/ShellReloadResponse.cs
+++ src/clients/Elsa.Api.Client/Resources/Shells/Responses/ShellReloadResponse.cs
@@ -6,6 +6,6 @@ public class ShellReloadResponse
 {
     public ShellReloadStatus Status { get; init; }
     public string? RequestedShellId { get; init; }
-    public DateTimeOffset ReloadedAt { get; init; }
-    public IReadOnlyCollection<ShellReloadItemResult> Shells { get; init; } = [];
+    public DateTimeOffset Timestamp { get; init; }
+    public string? Message { get; init; }
 }
--- src/modules/Elsa.Workflows.Api/Contracts/ShellReloadResult.cs
+++
@@ -1,9 +0,0 @@
-namespace Elsa.Workflows.Api.Contracts;
-
-internal class ShellReloadResult
-{
-    public ShellReloadStatus Status { get; init; }
-    public string? RequestedShellId { get; init; }
-    public DateTimeOffset ReloadedAt { get; init; }
-    public IReadOnlyCollection<ShellReloadItemResult> Shells { get; init; } = [];
-}
