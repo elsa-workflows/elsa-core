82892508de023c145a3c5cf2a743fdc85ac2d5b9
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2026-09-24 20:53:46 +0200

    Scope connection validity assessment with Secrets bridge

--- test/unit/Elsa.Secrets.UnitTests/SecretServiceLifetimeTests.cs
+++ test/unit/Elsa.Secrets.UnitTests/SecretServiceLifetimeTests.cs
@@ -1,13 +1,18 @@
+using Elsa.Common;
 using Elsa.Common.Multitenancy;
+using Elsa.Common.Services;
 using Elsa.Extensions;
 using Elsa.ExternalAuthentication.Contracts;
 using Elsa.ExternalAuthentication.Services;
+using Elsa.Identity.Contracts;
 using Elsa.Secrets.Contracts;
 using Elsa.Secrets.Features;
 using Elsa.Secrets.Persistence.EFCore.Extensions;
 using Elsa.Secrets.Persistence.EFCore.Sqlite.Extensions;
+using Elsa.Workflows;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using NSubstitute;
 
 namespace Elsa.Secrets.UnitTests;
 
@@ -20,6 +25,7 @@ public sealed class SecretServiceLifetimeTests
     {
         var services = new ServiceCollection();
         services.AddSingleton<IConfiguration>(new ConfigurationBuilder().Build());
+        services.AddSingleton<ISystemClock, SystemClock>();
         services.AddSingleton<ITenantAccessor, DefaultTenantAccessor>();
         var module = services.CreateModule();
         var secrets = module.Configure<SecretsFeature>();
@@ -27,7 +33,15 @@ public sealed class SecretServiceLifetimeTests
         module.Apply();
         if (withExternalAuthenticationBridge)
         {
-            services.AddSingleton<IExternalAuthenticationHandleHasher>(new HmacExternalAuthenticationHandleHasher());
+            services.AddSingleton(Substitute.For<IUserProvider>());
+            services.AddSingleton(Substitute.For<IUserStore>());
+            services.AddSingleton(Substitute.For<IRoleProvider>());
+            services.AddSingleton(Substitute.For<IElsaTokenService>());
+            services.AddSingleton(Substitute.For<IRoleAuthorizationService>());
+            services.AddSingleton(Substitute.For<IIdentityGenerator>());
+            services.AddSingleton(Substitute.For<IUserCredentialsValidator>());
+            services.AddSingleton(Substitute.For<IIdentityRefreshTokenService>());
+            services.AddExternalAuthenticationServices();
             services.AddElsaSecretsExternalAuthentication();
         }
 
@@ -44,6 +58,7 @@ public sealed class SecretServiceLifetimeTests
         {
             Assert.NotNull(scope.ServiceProvider.GetRequiredService<ISecretBindingResolver>());
             Assert.NotNull(scope.ServiceProvider.GetRequiredService<IManagedSecretBindingWriter>());
+            Assert.NotNull(scope.ServiceProvider.GetRequiredService<IIdentityProviderConnectionValidityAssessor>());
         }
     }
 }
